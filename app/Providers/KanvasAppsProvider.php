85f0539cc8451df807ddd864f35e58226d11e103
Rafael White <rwhite@mctekk.com>
2022-08-04 15:25:02 -0400

    reAdd check for apps table

--- app/Providers/KanvasAppsProvider.php
+++ app/Providers/KanvasAppsProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Exception;
+use Schema;
 use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
 use Kanvas\Apps\Apps\Models\Apps;
@@ -31,15 +32,17 @@ class KanvasAppsProvider extends ServiceProvider
         $domainName = $request->getHttpHost();
         $appKey = config('kanvas.app.id');
         // $app = !$domainBasedApp ? AppsRepository::findFirstByKey($appKey) : AppsRepository::getByDomainName($domainName);
-        $app = AppsRepository::findFirstByKey($appKey);
+        if (Schema::hasTable('apps') && Apps::find(1)) {
+            $app = AppsRepository::findFirstByKey($appKey);
 
-        if (!$app) {
-            $msg = !$domainBasedApp ? 'No App configure with this key ' . $appKey : 'No App configure by this domain ' . $domainName;
-            throw new Exception($msg);
+            if (!$app) {
+                $msg = !$domainBasedApp ? 'No App configure with this key ' . $appKey : 'No App configure by this domain ' . $domainName;
+                throw new Exception($msg);
+            }
+
+            $this->app->bind(Apps::class, function () use ($app) {
+                return $app;
+            });
         }
-
-        $this->app->bind(Apps::class, function () use ($app) {
-            return $app;
-        });
     }
 }
