b561dc07d82a4f0246c58bc2d17c10a603f12041
kaioken <max@mctekk.com>
2022-08-28 14:43:25 -0400

    fix

--- domains/Apps/Apps/Factories/AppsFactory.php
+++ domains/Apps/Apps/Factories/AppsFactory.php
@@ -27,6 +27,7 @@ class AppsFactory extends Factory
     {
         $totalApps  = Apps::count();
 
+        echo $totalApps; die();
         return [
             'url' => $this->faker->url(),
             'key' => (int) $totalApps === 0 ? config('kanvas.app.id') : Str::uuid(),
