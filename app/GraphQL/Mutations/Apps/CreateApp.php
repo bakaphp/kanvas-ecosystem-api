4cb94b6a53bc1f4f4a3d5cbc26f4504462b9417d
FredPeal <frederickpeal@gmail.com>
2022-09-07 10:35:52 -0400

    refactor: use request input

--- app/GraphQL/Mutations/Apps/CreateApp.php
+++ app/GraphQL/Mutations/Apps/CreateApp.php
@@ -15,17 +15,7 @@ final class CreateApp
     public function __invoke($_, array $request)
     {
         // TODO implement the resolver
-        $dto = AppsPostData::fromArray([
-            'name' => $request['input']['name'],
-            'url' => $request['input']['url'],
-            'description' => $request['input']['description'],
-            'domain' => $request['input']['domain'],
-            'is_actived' => $request['input']['is_actived'],
-            'ecosystem_auth' => $request['input']['ecosystem_auth'],
-            'payments_active' => $request['input']['payments_active'],
-            'is_public' => $request['input']['is_public'],
-            'domain_based' => $request['input']['domain_based']
-        ]);
+        $dto = AppsPostData::fromArray($request['input']);
         $action = new  CreateAppsAction($dto);
         return $action->execute();
     }
