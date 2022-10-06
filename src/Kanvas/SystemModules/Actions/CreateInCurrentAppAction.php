290c5d66713134d56f6cd9beaae17a08dad0456e
kaioken <max@mctekk.com>
2022-10-06 16:27:15 -0400

    feat : add system modules

---
+++ src/Kanvas/SystemModules/Actions/CreateInCurrentAppAction.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\SystemModules\Actions;
+
+use App\Exceptions\InternalServerErrorException;
+use Kanvas\Apps\Models\Apps;
+use Kanvas\SystemModules\Models\SystemModules;
+use Kanvas\Utils\Str;
+
+class CreateInCurrentAppAction
+{
+    /**
+     * Construct function.
+     */
+    public function __construct(
+        protected Apps $app
+    ) {
+    }
+
+    /**
+     * Invoke function.
+     *
+     * @return Apps
+     */
+    public function execute(string $class) : SystemModules
+    {
+        if (!class_exists($class)) {
+            throw new InternalServerErrorException('Class not found in this app');
+        }
+
+        return SystemModules::firstOrCreate([
+            'model_name' => $class,
+            'apps_id' => $this->app->getKey()
+        ], [
+            'model_name' => $class,
+            'name' => $class,
+            'apps_id' => $this->app->getKey(),
+            'slug' => Str::simpleSlug($class)
+        ]);
+    }
+}
--- src/Kanvas/SystemModules/Enums/Defaults.php
+++ src/Kanvas/SystemModules/Enums/Defaults.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Kanvas\SystemModules\Enums;
 
+use Kanvas\Companies\Models\Companies;
 use Kanvas\Contracts\EnumsInterface;
+use Kanvas\Roles\Models\Roles;
+use Kanvas\Users\Models\Users;
 
 enum Defaults implements EnumsInterface
 {
@@ -12,14 +15,13 @@ enum Defaults implements EnumsInterface
     case USERS;
     case ROLES;
 
-    public function getValue(): mixed
+    public function getValue() : mixed
     {
         return match ($this) {
-            self::COMPANIES =>
-            [
+            self::COMPANIES => [
                 'name' => 'Companies',
                 'slug' => 'companies',
-                'model_name' => "Kanvas\CompaniesModels\Companies",
+                'model_name' => Companies::class,
                 'parents_id' => '0',
                 'menu_order' => '0',
                 'show' => '1',
@@ -31,11 +33,10 @@ enum Defaults implements EnumsInterface
                 'mobile_tab_index' => '0',
                 'protected' => '0'
             ],
-            self::USERS =>
-            [
+            self::USERS => [
                 'name' => 'Users',
                 'slug' => 'users',
-                'model_name' => "Kanvas\UsersGroup\Models\Users",
+                'model_name' => Users::class,
                 'parents_id' => '0',
                 'menu_order' => '0',
                 'show' => '1',
@@ -47,14 +48,12 @@ enum Defaults implements EnumsInterface
                 'mobile_tab_index' => '0',
                 'protected' => '0'
             ],
-            self::ROLES =>
-            [
+            self::ROLES => [
                 'name' => 'Roles',
                 'slug' => 'roles',
-                'model_name' => 'Kanvas\Roles\Models\Roles',
-                'apps_id' => '1',
+                'model_name' => Roles::class,
                 'parents_id' => '0',
-                'menu_order' =>'0',
+                'menu_order' => '0',
                 'show' => '0',
                 'use_elastic' => '0',
                 'browse_fields' => '[{"name":"name","title":"Name","sortField":"name","filterable":true,"searchable":true},{"name":"description","title":"Description"}]',
