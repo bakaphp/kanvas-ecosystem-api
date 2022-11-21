6f567c956e28083e795df5ea0a1973678478c4d6
FredPeal <frederickpeal@gmail.com>
2022-11-21 16:18:52 -0400

    feat: add categories migration

---
+++ database/migrations/Inventory/2022_11_20_220422_add_categories.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_520_ci';
+            $table->id();
+            $table->bigInteger('apps_id')->unsigned();
+            $table->bigInteger('companies_id')->unsigned();
+            $table->bigInteger('parent_id')->unsigned()->nullable();
+            $table->char('uuid', 100);
+            $table->text('name');
+            $table->string('slug');
+            $table->char('code', 36);
+            $table->integer('position');
+            $table->boolean('is_deleted')->default(0);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->index('apps_id');
+            $table->index('companies_id');
+            $table->index('parent_id');
+            $table->index('uuid');
+            $table->index('slug');
+            $table->index('code');
+            $table->index('position');
+            $table->index('is_deleted');
+            $table->index('created_at');
+            $table->index('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
