2cf87618b2e0ddbc8d0bf3b3da1fda6f6a9bace4
FredPeal <frederickpeal@gmail.com>
2022-12-13 17:06:27 -0400

    feat: migration variant

---
+++ database/migrations/Inventory/2022_12_13_203208_add_products_variants.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products_variants', function (Blueprint $table) {
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_520_ci';
+            $table->id();
+            $table->bigInteger('products_id')->unsigned();
+            $table->char('uuid', 100)->unique();
+            $table->text('name');
+            $table->string('slug', 100);
+            $table->text('description');
+            $table->text('short_description')->nullable();
+            $table->text('html_description')->nullable();
+            $table->string('sku', 64);
+            $table->string('ean', 64);
+            $table->string('barcode', 64);
+            $table->string('serial_number', 190);
+            $table->boolean('is_published')->default(1);
+            $table->boolean('is_deleted')->default(0);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->foreign('products_id')->references('id')->on('products');
+            $table->index('products_id');
+            $table->index('uuid');
+            $table->index('sku');
+            $table->index('ean');
+            $table->index('barcode');
+            $table->index('serial_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products_variants');
+    }
+};
