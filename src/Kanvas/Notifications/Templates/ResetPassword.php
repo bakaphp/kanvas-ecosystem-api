ff89040e9a8949f468e073e8e924c871f2fb8934
arfenis <arfen00@gmail.com>
2023-01-19 15:53:42 -0400

    Create Resetpassword notifications template

---
+++ src/Kanvas/Notifications/Templates/ResetPassword.php
@@ -0,0 +1,48 @@
+<?php
+namespace Kanvas\Notifications\Templates;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Kanvas\Notifications\Notification;
+use Kanvas\Users\Models\Users;
+
+class ResetPassword extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public string $templateName = 'reset-password';
+
+    /**
+     * __construct
+     *
+     * @param  Users $user
+     * @return void
+     */
+    public function __construct(Users $user)
+    {
+        $this->entity = $user;
+        $this->setType('users');
+    }
+
+    /**
+     * via
+     *
+     * @return array
+     */
+    public function via(): array
+    {
+        return [...parent::via(), 'mail'];
+    }
+
+    /**
+     * getData
+     *
+     * @return array
+     */
+    public function getData(): array
+    {
+        return [
+            'name' => "{$this->entity->displayname}",
+        ];
+    }
+}
