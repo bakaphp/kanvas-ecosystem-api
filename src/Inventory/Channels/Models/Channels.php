386d9a4228e4c0dc5d9ee1810bb72924b69d6381
kaioken <max@mctekk.com>
2023-01-20 22:35:20 -0400

    refact : format

--- src/Inventory/Channels/Models/Channels.php
+++ src/Inventory/Channels/Models/Channels.php
@@ -1,18 +1,20 @@
 <?php
 declare(strict_types=1);
+
 namespace Kanvas\Inventory\Channels\Models;
 
-use Kanvas\Inventory\Models\BaseModel;
+use Baka\Traits\UuidTrait;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Companies\Models\Companies;
+use Kanvas\Inventory\Models\BaseModel;
 use Kanvas\Users\Models\Users;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Builder;
-use Baka\Traits\UuidTrait;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 
 /**
  * Class Channels.
+ *
  * @property int $id
  * @property int $companies_id
  * @property int $apps_id
@@ -34,11 +36,11 @@ class Channels extends BaseModel
     protected $guarded = [];
 
     /**
-     * Get the companies that owns the Warehouses
+     * Get the companies that owns the Warehouses.
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
-    public function companies(): BelongsTo
+    public function companies() : BelongsTo
     {
         return $this->belongsTo(Companies::class, 'companies_id');
     }
@@ -47,39 +49,41 @@ class Channels extends BaseModel
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
-    public function apps(): BelongsTo
+    public function apps() : BelongsTo
     {
         return $this->belongsTo(Apps::class, 'apps_id');
     }
 
     /**
-     * users
+     * users.
      *
      * @return BelongsTo
      */
-    public function users(): BelongsTo
+    public function users() : BelongsTo
     {
         return $this->belongsTo(Users::class, 'users_id');
     }
 
     /**
-     * scopeCompany
+     * scopeCompany.
      *
      * @param  Builder $query
+     *
      * @return Builder
      */
-    public function scopeCompany(Builder $query): Builder
+    public function scopeCompany(Builder $query) : Builder
     {
         return $query->where('companies_id', auth()->user()->default_company);
     }
 
     /**
-     * scopeApp
+     * scopeApp.
      *
      * @param  Builder $query
+     *
      * @return Builder
      */
-    public function scopeApp(Builder $query): Builder
+    public function scopeApp(Builder $query) : Builder
     {
         return $query->where('apps_id', app(Apps::class)->id);
     }
@@ -89,26 +93,31 @@ class Channels extends BaseModel
      *
      * @return Attribute
      */
-    protected function warehousesId(): Attribute
+    protected function warehousesId() : Attribute
     {
         return Attribute::make(
             get: fn () => $this->pivot->warehouses_id,
         );
     }
 
-    protected function discountedPrice(): Attribute
+    /**
+     * Discounts.
+     *
+     * @return Attribute
+     */
+    protected function discountedPrice() : Attribute
     {
         return Attribute::make(
             get: fn () => $this->pivot->discounted_price,
         );
     }
-    
+
     /**
      * Get the user's first name.
      *
      * @return Attribute
      */
-    protected function price(): Attribute
+    protected function price() : Attribute
     {
         return Attribute::make(
             get: fn () => $this->pivot->price,
@@ -120,7 +129,7 @@ class Channels extends BaseModel
      *
      * @return Attribute
      */
-    protected function isPublished(): Attribute
+    protected function isPublished() : Attribute
     {
         return Attribute::make(
             get: fn () => $this->pivot->is_published,
