fea8f928fcf267707b1ae5aed4d55254c91bc2e7
kaioken <max@mctekk.com>
2023-01-30 00:31:41 -0400

    feat : init social layer

--- app/GraphQL/Ecosystem/Mutations/Apps/ManageApp.php
+++ app/GraphQL/Ecosystem/Mutations/Apps/ManageApp.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\GraphQL\Ecosystem\Mutations\Apps;
@@ -15,32 +16,34 @@ use Kanvas\Users\Repositories\UsersRepository;
 final class ManageApp
 {
     /**
-     * @param  null  $_
-     * @param  array{}  $args
+     * @param null $_
+     * @param array{} $args
      */
     public function create($_, array $request)
     {
         // TODO implement the resolver
         $dto = AppInput::from($request['input']);
-        $action = new  CreateAppsAction($dto, auth()->user());
+        $action = new CreateAppsAction($dto, auth()->user());
+
         return $action->execute();
     }
 
     /**
-     * @param  null  $_
-     * @param  array{}  $args
+     * @param null $_
+     * @param array{} $args
      */
     public function update($_, array $request)
     {
         // TODO implement the resolver\
         $dto = AppInput::from($request['input']);
         $action = new UpdateAppsAction($dto, auth()->user());
+
         return $action->execute($request['id']);
     }
 
     /**
-     * @param  null  $_
-     * @param  array{}  $args
+     * @param null $_
+     * @param array{} $args
      */
     public function delete($_, array $request)
     {
@@ -51,7 +54,7 @@ final class ManageApp
 
         UsersRepository::userOwnsThisApp(auth()->user(), $app);
 
-        //@todo only app creator can delete app
+        // @todo only app creator can delete app
         $app->softDelete();
 
         return $app;
@@ -60,10 +63,7 @@ final class ManageApp
     /**
      * update.
      *
-     * @param  mixed $root
-     * @param  array $req
-     *
-     * @return AttributeModel
+     * @return Apps
      */
     public function restore(mixed $root, array $req) : Apps
     {
@@ -71,7 +71,7 @@ final class ManageApp
 
         UsersRepository::userOwnsThisApp(auth()->user(), $app);
 
-        //@todo only app creator can delete app
+        // @todo only app creator can delete app
         $app->is_deleted = StateEnums::NO->getValue();
         $app->is_actived = StateEnums::YES->getValue();
         $app->saveOrFail();
@@ -81,11 +81,6 @@ final class ManageApp
 
     /**
      * Save app setting.
-     *
-     * @param mixed $root
-     * @param array $req
-     *
-     * @return mixed
      */
     public function saveSettings(mixed $root, array $req) : mixed
     {
