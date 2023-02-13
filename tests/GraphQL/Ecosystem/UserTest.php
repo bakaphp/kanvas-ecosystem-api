cdbc3633ca3d428fc6e61cb440ddb85b04d6b36a
arfenis <arfen00@gmail.com>
2023-02-13 19:33:54 -0400

    Create user management test

---
+++ tests/GraphQL/Ecosystem/UserTest.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\GraphQL\Ecosystem;
+
+use Kanvas\Auth\DataTransferObject\LoginInput;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    protected static LoginInput $loginData;
+
+    /**
+     * Set login credentials.
+     *
+     * @return LoginInput
+     */
+    public static function loginData() : LoginInput
+    {
+        if (empty(self::$loginData)) {
+            self::$loginData = LoginInput::from([
+                'email' => fake()->email,
+                'password' => fake()->password(8),
+                'ip' => request()->ip()
+            ]);
+        }
+
+        return self::$loginData;
+    }
+
+    public function test_edit_user_data(): void
+    {
+        $loginData = self::loginData();
+        $firstname = fake()->firstName();
+        $lastname = fake()->lastName();
+        $displayname = fake()->firstName();
+
+        $response = $this->graphQL( /** @lang GraphQL */
+            '
+            mutation updateUser($data: UpdateUserInput!) {
+                updateUser(data: $data)
+                {
+                    firstname
+                    lastname
+                    displayname
+                }
+            }',
+            [
+                'data' => [
+                    'firstname' => $firstname,
+                    'lastname' => $lastname,
+                    'displayname' => $displayname
+                ],
+            ]
+        )
+        ->assertSuccessful()
+        ->assertSee('firstname',$firstname)
+        ->assertSee('lastname',$lastname)
+        ->assertSee('displayname',$displayname);
+    }
+}
