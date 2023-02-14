ab5873307711f2e6d268d5dba888c46378bb38c6
arfenis <arfen00@gmail.com>
2023-02-14 16:02:06 -0400

    Move mutations into one user management

--- app/GraphQL/Ecosystem/Mutations/Users/UserManagement.php
+++ app/GraphQL/Ecosystem/Mutations/Users/UserManagement.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Auth as AuthFacade;
 use Illuminate\Support\Facades\Hash;
 use Kanvas\Notifications\Templates\ChangePasswordUserLogged;
 use Kanvas\Users\Repositories\UsersRepository;
+use Kanvas\Auth\Services\UserManagement as UserManagementService;
+use Kanvas\Users\Models\Users;
 
 class UserManagement
 {
@@ -26,5 +28,21 @@ class UserManagement
         $user->saveOrFail();
         $user->notify(new ChangePasswordUserLogged($user));
         return true;
+    }
+
+    /**
+     * Update user information.
+     *
+     * @param  mixed $rootValue
+     * @param  array $request
+     *
+     * @return Users
+     */
+    public function updateUser(mixed $rootValue, array $request): Users
+    {
+        $userManagement = new UserManagementService(Users::getById(auth()->user()->id));
+        $user = $userManagement->update($request['data']);
+
+        return $user;
     }
 }
