c4d712e8e3efe8f990b0371a9b7f879098aaf947
arfenis <arfen00@gmail.com>
2023-02-18 03:57:43 -0400

    Add contact type for user response

--- app/GraphQL/Ecosystem/Types/Users.php
+++ app/GraphQL/Ecosystem/Types/Users.php
@@ -9,7 +9,7 @@ use Kanvas\Users\Models\Users as ModelsUsers;
 class Users
 {
     /**
-     * Return the user address formatted for the response type
+     * Return the user address formatted for the response type.
      *
      * @param ModelsUsers $user
      * @param array $request
@@ -21,6 +21,23 @@ class Users
             "address_1" => $user->address_1,
             "address_2" => $user->address_2,
             "zip_code" => $user->zip_code
+        ];
+
+        return $user;
+    }
+
+    /**
+     * Return the user contact info formatted for the response type.
+     *
+     * @param ModelsUsers $user
+     * @param array $request
+     * @return ModelsUsers
+     */
+    public function userContact(ModelsUsers $user, array $request): ModelsUsers
+    {
+        $user->contact = [
+            "phone_number" => $user->phone_number,
+            "cell_phone_number" => $user->cell_phone_number,
         ];
 
         return $user;
