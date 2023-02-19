96e337c7107d6901dd77b3c3d3bd724cc30e70d6
kaioken <max@mctekk.com>
2023-02-19 18:19:44 -0400

    refact : avoid duplicate file upload and attachment

--- src/Kanvas/Filesystem/Actions/AttachFilesystemAction.php
+++ src/Kanvas/Filesystem/Actions/AttachFilesystemAction.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Kanvas\Filesystem\Actions;
 
 use Illuminate\Database\Eloquent\Model as EloquentModel;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Kanvas\Filesystem\Models\Filesystem;
 use Kanvas\Filesystem\Models\FilesystemEntities;
 use Kanvas\Filesystem\Repositories\FilesystemEntitiesRepository;
 use Kanvas\SystemModules\Repositories\SystemModulesRepository;
+use Throwable;
 
 class AttachFilesystemAction
 {
@@ -35,12 +37,16 @@ class AttachFilesystemAction
         $update = (int) $id > 0;
 
         if ($update) {
-            $fileEntity = FilesystemEntitiesRepository::getByIdAdnEntity($id, $this->entity);
+            $fileEntity = FilesystemEntitiesRepository::getByIdAdnEntity((int) $id, $this->entity);
         } else {
-            $fileEntity = new FilesystemEntities();
-            $fileEntity->system_modules_id = $systemModule->getKey();
-            $fileEntity->companies_id = $this->filesystem->companies_id;
-            $fileEntity->entity_id = $this->entity->getKey();
+            try {
+                $fileEntity = FilesystemEntitiesRepository::getByIdAdnEntity((int) $this->entity->getKey(), $this->entity);
+            } catch (ModelNotFoundException $e) {
+                $fileEntity = new FilesystemEntities();
+                $fileEntity->system_modules_id = $systemModule->getKey();
+                $fileEntity->companies_id = $this->filesystem->companies_id;
+                $fileEntity->entity_id = $this->entity->getKey();
+            }
         }
 
         $fileEntity->filesystem_id = $this->filesystem->getKey();
