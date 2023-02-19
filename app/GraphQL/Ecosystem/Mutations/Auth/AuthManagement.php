9e114fedfd516dc36b77da34e4b1622a7b716ac1
FredPeal <frederickpeal@gmail.com>
2023-02-19 15:11:10 -0400

    refactor: merge  auth method in one file

---
+++ app/GraphQL/Ecosystem/Mutations/Auth/AuthManagement.php
@@ -0,0 +1,141 @@
+<?php
+declare(strict_types=1);
+namespace App\GraphQL\Ecosystem\Mutations\Auth;
+
+use GraphQL\Type\Definition\ResolveInfo;
+use Kanvas\Auth\Services\ForgotPassword as ForgotPasswordService;
+use Kanvas\Auth\Traits\AuthTrait;
+use Kanvas\Auth\Traits\TokenTrait;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
+use Kanvas\Auth\Actions\RegisterUsersAction;
+use Kanvas\Auth\DataTransferObject\RegisterInput;
+use Kanvas\Auth\DataTransferObject\LoginInput;
+
+class AuthManagement
+{
+    use TokenTrait;
+    use AuthTrait;
+
+    /**
+     * @param $rootValue
+     * @param array $args
+     * @param \Nuwave\Lighthouse\Support\Contracts\GraphQLContext|null $context
+     * @param \GraphQL\Type\Definition\ResolveInfo $resolveInfo
+     *
+     * @return array
+     *
+     * @throws \Exception
+     */
+    public function forgot(
+        mixed $rootValue,
+        array $request,
+        GraphQLContext $context = null,
+        ResolveInfo $resolveInfo
+    ) {
+        $user = new ForgotPasswordService();
+
+        $registeredUser = $user->forgot($request['data']['email']);
+        $tokenResponse = $registeredUser->createToken('kanvas-login')->toArray();
+
+        $request = request();
+
+        return [
+            'user' => $registeredUser,
+            'token' => $tokenResponse
+        ];
+    }
+
+    /**
+     * Reset user password.
+     *
+     * @param mixed $rootValue
+     * @param array $request
+     * @param GraphQLContext|null $context
+     * @param ResolveInfo $resolveInfo
+     *
+     * @return void
+     */
+    public function reset(
+        mixed $rootValue,
+        array $request,
+        GraphQLContext $context = null,
+        ResolveInfo $resolveInfo
+    ) {
+        $user = new ForgotPasswordService();
+
+        return $user->reset($request['data']['new_password'], $request['data']['hash_key']);
+    }
+
+    /**
+     * @param $rootValue
+     * @param array $args
+     * @param \Nuwave\Lighthouse\Support\Contracts\GraphQLContext|null $context
+     * @param \GraphQL\Type\Definition\ResolveInfo $resolveInfo
+     *
+     * @return array
+     *
+     * @throws \Exception
+     */
+    public function loginMutation(
+        mixed $rootValue,
+        array $request,
+        GraphQLContext $context = null,
+        ResolveInfo $resolveInfo
+    ) {
+        $email = $request['data']['email'];
+        $password = $request['data']['password'];
+        $request = request();
+
+        $user = $this->login(
+            LoginInput::from([
+                'email' => $email,
+                'password' => $password,
+                'ip' => $request->ip(),
+            ])
+        );
+
+        return $user->createToken('kanvas-login')->toArray();
+    }
+
+    /**
+     * @param $rootValue
+     * @param array $args
+     * @param \Nuwave\Lighthouse\Support\Contracts\GraphQLContext|null $context
+     * @param \GraphQL\Type\Definition\ResolveInfo $resolveInfo
+     *
+     * @return array
+     *
+     * @throws \Exception
+     */
+    public function register(
+        mixed $rootValue,
+        array $request,
+        GraphQLContext $context = null,
+        ResolveInfo $resolveInfo
+    ) {
+        Validator::make(
+            $request['data'],
+            [
+                'password' => [
+                    'required',
+                    'confirmed',
+                    Password::min(8)
+                ],
+            ]
+        )->validate();
+
+        $data = RegisterInput::fromArray($request['data']);
+        $user = new RegisterUsersAction($data);
+        $request = request();
+
+        $registeredUser = $user->execute();
+        $tokenResponse = $registeredUser->createToken('kanvas-login')->toArray();
+
+        return [
+            'user' => $registeredUser,
+            'token' => $tokenResponse
+        ];
+    }
+}
