4341d881bc64db041d450f93e0cd37ea458334fc
kaioken <max@mctekk.com>
2023-03-20 10:56:04 -0400

    refact : leads participants

--- tests/GraphQL/Guild/Leads/ParticipantsTest.php
+++ tests/GraphQL/Guild/Leads/ParticipantsTest.php
@@ -4,29 +4,33 @@ declare(strict_types=1);
 
 namespace Tests\GraphQL\Guild\Leads;
 
-use Kanvas\Enums\AppEnums;
 use Kanvas\Guild\Customers\Models\Peoples;
 use Kanvas\Guild\Leads\Models\Leads;
-use Kanvas\Inventory\Variants\Models\Variants;
 use Tests\TestCase;
 
 class ParticipantsTest extends TestCase
 {
     /**
      * testSave.
-     *
-     * @return void
      */
     public function testAddParticipants(): void
     {
         $company = auth()->user()->getCurrentCompany();
 
-        $people = Peoples::factory()->make([
+        $people = Peoples::factory()->create([
             'companies_id' => $company->getId(),
             'users_id' => auth()->user()->getId(),
         ]);
 
-        print_r($people->toArray()); die();
-    }
+        $lead = Leads::factory()->create([
+            'companies_id' => $company->getId(),
+            'companies_branches_id' => $company->branch()->firstOrFail()->getId(),
+            'users_id' => auth()->user()->getId(),
+            'people_id' => $people->getId(),
+            'leads_receivers_id' => 0,
+            'leads_owner_id' => auth()->user()->getId(),
+        ]);
 
+        //print_r($people->toArray()); die();
+    }
 }
