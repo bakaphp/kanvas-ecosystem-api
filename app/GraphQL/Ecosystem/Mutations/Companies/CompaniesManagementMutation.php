e63950fc816305b6b9a799a22ea949cb3d42a8cb
FredPeal <frederickpeal@gmail.com>
2023-03-19 22:34:22 -0400

    refactor: company management mutation

--- app/GraphQL/Ecosystem/Mutations/Companies/CompaniesManagementMutation.php
+++ app/GraphQL/Ecosystem/Mutations/Companies/CompaniesManagementMutation.php
@@ -1,7 +1,6 @@
 <?php
 
 declare(strict_types=1);
-
 namespace App\GraphQL\Ecosystem\Mutations\Companies;
 
 use Illuminate\Support\Facades\Auth;
@@ -17,8 +16,12 @@ use Kanvas\Companies\Models\Companies;
 use Kanvas\Companies\Actions\UpdateCompaniesAction;
 use Kanvas\Companies\DataTransferObject\CompaniesPutData;
 use Kanvas\Companies\Actions\DeleteCompaniesAction;
+use Kanvas\Companies\Repositories\CompaniesRepository;
+use Kanvas\Users\Models\Users;
+use Kanvas\Enums\StateEnums;
+use Illuminate\Auth\AuthenticationException;
 
-class CompaniesManagement
+class CompaniesManagementMutation
 {
     /**
      * createCompany
@@ -112,5 +115,123 @@ class CompaniesManagement
         $branch = $companyBranchDelete->execute($request['id']);
 
         return 'Successfully Delete Company Branch : ' . $branch->name;
+    }
+
+    /**
+     * insertInvite.
+     *
+     * @param  mixed $rootValue
+     * @param  array $request
+     *
+     * @return bool
+     */
+    public function addUserToBranch($rootValue, array $request): bool
+    {
+        $user = Users::getById($request['users_id']);
+        $branch = CompaniesBranches::getById($request['id']);
+        $company = $branch->company()->get()->first();
+
+        CompaniesRepository::userAssociatedToCompany(
+            $company,
+            auth()->user()
+        );
+
+        $company->associateUser(
+            $user,
+            StateEnums::YES->getValue(),
+            $branch
+        );
+
+        return true;
+    }
+
+    /**
+     * insertInvite.
+     *
+     * @param  mixed $rootValue
+     * @param  array $request
+     *
+     * @return bool
+     */
+    public function addUserToCompany($rootValue, array $request): bool
+    {
+        $user = Users::getById($request['users_id']);
+        $company = Companies::getById($request['id']);
+
+        CompaniesRepository::userAssociatedToCompany(
+            $company,
+            auth()->user()
+        );
+
+        $company->associateUser(
+            $user,
+            StateEnums::YES->getValue(),
+            CompaniesBranches::getGlobalBranch()
+        );
+
+        return true;
+    }
+
+    /**
+     * insertInvite.
+     *
+     * @param  mixed $rootValue
+     * @param  array $request
+     *
+     * @return bool
+     */
+    public function removeUserFromCompany($rootValue, array $request): bool
+    {
+        $user = Users::getById($request['users_id']);
+        $company = Companies::getById($request['id']);
+
+        if ($company->users_id == $user->getId()) {
+            throw new AuthenticationException('You can not remove yourself from the company');
+        }
+
+        CompaniesRepository::userAssociatedToCompany(
+            $company,
+            auth()->user()
+        );
+
+        $company->associateUser(
+            $user,
+            StateEnums::YES->getValue(),
+            CompaniesBranches::getGlobalBranch()
+        )->delete();
+
+        return true;
+    }
+
+    /**
+     * insertInvite.
+     *
+     * @param  mixed $rootValue
+     * @param  array $request
+     *
+     * @return bool
+     */
+    public function removeUserFromBranch($rootValue, array $request): bool
+    {
+        $user = Users::getById($request['users_id']);
+        $branch = CompaniesBranches::getById($request['id']);
+        $company = $branch->company()->get()->first();
+
+        if ($company->users_id == $user->getId()) {
+            throw new AuthenticationException('You can not remove yourself from the company');
+        }
+
+        CompaniesRepository::userAssociatedToCompany(
+            $company,
+            auth()->user()
+        );
+
+        $company->associateUser(
+            $user,
+            StateEnums::YES->getValue(),
+            $branch
+        )->delete();
+
+        return true;
     }
 }
