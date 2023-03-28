2ea69b9a529bee2e359d537232640a702d159fcc
FredPeal <frederickpeal@gmail.com>
2023-03-28 01:25:55 -0400

    refactor: use repository

--- app/GraphQL/Ecosystem/Mutations/Notifications/NotificationsManagementMutation.php
+++ app/GraphQL/Ecosystem/Mutations/Notifications/NotificationsManagementMutation.php
@@ -7,6 +7,7 @@ namespace App\GraphQL\Ecosystem\Mutations\Notifications;
 use Illuminate\Support\Facades\Notification;
 use Kanvas\Notifications\Templates\Blank;
 use Kanvas\Users\Models\Users;
+use Kanvas\Users\Repositories\UsersRepository;
 use Throwable;
 
 class NotificationsManagementMutation
@@ -16,7 +17,7 @@ class NotificationsManagementMutation
      */
     public function sendNotificationBaseOnTemplate(mixed $root, array $request)
     {
-        $users = Users::find($request['users_id']); //Maybe you need to use the repository to get the users by apps_id
+        $users = UsersRepository::findUsersByIds($request['users_id']); 
         $notification = new Blank(
             $request['template_name'],
             is_string($request['data']) ? json_decode($request['data']) : $request['data'], // This can have more validation like validate if is array o json
