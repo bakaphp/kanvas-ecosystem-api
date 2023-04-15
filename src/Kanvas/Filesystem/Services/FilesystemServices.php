cee036070e9f6154c3811c54d091bd0a31ae3de4
kaioken <max@mctekk.com>
2023-04-15 10:49:27 -0400

    feat : validate filesystem configuration

--- src/Kanvas/Filesystem/Services/FilesystemServices.php
+++ src/Kanvas/Filesystem/Services/FilesystemServices.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Kanvas\Apps\Models\Apps;
+use Kanvas\Exceptions\ValidationException;
 use Kanvas\Filesystem\Actions\CreateFilesystemAction;
 use Kanvas\Filesystem\Models\Filesystem as ModelsFilesystem;
 use Kanvas\Users\Models\Users;
@@ -15,6 +16,7 @@ use Kanvas\Users\Models\Users;
 class FilesystemServices
 {
     protected Filesystem $storage;
+
     /**
      * Construct function.
      */
@@ -26,10 +28,6 @@ class FilesystemServices
 
     /**
      * Upload a file using filesystem.
-     *
-     * @param UploadedFile $file
-     * @param Users $user
-     * @return ModelsFilesystem
      */
     public function upload(UploadedFile $file, Users $user): ModelsFilesystem
     {
@@ -46,24 +44,25 @@ class FilesystemServices
     /**
      * Build an on-demand Storage client based on the config filesystem service
      * of the current app.
-     *
-     * @return Filesystem
      */
     public function getStorageByDisk(): Filesystem
     {
         return match ($this->app->get('filesystem-service')) {
             'gcs' => $this->buildGoogleCloudStorage(),
             's3' => $this->buildS3Storage(),
+            default => $this->buildS3Storage(),
         };
     }
 
     /**
      * Build an on-demand google cloud storage using the (must) already saved service account-file.
-     *
-     * @return Filesystem
      */
     public function buildGoogleCloudStorage(): Filesystem
     {
+        if (empty($this->app->get('service-account-file')) || empty($this->app->get('cloud-bucket'))) {
+            throw new ValidationException('Invalid Google Cloud Storage credentials');
+        }
+
         return Storage::build([
             'driver' => 'gcs',
             'key_file' => $this->app->get('service-account-file'), // optional: Array of data that substitutes the .json file (see below)
@@ -72,18 +71,20 @@ class FilesystemServices
             'apiEndpoint' => null, // set storageClient apiEndpoint
             'visibility' => 'public', // optional: public|private
             'visibility_handler' => \League\Flysystem\GoogleCloudStorage\UniformBucketLevelAccessVisibility::class, // optional: set to \League\Flysystem\GoogleCloudStorage\UniformBucketLevelAccessVisibility::class to enable uniform bucket level access
-            'metadata' => ['cacheControl'=> 'public,max-age=86400'], // optional: default metadata
+            'metadata' => ['cacheControl' => 'public,max-age=86400'], // optional: default metadata
         ]);
     }
 
     /**
      * Build an on-demand aws s3 storage using the (must) already saved service-account-file
-     *
-     * @return Filesystem
      */
     public function buildS3Storage(): Filesystem
     {
-        $aws = $this->app->get('service-account-file');
+        $aws = (array) $this->app->get('service-account-file');
+
+        if (empty($aws['key']) || empty($aws['secret']) || empty($aws['region'])) {
+            throw new ValidationException('Invalid AWS credentials');
+        }
 
         return Storage::build([
             'driver' => 's3',
@@ -96,9 +97,6 @@ class FilesystemServices
 
     /**
      * Delete a file from the filesystem cloud service.
-     *
-     * @param ModelsFilesystem $file
-     * @return boolean
      */
     public function delete(ModelsFilesystem $file): bool
     {
