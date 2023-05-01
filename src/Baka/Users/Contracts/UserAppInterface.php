b8db710ad218bfcf9aa352bae9241a571e747312
kaioken <max@mctekk.com>
2023-04-30 20:58:35 -0400

    refact : user interface

--- src/Baka/Users/Contracts/UserAppInterface.php
+++ src/Baka/Users/Contracts/UserAppInterface.php
@@ -4,6 +4,23 @@ declare(strict_types=1);
 
 namespace Baka\Users\Contracts;
 
-interface UserAppInterface extends UserInterface
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Kanvas\Users\Models\Users;
+use Kanvas\Users\Models\UsersAssociatedApps;
+
+interface UserAppInterface
 {
+    public function user(): BelongsTo;
+
+    public function company(): BelongsTo;
+
+    public function app(): BelongsTo;
+
+    public function set(string $key, mixed $value): void;
+
+    public function get(string $key): mixed;
+
+    public static function registerUserApp(Users $user, string $password): UsersAssociatedApps;
+
+    public static function userOnApp(Users $user): bool;
 }
