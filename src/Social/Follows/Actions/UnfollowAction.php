ad563408cd84e48c049d1e41aeafa451229ef28d
FredPeal <frederickpeal@gmail.com>
2023-05-02 00:10:20 -0400

    refactor: add doc

--- src/Social/Follows/Actions/UnfollowAction.php
+++ src/Social/Follows/Actions/UnfollowAction.php
@@ -10,12 +10,20 @@ use Kanvas\Users\Models\Users;
 
 class UnFollowAction
 {
+    /**
+     * __construct
+     *
+     * @return void
+     */
     public function __construct(
         public Users $user,
         public EloquentModel $entity
     ) {
     }
 
+    /**
+     * execute
+     */
     public function execute(): bool
     {
         $follow = UsersFollowsRepository::getByUserAndEntity($this->user, $this->entity);
