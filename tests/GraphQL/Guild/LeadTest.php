e62ebacff47ec11b588d40fc3650d9adf84cc257
kaioken <max@mctekk.com>
2023-05-13 15:19:14 -0400

    fix : test

--- tests/GraphQL/Guild/LeadTest.php
+++ tests/GraphQL/Guild/LeadTest.php
@@ -18,6 +18,6 @@ class LeadTest extends TestCase
                         title
                     }
                 }
-            }')->assertSee('uuid');
+            }')->assertOk();
     }
 }
