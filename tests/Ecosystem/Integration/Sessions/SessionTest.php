7fbdef5a110d3345680d6ce9178b7c962eb19d25
kaioken <max@mctekk.com>
2023-05-14 10:19:51 -0400

    fix: logout params

--- tests/Ecosystem/Integration/Sessions/SessionTest.php
+++ tests/Ecosystem/Integration/Sessions/SessionTest.php
@@ -5,13 +5,7 @@ declare(strict_types=1);
 namespace Tests\Ecosystem\Integration\Filesystem;
 
 use Baka\Support\Str;
-use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\Auth;
 use Kanvas\Apps\Models\Apps;
-use Kanvas\Filesystem\Actions\AttachFilesystemAction;
-use Kanvas\Filesystem\Models\Filesystem;
-use Kanvas\Filesystem\Models\FilesystemEntities;
-use Kanvas\Filesystem\Services\FilesystemServices;
 use Kanvas\Sessions\Models\Sessions;
 use Kanvas\Users\Models\Users;
 use Tests\TestCase;
@@ -33,5 +27,4 @@ final class SessionTest extends TestCase
         $session = new Sessions();
         $this->assertIsBool($session->end($user, app(Apps::class)));
     }
-
 }
