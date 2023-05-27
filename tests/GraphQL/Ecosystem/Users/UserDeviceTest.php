9aafcad9e6e016bd8fa6c7d4fafdac2ec58c6013
kaioken <max@mctekk.com>
2023-05-27 13:37:58 -0400

    fix: test

--- tests/GraphQL/Ecosystem/Users/UserDeviceTest.php
+++ tests/GraphQL/Ecosystem/Users/UserDeviceTest.php
@@ -4,48 +4,67 @@ declare(strict_types=1);
 
 namespace Tests\GraphQL\Ecosystem\Users;
 
+use Baka\Support\Str;
 use Kanvas\Users\Models\Sources;
 use Tests\TestCase;
 
 class UserDeviceTest extends TestCase
 {
+    public function generateDevice(): array
+    {
+        $source = Sources::firstOrFail();
+
+        return [
+            'source_site' => $source->title,
+            'device_id' => Str::uuid(),
+            'source_username' => 'kanvasniche',
+        ];
+    }
+
     public function testLinkUserDevice()
     {
         $source = Sources::firstOrFail();
-        
+
         $this->graphQL(/** @lang GraphQL */ '
             mutation linkDevice($data: DeviceInput!) {
                 linkDevice(data: $data) 
             }
         ', [
-            'data' => [
-                'source_site' => $source->title,
-                'device_id' => '123456789',
-                'source_username' => 'kanvasniche',
-            ],
+            'data' => $this->generateDevice(),
         ])->assertJson([
             'data' => [
                 'linkDevice' => true,
             ],
         ]);
-    
     }
+
     public function testUnLinkUserDevice()
     {
         $source = Sources::firstOrFail();
+        $deviceData = $this->generateDevice();
+
+        $this->graphQL(/** @lang GraphQL */ '
+            mutation linkDevice($data: DeviceInput!) {
+                linkDevice(data: $data) 
+            }
+        ', [
+            'data' => $deviceData,
+        ])->assertJson([
+            'data' => [
+                'linkDevice' => true,
+            ],
+        ]);
+
         $this->graphQL(/** @lang GraphQL */ '
             mutation unLinkDevice($data: DeviceInput!) {
                 unLinkDevice(data: $data) 
             }
         ', [
-            'data' => [
-                'source_site' => $source->title,
-                'device_id' => '123456789',
-                'source_username' => 'kanvasniche',
-            ],
+            'data' => $deviceData,
+
         ])->assertJson([
             'data' => [
-                'linkDevice' => true,
+                'unLinkDevice' => true,
             ],
         ]);
     }
