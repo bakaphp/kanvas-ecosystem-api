0a4ef506071b37cedb2776ae3da7e03e38e09fcb
FredPeal <frederickpeal@gmail.com>
2023-05-29 23:59:33 -0400

    refactor: Deprecated in PHP 8.0: Required parameter

--- src/Social/MessagesTypes/DataTransferObject/MessageTypeInput.php
+++ src/Social/MessagesTypes/DataTransferObject/MessageTypeInput.php
@@ -6,15 +6,29 @@ namespace Kanvas\Social\MessagesTypes\DataTransferObject;
 
 use Spatie\LaravelData\Data;
 
+/**
+ *  class MessageTypeInput
+ *  @property int $apps_id
+ *  @property int $languages_id
+ *  @property string $name
+ *  @property string $verb
+ *  @property string $template
+ *  @property string $templates_plura
+ */
 class MessageTypeInput extends Data
-{
+{    
+    /**
+     * __construct
+     *
+     * @return void
+     */
     public function __construct(
         public int $apps_id = 0,
-        public int $languages_id,
-        public string $name,
-        public string $verb,
-        public string $template,
-        public string $templates_plura,
+        public int $languages_id = 0,
+        public string $name = '',
+        public string $verb = '',
+        public string $template = '',
+        public string $templates_plura = '',
     ) {
     }
 }
