f6e56d2bfaddc442b73e848925617f87f0fd5215
FredPeal <frederickpeal@gmail.com>
2023-05-31 01:36:09 -0400

    refactor: string and mixed message

--- src/Social/Messages/DataTransferObject/MessageInput.php
+++ src/Social/Messages/DataTransferObject/MessageInput.php
@@ -15,17 +15,17 @@ class MessageInput extends Data
      */
     public function __construct(
         public ?int $parent_id = null,
-        public ?int $parent_unique_id = null,
         public int $apps_id,
         public int $companies_id,
         public int $users_id,
         public int $message_types_id,
-        public mixed $message,
         public ?int $reactions_count = null,
         public ?int $comments_count = null,
         public ?int $total_liked = null,
         public ?int $total_saved = null,
-        public ?int $total_shared = null
+        public ?int $total_shared = null,
+        public mixed $message,
+        public ?string $parent_unique_id = null,
     ) {
     }
 }
