e9015b80d9b77c1497514bfe967b9c205c0ff75a
kaioken <max@mctekk.com>
2023-06-07 22:45:55 -0400

    feat :  merge with dev:

--- app/Providers/KanvasAppsProvider.php
+++ app/Providers/KanvasAppsProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Bouncer;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Schema as FacadesSchema;
 use Illuminate\Support\ServiceProvider;
 use Kanvas\AccessControlList\Enums\RolesEnums;
@@ -15,34 +16,36 @@ use Throwable;
 class KanvasAppsProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-    }
-
-    /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
         $appIdentifier = request()->header(AppEnums::KANVAS_APP_HEADER->getValue(), config('kanvas.app.id'));
 
-        if (FacadesSchema::hasTable('apps') && Apps::count() > 0) {
-            try {
-                $app = AppsRepository::findFirstByKey($appIdentifier);
-
-                $this->app->scoped(Apps::class, function () use ($app) {
-                    return $app;
-                });
-
-                //set app ACL scope
-                Bouncer::scope()->to(RolesEnums::getScope($app));
-            } catch (Throwable $e) {
-                $msg = 'No App configure with this key: ' . $appIdentifier;
-
-                throw new InternalServerErrorException($msg, $e->getMessage());
+        try {
+            if (App::runningInConsole() && ! FacadesSchema::hasTable('migrations')) {
+                // Skip the logic if running "php artisan package:discover --ansi" for the first time
+                return;
             }
+        } catch (Throwable $th) {
+            //we've reach here on the first time the container is build , since no db connection exist
+            return ;
+        }
+
+        try {
+            $app = AppsRepository::findFirstByKey($appIdentifier);
+
+            $this->app->scoped(Apps::class, function () use ($app) {
+                return $app;
+            });
+
+            //set app ACL scope
+            Bouncer::scope()->to(RolesEnums::getScope($app));
+        } catch (Throwable $e) {
+            throw new InternalServerErrorException(
+                'No App configure with this key: ' . $appIdentifier,
+                $e->getMessage()
+            );
         }
     }
 }
