e3691771ae526b8f2d90f33f02d0e23cb73f7529
Frederick Peñalo <frederickpeal@gmail.com>
2023-06-19 00:53:17 -0400

    refactor: add simple test

---
+++ tests/GraphQL/Social/UsersListsTest.php
@@ -0,0 +1,140 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\GraphQL\Social;
+
+use Tests\TestCase;
+
+class UsersListsTest extends TestCase
+{
+    /**
+     * testCreateUsersLists
+     *
+     * @return void
+     */
+    public function testCreateUsersLists()
+    {
+        $input =[
+            'name' => fake()->name(),
+            'description' => fake()->text(),
+            'is_public' => fake()->boolean(),
+            'is_default' => fake()->boolean(),
+        ];
+        $this->graphQL(
+            '
+                mutation createUserList($input: UserListInput!) {
+                    createUserList(input: $input) {
+                        name,
+                        description,
+                        is_public,
+                        is_default
+                    }
+                }
+            ',
+            [
+                'input' => $input
+            ]
+        )->assertJson([
+            'data' => [
+                'createUserList' => $input
+            ],
+        ]);
+    }
+
+    /**
+     * testUpdateUsersLists
+     *
+     * @return void
+     */
+    public function testUpdateUsersLists()
+    {
+        $input =[
+            'name' => fake()->name(),
+            'description' => fake()->text(),
+            'is_public' => fake()->boolean(),
+            'is_default' => fake()->boolean(),
+        ];
+        $response = $this->graphQL(
+            '
+                mutation createUserList($input: UserListInput!) {
+                    createUserList(input: $input) {
+                        id
+                    }
+                }
+            ',
+            [
+                'input' => $input
+            ]
+        );
+        $id = $response->json('data.createUserList.id');
+        $newName = fake()->name();
+        $this->graphQL(
+            '
+                mutation updateUserList($id: Int!, $input: UserListInput!) {
+                    updateUserList(id: $id, input: $input) {
+                        id
+                        name
+                    }
+                }
+            ',
+            [
+                'id' => $id,
+                'input' => [
+                    'name' => $newName,
+                    'description' => fake()->text(),
+                    'is_public' => fake()->boolean(),
+                    'is_default' => fake()->boolean(),
+                ],
+            ]
+        )->assertJson([
+            'data' => [
+                'updateUserList' => [
+                    'id' => $id,
+                    'name' => $newName,
+                ],
+            ],
+        ]);
+    }
+
+    /**
+     * testDeleteUsersLists
+     *
+     * @return void
+     */
+    public function testDeleteUsersLists()
+    {
+        $input =[
+            'name' => fake()->name(),
+            'description' => fake()->text(),
+            'is_public' => fake()->boolean(),
+            'is_default' => fake()->boolean(),
+        ];
+        $response = $this->graphQL(
+            '
+                mutation createUserList($input: UserListInput!) {
+                    createUserList(input: $input) {
+                        id
+                    }
+                }
+            ',
+            [
+                'input' => $input
+            ]
+        );
+        $id = $response->json('data.createUserList.id');
+        $this->graphQL(
+            '
+                mutation deleteUserList($id: Int!) {
+                    deleteUserList(id: $id) 
+                }
+            ',
+            [
+                'id' => $id,
+            ]
+        )->assertJson([
+            'data' => [
+                'deleteUserList' => true
+            ],
+        ]);
+    }
+}
