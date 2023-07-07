fe439743f39fc67c761eedae36e50e27517f3cf4
kaioken <max@mctekk.com>
2023-07-07 18:03:29 -0400

    refact : add note

--- src/Kanvas/Notifications/Templates/ResetPassword.php
+++ src/Kanvas/Notifications/Templates/ResetPassword.php
@@ -10,6 +10,7 @@ class ResetPassword extends Notification
 
     public function getData(): array
     {
+        //replace url for app link
         return [
            ...parent::getData(),
             'resetUrl' => $this->app->get('url') . '/reset-password/' . $this->toUser->getAppProfile($this->app)->user_activation_forgot,
