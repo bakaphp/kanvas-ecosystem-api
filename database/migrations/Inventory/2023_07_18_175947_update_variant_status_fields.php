5da35cbbb3e21f1ebbbb141c9375cf38d34d40c3
arfenis <arfen00@gmail.com>
2023-07-18 14:52:07 -0400

    Create migrations for create status table

---
+++ database/migrations/Inventory/2023_07_18_175947_update_variant_status_fields.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products_variants', function (Blueprint $table) {
+            $table->dropColumn('is_published');
+            $table->bigInteger('status_id')->unsigned()->after('sku');
+            $table->index('status_id');
+        });
+
+        Schema::table('products_variants_warehouses', function (Blueprint $table) {
+            $table->dropColumn('is_published');
+            $table->bigInteger('status_id')->unsigned()->after('sku');
+            $table->index('status_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
