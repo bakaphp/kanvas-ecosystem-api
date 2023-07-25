61b6d920438639385adfa6f926fe515cb03f810c
arfenis <arfen00@gmail.com>
2023-07-25 12:47:58 -0400

    Create status migration

--- database/migrations/Inventory/2023_07_18_175947_update_variant_status_fields.php
+++ database/migrations/Inventory/2023_07_18_175947_update_variant_status_fields.php
@@ -13,13 +13,13 @@ return new class extends Migration
     {
         Schema::table('products_variants', function (Blueprint $table) {
             $table->dropColumn('is_published');
-            $table->bigInteger('status_id')->unsigned()->after('sku');
+            $table->bigInteger('status_id')->unsigned()->after('sku')->nullable();
             $table->index('status_id');
         });
 
         Schema::table('products_variants_warehouses', function (Blueprint $table) {
             $table->dropColumn('is_published');
-            $table->bigInteger('status_id')->unsigned()->after('sku');
+            $table->bigInteger('status_id')->unsigned()->after('sku')->nullable();
             $table->index('status_id');
         });
     }
