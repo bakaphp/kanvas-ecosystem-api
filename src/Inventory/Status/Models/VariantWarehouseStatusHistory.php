7b507672ff4f96ada6257197b5a3671bf9bf33c4
arfenis <arfen00@gmail.com>
2023-07-28 18:07:33 -0400

    Add non companies app trait to model

--- src/Inventory/Status/Models/VariantWarehouseStatusHistory.php
+++ src/Inventory/Status/Models/VariantWarehouseStatusHistory.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace Kanvas\Inventory\Status\Models;
 
-use Baka\Traits\SlugTrait;
+use Baka\Traits\HasCompositePrimaryKeyTrait;
+use Baka\Traits\NoAppRelationshipTrait;
+use Baka\Traits\NoCompanyRelationshipTrait;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Kanvas\Inventory\Models\BaseModel;
-use Kanvas\Inventory\Variants\Models\Variants;
 use Kanvas\Inventory\Variants\Models\VariantsWarehouses;
-use Laravel\Scout\Searchable;
 
 /**
  * Class Attributes.
@@ -19,13 +19,15 @@ use Laravel\Scout\Searchable;
  * @property int products_variants_warehouse_id
  * @property string from_date
  */
-class variantWarehouseStatusHistory extends BaseModel
+class VariantWarehouseStatusHistory extends BaseModel
 {
-    use SlugTrait;
-    use Searchable;
+    use HasCompositePrimaryKeyTrait;
+    use NoAppRelationshipTrait;
+    use NoCompanyRelationshipTrait;
 
     protected $table = 'products_variants_warehouse_status_history';
     protected $guarded = [];
+    protected $primaryKey = ['products_variants_id', 'warehouses_id'];
 
     /**
      * Get the user that owns the Variants.
