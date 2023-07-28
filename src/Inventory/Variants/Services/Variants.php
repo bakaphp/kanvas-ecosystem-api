aeef21e000783360dbe98723b5bf7b900ca49a65
arfenis <arfen00@gmail.com>
2023-07-28 15:55:17 -0400

    Add a status id on variant bulk creation

--- src/Inventory/Variants/Services/Variants.php
+++ src/Inventory/Variants/Services/Variants.php
@@ -6,6 +6,7 @@ namespace Kanvas\Inventory\Variants\Services;
 
 use Baka\Users\Contracts\UserInterface;
 use Kanvas\Inventory\Products\Models\Products;
+use Kanvas\Inventory\Status\Repositories\StatusRepository;
 use Kanvas\Inventory\Variants\DataTransferObject\Variants as VariantsDto;
 use Kanvas\Inventory\Variants\Actions\CreateVariantsAction;
 use Kanvas\Inventory\Warehouses\Repositories\WarehouseRepository;
@@ -33,6 +34,10 @@ class Variants
             $variantModel = (new CreateVariantsAction($variantDto, $user))->execute();
             if (isset($variant['attributes'])) {
                 $variantModel->addAttributes($user, $variant['attributes']);
+            }
+            if (isset($variant['status_id'])) {
+                $status = StatusRepository::getById($variant['status_id']);
+                $variant->setStatus($status);
             }
 
             WarehouseRepository::getById($variantDto->warehouse_id, $variantDto->product->company()->get()->first());
