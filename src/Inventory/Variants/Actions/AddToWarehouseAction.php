e5879437f87ea572f23b1eb5cfabbf0d39724b68
arfenis <arfen00@gmail.com>
2023-07-28 18:07:18 -0400

    Add status history after warehouse creation

--- src/Inventory/Variants/Actions/AddToWarehouseAction.php
+++ src/Inventory/Variants/Actions/AddToWarehouseAction.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Kanvas\Inventory\Variants\Actions;
 
+use Kanvas\Inventory\Status\Actions\CreateStatusHistoryAction;
+use Kanvas\Inventory\Status\Repositories\StatusRepository;
 use Kanvas\Inventory\Variants\DataTransferObject\VariantsWarehouses as VariantsWarehousesDto;
 use Kanvas\Inventory\Variants\Models\Variants;
 use Kanvas\Inventory\Variants\Models\VariantsWarehouses;
@@ -33,7 +35,7 @@ class AddToWarehouseAction
             'warehouses_id' => $this->warehouses->getId(),
         ];
 
-        VariantsWarehouses::updateOrCreate(
+        $variantsWarehouses = VariantsWarehouses::updateOrCreate(
             $search,
             [
                 'quantity' => $this->variantsWarehousesDto->quantity,
@@ -52,6 +54,13 @@ class AddToWarehouseAction
                 'is_new' => $this->variantsWarehousesDto->is_new
             ]
         );
+
+        if($this->variantsWarehousesDto->status_id) {
+            (new CreateStatusHistoryAction(
+                StatusRepository::getById($this->variantsWarehousesDto->status_id),
+                $variantsWarehouses
+            ))->execute();
+        }
 
         return $this->variants;
     }
