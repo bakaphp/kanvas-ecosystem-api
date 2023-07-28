8fdebd51be4dd97787cfdfed847e61b2e3402bd6
arfenis <arfen00@gmail.com>
2023-07-28 18:07:49 -0400

    Create status history action

---
+++ src/Inventory/Status/Actions/CreateStatusHistoryAction.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Inventory\Status\Actions;
+
+use Kanvas\Inventory\Status\Models\Status;
+use Kanvas\Inventory\Status\Models\VariantWarehouseStatusHistory;
+use Kanvas\Inventory\Variants\Models\VariantsWarehouses;
+
+class CreateStatusHistoryAction
+{
+    /**
+     * __construct.
+     *
+     * @return void
+     */
+    public function __construct(
+        protected Status $status,
+        protected VariantsWarehouses $variantsWarehouses
+    ) {
+    }
+
+    /**
+     * execute.
+     *
+     * @return VariantWarehouseStatusHistory
+     */
+    public function execute(): VariantWarehouseStatusHistory
+    {
+        return VariantWarehouseStatusHistory::firstOrCreate([
+            'status_id' => $this->status->getId(),
+            'products_variants_warehouse_id' => $this->variantsWarehouses->getId(),
+        ], [
+            'from_date' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
