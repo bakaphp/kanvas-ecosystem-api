cf86b6155b83018948172f550f42047e35a42910
FredPeal <frederickpeal@gmail.com>
2023-08-15 01:44:55 -0400

    dump test

--- tests/GraphQL/Social/ReactionTest.php
+++ tests/GraphQL/Social/ReactionTest.php
@@ -164,12 +164,7 @@ class ReactionTest extends TestCase
                 'input' => $input,
             ]
         );
-        $response->dumpSession();
-        $response->assertJson([
-            'data' => [
-                'reactToEntity' => true,
-            ],
-        ]);
+        dump($response);
     }
 
     public function testGetReactions()
