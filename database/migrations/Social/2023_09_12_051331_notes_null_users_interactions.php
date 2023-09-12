bac732efb1d08a68d3bc93c8e4c70b1437f2d92b
FredPeal <frederickpeal@gmail.com>
2023-09-12 01:36:31 -0400

    fix: migration connection

--- database/migrations/Social/2023_09_12_051331_notes_null_users_interactions.php
+++ database/migrations/Social/2023_09_12_051331_notes_null_users_interactions.php
@@ -10,7 +10,7 @@ return new class () extends Migration {
      */
     public function up(): void
     {
-        Schema::table('users_interactions', function (Blueprint $table) {
+        Schema::connection('social')->table('users_interactions', function (Blueprint $table) {
             $table->string('notes')->nullable()->change();
         });
     }
