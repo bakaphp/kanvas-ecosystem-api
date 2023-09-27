aab22627f539da4da6346a67aeeabd86b1277a75
FredPeal <frederickpeal@gmail.com>
2023-09-27 15:19:19 -0400

    refactor: user interaction query and test

---
+++ app/GraphQL/Social/Queries/UsersInteractions/GetUserInteraction.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Social\Queries\UsersInteractions;
+
+use DB;
+use Kanvas\Social\UsersInteractions\Models\UserInteraction as UserInteractionModel;
+
+class GetUserInteraction
+{
+    public function __invoke($rootValue, array $args): array
+    {
+        $userInteraction = UserInteractionModel::where('entity_id', $args['entity_id'])
+            ->join('interactions', 'users_interactions.interactions_id', '=', 'interactions.id')
+            ->where('entity_namespace', $args['entity_namespace'])
+            ->select('interactions.name', 'interactions.title', DB::raw('count(*) as total'))
+            ->groupBy('interactions.name', 'interactions.title')
+            ->get();
+
+        return [
+                    'entity_id' => $args['entity_id'],
+                    'entity_namespace' => $args['entity_namespace'],
+                    'interactions' => $userInteraction,
+            ];
+    }
+}
