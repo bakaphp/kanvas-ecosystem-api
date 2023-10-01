2f9b12c5f964284336573df94312c7858f210a43
kaioken <max@mctekk.com>
2023-09-30 23:38:19 -0400

    Refac : prodcut attributes

--- app/GraphQL/Souk/Mutations/Orders/OrderManagementMutation.php
+++ app/GraphQL/Souk/Mutations/Orders/OrderManagementMutation.php
@@ -5,6 +5,11 @@ declare(strict_types=1);
 namespace App\GraphQL\Souk\Mutations\Orders;
 
 use Kanvas\Apps\Models\Apps;
+use Kanvas\Inventory\Variants\Models\Variants;
+use Kanvas\Social\Interactions\Actions\CreateInteraction;
+use Kanvas\Social\Interactions\DataTransferObject\Interaction;
+use Kanvas\Social\UsersInteractions\Actions\CreateUserInteractionAction;
+use Kanvas\Social\UsersInteractions\DataTransferObject\UserInteraction;
 use Kanvas\Souk\Orders\DataTransferObject\Order;
 use Kanvas\Souk\Payments\DataTransferObject\CreditCard;
 use Kanvas\Souk\Payments\Providers\AuthorizeNetProvider;
@@ -30,11 +35,34 @@ class OrderManagementMutation
 
         $response = $payment->chargeCreditCard($order);
 
+        //clean cart and add interaction
         if ($response != null) {
             if ($response->getMessages()->getResultCode() == 'Ok') {
                 $tresponse = $response->getTransactionResponse();
 
                 if ($tresponse != null && $tresponse->getMessages() != null) {
+                    //bought the variant so flag it
+                    $interaction = (new CreateInteraction(
+                        new Interaction(
+                            'bought',
+                            app(Apps::class),
+                            'Bought',
+                            'User bought a variant of a product'
+                        )
+                    ))->execute();
+
+                    foreach ($cart->getContent() as $item) {
+                        $userInteraction = (new CreateUserInteractionAction(
+                            new UserInteraction(
+                                $user,
+                                $interaction,
+                                (string) $item->id,
+                                Variants::class,
+                                'User bought a variant of a product'
+                            )
+                        ))->execute();
+                    }
+
                     return [
                         'transaction_id' => $tresponse->getTransId(),
                         'response_code' => $tresponse->getResponseCode(),
@@ -44,8 +72,8 @@ class OrderManagementMutation
                     ];
                 } else {
                     return [
-                      'error_code' => $tresponse->getErrors()[0]->getErrorCode(),
-                    'error_message' => $tresponse->getErrors()[0]->getErrorText(),
+                        'error_code' => $tresponse->getErrors()[0]->getErrorCode(),
+                        'error_message' => $tresponse->getErrors()[0]->getErrorText(),
                     ];
                 }
             } else {
