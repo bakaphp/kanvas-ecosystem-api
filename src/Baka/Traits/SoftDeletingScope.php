bc63891f9d9eae11299255d3a73559eb88936ffa
arfenis <arfen00@gmail.com>
2023-10-30 17:22:08 -0400

    Create softdelete trait to overwrite eloquent apply

---
+++ src/Baka/Traits/SoftDeletingScope.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Baka\Traits;
+
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletingScope as EloquentSoftDeletingScope;
+
+class SoftDeletingScope extends EloquentSoftDeletingScope
+{
+    /**
+     * Change the apply scope from Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        $builder->where($model->getQualifiedDeletedAtColumn(), '=', 0);
+    }
+
+}
