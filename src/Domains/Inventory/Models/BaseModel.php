1b67cec61a4d5d3aa6d3a8cb7c7088ca8ea98499
arfenis <arfen00@gmail.com>
2023-10-30 22:50:27 -0400

    rename2

--- src/Domains/Inventory/Models/BaseModel.php
+++ src/Domains/Inventory/Models/BaseModel.php
@@ -14,7 +14,7 @@ use Kanvas\Filesystem\Traits\HasFilesystemTrait;
 use Kanvas\Inventory\Traits\AppsIdTrait;
 use Kanvas\Inventory\Traits\CompaniesIdTrait;
 use Kanvas\Inventory\Traits\SourceTrait;
-use Baka\Traits\SoftDeletes;
+use Baka\Traits\SoftDeletesTrait;
 
 class BaseModel extends EloquentModel
 {
@@ -27,7 +27,7 @@ class BaseModel extends EloquentModel
     use HasCustomFields;
     use HasFilesystemTrait;
     //use Cachable;
-    use SoftDeletes;
+    use SoftDeletesTrait;
 
     protected $attributes = [
         'is_deleted' => 0,
