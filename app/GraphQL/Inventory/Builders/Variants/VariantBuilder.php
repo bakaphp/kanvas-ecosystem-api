9e9d9de0500e535b8e0a6cbce12a83789dfcd6e3
kaioken <max@mctekk.com>
2023-11-20 10:54:11 -0400

    refact: filter variants by admin

--- app/GraphQL/Inventory/Builders/Variants/VariantBuilder.php
+++ app/GraphQL/Inventory/Builders/Variants/VariantBuilder.php
@@ -6,7 +6,6 @@ namespace App\GraphQL\Inventory\Builders\Variants;
 
 use GraphQL\Type\Definition\ResolveInfo;
 use Illuminate\Database\Eloquent\Builder;
-use Kanvas\Inventory\Products\Models\Products;
 use Kanvas\Inventory\Variants\Models\Variants;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
@@ -20,8 +19,9 @@ class VariantBuilder
     ): Builder {
         $company = auth()->user()->getCurrentCompany();
 
-        Variants::setSearchIndex($company->getId());
-
+        if (! $user->isAppOwner()) {
+            Variants::setSearchIndex($company->getId());
+        }
         /**
          * @var Builder
          */
