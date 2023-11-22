9019ba1700bdac7810011610aeb87463f3d56aae
Rafael White <rwhite@mctekk.com>
2023-11-22 10:19:15 -0600

    add missing id to seeder row

--- database/seeders/TemplateSeeder.php
+++ database/seeders/TemplateSeeder.php
@@ -234,6 +234,7 @@ class TemplateSeeder extends Seeder
         ]);
 
         Templates::create([
+            'id' => 6,
             'apps_id' => 0,
             'users_id' => 1,
             'companies_id' => 0,
@@ -254,7 +255,7 @@ class TemplateSeeder extends Seeder
         ]);
 
         Templates::create([
-            'id' => 6,
+            'id' => 7,
             'apps_id' => 0,
             'users_id' => 1,
             'companies_id' => 0,
