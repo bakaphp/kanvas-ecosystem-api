b1f398071dad3235fcec7060c2026711ef2ead50
Rafael White <rwhite@mctekk.com>
2023-11-22 13:09:56 -0600

    get message type id from the last one inserted

--- database/seeders/NotificationTypesMessageLogicSeeder.php
+++ database/seeders/NotificationTypesMessageLogicSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Kanvas\Notifications\Models\NotificationTypes;
+use Kanvas\Social\MessagesTypes\Models\MessagesTypes;
 
 class NotificationTypesMessageLogicSeeder extends Seeder
 {
@@ -15,10 +16,11 @@ class NotificationTypesMessageLogicSeeder extends Seeder
     public function run(): void
     {
         $notificationType = NotificationTypes::latest()->first();
+        $messageType = MessagesTypes::latest()->first();
         DB::table('notification_types_message_logic')->insert(
             [
                 'apps_id' => 1,
-                'messages_type_id' => 1,
+                'messages_type_id' => $messageType->getId(),
                 'notifications_type_id' => $notificationType->getId(),
                 'logic' => '{
                     "conditions": "message.is_public == 1 and message.is_published == 1"
