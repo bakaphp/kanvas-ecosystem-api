a78d543bd41054a6b86f878c3b381deab8eac8e5
kaioken <max@mctekk.com>
2023-12-31 18:19:47 -0400

    fix: format

--- src/Kanvas/Notifications/Templates/ChangeEmailUserLogged.php
+++ src/Kanvas/Notifications/Templates/ChangeEmailUserLogged.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Kanvas\Notifications\Templates;
 
 use Kanvas\Notifications\Notification;
+
 /**
  * @deprecated version 2 , move to DynamicKanvasNotification
  */
