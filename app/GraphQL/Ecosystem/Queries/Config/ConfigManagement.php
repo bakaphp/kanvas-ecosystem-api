67c48e0a26c4a8156d492e839b590c4ca7a8006a
FredPeal <frederickpeal@gmail.com>
2024-01-03 13:26:22 -0400

    refactor: add tests

--- app/GraphQL/Ecosystem/Queries/Config/ConfigManagement.php
+++ app/GraphQL/Ecosystem/Queries/Config/ConfigManagement.php
@@ -4,14 +4,25 @@ declare(strict_types=1);
 
 namespace App\GraphQL\Ecosystem\Queries\Config;
 
+use Kanvas\Config\Enums\ConfigEnums;
+
 class ConfigManagement
 {
     public function getConfig(mixed $root, array $request): array
     {
-        $enum = $request['input']['module'];
+        $enum = $request['module'];
         $class = ConfigEnums::fromName($enum);
-        $entity = $class::getByUuid($request['input']['entity_uuid']);
+        $entity = $class::getByUuid($request['entity_uuid']);
+        $config = [];
+        foreach ($entity->getAll() as $key => $value) {
+            $config[] = [
+                'key' => $key,
+                'value' => $value,
+                'module' => $enum,
+                'entity_uuid' => $request['entity_uuid'],
+            ];
+        }
 
-        return $entity->getAll();
+        return $config;
     }
 }
