2e1c10ebfd0e0b45c490bec8f869248e65824163
arfenis <arfen00@gmail.com>
2024-01-04 01:59:44 -0400

    Create migration for price history

---
+++ database/migrations/Inventory/2024_01_02_162441_create-channel-price-history.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products_variants_warehouse_channel_price_history', function (Blueprint $table) {
+            $table->bigInteger('products_variants_id')->unsigned();
+            $table->bigInteger('channels_id')->unsigned();
+            $table->bigInteger('product_variants_warehouse_id')->unsigned();
+            $table->float('price', 10, 2);
+            $table->dateTime('from_date');
+            $table->timestamp('created_at')->useCurrent();
+            $table->boolean('is_deleted')->default(0);
+            $table->index('price', 'price_index');
+            $table->foreign('products_variants_id', 'products_variants_ref')->references('id')->on('products_variants');
+            $table->foreign('channels_id', 'channels_variants_ref')->references('id')->on('channels');
+            $table->foreign('product_variants_warehouse_id', 'variant_warehouse_channel_ref')->references('id')->on('products_variants_warehouses');
+            $table->index('created_at' ,'created_at_index');
+            $table->index('from_date', 'from_date_index');
+        });
+
+        Schema::dropIfExists('products_variants_warehouse_price_history');
+
+        Schema::create('products_variants_warehouses_price_history', function (Blueprint $table) {
+            $table->bigInteger('product_variants_warehouse_id')->unsigned();
+            $table->float('price', 10, 2);
+            $table->dateTime('from_date');
+            $table->timestamp('created_at')->useCurrent();
+            $table->boolean('is_deleted')->default(0);
+            $table->index('price');
+            $table->foreign('product_variants_warehouse_id', 'variant_warehouse_ref')->references('id')->on('products_variants_warehouses');
+            $table->index('created_at');
+            $table->index('from_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
