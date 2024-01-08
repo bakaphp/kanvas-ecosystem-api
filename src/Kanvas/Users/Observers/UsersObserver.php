6afcc14ce1959825a4c900468aba913bd25ac2b0
kaioken <max@mctekk.com>
2024-01-07 23:41:57 -0400

    refact: worklfow

--- src/Kanvas/Users/Observers/UsersObserver.php
+++ src/Kanvas/Users/Observers/UsersObserver.php
@@ -34,7 +34,6 @@ class UsersObserver
      */
     public function created(Users $user): void
     {
-        $user->fireWorkflow(WorkflowEnum::CREATED->value);
         /*  if ($user->isFirstSignup() && $user->createDefaultCompany()) {
              $createCompany = new CreateCompaniesAction(
                  new CompaniesPostData(
