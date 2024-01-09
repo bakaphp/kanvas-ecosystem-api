1965588e890fae0dfa442ea8c7c92b34bbcd6cc0
arfenis <arfen00@gmail.com>
2024-01-09 14:14:19 -0400

    Create migration fro default channel table

---
+++ database/migrations/Inventory/2024_01_09_171343_create-default-channel.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('channels', function (Blueprint $table) {
+            $table->boolean('is_default')->default(false);
+            $table->index(['is_default', 'companies_id'], 'is_default_companies_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
