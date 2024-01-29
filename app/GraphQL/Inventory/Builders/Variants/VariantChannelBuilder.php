91df11d7bc155c46b4735f86a59fbfaa1cf73487
kaioken <max@mctekk.com>
2024-01-29 14:04:16 -0400

    Refact: add search

--- app/GraphQL/Inventory/Builders/Variants/VariantChannelBuilder.php
+++ app/GraphQL/Inventory/Builders/Variants/VariantChannelBuilder.php
@@ -69,6 +69,9 @@ class VariantChannelBuilder
 
         $channel = Channels::getByUuid($channelUuid);
 
+        //set index
+        ModelsVariants::setSearchIndex((int) $channel->companies_id);
+
         /**
         * @var Builder
         */
