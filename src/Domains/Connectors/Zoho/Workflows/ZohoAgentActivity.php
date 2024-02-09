3b13ae396ca933f811a2af6743c7669ec8c36cb3
kaioken <max@mctekk.com>
2024-02-09 11:25:40 -0400

    feat: rotation

--- src/Domains/Connectors/Zoho/Workflows/ZohoAgentActivity.php
+++ src/Domains/Connectors/Zoho/Workflows/ZohoAgentActivity.php
@@ -14,6 +14,7 @@ use Kanvas\Companies\Models\Companies;
 use Kanvas\Connectors\Zoho\Enums\CustomFieldEnum;
 use Kanvas\Connectors\Zoho\ZohoService;
 use Kanvas\Guild\Agents\Models\Agent;
+use Kanvas\Guild\Rotations\Models\Rotation;
 use Kanvas\Users\Models\Users;
 use Kanvas\Users\Models\UsersInvite;
 use Kanvas\Workflow\Contracts\WorkflowActivityInterface;
@@ -93,6 +94,8 @@ class ZohoAgentActivity extends Activity implements WorkflowActivityInterface
 
     protected function createAgent(AppInterface $app, ZohoService $zohoService, UserInterface $user, Companies $company): array
     {
+        $companyDefaultUseRotation = $company->get('agent_use_rotation') ?? false;
+
         try {
             $userInvite = UsersInvite::fromCompany($company)->fromApp($app)->where('email', $user->email)->firstOrFail();
             $agentOwner = Agent::fromCompany($company)->where('users_id', $userInvite->users_id)->firstOrFail();
@@ -123,8 +126,19 @@ class ZohoAgentActivity extends Activity implements WorkflowActivityInterface
             }
         }
 
+        if ($companyDefaultUseRotation) {
+            try {
+                $rotation = Rotation::getByIdFromCompany($companyDefaultUseRotation, $company);
+                $rotationDefaultOwner = $rotation->getAgent();
+                $ownerMemberNumber = $rotationDefaultOwner->member_id;
+                $ownerId = $rotationDefaultOwner->users_linked_source_id;
+            } catch(Exception $e) {
+            }
+        }
+
         $companyDefaultOwnerSourceId = $company->get(CustomFieldEnum::ZOHO_USER_OWNER_ID->value);
         $companyDefaultOwnerMemberId = $company->get(CustomFieldEnum::ZOHO_USER_OWNER_MEMBER_NUMBER->value) ?? 1001;
+
         $agent = new Agent();
         $agent->users_id = $user->getId();
         $agent->companies_id = $company->getId();
--- src/Domains/Guild/Rotations/Models/Rotation.php
+++ src/Domains/Guild/Rotations/Models/Rotation.php
@@ -5,10 +5,15 @@ declare(strict_types=1);
 namespace Kanvas\Guild\Rotations\Models;
 
 use Baka\Traits\NoAppRelationshipTrait;
+use Baka\Users\Contracts\UserInterface;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Kanvas\Guild\Leads\Models\LeadRotationAgent;
 use Kanvas\Guild\Models\BaseModel;
 use Kanvas\Users\Models\Users;
+use RuntimeException;
 
 /**
  * Class Rotation.
@@ -40,5 +45,42 @@ class Rotation extends BaseModel
             'id',
             'users_id'
         );
+    }
+
+    public function agents(): HasMany
+    {
+        return $this->hasMany(LeadRotationAgent::class, 'leads_rotations_id');
+    }
+
+    public function getRandomLeadsRotationsAgents(): Collection
+    {
+        return $this->agents()->inRandomOrder()->get();
+    }
+
+    public function getAgent(): UserInterface
+    {
+        $agents = $this->getRandomLeadsRotationsAgents();
+        if ($agents->isEmpty()) {
+            throw new RuntimeException("This rotation doesn't have any users assigned to it " . $this->id);
+        }
+
+        $this->increment('hits');
+        $this->save();
+
+        foreach ($agents as $agent) {
+            $calculatedPercent = ($agent->hits / $this->hits) * 100;
+            if ($calculatedPercent < $agent->percent) {
+                $agent->increment('hits');
+                $agent->save();
+
+                return $agent->user;
+            }
+        }
+
+        $agent = $agents->first();
+        $agent->increment('hits');
+        $agent->save();
+
+        return $agent->user;
     }
 }
