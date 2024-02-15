04d756aa2185aed1970df126c98fdb8de51472df
FredPeal <frederickpeal@gmail.com>
2024-02-15 13:59:24 -0400

    refactor: override app

--- src/Kanvas/Companies/Jobs/DeleteCompanyJob.php
+++ src/Kanvas/Companies/Jobs/DeleteCompanyJob.php
@@ -11,6 +11,9 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Kanvas\Companies\Actions\DeleteCompaniesAction;
 use Kanvas\Users\Models\Users;
+use Kanvas\Apps\Models\Apps;
+use Illuminate\Support\Facades\Auth;
+use Baka\Traits\KanvasJobsTrait;
 
 class DeleteCompanyJob implements ShouldQueue
 {
@@ -18,16 +21,21 @@ class DeleteCompanyJob implements ShouldQueue
     use InteractsWithQueue;
     use Queueable;
     use SerializesModels;
+    use KanvasJobsTrait;
 
     public function __construct(
         public int $companiesId,
-        public Users $user
-    ) {
+        public Users $user,
+        public Apps $app
+        ) {
     }
 
     public function handle(): void
     {
-        $companyDelete = new DeleteCompaniesAction($this->user);
+        Auth::loginUsingId($this->user->getId());
+        $this->overwriteAppService($this->app);
+        
+        $companyDelete = new DeleteCompaniesAction($this->user, $this->app);
         $companyDelete->execute($this->companiesId);
     }
 }
