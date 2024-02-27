e95166ff2bd7a4841cb3bca98143b1304a41fe6f
FredPeal <frederickpeal@gmail.com>
2024-02-26 23:28:20 -0400

    feat: create command kanvas:fixed-kanvas-data

---
+++ app/Console/Commands/FixedKanvasData.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class FixedKanvasData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'kanvas:fixed-kanvas-data {fixed}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+
+    }
+    
+}
