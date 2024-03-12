8d9c9de01fd062210c0aaee5bf4b6cfc9e8f3c4a
FredPeal <frederickpeal@gmail.com>
2024-03-12 02:52:42 -0400

    refactor: system_modules not require and message type as verb

--- app/GraphQL/Social/Mutations/Messages/MessageManagementMutation.php
+++ app/GraphQL/Social/Mutations/Messages/MessageManagementMutation.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\GraphQL\Social\Mutations\Messages;
 
-use Baka\Exceptions\LightHouseCustomException;
 use Kanvas\Apps\Models\Apps;
+use Kanvas\Auth\Exceptions\AuthenticationException;
 use Kanvas\Social\Messages\Actions\CreateMessageAction;
 use Kanvas\Social\Messages\Actions\DistributeChannelAction;
 use Kanvas\Social\Messages\Actions\DistributeToUsers;
@@ -15,8 +15,6 @@ use Kanvas\Social\Messages\Enums\DistributionTypeEnum;
 use Kanvas\Social\Messages\Models\Message;
 use Kanvas\Social\MessagesTypes\Repositories\MessagesTypesRepository;
 use Kanvas\SystemModules\Models\SystemModules;
-use Kanvas\Users\Models\Users;
-use Kanvas\Auth\Exceptions\AuthenticationException;
 
 class MessageManagementMutation
 {
@@ -35,17 +33,11 @@ class MessageManagementMutation
     public function create(mixed $root, array $request): Message
     {
         $app = app(Apps::class);
-        /** @var Users $user */
         $user = auth()->user();
         $company = $user->getCurrentCompany();
-        /** @var array */
         $messageData = $request['input'];
-
-        $messageType = MessagesTypesRepository::getById((int)$messageData['message_types_id'], $app);
-
-        /** @var SystemModules $systemModule */
-        $systemModule = SystemModules::getById((int)$messageData['system_modules_id'], $app);
-
+        $messageType = MessagesTypesRepository::getByVerb($messageData['message_verb'], $app);
+        $systemModule = key_exists('system_modules_id', $messageData) ? SystemModules::getById((int)$messageData['system_modules_id'], $app) : null;
         $data = MessageInput::fromArray($messageData, $user, $messageType, $company, $app);
         $action = new CreateMessageAction($data, $systemModule, $messageData['entity_id']);
         $message = $action->execute();
