6ad854eb7f448a6ff5c20588b43b897e20c18fbc
arfenis <arfen00@gmail.com>
2024-03-11 21:32:02 -0400

    Cast to int the id requests

--- app/GraphQL/Inventory/Mutations/ProductsTypes/ProductsTypes.php
+++ app/GraphQL/Inventory/Mutations/ProductsTypes/ProductsTypes.php
@@ -45,7 +45,7 @@ class ProductsTypes
      */
     public function update(mixed $root, array $request): ProductsTypesModel
     {
-        $productType = ProductsTypesRepository::getById($request['id'], auth()->user()->getCurrentCompany());
+        $productType = ProductsTypesRepository::getById((int) $request['id'], auth()->user()->getCurrentCompany());
         $productType->update($request['input']);
         return $productType;
     }
@@ -60,7 +60,7 @@ class ProductsTypes
      */
     public function delete(mixed $root, array $request): bool
     {
-        $productType = ProductsTypesRepository::getById($request['id'], auth()->user()->getCurrentCompany());
+        $productType = ProductsTypesRepository::getById((int) $request['id'], auth()->user()->getCurrentCompany());
         return $productType->delete();
     }
 }
