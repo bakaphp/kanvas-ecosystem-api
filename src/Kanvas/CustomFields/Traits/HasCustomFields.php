dca6bc148399bd641791dcdece12d65a3d4db030
FredPeal <frederickpeal@gmail.com>
2024-03-17 10:01:54 -0400

    fix: is JSON

--- src/Kanvas/CustomFields/Traits/HasCustomFields.php
+++ src/Kanvas/CustomFields/Traits/HasCustomFields.php
@@ -156,7 +156,7 @@ trait HasCustomFields
         $modelName = get_class($this);
         $user = Auth::user();
 
-        $value = Str::isJson($value) || is_array($value) ? json_decode($value) : $value;
+        $value = Str::isJson($value) ? json_decode($value) : $value;
         $this->setInRedis($name, $value);
 
         $this->createCustomField($name);
@@ -323,8 +323,6 @@ trait HasCustomFields
 
     /**
      * Get a model from a custom field.
-     *
-     * @param  Companies $company
      */
     public static function getByCustomField(string $name, mixed $value, ?Companies $company = null): ?Model
     {
