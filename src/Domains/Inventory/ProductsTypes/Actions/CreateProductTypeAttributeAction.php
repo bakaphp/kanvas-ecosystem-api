4d6eb405d47712997f25cb94f9d51de16dfe370d
arfenis <arfen00@gmail.com>
2024-03-18 17:51:33 -0400

    Create action to create product type attributes

---
+++ src/Domains/Inventory/ProductsTypes/Actions/CreateProductTypeAttributeAction.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Inventory\ProductsTypes\Actions;
+
+use Baka\Users\Contracts\UserInterface;
+use Kanvas\Companies\Repositories\CompaniesRepository;
+use Kanvas\Inventory\ProductsTypes\DataTransferObject\ProductsTypesAttributes as ProductsTypesAttributesDto;
+use Kanvas\Inventory\ProductsTypes\Models\ProductsTypes;
+use Kanvas\Inventory\ProductsTypes\Models\ProductsTypesAttributes;
+
+class CreateProductTypeAttributeAction
+{
+    /**
+     * __construct.
+     *
+     * @return void
+     */
+    public function __construct(
+        protected ProductsTypesAttributesDto $data,
+        protected UserInterface $user
+    ) {
+    }
+
+    /**
+     * execute.
+     *
+     * @return ProductsTypes
+     */
+    public function execute(): ProductsTypesAttributes
+    {
+        CompaniesRepository::userAssociatedToCompany(
+            $this->data->productsTypes->company,
+            $this->user
+        );
+
+        return ProductsTypesAttributes::firstOrCreate([
+            'products_types_id' => $this->data->productsTypes->getId(),
+            'attributes_id' => $this->data->attributes->getId(),
+            'to_variants' => $this->data->toVariant,
+        ]);
+    }
+}
