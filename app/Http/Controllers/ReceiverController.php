da6f7e29a9969d9a31f1bc06561cd87e2b0e7ea8
kaioken <max@mctekk.com>
2024-03-26 09:41:54 -0400

    feat; rest receiver

---
+++ app/Http/Controllers/ReceiverController.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+use Kanvas\Apps\Models\Apps;
+use Kanvas\Connectors\Zoho\Workflows\ZohoLeadOwnerWorkflow;
+use Kanvas\Guild\Leads\Models\LeadReceiver;
+use Workflow\WorkflowStub;
+
+class ReceiverController extends BaseController
+{
+    /**
+     * Receiver system based on UUID
+     *
+     * @throws BindingResolutionException
+     */
+    public function store(string $uuid, Request $request): JsonResponse
+    {
+        $app = app(Apps::class);
+        $receiver = LeadReceiver::fromApp($app)->where('uuid', $uuid)->first();
+
+        if (! $receiver) {
+            return response()->json('Receiver not found', 404);
+        }
+
+        /**
+         * @todo
+         * This has to be a system where based on the receiver,
+         * the receiver will determine the system module and action it will execute.
+         * Then it will evaluate the data and send it to the queue to process.
+         */
+
+        $leadExternalId = $request->get('entity_id');
+
+        if ($receiver->rotation === null) {
+            return response()->json('Rotation not found', 404);
+        }
+
+        $workflow = WorkflowStub::make(ZohoLeadOwnerWorkflow::class);
+        $workflow->start($leadExternalId, $receiver, app(Apps::class), []);
+
+        return response()->json('Receiver processed');
+    }
+}
