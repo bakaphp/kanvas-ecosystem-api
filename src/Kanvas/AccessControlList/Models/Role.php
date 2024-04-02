d9362508b8362264d09a5857bcdca18d34c109f1
FredPeal <frederickpeal@gmail.com>
2024-04-02 03:45:07 -0400

    refactor: add counts to roles

--- src/Kanvas/AccessControlList/Models/Role.php
+++ src/Kanvas/AccessControlList/Models/Role.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Kanvas\AccessControlList\Models;
 
-use Silber\Bouncer\Database\Role as SilberRole;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Facades\Redis;
+use Kanvas\Users\Models\Users;
 use Laravel\Scout\Searchable;
+use Silber\Bouncer\Database\Role as SilberRole;
 
 /**
  * @property int $id
@@ -17,4 +20,31 @@ class Role extends SilberRole
 {
     use Searchable;
     protected $connection = 'mysql';
+
+    // public function users(): BelongsToMany
+    // {
+    //     return $this->belongsToMany(Users::class, 'assigned_roles', 'entity_id', 'role_id')->where('entity_type', Users::class);
+    // }
+
+    public function getUserCountAttribute(): int
+    {
+        $count = Redis::get('role:' . $this->id . ':users_count');
+        if (! $count) {
+            $count = $this->users()->count();
+            Redis::setex('role:' . $this->id . ':users_count', 120, $count);
+        }
+
+        return $count;
+    }
+
+    public function getAbilitiesCountAttribute(): int
+    {
+        $count = Redis::get('role:' . $this->id . ':abilities_count');
+        if (! $count) {
+            $count = $this->abilities()->count();
+            Redis::setex('role:' . $this->id . ':abilities_count', 120, $count);
+        }
+
+        return $count;
+    }
 }
