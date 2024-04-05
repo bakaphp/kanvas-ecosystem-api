0518bbd9e50be7e3b11ace908d78f313293ee402
arfenis <arfen00@gmail.com>
2024-04-05 10:28:23 -0400

    wip

--- src/Domains/Inventory/Shopify/Actions/CreateProductsAction.php
+++ src/Domains/Inventory/Shopify/Actions/CreateProductsAction.php
@@ -11,9 +11,9 @@ class CreateProductsAction
     public static function createProductShopify()
     {
         $config = array(
-            'ShopUrl' => 'bldstage.shopify.com',
-            'ApiKey' => 'ea47032c39eda36e3e91004a9c923e0c',
-            'Password' => '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'
+            'ShopUrl' => 'xxxx',
+            'ApiKey' => 'xxx',
+            'Password' => 'xxx'
         );
 
         $shopify = new ShopifySDK;
