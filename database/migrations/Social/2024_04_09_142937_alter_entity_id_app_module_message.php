7839831f3f9fa751011ce763f022f0af1eef89d8
FredPeal <frederickpeal@gmail.com>
2024-04-09 10:32:38 -0400

    refactor: entity_id string

---
+++ database/migrations/Social/2024_04_09_142937_alter_entity_id_app_module_message.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        Schema::connection('social')->table('app_module_message', function ($table) {
+            $table->string('entity_id', 255)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::connection('social')->table('app_module_message', function ($table) {
+            $table->integer('entity_id')->change();
+        });
+    }
+};
