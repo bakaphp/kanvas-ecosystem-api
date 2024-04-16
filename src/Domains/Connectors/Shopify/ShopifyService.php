c5fc61535d97964d3056ff20aee4b5c7277efc4f
arfenis <arfen00@gmail.com>
2024-04-16 04:09:09 -0400

    Create method to add product to shopify

--- src/Domains/Connectors/Shopify/ShopifyService.php
+++ src/Domains/Connectors/Shopify/ShopifyService.php
@@ -8,7 +8,10 @@ use Baka\Contracts\AppInterface;
 use Baka\Contracts\CompanyInterface;
 use Kanvas\Connectors\Shopify\DataTransferObject\Shopify as ShopifyDto;
 use Kanvas\Connectors\Shopify\Enums\CustomFieldEnum;
+use Kanvas\Connectors\Shopify\Enums\StatusEnum;
+use Kanvas\Inventory\Products\Models\Products;
 use Kanvas\Inventory\Regions\Models\Regions;
+use Kanvas\Inventory\Variants\Models\Variants;
 use PHPShopify\ShopifySDK;
 
 class ShopifyService
@@ -40,5 +43,44 @@ class ShopifyService
         ];
 
         return $data->company->set($clientCredentialNaming, $configData);
+    }
+
+    /**
+     * Map and create an product on shopify sdk.
+     *
+     * @param Products $product
+     * @param StatusEnum $status
+     * @return array
+     */
+    public function createProduct(Products $product, StatusEnum $status): array
+    {
+        $productInfo = [
+            'title' => $product->name,
+            'body_html' => $product->description,
+            'product_type' => $product->productsTypes->name,
+            'status' => $status->value
+        ];
+
+        foreach($product->variants as $variant)
+        {
+            $productInfo['variants'][] = $this->mapVariant($variant);
+        }
+
+        return $this->shopifySdk->Product->post($productInfo);
+    }
+
+    /**
+     * Map the data from the variant into the array
+     *
+     * @param Variants $variant
+     * @return array
+     */
+    public function mapVariant(Variants $variant): array
+    {
+        return [
+            'option1' => $variant->name,
+            'sku' => $variant->sku,
+            'barcode' => $variant->barcode,
+        ];
     }
 }
