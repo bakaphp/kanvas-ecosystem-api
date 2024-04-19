0adc7c408869a9590692f8b645ac8f2879dd06bb
kaioken <max@mctekk.com>
2024-04-19 02:06:13 -0400

    feat: variant channnel default price

---
+++ app/GraphQL/Inventory/Types/MetadataType.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Inventory\Types;
+
+use Kanvas\Inventory\Variants\Models\Variants;
+
+class MetadataType
+{
+    public function linkedStores(Variants $variant, array $request): array
+    {
+        return [
+            'shopify' => null,
+        ];
+    }
+}
