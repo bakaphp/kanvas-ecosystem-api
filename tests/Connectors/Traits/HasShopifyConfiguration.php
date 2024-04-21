06fdf1feab5c755d6ba02db49d22d705c1242419
kaioken <max@mctekk.com>
2024-04-21 00:56:36 -0400

    fix: test

--- tests/Connectors/Traits/HasShopifyConfiguration.php
+++ tests/Connectors/Traits/HasShopifyConfiguration.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Connectors\Traits;
 
-use Kanvas\Apps\Models\Apps;
 use Kanvas\Connectors\Shopify\DataTransferObject\Shopify;
-use Kanvas\Connectors\Shopify\Enums\CustomFieldEnum;
 use Kanvas\Connectors\Shopify\Services\ShopifyConfigurationService;
 use Kanvas\Inventory\Products\Models\Products;
 use Kanvas\Inventory\Regions\Models\Regions;
@@ -15,9 +13,7 @@ trait HasShopifyConfiguration
 {
     public function setupShopifyConfiguration(Products $product, Regions $region): void
     {
-        if (app(Apps::class)->get(CustomFieldEnum::SHOPIFY_API_KEY->value) !== null
-            && app(Apps::class)->get(CustomFieldEnum::SHOPIFY_API_SECRET->value) !== null
-            && app(Apps::class)->get(CustomFieldEnum::SHOP_URL->value) !== null) {
+        if (! getenv('TEST_SHOPIFY_API_KEY') || ! getenv('TEST_SHOPIFY_API_SECRET') || ! getenv('TEST_SHOPIFY_SHOP_URL')) {
             return;
         }
 
