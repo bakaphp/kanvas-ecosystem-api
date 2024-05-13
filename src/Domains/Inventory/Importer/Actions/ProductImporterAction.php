87e4cce43134a4e06fcda89536766c62bd89a366
kaioken <max@mctekk.com>
2024-05-13 13:48:29 -0400

    refact: remove images when uplading

--- src/Domains/Inventory/Importer/Actions/ProductImporterAction.php
+++ src/Domains/Inventory/Importer/Actions/ProductImporterAction.php
@@ -88,6 +88,7 @@ class ProductImporterAction
             }
 
             if (! empty($this->importedProduct->files)) {
+                $this->product->deleteFiles();
                 foreach ($this->importedProduct->files as $file) {
                     $this->product->addFileFromUrl($file['url'], $file['name']);
                 }
