3400f038dac6aa978a024ba44d425348c6db7ea3
FredPeal <frederickpeal@gmail.com>
2024-05-16 01:18:55 -0400

    refactor: create assign to default company

---
+++ src/Kanvas/Users/Workflows/Activities/AssignToDefaultCompanyActivity.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Users\Workflows\Activities;
+
+use Kanvas\Apps\Models\Apps;
+use Kanvas\Apps\Repositories\AppsRepository;
+use Kanvas\Enums\StateEnums;
+use Kanvas\Users\Models\Users;
+use Workflow\Activity;
+
+class AssignToDefaultCompanyActivity extends Activity
+{
+    public function execute(Users $user, Apps $app, array $param): void
+    {
+        $appKey = config('kanvas.app.id');
+        $app = AppsRepository::findFirstByKey($appKey);
+        $param['company']->associateApp($app);
+        $param['company']->associateUserApp(
+            $user,
+            $app,
+            StateEnums::ON->getValue()
+        );
+    }
+}
