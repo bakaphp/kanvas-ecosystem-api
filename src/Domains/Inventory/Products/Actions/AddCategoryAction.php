329c1504d962b4f588b7965b1978dec25c2c0c0b
arfenis <arfen00@gmail.com>
2024-05-29 13:19:47 -0400

    Create action to add Category

---
+++ src/Domains/Inventory/Products/Actions/AddCategoryAction.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Inventory\Products\Actions;
+
+use Kanvas\Inventory\Categories\Models\Categories;
+use Kanvas\Inventory\Products\Models\Products;
+use Kanvas\Inventory\Products\Models\ProductsCategories;
+
+class AddCategoryAction
+{
+    public function __construct(
+        protected Products $product,
+        protected Categories $category
+    ) {
+    }
+
+    public function execute(): void
+    {
+        ProductsCategories::firstOrCreate(
+            [
+                'categories_id' => $this->category->getId(),
+                'products_id' => $this->product->getId(),
+            ]
+        );
+    }
+}
