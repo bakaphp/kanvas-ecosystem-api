752583beb42f5930c2fa70e80c82d18b30a932e8
kaioken <max@mctekk.com>
2024-06-01 17:00:01 -0400

    test

---
+++ tests/Ecosystem/Integration/Auth/FacebookLoginTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Ecosystem\Integration\Auth;
+
+use Kanvas\Apps\Models\Apps;
+use Kanvas\Auth\Socialite\SocialManager;
+use Kanvas\Enums\AppSettingsEnums;
+use Tests\TestCase;
+
+final class FacebookLoginTest extends TestCase
+{
+    public function testLoginWinJWTToken()
+    {
+      
+    }
+}
