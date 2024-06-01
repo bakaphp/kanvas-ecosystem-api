a08d73e57103b7ac753a82fe0badc3ef8cf03c81
kaioken <max@mctekk.com>
2024-06-01 18:15:18 -0400

    fix token

--- tests/Ecosystem/Integration/Auth/FacebookLoginTest.php
+++ tests/Ecosystem/Integration/Auth/FacebookLoginTest.php
@@ -4,12 +4,28 @@ declare(strict_types=1);
 
 namespace Tests\Ecosystem\Integration\Auth;
 
+use Firebase\JWT\JWT;
+use Kanvas\Apps\Models\Apps;
+use Kanvas\Auth\Socialite\SocialManager;
+use Kanvas\Enums\AppSettingsEnums;
 use Tests\TestCase;
 
 final class FacebookLoginTest extends TestCase
 {
     public function testLoginWinJWTToken()
     {
+        /**
+         * @todo look for a way to make this work on the CI
+         */
+        $token = "";
+       
+        $app = app(Apps::class);
+        $app->set(AppSettingsEnums::SOCIALITE_PROVIDER_FACEBOOK->getValue(), ['client_id' => '1234567890']);
 
+        $socialManager = SocialManager::getDriver('facebook', $app);
+
+        //$socialManager->getUserFromToken($token);
     }
+
+    
 }
