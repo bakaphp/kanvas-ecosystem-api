220bc469d238e4532cfe4f0e1fbbab6ce6ab9c70
kaioken <max@mctekk.com>
2024-06-08 09:04:16 -0400

    refact: filter by app

--- app/GraphQL/Social/Queries/Tags/TagsQueries.php
+++ app/GraphQL/Social/Queries/Tags/TagsQueries.php
@@ -5,19 +5,21 @@ declare(strict_types=1);
 namespace App\GraphQL\Social\Queries\Tags;
 
 use Baka\Enums\StateEnums;
+use Kanvas\Apps\Models\Apps;
 use Kanvas\Social\Tags\Models\Tag;
 use Kanvas\SystemModules\Repositories\SystemModulesRepository;
-use Kanvas\Apps\Models\Apps;
 
 class TagsQueries
 {
     public function getTagsBuilder(mixed $root, array $args): mixed
     {
-        $systemModule = SystemModulesRepository::getByModelName($root::class, app(Apps::class));
+        $app = app(Apps::class);
+        $systemModule = SystemModulesRepository::getByModelName($root::class, $app);
 
         return Tag::whereHas('taggables', function ($query) use ($root, $systemModule) {
             $query->where('entity_id', $root->getKey());
             $query->where('entity_namespace', $systemModule->model_name);
+            $query->where('apps_id', $systemModule->apps_id);
         })->where('is_deleted', StateEnums::NO->getValue());
     }
 }
