7023663e6570f8b5b397caf990e4b12a3917455a
arfenis <arfen00@gmail.com>
2024-06-12 16:14:23 -0400

    Searchable warehouses

--- src/Domains/Inventory/Warehouses/Models/Warehouses.php
+++ src/Domains/Inventory/Warehouses/Models/Warehouses.php
@@ -13,6 +13,9 @@ use Kanvas\Inventory\Models\BaseModel;
 use Kanvas\Inventory\Regions\Models\Regions;
 use Kanvas\Inventory\Traits\DefaultTrait;
 use Kanvas\Inventory\Variants\Models\VariantsWarehouses;
+use Laravel\Scout\EngineManager;
+use Laravel\Scout\Engines\Engine;
+use Laravel\Scout\Searchable;
 
 /**
  * Class Warehouses.
@@ -35,6 +38,9 @@ class Warehouses extends BaseModel
 {
     use UuidTrait;
     use DefaultTrait;
+    use Searchable {
+        search as public traitSearch;
+    }
 
     protected $table = 'warehouses';
 
@@ -203,5 +209,25 @@ class Warehouses extends BaseModel
         }
 
         return 0;
+    }
+
+    /**
+     * Get the engine used to index the model.
+     */
+    public function searchableUsing(): Engine
+    {
+        return app(EngineManager::class)->engine('database');
+    }
+
+    public function toSearchableArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'uuid' => $this->uuid,
+            'regions_id' => $this->regions_id,
+            'location' => $this->location,
+            'is_published' => $this->is_published,
+        ];
     }
 }
