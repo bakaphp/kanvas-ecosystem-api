ee4df9263c921288b2a27e600218e2d893654a9f
kaioken <max@mctekk.com>
2024-06-11 23:29:12 -0400

    refact: people tags

---
+++ src/Domains/Social/Tags/Traits/HasTagsTrait.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Social\Tags\Traits;
+
+use Baka\Contracts\AppInterface;
+use Baka\Contracts\CompanyInterface;
+use Baka\Users\Contracts\UserInterface;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Kanvas\Social\Tags\Actions\CreateTagAction;
+use Kanvas\Social\Tags\DataTransferObjects\Tag;
+use Kanvas\Social\Tags\Models\Tag as ModelsTag;
+use Kanvas\Social\Tags\Models\TagEntity;
+
+trait HasTagsTrait
+{
+    public function tags(bool $userCompany = true): MorphToMany
+    {
+        $dbConnection = config('database.connections.social.database');
+
+        $query = $this->morphToMany(ModelsTag::class, 'taggable', $dbConnection . '.tags_entities', 'tags_id', 'entity_id')
+            ->using(TagEntity::class)
+            ->wherePivot('apps_id', $this->apps_id);
+
+        if ($userCompany) {
+            $query->wherePivot('companies_id', $this->companies_id);
+        }
+
+        return $query;
+    }
+
+    public function addTag(
+        string $tag,
+        ?AppInterface $app = null,
+        ?UserInterface $user = null,
+        ?CompanyInterface $company = null
+    ): void {
+        $app = $this->app ?? $app;
+        $user = $this->user ?? $user;
+        $company = $company ?? $this->company;
+
+        $tag = (new CreateTagAction(
+            new Tag(
+                $app,
+                $user,
+                $company,
+                $tag
+            )
+        ))->execute();
+
+        $this->tags()->attach($tag->getId(), [
+            'entity_namespace' => self::class,
+            'companies_id' => $company->getId(),
+            'apps_id' => $app->getId(),
+            'users_id' => $user->getId(),
+            'taggable_type' => ModelsTag::class,
+            'is_deleted' => 0,
+        ]);
+    }
+
+    public function addTags(
+        array $tags,
+        ?AppInterface $app = null,
+        ?UserInterface $user = null,
+        ?CompanyInterface $company = null
+    ): void {
+        foreach ($tags as $tag) {
+            $this->addTag($tag, $app, $user, $company);
+        }
+    }
+
+    public function removeTag(string $tag): void
+    {
+        $this->tags()->where('name', $tag)->delete();
+    }
+
+    public function removeTags(array $tags): void
+    {
+        $this->tags()->whereIn('name', $tags)->delete();
+    }
+
+    public function syncTags(array $tags): void
+    {
+        $this->tags()->detach();
+        $this->addTags($tags);
+    }
+}
