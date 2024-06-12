2212214b3fb8357922d36bd21d1c4fcdd54a0338
kaioken <max@mctekk.com>
2024-06-12 00:11:39 -0400

    refact: entity tags

--- src/Domains/Guild/Leads/Factories/LeadFactory.php
+++ src/Domains/Guild/Leads/Factories/LeadFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Kanvas\Guild\Leads\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Kanvas\Apps\Models\Apps;
 use Kanvas\Companies\Models\Companies;
 use Kanvas\Enums\AppEnums;
 use Kanvas\Guild\Customers\Models\People;
@@ -17,6 +18,7 @@ class LeadFactory extends Factory
     public function definition()
     {
         $company = Companies::factory()->create();
+        $app = app(Apps::class);
         return [
             'firstname' => fake()->firstName,
             'lastname' => fake()->lastName,
@@ -24,7 +26,7 @@ class LeadFactory extends Factory
             'companies_id' => $company->getId(),
             'companies_branches_id' => AppEnums::GLOBAL_COMPANY_ID->getValue(),
             'users_id' => 1,
-            'people_id' => People::factory()->create()->getId(),
+            'people_id' => People::factory()->withAppId($app->getId())->withCompanyId($company->getId())->create()->getId(),
             'leads_receivers_id' => 0,
             'leads_owner_id' => 1,
         ];
