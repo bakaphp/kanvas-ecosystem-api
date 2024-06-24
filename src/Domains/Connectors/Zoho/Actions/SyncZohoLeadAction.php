5c330da239fdf330af5ea905edb19fc6a80d81c5
kaioken <max@mctekk.com>
2024-06-24 11:54:29 -0400

    fix: RC1 issues

--- src/Domains/Connectors/Zoho/Actions/SyncZohoLeadAction.php
+++ src/Domains/Connectors/Zoho/Actions/SyncZohoLeadAction.php
@@ -7,6 +7,7 @@ namespace Kanvas\Connectors\Zoho\Actions;
 use Baka\Contracts\AppInterface;
 use Baka\Contracts\CompanyInterface;
 use Baka\Support\Str;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Kanvas\Connectors\Zoho\Enums\CustomFieldEnum;
 use Kanvas\Connectors\Zoho\ZohoService;
@@ -39,6 +40,17 @@ class SyncZohoLeadAction
             $this->zohoLeadId,
             $this->company
         );
+
+        if (! $localLead) {
+            $table = (new Lead())->getTable();
+            $localLead = Lead::join(DB::connection('ecosystem')->getDatabaseName() . '.apps_custom_fields', 'apps_custom_fields.entity_id', '=', $table . '.id')
+                ->where('apps_custom_fields.companies_id', $this->company->getId())
+                ->where('apps_custom_fields.model_name', 'Gewaer\\Models\\Leads')
+                ->where('apps_custom_fields.name', CustomFieldEnum::ZOHO_LEAD_ID->value)
+                ->where('apps_custom_fields.value', $this->zohoLeadId)
+                ->select($table . '.*')
+                ->first();
+        }
 
         if (! $localLead) {
             return ;
