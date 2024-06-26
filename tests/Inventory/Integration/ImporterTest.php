8403991db2cacc57461c8993fbe8db926ac49ab1
kaioken <max@mctekk.com>
2024-06-26 18:15:30 -0400

    fix: type

--- tests/Inventory/Integration/ImporterTest.php
+++ tests/Inventory/Integration/ImporterTest.php
@@ -20,135 +20,135 @@ final class ImporterTest extends TestCase
 {
     public function testImportAction(): void
     {
-    $company = auth()->user()->getCurrentCompany();
+        $company = auth()->user()->getCurrentCompany();
 
-    $setupCompany = new Setup(
-        app(Apps::class),
-        auth()->user(),
-        $company
-    );
-    $setupCompany->run();
-
-    $attributes = [
-        'attributes' => [
-            [
-                'name' => fake()->word(),
-                'value' => fake()->word(),
-            ],
-            [
-                'name' => fake()->word(),
-                'value' => fake()->word(),
-            ],
-        ],
-    ];
-
-    $region = RegionRepository::getByName('default', $company);
-
-    $warehouse = [
-        'name' => fake()->word(),
-        'regions_id' => $region->getId(),
-        'is_default' => true,
-        'is_published' => true,
-    ];
-
-    $warehouseData = (new CreateWarehouseAction(
-        WarehousesDto::viaRequest($warehouse, auth()->user(), $company),
-        auth()->user()
-    ))->execute();
-
-    $statusData = (new CreateStatusAction(
-        new Status(
+        $setupCompany = new Setup(
             app(Apps::class),
+            auth()->user(),
+            $company
+        );
+        $setupCompany->run();
+
+        $attributes = [
+            'attributes' => [
+                [
+                    'name' => fake()->word(),
+                    'value' => fake()->word(),
+                ],
+                [
+                    'name' => fake()->word(),
+                    'value' => fake()->word(),
+                ],
+            ],
+        ];
+
+        $region = RegionRepository::getByName('default', $company);
+
+        $warehouse = [
+            'name' => fake()->word(),
+            'regions_id' => $region->getId(),
+            'is_default' => true,
+            'is_published' => true,
+        ];
+
+        $warehouseData = (new CreateWarehouseAction(
+            WarehousesDto::viaRequest($warehouse, auth()->user(), $company),
+            auth()->user()
+        ))->execute();
+
+        $statusData = (new CreateStatusAction(
+            new Status(
+                app(Apps::class),
+                $company,
+                auth()->user(),
+                'Default',
+                true
+            ),
+            auth()->user()
+        ))->execute();
+
+        $productData = ProductImporter::from([
+            'name' => fake()->word(),
+            'description' => fake()->sentence(),
+            'slug' => fake()->slug(),
+            'sku' => fake()->time(),
+            'price' => fake()->randomNumber(2),
+            'quantity' => fake()->randomNumber(2),
+            'isPublished' => true,
+            'files' => [
+                [
+                    'url' => fake()->imageUrl(),
+                    'name' => fake()->word(),
+                ],
+                [
+                    'url' => fake()->imageUrl(),
+                    'name' => fake()->word(),
+                ],
+            ],
+            'variants' => [
+                [
+                    'name' => fake()->word(),
+                    //'warehouse_id' => $warehouseData->getId(),
+                    'warehouse' => [
+                        'id' => $warehouseData->getId(),
+                        'price' => fake()->randomNumber(2),
+                        'quantity' => fake()->randomNumber(2),
+                        'sku' => fake()->time(),
+                        'is_new' => fake()->boolean(),
+                        'status' => $statusData,
+                    ],
+                    'description' => fake()->sentence(),
+                    'sku' => fake()->time(),
+                    'price' => fake()->randomNumber(2),
+                    'is_published' => true,
+                    'slug' => fake()->slug(),
+                    ...$attributes,
+                    'files' => [
+                        [
+                            'url' => fake()->imageUrl(),
+                            'name' => fake()->word(),
+                        ],
+                        [
+                            'url' => fake()->imageUrl(),
+                            'name' => fake()->word(),
+                        ],
+                    ],
+                ],
+                [
+                    'name' => fake()->word(),
+                    //'warehouse_id' => $warehouseData->getId(),
+                    'warehouse' => [
+                        'id' => $warehouseData->getId(),
+                        'price' => fake()->randomNumber(2),
+                        'quantity' => fake()->randomNumber(2),
+                        'sku' => fake()->time(),
+                        'is_new' => fake()->boolean(),
+                    ],
+                    'description' => fake()->sentence(),
+                    'sku' => fake()->time(),
+                    'price' => fake()->randomNumber(2),
+                    'is_published' => true,
+                    'slug' => fake()->slug(),
+                    ...$attributes,
+                ],
+            ],
+            'categories' => [
+                [
+                    'name' => fake()->word(),
+                    'code' => (string) fake()->randomNumber(3),
+                    'position' => fake()->randomNumber(1),
+                ],
+            ],
+            ...$attributes,
+        ]);
+
+        $productImporter = new ProductImporterAction(
+            $productData,
             $company,
             auth()->user(),
-            'Default',
-            true
-        ),
-        auth()->user()
-    ))->execute();
+            $region
+        );
 
-    $productData = ProductImporter::from([
-        'name' => fake()->word(),
-        'description' => fake()->sentence(),
-        'slug' => fake()->slug(),
-        'sku' => fake()->time(),
-        'price' => fake()->randomNumber(2),
-        'quantity' => fake()->randomNumber(2),
-        'isPublished' => true,
-        'files' => [
-            [
-                'url' => fake()->imageUrl(),
-                'name' => fake()->word(),
-            ],
-            [
-                'url' => fake()->imageUrl(),
-                'name' => fake()->word(),
-            ],
-        ],
-        'variants' => [
-            [
-                'name' => fake()->word(),
-                //'warehouse_id' => $warehouseData->getId(),
-                'warehouse' => [
-                    'id' => $warehouseData->getId(),
-                    'price' => fake()->randomNumber(2),
-                    'quantity' => fake()->randomNumber(2),
-                    'sku' => fake()->time(),
-                    'is_new' => fake()->boolean(),
-                    'status' => $statusData,
-                ],
-                'description' => fake()->sentence(),
-                'sku' => fake()->time(),
-                'price' => fake()->randomNumber(2),
-                'is_published' => true,
-                'slug' => fake()->slug(),
-                ...$attributes,
-                'files' => [
-                    [
-                        'url' => fake()->imageUrl(),
-                        'name' => fake()->word(),
-                    ],
-                    [
-                        'url' => fake()->imageUrl(),
-                        'name' => fake()->word(),
-                    ],
-                ],
-            ],
-            [
-                'name' => fake()->word(),
-                //'warehouse_id' => $warehouseData->getId(),
-                'warehouse' => [
-                    'id' => $warehouseData->getId(),
-                    'price' => fake()->randomNumber(2),
-                    'quantity' => fake()->randomNumber(2),
-                    'sku' => fake()->time(),
-                    'is_new' => fake()->boolean(),
-                ],
-                'description' => fake()->sentence(),
-                'sku' => fake()->time(),
-                'price' => fake()->randomNumber(2),
-                'is_published' => true,
-                'slug' => fake()->slug(),
-                ...$attributes,
-            ],
-        ],
-        'categories' => [
-            [
-                'name' => fake()->word(),
-                'code' => (string) fake()->randomNumber(3),
-                'position' => fake()->randomNumber(1),
-            ],
-        ],
-        ...$attributes,
-    ]);
-
-    $productImporter = new ProductImporterAction(
-        $productData,
-        $company,
-        auth()->user(),
-        $region
-    );
-
-    $this->assertInstanceOf(Products::class, $productImporter->execute());
-}
+        $this->assertInstanceOf(Products::class, $productImporter->execute());
+    }
 }
