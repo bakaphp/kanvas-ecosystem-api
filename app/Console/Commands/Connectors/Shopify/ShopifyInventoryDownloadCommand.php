22f081526231594358a4574388013785155a3f2f
kaioken <max@mctekk.com>
2024-07-18 09:38:33 -0400

    fix: model

--- app/Console/Commands/Connectors/Shopify/ShopifyInventoryDownloadCommand.php
+++ app/Console/Commands/Connectors/Shopify/ShopifyInventoryDownloadCommand.php
@@ -8,6 +8,7 @@ use Illuminate\Console\Command;
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Companies\Models\CompaniesBranches;
 use Kanvas\Connectors\Shopify\Actions\DownloadAllShopifyProductsAction;
+use Kanvas\Inventory\Channels\Models\Channels;
 use Kanvas\Inventory\Warehouses\Models\Warehouses;
 
 class ShopifyInventoryDownloadCommand extends Command
@@ -35,14 +36,15 @@ class ShopifyInventoryDownloadCommand extends Command
     {
         $app = Apps::getById((int) $this->argument('app_id'));
         $branch = CompaniesBranches::getById((int) $this->argument('branch_id'));
-        $warehouse = Warehouses::fromApp($app)->where('id', $this->argument('warehouse_id'))->firstOrFail();
-        $channel = $this->argument('channel_id') ? $warehouse->channels()->where('id', $this->argument('channel_id'))->firstOrFail() : null;
+        $warehouse = Warehouses::fromApp($app)->fromCompany($branch->company)->where('id', $this->argument('warehouse_id'))->firstOrFail();
+        $channel = $this->argument('channel_id') ? Channels::fromApp($app)->fromCompany($branch->company)->where('id', $this->argument('channel_id'))->firstOrFail() : null;
 
         $downloadProduct = new DownloadAllShopifyProductsAction(
             $warehouse->app,
             $warehouse,
             $branch,
-            $branch->company->user
+            $branch->company->user,
+            $channel
         );
 
         $total = $downloadProduct->execute();
--- src/Domains/Connectors/Shopify/Services/ShopifyProductService.php
+++ src/Domains/Connectors/Shopify/Services/ShopifyProductService.php
@@ -8,9 +8,9 @@ use Baka\Contracts\AppInterface;
 use Baka\Contracts\CompanyInterface;
 use Baka\Users\Contracts\UserInterface;
 use Kanvas\Connectors\Shopify\Enums\CustomFieldEnum;
+use Kanvas\Inventory\Channels\Models\Channels;
 use Kanvas\Inventory\Regions\Models\Regions;
 use Kanvas\Inventory\Warehouses\Models\Warehouses;
-use Kanvas\Social\Channels\Models\Channel;
 
 class ShopifyProductService
 {
@@ -23,11 +23,11 @@ class ShopifyProductService
         protected string|int $productId,
         protected ?UserInterface $user = null,
         protected ?Warehouses $warehouses = null,
-        protected ?Channel $channel = null
+        protected ?Channels $channel = null
     ) {
         $this->user = $user ?? $this->company->user;
         $this->warehouses = $warehouses ?? Warehouses::fromCompany($this->company)->where('is_default', 1)->where('regions_id', $this->region->id)->firstOrFail();
-        $this->channel = $channel ?? Channel::fromCompany($this->company)->where('is_default', 1)->firstOrFail();
+        $this->channel = $channel ?? Channels::fromCompany($this->company)->where('is_default', 1)->firstOrFail();
     }
 
     public function mapProduct(array $shopifyProduct): array
