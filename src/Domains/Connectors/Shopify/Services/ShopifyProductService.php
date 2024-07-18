4e22b20426072a4cf8a039c5ffc712fb26e4dcc1
kaioken <max@mctekk.com>
2024-07-18 11:24:21 -0400

    fix: integartion

--- src/Domains/Connectors/Shopify/Services/ShopifyProductService.php
+++ src/Domains/Connectors/Shopify/Services/ShopifyProductService.php
@@ -100,7 +100,7 @@ class ShopifyProductService
                 'files' => $this->files['filesSystemVariantImages'][$variant['id']] ?? [],
                 'source' => ShopifyConfigurationService::getKey(CustomFieldEnum::SHOPIFY_VARIANT_ID->value, $this->company, $this->app, $this->region),
                 'sourceId' => $variant['id'],
-                'customFields' => [
+                'custom_fields' => [
                     [
                         'name' => ShopifyConfigurationService::getKey(CustomFieldEnum::SHOPIFY_VARIANT_ID->value, $this->company, $this->app, $this->region),
                         'data' => $variant['id'],
