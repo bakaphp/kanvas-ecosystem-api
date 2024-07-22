a9db93c22532763d1f54e0dad949febcc1a0a229
kaioken <max@mctekk.com>
2024-07-21 20:48:25 -0400

    feat: shopify metafields + attributes

---
+++ src/Domains/Connectors/Shopify/Services/ShopifyVariantMetafieldService.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Connectors\Shopify\Services;
+
+use Baka\Contracts\AppInterface;
+use Baka\Contracts\CompanyInterface;
+use Baka\Support\Str;
+use Exception;
+use Kanvas\Connectors\Shopify\Client;
+use Kanvas\Connectors\Shopify\Enums\CustomFieldEnum;
+use Kanvas\Inventory\Regions\Models\Regions;
+use Kanvas\Inventory\Variants\Models\Variants;
+use PHPShopify\ShopifySDK;
+
+class ShopifyVariantMetafieldService
+{
+    protected ShopifySDK $shopifySdk;
+    protected array $types = [
+        'string' => 'multi_line_text_field',
+        'json' => 'json',
+        'integer' => 'number_integer',
+        'double' => 'number_decimal',
+    ];
+
+    public function __construct(
+        protected AppInterface $app,
+        protected CompanyInterface $company,
+        protected Regions $region,
+        protected Variants $variant
+    ) {
+        $this->shopifySdk = Client::getInstance($app, $company, $region);
+    }
+
+    public function setMetaField(): bool
+    {
+        $attributes = $this->variant->attributes;
+        $shopifyProductVariantId = $this->variant->getShopifyId($this->region);
+        $shopifyProduct = $this->shopifySdk->Product($this->variant->product->getShopifyId($this->region));
+        $shopifyMetaFields = $this->variant->get(CustomFieldEnum::SHOPIFY_META_FIELD_ID->value) ?? [];
+
+        $i = 0;
+        foreach ($attributes as $attribute) {
+            if (! $attribute->is_filterable) {
+                $this->deleteMetaFieldIfExists($shopifyMetaFields, $attribute, $shopifyProduct, $shopifyProductVariantId);
+                continue;
+            }
+
+            $type = $this->determineType($attribute->value);
+            $attributeValue = $type === 'json' ? json_encode($attribute->value) : $attribute->value;
+
+            $mutationGraphql = [
+                'namespace' => 'attributes',
+                'key' => $attribute->name,
+                'value' => $attributeValue,
+                'type' => $this->types[$type],
+            ];
+
+            $metaField = $shopifyProduct->Variant($shopifyProductVariantId)->Metafield->post($mutationGraphql);
+
+            $shopifyMetaFields[$this->region->id][$attribute->id] = $metaField['id'];
+            $i++;
+        }
+
+        $this->variant->set(CustomFieldEnum::SHOPIFY_META_FIELD_ID->value, $shopifyMetaFields);
+
+        return $attributes->count() === $i;
+    }
+
+    protected function deleteMetaFieldIfExists(array &$shopifyMetaFields, $attribute, $shopifyProduct, $shopifyProductVariantId): void
+    {
+        $metaFieldId = $shopifyMetaFields[$this->region->id][$attribute->id] ?? null;
+
+        if ($metaFieldId) {
+            try {
+                $shopifyProduct->Variant($shopifyProductVariantId)->Metafield($metaFieldId)->delete();
+            } catch (Exception $e) {
+                // Handle exception if needed
+            }
+            unset($shopifyMetaFields[$this->region->id][$attribute->id]);
+        }
+    }
+
+    protected function determineType(mixed $value): string
+    {
+        $type = gettype($value);
+
+        return match ($type) {
+            'array' => 'json',
+            'string' => Str::isJson($value) ? 'json' : 'string',
+            default => $type,
+        };
+    }
+
+    public function getMetaField(): array
+    {
+        $shopifyProductVariantId = $this->variant->getShopifyId($this->region);
+        $shopifyProduct = $this->shopifySdk->Product($this->variant->product->getShopifyId($this->region));
+
+        return $shopifyProduct->Variant($shopifyProductVariantId)->Metafield->get();
+    }
+}
