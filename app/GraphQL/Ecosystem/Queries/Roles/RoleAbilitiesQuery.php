59a59b46bb6ec132c996fdd614d12a9517a25ca4
FredPeal <frederickpeal@gmail.com>
2024-07-23 10:17:20 -0400

    refactor: clean pr

--- app/GraphQL/Ecosystem/Queries/Roles/RoleAbilitiesQuery.php
+++ app/GraphQL/Ecosystem/Queries/Roles/RoleAbilitiesQuery.php
@@ -4,15 +4,20 @@ declare(strict_types=1);
 
 namespace App\GraphQL\Ecosystem\Queries\Roles;
 
+use Illuminate\Support\Facades\Redis;
+use Kanvas\AccessControlList\Repositories\RolesRepository;
+use Kanvas\Companies\Repositories\CompaniesRepository;
 use Kanvas\Users\Repositories\UsersRepository;
+use Kanvas\AccessControlList\Enums\RolesEnums;
 
 class RoleAbilitiesQuery
 {
     public function getAllAbilities(mixed $root, array $query): array
     {
+        $company = $query['companyId'] ? CompaniesRepository::getById((int)$query['companyId']) : auth()->user()->getCurrentCompany();
         $abilities = UsersRepository::getUserOfCompanyById(
-            auth()->user()->getCurrentCompany(),
-            $query['userId']
+            $company,
+            (int)$query['userId']
         )->getAbilities();
 
         $mapAbilities = $abilities->map(function ($ability) {
@@ -20,5 +25,15 @@ class RoleAbilitiesQuery
         });
 
         return $mapAbilities->all();
+    }
+
+    public function getAllAbilitiesByRoles(mixed $root, array $request): array
+    {
+        $roles = RolesRepository::getMapAbilityInModules($request['role']);
+        if ($map = Redis::get(RolesEnums::KEY_MAP->value)) {
+            return $map;
+        }
+        Redis::set(RolesEnums::KEY_MAP->value, $roles);
+        return $roles;
     }
 }
