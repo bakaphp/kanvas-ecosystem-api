905fdfcd31fa6cb09fe8dd98a4a3a9abe7e05ac9
arfenis <arfen00@gmail.com>
2024-08-02 14:08:08 -0400

    delete validation

--- app/GraphQL/Inventory/Mutations/Warehouses/Warehouse.php
+++ app/GraphQL/Inventory/Mutations/Warehouses/Warehouse.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\GraphQL\Inventory\Mutations\Warehouses;
 
+use Kanvas\Companies\Repositories\CompaniesRepository;
 use Kanvas\Inventory\Regions\Repositories\RegionRepository;
+use Kanvas\Inventory\Variants\Actions\DeleteWarehouseAction;
 use Kanvas\Inventory\Warehouses\Actions\CreateWarehouseAction;
 use Kanvas\Inventory\Warehouses\DataTransferObject\Warehouses as WarehousesDto;
 use Kanvas\Inventory\Warehouses\Models\Warehouses;
@@ -55,6 +57,11 @@ class Warehouse
     public function delete(mixed $root, array $request): bool
     {
         $warehouse = WarehouseRepository::getById((int) $request['id'], auth()->user()->getCurrentCompany());
+
+        CompaniesRepository::userAssociatedToCompany(
+            $warehouse->company,
+            auth()->user()
+        );
 
         return $warehouse->delete();
     }
