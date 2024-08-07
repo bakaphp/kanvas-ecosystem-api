1df3fbd5c56479d02443c23785ca20794d8ca4f5
kaioken <max@mctekk.com>
2024-08-07 19:41:01 -0400

    refact: view message

--- src/Domains/Guild/Leads/Actions/CreateLeadAction.php
+++ src/Domains/Guild/Leads/Actions/CreateLeadAction.php
@@ -36,7 +36,7 @@ class CreateLeadAction
         $newLead = new Lead();
         $newLead->leads_owner_id = $this->leadData->leads_owner_id;
         $organization = null;
-        
+
         if (! $this->leadData->leads_owner_id) {
             try {
                 $newLead->leads_owner_id = LeadsRepository::getDefaultReceiver($this->leadData->branch)->agents_id;
