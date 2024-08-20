7cded843d142b57bd3894b86724bc39e4a23d0a9
Rafael White <rwhite@mctekk.com>
2024-08-20 13:33:41 -0400

    add is_public column on messages migration

---
+++ database/migrations/Social/2024_08_20_173143_add_is_public_messages_column.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->integer('is_public')->after('total_disliked')->nullable()->default(0)->index('is_public');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->dropColumn('is_public');
+        });
+    }
+};
