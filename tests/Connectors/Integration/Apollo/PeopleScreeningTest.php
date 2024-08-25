f41dfe360bfed12ba1bc673e92109e9e2aa3d9c1
kaioken <max@mctekk.com>
2024-08-25 19:17:58 -0400

    send: test

--- tests/Connectors/Integration/Apollo/PeopleScreeningTest.php
+++ tests/Connectors/Integration/Apollo/PeopleScreeningTest.php
@@ -15,24 +15,24 @@ final class PeopleScreeningTest extends TestCase
 {
     public function testPeopleScreening(): void
     {
-        /*   $app = app(Apps::class);
-          $user = auth()->user();
-          $company = $user->getCurrentCompany();
-          $app->set(ConfigurationEnum::APOLLO_API_KEY->value, getenv('TEST_APOLLO_KEY'));
+        $app = app(Apps::class);
+        $user = auth()->user();
+        $company = $user->getCurrentCompany();
+        $app->set(ConfigurationEnum::APOLLO_API_KEY->value, getenv('TEST_APOLLO_KEY'));
 
-          $people = People::factory()->withAppId($app->getId())->withCompanyId($company->getId())->create();
-          $company = $people->company;
+        $people = People::factory()->withAppId($app->getId())->withCompanyId($company->getId())->create();
+        $company = $people->company;
 
-          $activity = new ScreeningPeopleActivity(
-              0,
-              now()->toDateTimeString(),
-              StoredWorkflow::make(),
-              []
-          );
+        $activity = new ScreeningPeopleActivity(
+            0,
+            now()->toDateTimeString(),
+            StoredWorkflow::make(),
+            []
+        );
 
-          $result = $activity->execute($people, $app, ['company' => $company]);
+        $result = $activity->execute($people, $app, ['company' => $company]);
 
-          $this->assertSame('success', $result['status']);
-          $this->assertSame($people->getId(), $result['people_id']); */
+        $this->assertSame('success', $result['status']);
+        $this->assertSame($people->getId(), $result['people_id']);
     }
 }
