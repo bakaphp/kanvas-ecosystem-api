720d2a8d375295e87e76c38f8e8dead9897f1c33
FredPeal <frederickpeal@gmail.com>
2024-08-26 02:13:24 -0400

    refactor: move to dashboard

---
+++ src/Domains/Dashboard/Actions/SetDefaultDashboardFieldAction.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Dashboard\Actions;
+
+use Kanvas\Companies\Models\Companies;
+use Kanvas\Dashboard\Repositories\DashboardRepositories;
+use Kanvas\Dashboard\Enums\DashboardEnum;
+
+class SetDefaultDashboardFieldAction
+{
+    public function __construct(
+        private Companies $company,
+        private ?string $field = null,
+        private ?string $value = null
+    ) {
+    }
+
+    public function execute(): void
+    {
+        $defaultEnumValue = DashboardEnum::DEFAULT_ENUM->value;
+        $fields = $this->company->get($defaultEnumValue) ?? [];
+
+        if (!is_null($this->field)) {
+            $fields[$this->field] = $this->value;
+            $this->company->set($defaultEnumValue, $fields);
+        }
+
+        $defaultFields = DashboardRepositories::getDefaultFields();
+        $mergedFields = array_merge($defaultFields, $fields);
+
+        $this->company->set($defaultEnumValue, $mergedFields);
+    }
+}
