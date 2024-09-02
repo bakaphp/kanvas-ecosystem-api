b529c83b15d4e4962b12450b1bf6fa7539a5da5c
kaioken <max@mctekk.com>
2024-09-01 23:01:25 -0400

    feat: simple command to send daily emails

---
+++ app/Console/Commands/Ecosystem/UserEngagementReminderCommand.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands\Guild;
+
+use Baka\Enums\StateEnums;
+use Baka\Traits\KanvasJobsTrait;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Notification;
+use Kanvas\Apps\Models\Apps;
+use Kanvas\Enums\AppEnums;
+use Kanvas\Notifications\Templates\Blank;
+use Kanvas\Users\Models\UsersAssociatedApps;
+
+class UserEngagementReminderCommand extends Command
+{
+    use KanvasJobsTrait;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'kanvas:users-engagement-reminder {app_id?} ';
+
+    /**
+     * The console command description.
+     *
+     * @var string|null
+     */
+    protected $description = 'Simple daily email reminder to get users to engage with the app again';
+
+    public function handle(): void
+    {
+        $app = Apps::getById($this->argument('app_id'));
+        $this->overwriteAppService($app);
+
+        $this->info('Sending User Engagement Reminder for app ' . $app->name . ' - ' . date('Y-m-d'));
+        //get the list of user form UsersAssociatedApps chuck by 100 records
+        $users = UsersAssociatedApps::fromApp($app)
+            ->where('is_delete', StateEnums::NO)
+            ->where('companies_id', AppEnums::GLOBAL_COMPANY_ID->getValue())
+            ->chunk(100, function ($users) use ($app) {
+                foreach ($users as $user) {
+                    //send email to user
+                    $this->sendEmail($user, $app);
+                }
+            });
+    }
+
+    public function sendEmail(UsersAssociatedApps $user, Apps $app): void
+    {
+        //send email to user
+        $this->info('Sending email to user ' . $user->email);
+
+        $lastVisitInDays = Carbon::parse($user->lastvisit)->diffInDays(Carbon::now());
+        $this->info('Last visit in days ' . $lastVisitInDays);
+
+        $engagementEmailTemplateConfiguration = $app->get('engagement_email_template') ?? [];
+
+        if (empty($engagementEmailTemplateConfiguration)) {
+            $this->info('No email template configuration found');
+
+            return;
+        }
+
+        if (empty($engagementEmailTemplateConfiguration[$lastVisitInDays])) {
+            $this->info('No email template configuration found for ' . $lastVisitInDays . ' days');
+
+            return;
+        }
+
+        $emailTemplate = $engagementEmailTemplateConfiguration[$lastVisitInDays];
+        $notification = new Blank(
+            $emailTemplate,
+            ['app' => $app, 'user' => $user->user],
+            ['mail'],
+            $user
+        );
+
+        Notification::route('mail', $user->email)->notify($notification);
+        $this->info('Email sent to ' . $user->email);
+    }
+}
