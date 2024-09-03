f67880cf0793ae885f108ad5ccee0fa49bae7611
Rafael White <rwhite@mctekk.com>
2024-09-03 15:14:18 -0400

    remove testing code

--- app/Console/Commands/Ecosystem/Notifications/MailAllAppUsersCommand.php
+++ app/Console/Commands/Ecosystem/Notifications/MailAllAppUsersCommand.php
@@ -40,34 +40,22 @@ class MailAllAppUsersCommand extends Command
         $app = Apps::getById((int) $this->argument('apps_id'));
         $this->overwriteAppService($app);
 
-        // $users = UsersAssociatedApps::fromApp($app)
-        //     ->where('is_delete', StateEnums::NO)
-        //     ->where('companies_id', AppEnums::GLOBAL_COMPANY_ID->getValue())
-        //     ->chunk(100, function ($users) use ($app) {
-        //         foreach ($users as $user) {
-        //             $notification = new Blank(
-        //                 $this->argument('email_template_name'),
-        //                 ['userFirstname' => $user->firstname],
-        //                 ['mail'],
-        //                 $user
-        //             );
-        //             $notification->setSubject($this->argument('subject'));
-        //             Notification::route('mail', $user->email)->notify($notification);
-        //             $this->info('Email Successfully sent to: ' . $user->getId() . ' on app: ' . $app->getId());
-        //             $this->newLine();
-        //         }
-        //     });
-
-        $user = Users::find(1817);
-        $notification = new Blank(
-            $this->argument('email_template_name'),
-            ['userFirstname' => $user->firstname],
-            ['mail'],
-            $user
-        );
-        $notification->setSubject($this->argument('subject'));
-        Notification::route('mail', $user->email)->notify($notification);
-        $this->info('Email Successfully sent to: ' . $user->getId() . ' on app: ' . $app->getId());
-        $this->newLine();
+        $users = UsersAssociatedApps::fromApp($app)
+            ->where('is_delete', StateEnums::NO)
+            ->where('companies_id', AppEnums::GLOBAL_COMPANY_ID->getValue())
+            ->chunk(100, function ($users) use ($app) {
+                foreach ($users as $user) {
+                    $notification = new Blank(
+                        $this->argument('email_template_name'),
+                        ['userFirstname' => $user->firstname],
+                        ['mail'],
+                        $user
+                    );
+                    $notification->setSubject($this->argument('subject'));
+                    Notification::route('mail', $user->email)->notify($notification);
+                    $this->info('Email Successfully sent to: ' . $user->getId() . ' on app: ' . $app->getId());
+                    $this->newLine();
+                }
+            });
     }
 }
