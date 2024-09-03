b783f8b4b2fe004a55128e1a3b262b8513d44107
Rafael White <rwhite@mctekk.com>
2024-09-03 14:41:45 -0400

    test mail using an example user

--- app/Console/Commands/Ecosystem/Notifications/MailAllAppUsersCommand.php
+++ app/Console/Commands/Ecosystem/Notifications/MailAllAppUsersCommand.php
@@ -12,6 +12,7 @@ use Kanvas\Apps\Models\Apps;
 use Kanvas\Enums\AppEnums;
 use Kanvas\Notifications\Templates\Blank;
 use Kanvas\Users\Models\UsersAssociatedApps;
+use Kanvas\Users\Models\Users;
 
 class MailAllAppUsersCommand extends Command
 {
@@ -39,22 +40,34 @@ class MailAllAppUsersCommand extends Command
         $app = Apps::getById((int) $this->argument('apps_id'));
         $this->overwriteAppService($app);
 
-        $users = UsersAssociatedApps::fromApp($app)
-            ->where('is_delete', StateEnums::NO)
-            ->where('companies_id', AppEnums::GLOBAL_COMPANY_ID->getValue())
-            ->chunk(100, function ($users) use ($app) {
-                foreach ($users as $user) {
-                    $notification = new Blank(
-                        $this->argument('email_template_name'),
-                        ['userFirstname' => $user->firstname],
-                        ['mail'],
-                        $user
-                    );
-                    $notification->setSubject($this->argument('subject'));
-                    Notification::route('mail', $user->email)->notify($notification);
-                    $this->info('Email Successfully sent to: ' . $user->getId() . ' on app: ' . $app->getId());
-                    $this->newLine();
-                }
-            });
+        // $users = UsersAssociatedApps::fromApp($app)
+        //     ->where('is_delete', StateEnums::NO)
+        //     ->where('companies_id', AppEnums::GLOBAL_COMPANY_ID->getValue())
+        //     ->chunk(100, function ($users) use ($app) {
+        //         foreach ($users as $user) {
+        //             $notification = new Blank(
+        //                 $this->argument('email_template_name'),
+        //                 ['userFirstname' => $user->firstname],
+        //                 ['mail'],
+        //                 $user
+        //             );
+        //             $notification->setSubject($this->argument('subject'));
+        //             Notification::route('mail', $user->email)->notify($notification);
+        //             $this->info('Email Successfully sent to: ' . $user->getId() . ' on app: ' . $app->getId());
+        //             $this->newLine();
+        //         }
+        //     });
+
+        $user = Users::find(1817);
+        $notification = new Blank(
+            $this->argument('email_template_name'),
+            ['userFirstname' => $user->firstname],
+            ['mail'],
+            $user
+        );
+        $notification->setSubject($this->argument('subject'));
+        Notification::route('mail', $user->email)->notify($notification);
+        $this->info('Email Successfully sent to: ' . $user->getId() . ' on app: ' . $app->getId());
+        $this->newLine();
     }
 }
