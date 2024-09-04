190378849e7c6ef62b04795129fb916b81275d91
kaioken <max@mctekk.com>
2024-09-03 23:31:04 -0400

    feat: add integartion test

---
+++ tests/ActionEngine/Integrations/TaskEngagementItemTaskTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\ActionEngine\Integration;
+
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Kanvas\ActionEngine\Tasks\Repositories\TaskEngagementItemRepository;
+use Kanvas\Guild\Customers\Models\People;
+use Kanvas\Guild\Leads\Models\Lead;
+use Tests\TestCase;
+
+final class TaskEngagementItemTaskTest extends TestCase
+{
+    public function testGetLeadTaskItems(): void
+    {
+        $company = auth()->user()->getCurrentCompany();
+
+        /**
+         * @todo move to factory
+         */
+        $people = new People();
+        $people->users_id = auth()->user()->getId();
+        $people->companies_id = $company->getId();
+        $people->name = 'Test People';
+        $people->saveOrFail();
+
+        $lead = new Lead();
+        $lead->companies_id = $company->getId();
+        $lead->companies_branches_id = $company->branch()->firstOrFail()->getId();
+        $lead->users_id = auth()->user()->getId();
+        $lead->people_id = $people->getId();
+        $lead->title = 'Test Lead';
+        $lead->leads_receivers_id = 0;
+        $lead->leads_owner_id = $lead->users_id;
+        $lead->saveOrFail();
+
+        $leadTaskItems = TaskEngagementItemRepository::getLeadsTaskItems($lead);
+
+        $this->assertInstanceOf(Builder::class, $leadTaskItems);
+        $this->assertIsArray($leadTaskItems->get()->toArray());
+       
+    }
+}
