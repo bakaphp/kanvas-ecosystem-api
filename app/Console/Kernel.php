f8e1bc64e864ae4ea60299dca3ab13c5aaad212c
Rafael White <rwhite@mctekk.com>
2024-09-04 17:20:29 -0400

    add schedule commands for 4 week campaign

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,6 +9,8 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Spatie\Health\Commands\DispatchQueueCheckJobsCommand;
 use Spatie\Health\Commands\RunHealthChecksCommand;
 use Spatie\Health\Commands\ScheduleCheckHeartbeatCommand;
+use App\Console\Commands\Ecosystem\Notifications\MailunregisteredUsersCampaignCommand;
+use Kanvas\Apps\Models\Apps;
 
 class Kernel extends ConsoleKernel
 {
@@ -31,6 +33,37 @@ class Kernel extends ConsoleKernel
             $schedule->command(MailCaddieLabCommand::class, [getenv('CADDIE_APP_KEY')])
                 ->dailyAt('13:00')
                 ->timezone('America/New_York');
+        }
+
+        $app = Apps::find(getenv('CUSTOM_APP_ID'));
+        $campaignWeek = (int)ceil(now()->day / 7);
+        $dayOftheWeek = 2;
+        $timeOfDay = '2:30';
+
+        switch ($campaignWeek) {
+            case 1:
+                $schedule->command(MailunregisteredUsersCampaignCommand::class, [
+                    $app->getId(), 'introducing_prompt_mine', 'Discover Your New Creative Tool: Introducing Prompt Mine'
+                ])->weeklyOn($dayOftheWeek, $timeOfDay);
+                break;
+
+            case 2:
+                $schedule->command(MailunregisteredUsersCampaignCommand::class, [
+                    $app->getId(), 'get_inspired_with_prompt_mine', 'Get Inspired with Prompt Mine'
+                ])->weeklyOn($dayOftheWeek, $timeOfDay);
+                break;
+
+            case 3:
+                $schedule->command(MailunregisteredUsersCampaignCommand::class, [
+                    $app->getId(), 'see_what_trending_on_prompt_mine', ''
+                ])->weeklyOn($dayOftheWeek, $timeOfDay);
+                break;
+
+            case 4:
+                $schedule->command(MailunregisteredUsersCampaignCommand::class, [
+                    $app->getId(), 'elevate_your_ai_experience', 'Elevate Your AI Experience with Prompt Mine'
+                ])->weeklyOn($dayOftheWeek, $timeOfDay);
+                break;
         }
     }
 
