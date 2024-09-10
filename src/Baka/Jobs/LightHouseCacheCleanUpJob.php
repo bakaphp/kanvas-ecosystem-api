d2d3f8a3ee5124dacd7482edfa4568044dcacaba
kaioken <max@mctekk.com>
2024-09-10 14:15:40 -0400

    refact: unique jobs

--- src/Baka/Jobs/LightHouseCacheCleanUpJob.php
+++ src/Baka/Jobs/LightHouseCacheCleanUpJob.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace Baka\Jobs;
 
+use Baka\Support\Str;
 use Baka\Traits\KanvasJobsTrait;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
 
 class LightHouseCacheCleanUpJob implements ShouldQueue
 {
@@ -17,6 +19,8 @@ class LightHouseCacheCleanUpJob implements ShouldQueue
     use InteractsWithQueue;
     use Queueable;
     use KanvasJobsTrait;
+
+    public $uniqueFor = 300;
 
     public function __construct(
         protected Model $model
@@ -28,5 +32,17 @@ class LightHouseCacheCleanUpJob implements ShouldQueue
         if (method_exists($this->model, 'clearLightHouseCache')) {
             $this->model->clearLightHouseCache();
         }
+    }
+
+    public function uniqueId(): string
+    {
+        return Str::simpleSlug(get_class($this->model) . '-' . $this->model->getKey());
+    }
+    
+    public function middleware(): array
+    {
+        return [
+            (new WithoutOverlapping($this->uniqueId()))->expireAfter($this->uniqueFor),
+        ];
     }
 }
