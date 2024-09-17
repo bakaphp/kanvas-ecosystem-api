5522d6a47decb7a5f71c7b0c8548065fbc4962e5
arfenis <arfen00@gmail.com>
2024-09-16 20:33:41 -0400

    Create integration action history

---
+++ src/Domains/Workflow/Integrations/Actions/AddEntityIntegrationHistoryAction.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Workflow\Integrations\Actions;
+
+use Kanvas\Apps\Models\Apps;
+use Kanvas\Workflow\Integrations\DataTransferObject\EntityIntegrationHistory;
+use Kanvas\Workflow\Integrations\Models\EntityIntegrationHistory as ModelsEntityIntegrationHistory;
+use Kanvas\Workflow\Integrations\Models\Status;
+
+class AddEntityIntegrationHistoryAction
+{
+    /**
+     * __construct.
+     *
+     * @return void
+     */
+    public function __construct(
+        protected EntityIntegrationHistory $dto,
+        protected Apps $app,
+        protected Status $status
+    ) {
+    }
+
+    /**
+     * execute.
+     *
+     * @return ModelsEntityIntegrationHistory
+     */
+    public function execute(): ModelsEntityIntegrationHistory
+    {
+        $integrationHistory = new ModelsEntityIntegrationHistory();
+        $integrationHistory->entity_namespace = get_class($this->dto->entity);
+        $integrationHistory->entity_id = $this->dto->entity->getId();
+        $integrationHistory->integrations_company_id = $this->dto->integrationCompany->getId();
+        $integrationHistory->status_id = $this->dto->status->getId();
+        $integrationHistory->response = $this->dto->response;
+        $integrationHistory->exception = $this->dto->exception;
+
+        $integrationHistory->saveOrFail();
+        return $integrationHistory;
+    }
+}
