9379a12b2b4e1cdd22a5aa005121babe51674836
arfenis <arfen00@gmail.com>
2024-10-01 16:21:02 -0400

    remove update is_default hotfix

--- src/Kanvas/Companies/Branches/Actions/UpdateCompanyBranchActions.php
+++ src/Kanvas/Companies/Branches/Actions/UpdateCompanyBranchActions.php
@@ -30,13 +30,13 @@ class UpdateCompanyBranchActions
 
         //@todo Add observer for is_default value on company branches.
         // Validate if is_default is send otherwise get the existed value.
-        $this->data->is_default = $this->data->is_default ?? $companyBranch->is_default;
+        // $this->data->is_default = $this->data->is_default ?? $companyBranch->is_default;
 
         if ($this->data->is_default === StateEnums::YES->getValue()) {
             $company->branches()->update(['is_default' => StateEnums::NO->getValue()]);
         }
 
-        $companyBranch->is_default = $this->data->is_default;
+        // $companyBranch->is_default = $this->data->is_default;
         $companyBranch->name = $this->data->name;
         $companyBranch->address = $this->data->address;
         $companyBranch->email = $this->data->email;
