7798f765402e23ce0ebdf71cd471d40671f948ab
Rafael White <rwhite@mctekk.com>
2024-10-03 13:38:07 -0400

    add single product import batching

---
+++ src/Domains/Inventory/Importer/Jobs/SingleProductImporterJob.php
@@ -0,0 +1,151 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Inventory\Importer\Jobs;
+
+use Baka\Contracts\AppInterface;
+use Baka\Traits\KanvasJobsTrait;
+use Baka\Users\Contracts\UserInterface;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Kanvas\Companies\Models\Companies;
+use Kanvas\Companies\Models\CompaniesBranches;
+use Kanvas\Filesystem\Models\FilesystemImports;
+use Kanvas\Inventory\Importer\Actions\ProductImporterAction;
+use Kanvas\Inventory\Importer\DataTransferObjects\ProductImporter;
+use Kanvas\Inventory\Regions\Models\Regions;
+use Kanvas\Inventory\Variants\Models\Variants;
+use Illuminate\Bus\Batchable;
+
+use function Sentry\captureException;
+
+use Throwable;
+
+class SingleProductImporterJob implements ShouldQueue, ShouldBeUnique
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+    use KanvasJobsTrait;
+    use Batchable;
+
+    /**
+    * The number of seconds after which the job's unique lock will be released.
+    *
+    * @var int
+    */
+    public $uniqueFor = 60;
+
+    public function __construct(
+        public string $jobUuid,
+        public array $request,
+        public CompaniesBranches $branch,
+        public UserInterface $user,
+        public Regions $region,
+        public AppInterface $app,
+        public ?FilesystemImports $filesystemImport = null
+    ) {
+        $this->onQueue('imports')->delay(now()->addMinutes(5));
+
+        if (App::environment('production')) {
+            $this->uniqueFor = 15 * 60;
+        }
+    }
+
+    /**
+     * Get the unique ID for the job.
+     */
+    public function uniqueId(): string
+    {
+        // Create a unique hash of the importer array
+        $importerHash = md5(json_encode($this->request));
+
+        return $this->app->getId() . $this->branch->getId() . $this->region->getId() . $importerHash;
+    }
+
+    public function middleware(): array
+    {
+        return [
+            (new WithoutOverlapping($this->uniqueId()))->expireAfter($this->uniqueFor),
+        ];
+    }
+
+    /**
+     * handle.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        config(['laravel-model-caching.disabled' => true]);
+        Auth::loginUsingId($this->user->getId());
+        $this->overwriteAppService($this->app);
+        $this->overwriteAppServiceLocation($this->branch);
+
+        // /**
+        //  * @var Companies
+        //  */
+        $company = $this->branch->company()->firstOrFail();
+        // $totalItems = count($this->importer);
+        // $totalProcessSuccessfully = 0;
+        // $totalProcessFailed = 0;
+        // $errors = [];
+
+        //mark all variants as unsearchable for this company before running the import
+        /*         Variants::fromCompany($company)->chunkById(100, function ($variants) {
+                    $variants->unsearchable();
+                }, $column = 'id'); */
+
+        if ($this->filesystemImport) {
+            $this->filesystemImport->update([
+                'status' => 'processing', //move to enums
+            ]);
+        }
+
+
+        try {
+            (new ProductImporterAction(
+                ProductImporter::from($this->request),
+                $company,
+                $this->user,
+                $this->region,
+                $this->app
+            ))->execute();
+            // $totalProcessSuccessfully++;
+        } catch (Throwable $e) {
+            $errors[] = [
+                'message' => $e->getMessage(),
+                'trace' => $e->getTraceAsString(),
+                'request' => $this->request,
+            ];
+            Log::error($e->getMessage());
+            captureException($e);
+            // $totalProcessFailed++;
+        }
+
+        // if ($this->filesystemImport) {
+        //     $this->filesystemImport->update([
+        //         'results' => [
+        //             'total_items' => $totalItems,
+        //             'total_process_successfully' => $totalProcessSuccessfully,
+        //             'total_process_failed' => $totalProcessFailed,
+        //         ],
+        //         'exception' => $errors,
+        //         'status' => 'completed',
+        //         'finished_at' => now(),
+        //     ]);
+        // }
+
+        //handle failed jobs
+    }
+}
