5a5d0f0835ff97104cd2b96085d6b2d211bd0db6
kaioken <max@mctekk.com>
2024-10-05 15:52:15 -0400

    hotfix: email notification

--- app/Console/Commands/Connectors/Notifications/MailCaddieLabCommand.php
+++ app/Console/Commands/Connectors/Notifications/MailCaddieLabCommand.php
@@ -9,13 +9,14 @@ use Kanvas\Guild\Customers\Repositories\PeoplesRepository;
 
 class MailCaddieLabCommand extends Command
 {
-    protected $signature = 'kanvas:internal-mail-caddie-lab {apps_id} {email?}';
+    protected $signature = 'kanvas:internal-mail-caddie-lab {apps_id} {ignoreConfirm?} {email?}';
 
     public function handle()
     {
         $this->info('Sending internal mail to Caddie Lab');
 
         $app = Apps::getById($this->argument('apps_id'));
+        $ignoreConfirm = $this->argument('ignoreConfirm');
         $email = $this->argument('email');
 
         $peoplesIn7Days = PeoplesRepository::getByDaysCreated(7, $app);
@@ -24,11 +25,15 @@ class MailCaddieLabCommand extends Command
         $this->info('We will be sending email to ' . count($peoplesIn7Days) . ' people that have been created in the last 7 days');
         $this->info('We will be sending email to ' . count($peoplesIn28Days) . ' people that have been created in the last 28 days');
 
-        if ($this->confirm('Are you sure you want to send this email?')) {
-            MailCaddieLabJob::dispatch($app, $email);
-            $this->info('Emails have been dispatched.');
-        } else {
-            $this->info('Email sending has been canceled.');
+        if (! $ignoreConfirm || $ignoreConfirm === 'false') {
+            if (! $this->confirm('Are you sure you want to send this email?')) {
+                $this->info('Email sending has been canceled.');
+
+                return;
+            }
         }
+
+        MailCaddieLabJob::dispatch($app, $email);
+        $this->info('Emails have been dispatched.');
     }
 }
