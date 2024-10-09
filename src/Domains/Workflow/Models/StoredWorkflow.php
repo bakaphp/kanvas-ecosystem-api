22ea8d4127975c2f6d168bcf44726e015d5f85d6
arfenis <arfen00@gmail.com>
2024-10-09 04:24:38 -0400

    add method to get activity name on workflow

--- src/Domains/Workflow/Models/StoredWorkflow.php
+++ src/Domains/Workflow/Models/StoredWorkflow.php
@@ -9,4 +9,9 @@ use Workflow\Models\StoredWorkflow as ModelsStoredWorkflow;
 class StoredWorkflow extends ModelsStoredWorkflow
 {
     protected $connection = 'workflow';
+
+    public function getActivityName(): string
+    {
+        return class_basename($this->logs->first()->class);
+    }
 }
