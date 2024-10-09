d2d21a47bb7114063b15d85b6283375b199867a8
arfenis <arfen00@gmail.com>
2024-10-09 04:26:21 -0400

    add builder to return integration status history

---
+++ app/GraphQL/Workflow/Builders/Integrations/IntegrationsBuilder.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Workflow\Builders\Integrations;
+
+use GraphQL\Type\Definition\ResolveInfo;
+use Illuminate\Database\Eloquent\Builder;
+use Kanvas\Exceptions\InternalServerErrorException;
+use Kanvas\SystemModules\Repositories\SystemModulesRepository;
+use Kanvas\Workflow\Integrations\Models\EntityIntegrationHistory;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class IntegrationsBuilder
+{
+    public function integrationEntityHistoryByEntity(
+        mixed $root,
+        array $args,
+        GraphQLContext $context,
+        ResolveInfo $resolveInfo
+    ): Builder {
+        $systemModuleUuid = $args['system_module_uuid'];
+        $systemModule = SystemModulesRepository::getByUuidOrModelName($systemModuleUuid,);
+
+        if (!class_exists($entity = $systemModule->model_name)) {
+            throw new InternalServerErrorException('System Module not found.');
+        }
+
+       $entity = $entity::getById($args['entity_id']);
+
+       return EntityIntegrationHistory::where('entity_namespace', get_class($entity))
+        ->where('entity_id', $entity->getId());
+    }
+
+}
