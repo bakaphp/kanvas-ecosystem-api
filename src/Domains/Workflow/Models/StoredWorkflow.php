a64fa1860746222d9c3e15127cea27d5c333c238
arfenis <arfen00@gmail.com>
2024-10-09 16:47:56 -0400

    unserialize data on workflow response

--- src/Domains/Workflow/Models/StoredWorkflow.php
+++ src/Domains/Workflow/Models/StoredWorkflow.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Kanvas\Workflow\Models;
 
+use Illuminate\Http\JsonResponse;
 use Workflow\Models\StoredWorkflow as ModelsStoredWorkflow;
 
 class StoredWorkflow extends ModelsStoredWorkflow
@@ -13,5 +14,21 @@ class StoredWorkflow extends ModelsStoredWorkflow
     public function getActivityName(): string
     {
         return class_basename($this->logs->first()->class);
+    }
+
+    public function getUnserializeArgument(): JsonResponse
+    {        
+        $unserialize = unserialize($this->arguments)->getClosure();
+        $data = $unserialize();
+
+        return response()->json($data);
+    }
+
+    public function getUnserializeOutput(): JsonResponse
+    {
+        $unserialize = unserialize($this->output)->getClosure();
+        $data = $unserialize();
+
+        return response()->json($data);
     }
 }
