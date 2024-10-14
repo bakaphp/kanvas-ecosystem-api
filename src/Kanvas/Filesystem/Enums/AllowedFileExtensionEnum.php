32f78dba3d879996f1ba278a6296a80f50e57ef6
kaioken <max@mctekk.com>
2024-10-14 11:04:06 -0400

    feat; allow multiple uploads

---
+++ src/Kanvas/Filesystem/Enums/AllowedFileExtensionEnum.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Filesystem\Enums;
+
+enum AllowedFileExtensionEnum
+{
+    case ONLY_IMAGES;
+    case WORK_FILES;
+
+    public function getAllowedExtensions(): array
+    {
+        return match($this) {
+            self::ONLY_IMAGES => ['jpg', 'jpeg', 'png', 'gif', 'svg', 'webp', 'json', 'pdf', 'txt', 'text'],
+            self::WORK_FILES => ['doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'txt', 'pdf'],
+        };
+    }
+}
