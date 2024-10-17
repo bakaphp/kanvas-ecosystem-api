f06bafc7586e80a37abbfdf846bc8ea2887aeddf
FredPeal <frederickpeal@gmail.com>
2024-10-17 12:04:45 -0400

    refactor: fix

--- src/Kanvas/Filesystem/Actions/ImportDataFromFilesystemAction.php
+++ src/Kanvas/Filesystem/Actions/ImportDataFromFilesystemAction.php
@@ -144,7 +144,7 @@ class ImportDataFromFilesystemAction
                 break;
             case Event::class:
                 $job = ImporterEventJob::class;
-
+                
                 break;
             default:
                 break;
