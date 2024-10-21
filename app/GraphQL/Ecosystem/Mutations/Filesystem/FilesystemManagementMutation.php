f14626c04bb032eab3f7748af6ebcde785e50045
arfenis <arfen00@gmail.com>
2024-10-21 14:34:50 -0400

    Add try catch

--- app/GraphQL/Ecosystem/Mutations/Filesystem/FilesystemManagementMutation.php
+++ app/GraphQL/Ecosystem/Mutations/Filesystem/FilesystemManagementMutation.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\GraphQL\Ecosystem\Mutations\Filesystem;
 
 use Kanvas\Apps\Models\Apps;
+use Kanvas\Exceptions\ModelNotFoundException;
 use Kanvas\Filesystem\Actions\AttachFilesystemAction;
 use Kanvas\Filesystem\DataTransferObject\FilesystemAttachInput;
 use Kanvas\Filesystem\Models\Filesystem;
@@ -59,8 +60,12 @@ class FilesystemManagementMutation
             return false;
         }
         $response = $fileEntity->softDelete();
-        $systemModule = $fileEntity->systemModule->model_name;
-        ($systemModule::getById($fileEntity->entity_id))->clearLightHouseCacheJob();
+
+        try {
+            $systemModule = $fileEntity->systemModule->model_name;
+            ($systemModule::getById($fileEntity->entity_id))->clearLightHouseCacheJob();
+        } catch(ModelNotFoundException $e) {
+        }
 
         return $response;
     }
@@ -85,8 +90,11 @@ class FilesystemManagementMutation
 
             if ($fileEntity->softDelete()) {
                 $i++;
-                $systemModule = $fileEntity->systemModule->model_name;
-                ($systemModule::getById($fileEntity->entity_id))->clearLightHouseCacheJob();
+                try {
+                    $systemModule = $fileEntity->systemModule->model_name;
+                    ($systemModule::getById($fileEntity->entity_id))->clearLightHouseCacheJob();
+                } catch(ModelNotFoundException $e) {
+                }
             }
         }
 
