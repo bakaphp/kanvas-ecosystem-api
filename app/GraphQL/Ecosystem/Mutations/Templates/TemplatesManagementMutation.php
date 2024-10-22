ada365457d2b7fb5fd9a040fdec07e24d0c3b4bd
Rafael White <rwhite@mctekk.com>
2024-10-22 14:31:06 -0400

    add template management mutation php and graphql file

---
+++ app/GraphQL/Ecosystem/Mutations/Templates/TemplatesManagementMutation.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Ecosystem\Mutations\Templates;
+
+use Baka\Users\Contracts\UserInterface;
+use Exception;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Kanvas\Apps\Models\Apps;
+use Kanvas\Companies\Actions\CreateCompaniesAction;
+use Kanvas\Companies\Actions\UpdateCompaniesAction;
+use Kanvas\Companies\DataTransferObject\Company;
+use Kanvas\Companies\Jobs\DeleteCompanyJob;
+use Kanvas\Companies\Models\Companies;
+use Kanvas\Companies\Models\CompaniesBranches;
+use Kanvas\Companies\Repositories\CompaniesRepository;
+use Kanvas\Enums\StateEnums;
+use Kanvas\Filesystem\Actions\AttachFilesystemAction;
+use Kanvas\Filesystem\Enums\AllowedFileExtensionEnum;
+use Kanvas\Filesystem\Services\FilesystemServices;
+use Kanvas\Filesystem\Traits\HasMutationUploadFiles;
+use Kanvas\Users\Actions\AssignRoleAction;
+use Kanvas\Users\Models\Users;
+use Kanvas\Users\Models\UsersAssociatedApps;
+use Kanvas\Users\Models\UsersAssociatedCompanies;
+use Kanvas\Users\Repositories\UsersRepository;
+use Nuwave\Lighthouse\Exceptions\AuthorizationException;
+use Kanvas\Templates\Actions\CreateTemplateAction;
+use Kanvas\Templates\DataTransferObject\TemplateInput;
+use Kanvas\Templates\Models\Templates;
+
+class TemplatesManagementMutation
+{
+    /**
+     * createCompany
+     */
+    public function createOrUpdate(mixed $root, array $request): Templates
+    {
+        if (! auth()->user()->isAdmin()) {
+            throw new AuthorizationException('Only admin can create or update templates, please contact your admin');
+        }
+
+        $user = auth()->user();
+
+        //We need to save the subject and content into email_template_variables
+        //we put the content inside the template.
+
+        // $templateVariablesDto =  TemplateVariables::viaRequest();
+
+        $dto =  new TemplateInput(
+            app(Apps::class),
+            $request['input']['name'],
+            $request['input']['template'],
+            $user->getCurrentCompany(),
+            $user
+        );
+
+        $action = new CreateTemplateAction($dto);
+
+
+        return $action->execute();
+    }
+
+}
