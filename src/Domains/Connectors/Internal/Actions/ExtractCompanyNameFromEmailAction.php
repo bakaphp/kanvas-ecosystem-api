4257277f02ef51a98e2c76e1f608c7dec40a8cc6
kaioken <max@mctekk.com>
2024-10-27 09:42:01 -0400

    feat: extract company name

--- src/Domains/Connectors/Internal/Actions/ExtractCompanyNameFromEmailAction.php
+++ src/Domains/Connectors/Internal/Actions/ExtractCompanyNameFromEmailAction.php
@@ -8,16 +8,17 @@ use Illuminate\Support\Str;
 
 class ExtractCompanyNameFromEmailAction
 {
-    protected array $publicEmailProviders = [
-        'gmail.com', 'yahoo.com', 'hotmail.com', 'outlook.com', 'aol.com', 'icloud.com',
-        'live.com', 'msn.com', 'mail.com', 'protonmail.com', // Add more public providers here
-    ];
+    protected array $publicEmailProviders = [];
+
+    public function __construct()
+    {
+        $this->setPublicEmailProviders();
+    }
 
     public function execute(string $email): ?string
     {
         $domain = $this->extractDomain($email);
 
-        // Skip if it's a public email provider
         if ($this->isPublicEmailProvider($domain)) {
             return null;
         }
@@ -80,5 +81,131 @@ class ExtractCompanyNameFromEmailAction
         }
 
         return null;
+    }
+
+    protected function setPublicEmailProviders(): void
+    {
+        $this->publicEmailProviders = [
+            // Major international providers
+            'gmail.com',
+            'yahoo.com',
+            'hotmail.com',
+            'outlook.com',
+            'aol.com',
+            'icloud.com',
+            'live.com',
+            'msn.com',
+            'mail.com',
+            'protonmail.com',
+
+            // Additional popular providers
+            'yandex.com',
+            'yandex.ru',
+            'zoho.com',
+            'yahoo.co.uk',
+            'yahoo.co.jp',
+            'yahoo.fr',
+            'yahoo.de',
+            'yahoo.it',
+            'yahoo.es',
+            'gmx.com',
+            'gmx.de',
+            'gmx.net',
+            'tutanota.com',
+            'tutanota.de',
+            'fastmail.com',
+            'mailbox.org',
+            'me.com',
+            'mac.com',
+            'web.de',
+            'inbox.com',
+            'seznam.cz',
+            'wp.pl',
+            'comcast.net',
+            'verizon.net',
+            'rediffmail.com',
+            'libero.it',
+            'free.fr',
+            'laposte.net',
+            'orange.fr',
+            'optonline.net',
+            'rocketmail.com',
+            'att.net',
+            'sbcglobal.net',
+            'rambler.ru',
+            'mail.ru',
+            'cox.net',
+            'wanadoo.fr',
+            'earthlink.net',
+            'btinternet.com',
+            'charter.net',
+            'shaw.ca',
+            'bellsouth.net',
+
+            // Common disposable/temporary email providers
+            'tempmail.com',
+            'temp-mail.org',
+            'guerrillamail.com',
+            'guerrillamail.net',
+            'guerrillamail.org',
+            'guerrillamail.biz',
+            'sharklasers.com',
+            'grr.la',
+            'maildrop.cc',
+            'harakirimail.com',
+            'yopmail.com',
+            'yopmail.fr',
+            'yopmail.net',
+            'cool.fr.nf',
+            'jetable.org',
+            'nospam.ze.tc',
+            'nomail.xl.cx',
+            'mega.zik.dj',
+            'speed.1s.fr',
+            'courriel.fr.nf',
+            'moncourrier.fr.nf',
+            'monemail.fr.nf',
+            'monmail.fr.nf',
+            '10minutemail.com',
+            '10minutemail.net',
+            '10minutemail.org',
+            'tempinbox.com',
+            'mailinator.com',
+            'mailinator.net',
+            'mailinator.org',
+            'mailinator.com',
+            'mailinator.net',
+            'trashmail.net',
+            'trashmail.com',
+            'trashmail.org',
+            'spamgourmet.com',
+            'tempmail.net',
+            'throwawaymail.com',
+            'dispostable.com',
+            'byom.de',
+            'wegwerfmail.de',
+            'wegwerfmail.net',
+            'wegwerfmail.org',
+            'fakeinbox.com',
+            'fakeinbox.net',
+            'fakeinbox.org',
+            'trbvm.com',
+            'drdrb.net',
+            'tempmail2.com',
+            'deadaddress.com',
+            'spambox.us',
+            'tempemail.net',
+            'fakemailgenerator.com',
+            'armyspy.com',
+            'cuvox.de',
+            'dayrep.com',
+            'einrot.com',
+            'fleckens.hu',
+            'gustr.com',
+            'jourrapide.com',
+            'rhyta.com',
+            'superrito.com',
+            'teleworm.us',
+        ];
     }
 }
