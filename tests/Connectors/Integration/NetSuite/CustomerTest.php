951d3c98e789de12fbf69cd7474d5ec2bf122e30
kaioken <max@mctekk.com>
2024-10-30 22:52:22 -0400

    refact: add tes

--- tests/Connectors/Integration/NetSuite/CustomerTest.php
+++ tests/Connectors/Integration/NetSuite/CustomerTest.php
@@ -7,6 +7,8 @@ namespace Tests\Connectors\Integration\NetSuite;
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Companies\Models\Companies;
 use Kanvas\Connectors\NetSuite\Actions\SyncCompanyWithNetSuiteAction;
+use Kanvas\Connectors\NetSuite\Actions\SyncNetSuiteCustomerWithCompanyAction;
+use Kanvas\Connectors\NetSuite\Actions\SyncNetSuiteCustomerWithPeopleAction;
 use Kanvas\Connectors\NetSuite\Client;
 use Kanvas\Connectors\NetSuite\Enums\CustomFieldEnum;
 use Kanvas\Connectors\NetSuite\Services\NetSuiteCustomerService;
@@ -30,14 +32,37 @@ final class CustomerTest extends TestCase
 
     public function testGetCustomerInfo()
     {
-        /*  $company = Companies::first();
-         $app = app(Apps::class);
-         //$customerId = '123'; // Replace with a valid customer ID
+        $company = Companies::first();
+        $app = app(Apps::class);
 
-         $netSuiteService = new NetSuiteCustomerService($app, $company);
-         $netSuiteId = $company->get(CustomFieldEnum::NET_SUITE_CUSTOMER_ID->value);
-         $customerInfo = $netSuiteService->getCustomerInfo($company->get(CustomFieldEnum::NET_SUITE_CUSTOMER_ID->value));
+        $netSuiteService = new NetSuiteCustomerService($app, $company);
+        $netSuiteId = getenv('NET_SUITE_CUSTOMER_ID');
+        //$customerInfo = $netSuiteService->getCustomerById($company->get(CustomFieldEnum::NET_SUITE_CUSTOMER_ID->value));
+        $customerInfo = $netSuiteService->getCustomerById($netSuiteId);
 
-         $this->assertEquals($netSuiteId, $customerInfo->internalId); */
+        $this->assertEquals($netSuiteId, $customerInfo->internalId);
+    }
+
+    public function testSyncNetSuiteCompanyWithKanvasCompany()
+    {
+        $company = Companies::first();
+        $app = app(Apps::class);
+        $companyIdToSync = getenv('NET_SUITE_CUSTOMER_ID');
+        $syncCompany = new SyncNetSuiteCustomerWithCompanyAction($app, $company);
+        $result = $syncCompany->execute($companyIdToSync);
+
+        $this->assertEquals($result->get(CustomFieldEnum::NET_SUITE_CUSTOMER_ID->value), $companyIdToSync);
+    }
+
+    public function testSyncNetSuiteCustomerWithPeople()
+    {
+        $company = Companies::first();
+        $app = app(Apps::class);
+        $customerId = getenv('NET_SUITE_CUSTOMER_ID');
+
+        $syncCustomer = new SyncNetSuiteCustomerWithPeopleAction($app, $company);
+        $result = $syncCustomer->execute($customerId);
+
+        $this->assertEquals($result->get(CustomFieldEnum::NET_SUITE_CUSTOMER_ID->value), $customerId);
     }
 }
