d125860f51c02dd34b039f3e55d8ae0844adc9de
Keysie27 <keysiesanchezrosario270301@gmail.com>
2024-11-01 14:13:00 -0400

    Refactor optional parameters on dto

--- src/Domains/Subscription/Importer/DataTransferObjects/PlanImporter.php
+++ src/Domains/Subscription/Importer/DataTransferObjects/PlanImporter.php
@@ -9,10 +9,11 @@ use Spatie\LaravelData\Data;
 class PlanImporter extends Data
 {
     public function __construct(
-        public ?int $apps_id = null,
-        public ?string $name = null,
+
+        public int $apps_id,
+        public string $name,
+        public string $stripe_id,
         public ?string $description = null,
-        public ?string $stripe_id = null,
         public ?bool $is_active = true,
         public ?bool $is_default = false
     ) {
--- src/Domains/Subscription/Importer/DataTransferObjects/PriceImporter.php
+++ src/Domains/Subscription/Importer/DataTransferObjects/PriceImporter.php
@@ -9,11 +9,11 @@ use Spatie\LaravelData\Data;
 class PriceImporter extends Data
 {
     public function __construct(
-        public ?float $amount = null,
-        public ?string $currency = null,
-        public ?string $interval = null,
-        public ?string $apps_plans_id = null,
-        public ?string $stripe_id = null,
+        public float $amount,
+        public string $currency,
+        public string $interval,
+        public string $apps_plans_id,
+        public string $stripe_id,
         public ?bool $is_active = true,
         public ?bool $is_default = false
     ) {
