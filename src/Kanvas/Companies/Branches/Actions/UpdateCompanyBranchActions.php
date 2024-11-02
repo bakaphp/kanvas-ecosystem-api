563667ce4d68001043320fa7fdeba0cf12a26d90
FredPeal <frederickpeal@gmail.com>
2024-11-02 01:19:37 -0400

    refactor:  remove address

--- src/Kanvas/Companies/Branches/Actions/UpdateCompanyBranchActions.php
+++ src/Kanvas/Companies/Branches/Actions/UpdateCompanyBranchActions.php
@@ -6,7 +6,6 @@ namespace Kanvas\Companies\Branches\Actions;
 
 use Kanvas\Companies\Branches\DataTransferObject\CompaniesBranchPutData;
 use Kanvas\Companies\Models\CompaniesBranches;
-use Kanvas\Companies\Models\CompaniesBranchesAddress;
 use Kanvas\Companies\Repositories\CompaniesRepository;
 use Kanvas\Enums\StateEnums;
 use Kanvas\Users\Models\Users;
@@ -42,22 +41,6 @@ class UpdateCompanyBranchActions
 
         if ($this->data->files) {
             $companyBranch->addMultipleFilesFromUrl($this->data->files);
-        }
-
-        if ($this->data->address) {
-            foreach ($this->data->address as $address) {
-                CompaniesBranchesAddress::updateOrCreate([
-                    'companies_branches_id' => $companyBranch->getId(),
-                    'address' => $address['address'] ?? null,
-                    'city' => $address['city'] ?? null,
-                    'state' => $address['state'] ?? null,
-                    'zip' => $address['zip'] ?? null,
-                    'countries_id' => $address['country_id'] ?? null,
-                    'states_id' => $address['state_id'] ?? null,
-                    'cities_id' => $address['city_id'] ?? null,
-                    'is_default' => $address['is_default'] ?? 0,
-                ]);
-            }
         }
 
         $company->associateUser($this->user, StateEnums::YES->getValue(), $companyBranch);
