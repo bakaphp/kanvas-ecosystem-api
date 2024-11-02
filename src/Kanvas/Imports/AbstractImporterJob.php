21ecfa1cce25006c519b36b4d3f682ef35f80c4e
FredPeal <frederickpeal@gmail.com>
2024-11-02 16:13:01 -0400

    draft: refactor jobs

--- src/Kanvas/Imports/AbstractImporterJob.php
+++ src/Kanvas/Imports/AbstractImporterJob.php
@@ -7,6 +7,7 @@ namespace Kanvas\Imports;
 use Baka\Contracts\AppInterface;
 use Baka\Traits\KanvasJobsTrait;
 use Baka\Users\Contracts\UserInterface;
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -15,10 +16,15 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\Middleware\WithoutOverlapping;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\App;
+use InvalidArgumentException;
 use Kanvas\Companies\Models\Companies;
 use Kanvas\Companies\Models\CompaniesBranches;
 use Kanvas\Filesystem\Models\FilesystemImports;
 use Kanvas\Inventory\Regions\Models\Regions;
+
+use function Sentry\captureException;
+
+use Throwable;
 
 abstract class AbstractImporterJob implements ShouldQueue, ShouldBeUnique
 {
@@ -27,6 +33,13 @@ abstract class AbstractImporterJob implements ShouldQueue, ShouldBeUnique
     use Queueable;
     use SerializesModels;
     use KanvasJobsTrait;
+
+    protected int $totalItems = 0 ;
+    protected int $totalProcessSuccessfully = 0;
+    protected int $totalProcessFailed = 0;
+    protected int $totalCreated = 0;
+    protected int $totalUpdated = 0;
+    protected array $errors = [];
 
     /**
     * The number of seconds after which the job's unique lock will be released.
@@ -84,7 +97,66 @@ abstract class AbstractImporterJob implements ShouldQueue, ShouldBeUnique
      *
      * @return void
      */
-    abstract public function handle();
+    public function handle()
+    {
+        $this->startFilesystemMapperImport();
+        $this->processImport();
+        $this->finishFilesystemMapperImport(
+            $this->totalItems,
+            $this->totalProcessSuccessfully,
+            $this->totalProcessFailed,
+            $this->errors
+        );
+        $this->notificationStatus(
+            $this->totalItems,
+            $this->totalProcessSuccessfully,
+            $this->totalProcessFailed,
+            $this->totalCreated,
+            $this->totalUpdated,
+            $this->errors,
+            $this->branch->company()->firstOrFail()
+        );
+    }
+
+    abstract public function processImport(): void;
+
+    protected function incrementCount(string $property, int $total = 1): void
+    {
+        if (property_exists($this, $property)) {
+            $this->$property += $total;
+        } else {
+            throw new InvalidArgumentException("Property {$property} does not exist.");
+        }
+    }
+
+    protected function incrementCreatedCount(int $total = 1): void
+    {
+        $this->incrementCount('totalCreated', $total);
+
+        $this->incrementTotalProcessSuccessfully($total);
+    }
+
+    protected function incrementUpdatedCount(int $total = 1): void
+    {
+        $this->incrementCount('totalUpdated', $total);
+
+        $this->incrementTotalProcessSuccessfully($total);
+    }
+
+    protected function incrementTotalProcessSuccessfully(int $total = 1): void
+    {
+        $this->totalProcessSuccessfully += $total;
+    }
+
+    protected function incrementTotalProcessFailed(int $total = 1, Exception|Throwable $e): void
+    {
+        $this->totalProcessFailed += $total;
+        $this->errors[] = [
+            'message' => $e->getMessage(),
+            'trace' => $e->getTraceAsString(),
+        ];
+        captureException($e);
+    }
 
     protected function startFilesystemMapperImport(): void
     {
