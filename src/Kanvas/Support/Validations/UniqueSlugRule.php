85dcf0b5d342650edffcfb9d53f1646584470e37
arfenis <arfen00@gmail.com>
2024-11-05 15:25:23 -0400

    Add unique validation into ecosystem

---
+++ src/Kanvas/Support/Validations/UniqueSlugRule.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Support\Validations;
+
+use Baka\Contracts\AppInterface;
+use Baka\Contracts\CompanyInterface;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Kanvas\Models\BaseModel;
+
+class UniqueSlugRule implements ValidationRule
+{
+    public function __construct(
+        protected AppInterface $app,
+        protected CompanyInterface $company,
+        protected ?BaseModel $model = null
+    ) {
+    }
+
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $query = $this->model::where('slug', $value)
+            ->fromCompany($this->company)
+            ->fromApp($this->app);
+
+        if ($this->model) {
+            $query->where('id', '!=', $this->model->getId());
+        }
+
+        if ($query->exists()) {
+            $fail("The $attribute has already been taken.");
+        }
+    }
+}
