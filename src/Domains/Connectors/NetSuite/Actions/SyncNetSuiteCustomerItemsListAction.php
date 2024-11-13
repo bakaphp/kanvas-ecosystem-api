cc47944293b72af68bebf10d0e5d4ab48f92d36d
kaioken <max@mctekk.com>
2024-11-13 12:32:47 -0400

    Fix: issue with phpstan

--- src/Domains/Connectors/NetSuite/Actions/SyncNetSuiteCustomerItemsListAction.php
+++ src/Domains/Connectors/NetSuite/Actions/SyncNetSuiteCustomerItemsListAction.php
@@ -55,7 +55,7 @@ class SyncNetSuiteCustomerItemsListAction
                 user: $this->mainAppCompany->user,
                 name: $this->buyerCompany->name,
                 description: $this->buyerCompany->name . ' channel',
-                slug: (string) $this->buyerCompany->getId()
+                slug: (string) $this->buyerCompany->uuid
             ),
             $this->mainAppCompany->user
         );
