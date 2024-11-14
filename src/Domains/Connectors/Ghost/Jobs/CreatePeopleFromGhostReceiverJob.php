21f4998db4c82500049a8f971cd6aca454fd9e3b
kaioken <max@mctekk.com>
2024-11-14 13:40:29 -0400

    feat: ghost receiver

---
+++ src/Domains/Connectors/Ghost/Jobs/CreatePeopleFromGhostReceiverJob.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Connectors\Ghost\Jobs;
+
+use Kanvas\Guild\Customers\Actions\CreatePeopleAction;
+use Kanvas\Guild\Customers\DataTransferObject\Address;
+use Kanvas\Guild\Customers\DataTransferObject\Contact;
+use Kanvas\Guild\Customers\DataTransferObject\People;
+use Kanvas\Guild\Customers\Enums\ContactTypeEnum;
+use Kanvas\Workflow\Jobs\ProcessWebhookJob;
+use Spatie\LaravelData\DataCollection;
+
+class CreatePeopleFromGhostReceiverJob extends ProcessWebhookJob
+{
+    public function execute(): array
+    {
+        $payload = $this->webhookRequest->payload['members'][0];
+
+        if (empty($payload)) {
+            return [
+                'message' => 'No data found',
+                'payload' => $this->webhookRequest->payload,
+            ];
+        }
+
+        $name = explode(' ', $payload['name']);
+        $firstname = $name[0];
+        $lastname = $name[1] ?? null;
+
+        $customerEmail = [
+            [
+                'value' => $payload['email'],
+                'contacts_types_id' => ContactTypeEnum::EMAIL->value,
+                'weight' => 0,
+            ],
+        ];
+
+        $tags = [];
+        foreach ($payload['labels'] as $label) {
+            $tags[] = $label['name'];
+        }
+
+        $customFields = [
+            [
+                'key' => 'status',
+                'value' => true,
+            ],
+            [
+                'key' => 'paid_subscription',
+                'value' => $payload['status'] !== 'free',
+            ],
+            [
+                'key' => 'subscribed_to_emails',
+                'value' => true,
+            ],
+        ];
+        $newsletters = [];
+        foreach ($payload['newsletters'] as $newsletter) {
+            $newsletters[] = [
+                'id' => $newsletter['id'],
+                'name' => $newsletter['name'],
+                'description' => $newsletter['description'],
+                'status' => $newsletter['status'],
+            ];
+        }
+
+        if (! empty($newsletters)) {
+            $customFields[] = [
+                'key' => 'newsletters',
+                'value' => $newsletters,
+            ];
+        }
+
+        $createPeople = new CreatePeopleAction(
+            People::from([
+                'app' => $this->webhookRequest->receiverWebhook->app,
+                'branch' => $this->webhookRequest->receiverWebhook->company->defaultBranch,
+                'user' => $this->webhookRequest->receiverWebhook->user,
+                'firstname' => $firstname,
+                'middlename' => null,
+                'lastname' => $lastname,
+                'contacts' => Contact::collect($customerEmail ?? [], DataCollection::class),
+                'address' => Address::collect([], DataCollection::class),
+                'dob' => null,
+                'facebook_contact_id' => null,
+                'google_contact_id' => null,
+                'apple_contact_id' => null,
+                'linkedin_contact_id' => null,
+                'tags' => $tags ?? [],
+                'custom_fields' => $customFields ?? [],
+            ])
+        );
+        $people = $createPeople->execute();
+
+        return [
+            'message' => 'People created successfully',
+            'people' => $people->getId(),
+        ];
+    }
+}
--- tests/Connectors/Integration/Ghosts/CreatePeopleSubscriptionTest.php
+++ tests/Connectors/Integration/Ghosts/CreatePeopleSubscriptionTest.php
@@ -7,6 +7,7 @@ namespace Tests\Connectors\Integration\Ghosts;
 use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\Facades\Request;
 use Kanvas\Apps\Models\Apps;
+use Kanvas\Connectors\Ghost\Jobs\CreatePeopleFromGhostReceiverJob;
 use Kanvas\Connectors\Ghost\Jobs\UpdatePeopleGhostSubscriptionJob;
 use Kanvas\Guild\Customers\Models\Contact;
 use Kanvas\Guild\Customers\Models\People;
@@ -60,5 +61,116 @@ final class CreatePeopleSubscriptionTest extends TestCase
         $this->assertArrayHasKey('success', $result);
         $this->assertArrayHasKey('data', $result);
         $this->assertTrue($result['success']);
+    }
+
+    public function testCreatePeopleSubscription()
+    {
+        $app = app(Apps::class);
+        $user = auth()->user();
+        $company = $user->getCurrentCompany();
+
+        $workflowAction = WorkflowAction::firstOrCreate([
+            'name' => 'Create People',
+            'model_name' => CreatePeopleFromGhostReceiverJob::class,
+        ]);
+
+        $jsonPayload = '{
+                "members": [
+                    {
+                        "id": "668d48281a218a00015d0900",
+                        "uuid": "e6267a0a-a10b-4b9f-8d58-a6445cde8144",
+                        "email": "jerly@kanvas.dev",
+                        "name": "Jerly Rosa",
+                        "note": null,
+                        "geolocation": null,
+                        "subscribed": true,
+                        "created_at": "2024-07-09T14:24:40.000Z",
+                        "updated_at": "2024-07-09T14:24:40.000Z",
+                        "labels": [
+                            {
+                                "id": "6643c63f7506990001828d62",
+                                "name": "company:kanvas",
+                                "slug": "company-kanvas",
+                                "created_at": "2024-05-14T20:14:55.000Z",
+                                "updated_at": "2024-05-14T20:14:55.000Z"
+                            },
+                            {
+                                "id": "6685713ef5713b00017fe4fd",
+                                "name": "report:test-3",
+                                "slug": "report-test-3",
+                                "created_at": "2024-07-03T15:41:50.000Z",
+                                "updated_at": "2024-07-03T15:41:50.000Z"
+                            },
+                            {
+                                "id": "6643c63f7506990001828d64",
+                                "name": "title:dev",
+                                "slug": "title-dev",
+                                "created_at": "2024-05-14T20:14:55.000Z",
+                                "updated_at": "2024-05-14T20:14:55.000Z"
+                            }
+                        ],
+                        "subscriptions": [],
+                        "avatar_image": "",
+                        "comped": false,
+                        "email_count": 5,
+                        "email_opened_count": 0,
+                        "email_open_rate": 0,
+                        "status": "free",
+                        "last_seen_at": null,
+                        "attribution": {
+                            "id": null,
+                            "type": null,
+                            "url": null,
+                            "title": null,
+                            "referrer_source": "Integration: Api",
+                            "referrer_medium": "Admin API",
+                            "referrer_url": null
+                        },
+                        "unsubscribe_url": "",
+                        "tiers": [],
+                        "email_suppression": {
+                            "suppressed": false,
+                            "info": null
+                        },
+                        "newsletters": [
+                            {
+                                "id": "661eb361c96051000859617f",
+                                "name": "MC Kanvas",
+                                "description": null,
+                                "status": "active"
+                            },
+                            {
+                                "id": "6724e7b98b3f59000107e32f",
+                                "name": "test",
+                                "description": "test",
+                                "status": "active"
+                            }
+                        ]
+                    }
+                ]
+            }';
+
+        $payload = json_decode($jsonPayload, true);
+
+        $receiverWebhook = ReceiverWebhook::factory()
+               ->app($app->getId())
+               ->user($user->getId())
+               ->company($company->getId())
+               ->create([
+                      'action_id' => $workflowAction->getId(),
+               ]);
+
+        $request = Request::create('https://localhost/ghosttest', 'POST', $payload);
+
+        // Execute the action and get the webhook request
+        $webhookRequest = (new ProcessWebhookAttemptAction($receiverWebhook, $request))->execute();
+
+        // Fake the queue
+        Queue::fake();
+        $job = new CreatePeopleFromGhostReceiverJob($webhookRequest);
+        $result = $job->handle();
+
+        $this->assertArrayHasKey('people', $result);
+        $this->assertInstanceOf(People::class, People::getById($result['people']));
     }
 }
