0e3fb8c88f6a95fda93dc3f0306e69bf7258bf16
kaioken <max@mctekk.com>
2024-11-14 04:01:08 -0400

    replace: array

--- src/Domains/Guild/Leads/Jobs/CreateLeadsFromReceiverJob.php
+++ src/Domains/Guild/Leads/Jobs/CreateLeadsFromReceiverJob.php
@@ -52,8 +52,8 @@ class CreateLeadsFromReceiverJob extends ProcessWebhookJob
             $payload['status_id'] = $leadReceiver->app->get(AppEnum::APP_DEFAULT_RECEIVER_LEAD_STATUS->value);
         }
 
-        $payload['leads_types_id'] = $payload['leads_types_id'] ?? $leadReceiver->lead_types_id;
-        $payload['lead_sources_id'] = $payload['lead_sources_id'] ?? $leadReceiver->leads_sources_id;
+        $payload['type_id'] = $payload['type_id'] ?? $leadReceiver->lead_types_id;
+        $payload['source_id'] = $payload['source_id'] ?? $leadReceiver->leads_sources_id;
 
         $createLead = new CreateLeadAction(
             Lead::viaRequest(
