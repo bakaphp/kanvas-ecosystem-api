100fff720de1ff57c08b0f63b182ee234cbf9681
Rafael White <rwhite@mctekk.com>
2024-11-15 11:42:34 -0500

    rename pay action

--- src/Domains/Wallet/Actions/PayAction.php
+++ src/Domains/Wallet/Actions/PayAction.php
@@ -10,7 +10,7 @@ use Kanvas\Dashboard\Repositories\DashboardRepositories;
 use Kanvas\Dashboard\Enums\DashboardEnum;
 use Exception;
 
-class PayItemAction
+class PayAction
 {
     public function __construct(
         private Users $user,
@@ -30,9 +30,5 @@ class PayItemAction
         //We need to check if the user has enough to buy the product, if he does then we try to purchase the product in Stripe.
 
         $this->user->pay($this->item); //we need to check here if the item in Stripe has been bought first and if the user
-
-        if (!(bool)$this->user->paid($this->item)) {
-            throw new Exception('Item could not be bought');
-        }
     }
 }
