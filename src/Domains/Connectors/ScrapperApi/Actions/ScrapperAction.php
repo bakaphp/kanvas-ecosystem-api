fa53b0a310332f54982718a0d872775c612ded6f
FredPeal <frederickpeal@gmail.com>
2024-11-19 15:54:14 -0400

    fix: scrapper api change his structure

--- src/Domains/Connectors/ScrapperApi/Actions/ScrapperAction.php
+++ src/Domains/Connectors/ScrapperApi/Actions/ScrapperAction.php
@@ -16,7 +16,7 @@ use Kanvas\Inventory\Importer\Jobs\ProductImporterJob;
 use Kanvas\Inventory\Products\Models\Products;
 use Kanvas\Inventory\Regions\Models\Regions;
 use Kanvas\Users\Models\Users;
-
+use Exception;
 use function Sentry\captureException;
 
 use Throwable;
@@ -51,7 +51,11 @@ class ScrapperAction
             $scrapperProducts++;
 
             try {
-                $asin = $result['asin'];
+                if (preg_match('/(?:asin=|dp\/)([A-Z0-9]{10})/', $result['url'], $matches)) {
+                    $asin = $matches[1];
+                } else {
+                    throw new Exception('ASIN not found');
+                }
                 $productModel = Products::getBySlug($asin, $this->companyBranch->company);
                 $product = $repository->getByAsin($asin);
                 $product = array_merge($product, $result);
