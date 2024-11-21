53f3e7eace60299da7fbb5a64cfc9afc8a282202
kaioken <max@mctekk.com>
2024-11-21 03:08:03 -0400

    feat: channel price

---
+++ src/Domains/Souk/Enums/ConfigurationEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Souk\Enums;
+
+enum ConfigurationEnum: string
+{
+    case COMPANY_CUSTOM_CHANNEL_PRICING = 'souk_company_custom_channel_pricing';
+}
