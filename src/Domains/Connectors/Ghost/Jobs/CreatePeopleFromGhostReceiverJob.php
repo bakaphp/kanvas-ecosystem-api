68a475bb627311927b14c6ace6604b378896f8e3
kaioken <max@mctekk.com>
2024-11-24 10:26:07 -0400

    fix

--- src/Domains/Connectors/Ghost/Jobs/CreatePeopleFromGhostReceiverJob.php
+++ src/Domains/Connectors/Ghost/Jobs/CreatePeopleFromGhostReceiverJob.php
@@ -27,16 +27,15 @@ class CreatePeopleFromGhostReceiverJob extends ProcessWebhookJob
             ];
         }
 
-        if ($payload['name']) {
+        if (! empty($payload['name'])) {
             $name = explode(' ', $payload['name']);
             $firstname = $name[0];
             $lastname = $name[1] ?? null;
         } else {
             $name = explode('@', $payload['email']);
             $firstname = $name[0];
-            $lastname =  null;
+            $lastname = null;
         }
-
 
         $customerEmail = [
             [
@@ -69,31 +68,41 @@ class CreatePeopleFromGhostReceiverJob extends ProcessWebhookJob
                 'value' => $payload['uuid'],
             ],
         ];
-
-        foreach ($payload['labels'] as $label) {
-            if (Str::contains($label['name'], ':')) {
-                // Split "key:value" into key and value for custom fields
-                [$key, $value] = explode(':', $label['name'], 2);
-                $customFields[] = [
-                    'key' => $key,
-                    'value' => $value,
-                ];
-                if ($key === 'report') {
+        $unlockedReports = [];
+        if (isset($payload['labels']) && empty($payload['labels'])) {
+            foreach ($payload['labels'] as $label) {
+                if (Str::contains($label['name'], ':')) {
+                    // Split "key:value" into key and value for custom fields
+                    [$key, $value] = explode(':', $label['name'], 2);
+                    $customFields[] = [
+                        'key' => $key,
+                        'value' => $value,
+                    ];
+                    if ($key === 'report') {
+                        $tags[] = $label['name'];
+                        $unlockedReports[] = $value;
+                    }
+                } else {
                     $tags[] = $label['name'];
                 }
-            } else {
-                $tags[] = $label['name'];
             }
+
+            $customFields[] = [
+                'key' => CustomFieldEnum::GHOST_UNLOCK_CUSTOM_FIELD->value,
+                'value' => $unlockedReports,
+            ];
         }
 
         $newsletters = [];
-        foreach ($payload['newsletters'] as $newsletter) {
-            $newsletters[] = [
-                'id' => $newsletter['id'],
-                'name' => $newsletter['name'],
-                'description' => $newsletter['description'],
-                'status' => $newsletter['status'],
-            ];
+        if (isset($payload['newsletters']) && ! empty($payload['newsletters'])) {
+            foreach ($payload['newsletters'] as $newsletter) {
+                $newsletters[] = [
+                    'id' => $newsletter['id'],
+                    'name' => $newsletter['name'],
+                    'description' => $newsletter['description'],
+                    'status' => $newsletter['status'],
+                ];
+            }
         }
 
         if (! empty($newsletters)) {
