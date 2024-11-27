1a005ed27508e530cdfe1a210943d6eee365fd7b
kaioken <max@mctekk.com>
2024-11-27 17:13:02 -0400

    feat: add destination service

---
+++ src/Domains/Connectors/ESim/Services/DestinationService.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Connectors\ESim\Services;
+
+use Baka\Contracts\AppInterface;
+use Baka\Contracts\CompanyInterface;
+use Kanvas\Connectors\ESim\Client;
+
+class DestinationService
+{
+    protected Client $client;
+
+    public function __construct(
+        protected AppInterface $app,
+        protected CompanyInterface $company
+    ) {
+        $this->client = new Client($app, $company);
+    }
+
+    public function getPlans(
+        ?string $code = null,
+        ?string $provider = null,
+        ?string $coverage = null,
+        ?int $page = 1,
+        ?int $limit = 25
+    ): array {
+        $query = http_build_query([
+            'code' => $code,
+            'provider' => $provider,
+            'coverage' => $coverage,
+            'page' => $page,
+            'limit' => $limit
+        ]);
+
+        return $this->client->get('/api/v1/esimgo/plans?' . $query);
+    }
+}
