8464e78c8cb280a05417d174ec4e2f9bd2af6856
FredPeal <frederickpeal@gmail.com>
2024-11-26 23:19:50 -0400

    fix: mutation replay webhook call

--- app/GraphQL/Workflow/Mutations/ReceiverWebhookMutationManagement.php
+++ app/GraphQL/Workflow/Mutations/ReceiverWebhookMutationManagement.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace App\GraphQL\Workflow\Mutations;
 
+use Exception;
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Workflow\Actions\ReplayAttemptWebhookCallAction;
 use Kanvas\Workflow\Models\ReceiverWebhookCall;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
-use Exception;
 
 class ReceiverWebhookMutationManagement
 {
-    public function replyWebhookCall(array $args, GraphQLContext $context): string
+    public function replyWebhookCall(mixed $root, array $args, GraphQLContext $context): string
     {
         $app = app(Apps::class);
         $receiverWebhookCall = ReceiverWebhookCall::whereRelation(
@@ -20,7 +20,7 @@ class ReceiverWebhookMutationManagement
             'apps_id',
             $app->getId()
         )
-            ->find($args['id']);
+        ->find($args['id']);
         if (! $receiverWebhookCall) {
             throw new Exception('Webhook call not found');
         }
