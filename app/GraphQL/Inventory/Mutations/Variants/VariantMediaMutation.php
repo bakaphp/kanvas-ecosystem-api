40165fc299ba1136f78c7e49eda100039e945963
kaioken <max@mctekk.com>
2024-11-27 15:12:08 -0400

    feat: upload files

--- app/GraphQL/Inventory/Mutations/Variants/VariantMediaMutation.php
+++ app/GraphQL/Inventory/Mutations/Variants/VariantMediaMutation.php
@@ -7,18 +7,19 @@ namespace App\GraphQL\Inventory\Mutations\Products;
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Filesystem\Traits\HasMutationUploadFiles;
 use Kanvas\Inventory\Products\Models\Products;
+use Kanvas\Inventory\Variants\Models\Variants;
 
-class ProductMediaMutation
+class VariantMediaMutation
 {
     use HasMutationUploadFiles;
 
-    public function attachFileToProduct(mixed $root, array $request): Products
+    public function attachFileToVariant(mixed $root, array $request): Products
     {
         $app = app(Apps::class);
-        $product = Products::getById((int) $request['id'], $app);
+        $variant = Variants::getById((int) $request['id'], $app);
 
         return $this->uploadFileToEntity(
-            model: $product,
+            model: $variant,
             app: $app,
             user: auth()->user(),
             request: $request
