92fb20d5e3aeb7e3399369c3e8590daf5792dfdf
kaioken <max@mctekk.com>
2024-11-30 22:32:53 -0400

    refact: async receiver

--- src/Domains/Guild/Customers/Enums/ContactTypeEnum.php
+++ src/Domains/Guild/Customers/Enums/ContactTypeEnum.php
@@ -9,4 +9,13 @@ enum ContactTypeEnum: int
     case EMAIL = 1;
     case PHONE = 2;
     case CELLPHONE = 3;
+
+    public function getName(): string
+    {
+        return match ($this->value) {
+            self::EMAIL => 'Email',
+            self::PHONE => 'Phone',
+            self::CELLPHONE => 'Cellphone',
+        };
+    }
 }
