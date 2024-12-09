87c0127c45a58051fbef83865b3d89226233072d
kaioken <max@mctekk.com>
2024-12-09 17:30:56 -0400

    hotfix

--- app/GraphQL/Ecosystem/Queries/Companies/UserManagementQuery.php
+++ app/GraphQL/Ecosystem/Queries/Companies/UserManagementQuery.php
@@ -9,8 +9,6 @@ use GraphQL\Type\Definition\ResolveInfo;
 use Illuminate\Database\Eloquent\Builder;
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Users\Models\Users;
-use Kanvas\Users\Models\UsersInvite;
-use Kanvas\Users\Repositories\UsersInviteRepository;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class UserManagementQuery
@@ -23,22 +21,44 @@ class UserManagementQuery
         $companiesId = auth()->user()->isAdmin() && ! empty($args['companies_id']) ? $args['companies_id'] : auth()->user()->currentCompanyId();
         $app = app(Apps::class);
 
-        return Users::select('users.id', 'users.uuid', 'users.dob', 'users.sex', 'users_associated_apps.*', 'users.phone_number', 'users.cell_phone_number')
-            ->join(
-                'users_associated_company',
-                'users_associated_company.users_id',
-                'users.id'
-            )
-            ->join(
-                'users_associated_apps',
-                'users_associated_apps.users_id',
-                'users.id'
-            )
-            ->where('users_associated_company.companies_id', $companiesId)
-            ->where('users_associated_apps.apps_id', $app->getId())
-            ->where('users_associated_company.is_deleted', StateEnums::NO->getValue())
-            ->where('users_associated_apps.is_deleted', StateEnums::NO->getValue())
-            ->groupBy('users.id');
+        return Users::select(
+            'users_associated_apps.users_id as id', // User ID
+            'users.uuid',
+            'users.dob',
+            'users.sex',
+            'users_associated_apps.firstname',
+            'users_associated_apps.lastname',
+            'users_associated_apps.email',
+            'users.phone_number',
+            'users.cell_phone_number',
+            'users_associated_apps.apps_id',
+            'users_associated_apps.companies_id',
+            'users_associated_apps.identify_id',
+            'users_associated_apps.two_step_phone_number',
+            'users_associated_apps.is_active',
+            'users_associated_apps.user_active',
+            'users_associated_apps.user_role',
+            'users_associated_apps.displayname',
+            'users_associated_apps.phone_verified_at',
+            'users_associated_apps.email_verified_at',
+            'users_associated_apps.created_at',
+            'users_associated_apps.updated_at'
+        )
+        ->join(
+            'users_associated_company',
+            'users_associated_company.users_id',
+            'users.id'
+        )
+        ->join(
+            'users_associated_apps',
+            'users_associated_apps.users_id',
+            'users.id'
+        )
+        ->where('users_associated_company.companies_id', $companiesId)
+        ->where('users_associated_apps.apps_id', $app->getId())
+        ->where('users_associated_company.is_deleted', StateEnums::NO->getValue())
+        ->where('users_associated_apps.is_deleted', StateEnums::NO->getValue())
+        ->groupBy('users_associated_apps.users_id'); // Group by the correct user ID
     }
 
     /**
@@ -49,7 +69,29 @@ class UserManagementQuery
         $branchId = auth()->user()->isAdmin() && ! empty($args['getCurrentBranch']) ? $args['getCurrentBranch'] : auth()->user()->currentBranchId();
         $app = app(Apps::class);
 
-        return Users::select('users.id', 'users.uuid', 'users.dob', 'users.sex', 'users_associated_apps.*', 'users.phone_number', 'users.cell_phone_number')
+        return Users::select(
+            'users_associated_apps.users_id as id', // User ID
+            'users.uuid',
+            'users.dob',
+            'users.sex',
+            'users_associated_apps.firstname',
+            'users_associated_apps.lastname',
+            'users_associated_apps.email',
+            'users.phone_number',
+            'users.cell_phone_number',
+            'users_associated_apps.apps_id',
+            'users_associated_apps.companies_id',
+            'users_associated_apps.identify_id',
+            'users_associated_apps.two_step_phone_number',
+            'users_associated_apps.is_active',
+            'users_associated_apps.user_active',
+            'users_associated_apps.user_role',
+            'users_associated_apps.displayname',
+            'users_associated_apps.phone_verified_at',
+            'users_associated_apps.email_verified_at',
+            'users_associated_apps.created_at',
+            'users_associated_apps.updated_at'
+        )
         ->join(
             'users_associated_company',
             'users_associated_company.users_id',
@@ -76,7 +118,6 @@ class UserManagementQuery
             'users_associated_apps.apps_id',
             $app->getId()
         )
-        ->groupBy('users.id')
-        ->select('users.*');
+        ->groupBy('users_associated_apps.users_id'); // Group by the correct user ID
     }
 }
