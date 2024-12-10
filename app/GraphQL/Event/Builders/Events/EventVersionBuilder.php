54e485107ae6e3bae348994c0d476c7b52608212
arfenis <arfen00@gmail.com>
2024-12-10 15:32:36 -0400

    people by event version

--- app/GraphQL/Event/Builders/Events/EventVersionBuilder.php
+++ app/GraphQL/Event/Builders/Events/EventVersionBuilder.php
@@ -6,10 +6,13 @@ namespace App\GraphQL\Event\Builders\Events;
 
 use Baka\Enums\StateEnums;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Event\Events\Models\EventVersion;
 use Kanvas\Event\Events\Models\EventVersionParticipant;
 use Kanvas\Event\Events\Models\EventVersionParticipantDate;
+use Kanvas\Event\Participants\Models\Participant;
+use Kanvas\Guild\Customers\Models\People;
 
 class EventVersionBuilder
 {
@@ -38,5 +41,23 @@ class EventVersionBuilder
     {
         return EventVersionParticipantDate::where('event_version_id', $root->id)
                 ->where('is_deleted', StateEnums::NO->getValue());
+    }
+
+    public function getHasEventVersion(mixed $root, array $args): Builder
+    {
+        $eventVersionId = $args['HAS']['condition']['value'];
+        $participant = Participant::getFullTableName();
+        $eventVersionParticipant = EventVersionParticipant::getFullTableName();
+
+        $peoples = People::select('*')
+            ->join($participant, 'peoples.id', '=', $participant . '.people_id')
+            ->join($eventVersionParticipant, $participant . '.id', '=', $eventVersionParticipant . '.participant_id')
+            ->when($eventVersionId, fn ($query) =>
+                $query->where($eventVersionParticipant . '.event_version_id', $eventVersionId)
+            )
+            ->where($eventVersionParticipant . '.event_version_id', $eventVersionId)
+            ->distinct();
+
+        return $peoples;
     }
 }
