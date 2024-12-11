40fd7a45cf57406e35b6a39ce05991fa67f40460
kaioken <max@mctekk.com>
2024-12-11 01:59:33 -0400

    refact: sync email

--- app/Console/Commands/Ecosystem/KanvasEmailTemplateSync.php
+++ app/Console/Commands/Ecosystem/KanvasEmailTemplateSync.php
@@ -19,7 +19,7 @@ class KanvasEmailTemplateSync extends Command
      * @var string
      */
     protected $signature = 'kanvas:email-template-sync {app_id} {user_id} {--overwrite=true}';
-    
+
     /**
      * The console command description.
      *
