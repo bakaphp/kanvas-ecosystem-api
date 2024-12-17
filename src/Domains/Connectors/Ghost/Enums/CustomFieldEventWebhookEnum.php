ea03858b75242bc6adcc719b18ac2cd2fbd86ed5
FredPeal <frederickpeal@gmail.com>
2024-12-17 00:38:28 -0400

    refactor: ghost event webhook refactor after update by ghost

---
+++ src/Domains/Connectors/Ghost/Enums/CustomFieldEventWebhookEnum.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+namespace Kanvas\Connectors\Ghost\Enums;
+
+enum CustomFieldEventWebhookEnum: string
+{
+    case WEBHOOK_IS_REPORT_EVENT = 'event_webhook_is_report_event';
+
+}
--- src/Domains/Connectors/Ghost/Jobs/CreateEventFromGhostReceiverJob.php
+++ src/Domains/Connectors/Ghost/Jobs/CreateEventFromGhostReceiverJob.php
@@ -11,13 +11,14 @@ use Kanvas\Event\Events\DataTransferObject\Event;
 use Kanvas\Event\Events\Actions\CreateEventAction;
 use Kanvas\Connectors\Ghost\Enums\CustomFieldEnum;
 use Illuminate\Support\Str;
+use Kanvas\Connectors\Ghost\Enums\CustomFieldEventWebhookEnum;
 
 class CreateEventFromGhostReceiverJob extends ProcessWebhookJob
 {
     public function execute(): array
     {
         $company = $this->webhookRequest->receiverWebhook->company;
-        $payload = $this->webhookRequest->payload['posts'][0];
+        $payload = $this->webhookRequest->payload['post']['current'];
         $eventType = $this->getType($payload);
         if (! $eventType) {
             return [];
@@ -26,8 +27,8 @@ class CreateEventFromGhostReceiverJob extends ProcessWebhookJob
                     ->where('apps_id', $this->webhookRequest->receiverWebhook->app->getId())
                     ->first();
         $data = [
-            'name' => $payload['primary_tag']['name'],
-            'slug' => Str::slug($payload['primary_tag']['name']),
+            'name' => $payload['title'],
+            'slug' => $payload['slug'],
             'type_id' => $eventType->getId(),
             'category_id' => $category->getId(),
             'dates' => [
@@ -48,10 +49,13 @@ class CreateEventFromGhostReceiverJob extends ProcessWebhookJob
 
     public function getType(array $payload): ?EventType
     {
-        $appSetting = $this->webhookRequest->receiverWebhook->app->get(CustomFieldEnum::WEBHOOK_IS_REPORT_EVENT->value);
-        $eventType = $payload['primary_tag']['name'];
-        if (! in_array($eventType, $appSetting)) {
-            return null;
+        $primaryTag = $payload['primary_tag'];
+        $app = $this->webhookRequest->receiverWebhook->app;
+        if (isset($primaryTag['is_report']) && $primaryTag['is_report']) {
+            $eventType = $app->get(CustomFieldEventWebhookEnum::WEBHOOK_IS_REPORT_EVENT->value);
+            if (!$eventType) {
+                return null;
+            }
         }
         return EventType::where('apps_id', $this->webhookRequest->receiverWebhook->app->getId())
              ->where('companies_id', $this->webhookRequest->receiverWebhook->company->getId())
