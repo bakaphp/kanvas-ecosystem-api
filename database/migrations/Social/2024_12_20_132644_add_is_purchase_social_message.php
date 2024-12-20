d2e7c75110d9a9e99a55953e27f46409eec7ae81
kaioken <max@mctekk.com>
2024-12-20 09:31:37 -0400

    feat: add is purchase to message

--- database/migrations/Social/2024_12_20_132644_add_is_purchase_social_message.php
+++ database/migrations/Social/2024_12_20_132644_add_is_purchase_social_message.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,7 +13,6 @@ return new class extends Migration
         Schema::table('user_messages', function (Blueprint $table) {
             $table->tinyInteger('is_purchase')->after('is_shared')->default(0)->index('is_purchase');
         });
-
     }
 
     /**
