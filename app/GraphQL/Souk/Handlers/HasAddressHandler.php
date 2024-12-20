1b9a4bdb620fa304735c263aec72ca7db648f779
FredPeal <frederickpeal@gmail.com>
2024-12-20 02:35:44 -0400

    refactor: for nested where conditions

--- app/GraphQL/Souk/Handlers/HasAddressHandler.php
+++ app/GraphQL/Souk/Handlers/HasAddressHandler.php
@@ -20,13 +20,34 @@ final class HasAddressHandler extends WhereConditionsHandler
         string $boolean = 'and',
     ): void {
         $addressBuilder = Address::query();
-
         if ($column = $whereConditions['column'] ?? null) {
             $this->assertValidColumnReference($column);
             $this->operator->applyConditions($addressBuilder, $whereConditions, $boolean);
         }
         // @to do: add nested where conditions
         $addressBuilder->from((new Address())->getFullTableName());
+
+        if (array_key_exists('AND', $whereConditions)) {
+            $this->nestedConditions($addressBuilder, $whereConditions['AND'][0], 'and');
+        } elseif (array_key_exists('OR', $whereConditions)) {
+            $this->nestedConditions($addressBuilder, $whereConditions['OR'][0], 'or');
+        }
+
         $builder->whereExists($addressBuilder);
+    }
+
+    public function nestedConditions(object $builder, array $whereConditions, string $boolean = 'and'): bool
+    {
+        if ($column = $whereConditions['column'] ?? null) {
+            $this->assertValidColumnReference($column);
+            $this->operator->applyConditions($builder, $whereConditions, $boolean);
+        }
+        if (array_key_exists('AND', $whereConditions)) {
+            $this->nestedConditions($builder, $whereConditions['AND'][0], 'and');
+        } elseif (array_key_exists('OR', $whereConditions)) {
+            $this->nestedConditions($builder, $whereConditions['OR'][0], 'or');
+        }
+
+        return true;
     }
 }
