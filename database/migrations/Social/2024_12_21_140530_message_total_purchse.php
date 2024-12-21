53cbd5503ade5a3883dc1790be2c5c622e9402a5
kaioken <max@mctekk.com>
2024-12-21 10:12:55 -0400

    feat: interactions

--- database/migrations/Social/2024_12_21_140530_message_total_purchse.php
+++ database/migrations/Social/2024_12_21_140530_message_total_purchse.php
@@ -5,7 +5,6 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    
     public function up(): void
     {
         Schema::table('messages', function (Blueprint $table) {
