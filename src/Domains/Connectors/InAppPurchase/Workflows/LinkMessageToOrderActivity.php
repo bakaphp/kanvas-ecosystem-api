d9d9a369835ee52c1327fafb75cf17e5b557fb9d
kaioken <max@mctekk.com>
2024-12-20 21:41:38 -0400

    feat: add message purchse to channel

--- src/Domains/Connectors/InAppPurchase/Workflows/LinkMessageToOrderActivity.php
+++ src/Domains/Connectors/InAppPurchase/Workflows/LinkMessageToOrderActivity.php
@@ -6,10 +6,13 @@ namespace Kanvas\Connectors\InAppPurchase\Workflows;
 
 use Baka\Contracts\AppInterface;
 use Illuminate\Database\Eloquent\Model;
+use Kanvas\Social\Channels\Actions\CreateChannelAction;
+use Kanvas\Social\Channels\DataTransferObject\Channel;
 use Kanvas\Social\Messages\Actions\CreateAppModuleMessageAction;
 use Kanvas\Social\Messages\Models\Message;
 use Kanvas\Souk\Orders\Models\Order;
 use Kanvas\SystemModules\Repositories\SystemModulesRepository;
+use Kanvas\Users\Models\Users;
 use Kanvas\Workflow\Contracts\WorkflowActivityInterface;
 use Kanvas\Workflow\KanvasActivity;
 
@@ -38,6 +41,24 @@ class LinkMessageToOrderActivity extends KanvasActivity implements WorkflowActiv
             ['message_id' => $message->getId(), 'message' => $message->message]
         );
         $order->saveOrFail();
+        $user = $order->user;
+
+        $newPurchaseMessageChannel = new CreateChannelAction(
+            new Channel(
+                apps: $app,
+                companies: $message->company,
+                users: $order->user,
+                entity_id: $user->getId(),
+                entity_namespace: Users::class,
+                name: 'Purchase Message',
+                description: 'Purchase Message Channel',
+                slug: 'PMC-' . $user->uuid
+            ),
+            $order->user
+        );
+
+        $purchaseChannel = $newPurchaseMessageChannel->execute();
+        $purchaseChannel->addMessage($message, $user);
 
         return [
             'order' => $order->id,
