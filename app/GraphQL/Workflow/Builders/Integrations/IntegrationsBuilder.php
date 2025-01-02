fe5ecf176b5b3e508ee5fa54d2d733000496b4c3
arfenis <arfen00@gmail.com>
2025-01-02 16:55:20 -0400

    Add filter by region for workflows get

--- app/GraphQL/Workflow/Builders/Integrations/IntegrationsBuilder.php
+++ app/GraphQL/Workflow/Builders/Integrations/IntegrationsBuilder.php
@@ -8,8 +8,10 @@ use GraphQL\Type\Definition\ResolveInfo;
 use Illuminate\Database\Eloquent\Builder;
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Exceptions\InternalServerErrorException;
+use Kanvas\Regions\Models\Regions;
 use Kanvas\SystemModules\Repositories\SystemModulesRepository;
 use Kanvas\Workflow\Integrations\Models\EntityIntegrationHistory;
+use Kanvas\Workflow\Integrations\Models\IntegrationsCompany;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class IntegrationsBuilder
@@ -31,5 +33,35 @@ class IntegrationsBuilder
 
         return EntityIntegrationHistory::where('entity_namespace', get_class($entity))
             ->where('entity_id', $entity->getId());
+    }
+
+    public function getHasRegion(mixed $root, array $args): Builder
+    {
+        $regionTable = Regions::getFullTableName();
+
+        $integrationsCompanyTable = IntegrationsCompany::getFullTableName();
+        $entityIntegrationHistoryTable = EntityIntegrationHistory::getFullTableName();
+
+        $root->select([
+                'entity_integration_history.*',
+                'entity_integration_history.id as id'
+            ])
+            ->join($integrationsCompanyTable, $entityIntegrationHistoryTable . '.integrations_company_id', '=', $integrationsCompanyTable . '.id')
+            ->join($regionTable, $integrationsCompanyTable . '.region_id', '=', $regionTable . '.id')
+            ->distinct();
+
+        if (isset($args['HAS']['condition'])) {
+            $column = $args['HAS']['condition']['column'] ?? null;
+            $value = $args['HAS']['condition']['value'] ?? null;
+            if ($column && $value) {
+                $root->when(
+                    $value,
+                    fn($query) =>
+                    $query->where($regionTable . '.' . $column, $value)
+                );
+            }
+        }
+
+        return $root;
     }
 }
