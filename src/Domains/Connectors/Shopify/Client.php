cb9a8e1ed6f2ee977d0d7e55f73a4a5a6d4caf46
arfenis <arfen00@gmail.com>
2025-01-15 13:25:42 -0400

    create instance of client on validation

--- src/Domains/Connectors/Shopify/Client.php
+++ src/Domains/Connectors/Shopify/Client.php
@@ -15,6 +15,7 @@ use PHPShopify\ShopifySDK;
 class Client
 {
     protected static ?ShopifySDK $instance = null;
+    protected static array $instances = [];
 
     /**
      * Singleton.
@@ -23,13 +24,36 @@ class Client
     {
     }
 
+        /**
+     * Get unique key for the connection.
+     */
+    protected static function getConnectionKey(AppInterface $app, CompanyInterface $company, Regions $region): string 
+    {
+        return sprintf('app_%d-company_%d-region_%d', $app->id, $company->id, $region->id);
+    }
+
     public static function getInstance(AppInterface $app, CompanyInterface $company, Regions $region): ShopifySDK
     {
-        if (! self::$instance) {
-            self::$instance = self::createInstance($app, $company, $region);
+        $key = self::getConnectionKey($app, $company, $region);
+
+        if (!isset(self::$instances[$key])) {
+            self::$instances[$key] = self::createInstance($app, $company, $region);
+            
+            // Optional: Implement cleanup for old connections
+            if (count(self::$instances) > 100) { // Adjust limit as needed
+                array_shift(self::$instances);
+            }
         }
 
-        return self::$instance;
+        return self::$instances[$key];
+    }
+
+    public static function getInstanceValidation(AppInterface $app, CompanyInterface $company, Regions $region): ShopifySDK
+    {
+        $key = self::getConnectionKey($app, $company, $region);
+        self::$instances[$key] = self::createInstance($app, $company, $region);
+
+        return self::$instances[$key];
     }
 
     protected static function createInstance(AppInterface $app, CompanyInterface $company, Regions $region): ShopifySDK
