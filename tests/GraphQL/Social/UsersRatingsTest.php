5f203a6de8878913289b6dae45e8bc6ca80ec1d5
FredPeal <frederickpeal@gmail.com>
2025-01-15 19:32:50 -0400

    refactor: add ratings handler to product

--- tests/GraphQL/Social/UsersRatingsTest.php
+++ tests/GraphQL/Social/UsersRatingsTest.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace Tests\GraphQL\Social;
 
+use Kanvas\Apps\Models\Apps;
+use Kanvas\Inventory\Products\Models\Products;
+use Kanvas\SystemModules\Repositories\SystemModulesRepository;
+use Tests\GraphQL\Inventory\Traits\InventoryCases;
 use Tests\TestCase;
 
 class UsersRatingsTest extends TestCase
 {
+    use InventoryCases;
+
     public function testCreateUsersRatings()
     {
         $input = [
@@ -34,5 +40,128 @@ class UsersRatingsTest extends TestCase
                 ],
             ],
         ]);
+    }
+
+    public function testUpdateUsersRatings(): void
+    {
+        $input = [
+            'system_module_id' => 1,
+            'entity_id' => 1,
+            'rating' => 5.0,
+            'comment' => 'Great',
+        ];
+        $this->graphQL(
+            '
+                mutation createUserRating($input: UsersRatingsInput!) {
+                    createUserRating(input: $input) {
+                    id,
+                        rating
+                    }
+                }
+            ',
+            [
+                'input' => $input,
+            ]
+        )->assertJson([
+            'data' => [
+                'createUserRating' => [
+                    'rating' => 5.0,
+                ],
+            ],
+        ]);
+
+        $input['rating'] = 4.0;
+
+        $this->graphQL(
+            '
+                mutation createUserRating($input: UsersRatingsInput!) {
+                    createUserRating(input: $input) {
+                    id,
+                        rating
+                    }
+                }
+            ',
+            [
+                'input' => $input,
+            ]
+        )->assertJson([
+            'data' => [
+                'createUserRating' => [
+                    'rating' => 4.0,
+                ],
+            ],
+        ]);
+    }
+
+    public function testFilterUsersRatings(): void
+    {
+        $data = [
+            'name' => fake()->name,
+            'description' => fake()->text,
+            'sku' => fake()->time,
+            'attributes' => [
+                [
+                    'name' => fake()->name,
+                    'value' => fake()->name,
+                ],
+            ],
+        ];
+
+        $response = $this->createProduct($data);
+        $productId = $response->json('data.createProduct.id');
+        $systemModule = SystemModulesRepository::getByName(Products::class, app(Apps::class));
+        $input = [
+            'system_module_id' => $systemModule->getId(),
+            'entity_id' => $productId,
+            'rating' => 5.0,
+            'comment' => 'Great',
+        ];
+        $this->graphQL(
+            '
+                mutation createUserRating($input: UsersRatingsInput!) {
+                    createUserRating(input: $input) {
+                    id,
+                        rating
+                    }
+                }
+            ',
+            [
+                'input' => $input,
+            ]
+        )->assertJson([
+            'data' => [
+                'createUserRating' => [
+                    'rating' => 5.0,
+                ],
+            ],
+        ]);
+
+        $this->graphQL('
+            query {
+                products(
+                    whereRating: {
+                        column: RATING,
+                        operator: EQ,
+                        value: 5
+                    }
+                ) {
+                    data {
+                        name
+                        description
+                    }
+                }
+            }')
+            ->assertJson([
+                'data' => [
+                    'products' => [
+                        'data' => [
+                            [
+                                'name' => $data['name'],
+                                'description' => $data['description'],
+                            ],
+                        ],
+                    ],
+                ],
+            ]);
     }
 }
