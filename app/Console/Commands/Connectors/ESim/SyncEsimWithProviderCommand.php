a3bf5325d38a726ad55da783bd24e1b863300605
kaioken <max@mctekk.com>
2025-02-09 09:42:40 -0400

    hotfix currency

--- app/Console/Commands/Connectors/ESim/SyncEsimWithProviderCommand.php
+++ app/Console/Commands/Connectors/ESim/SyncEsimWithProviderCommand.php
@@ -215,6 +215,8 @@ class SyncEsimWithProviderCommand extends Command
             IccidStatusEnum::EXPIRED->value,
             IccidStatusEnum::COMPLETED->value,
             IccidStatusEnum::DELETED->value,
+            IccidStatusEnum::DISABLED->value,
+            IccidStatusEnum::DISABLE->value,
         ];
 
         if (in_array(strtolower($response['bundleState']), $inactiveStatuses, true)) {
