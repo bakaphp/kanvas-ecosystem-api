3d006864729a4e42d7a047e3ce8c6a675291ebf2
kaioken <max@mctekk.com>
2025-02-11 16:05:30 -0400

    feat: add start engagement to pipeline

--- src/Domains/Workflow/Rules/Models/Action.php
+++ src/Domains/Workflow/Rules/Models/Action.php
@@ -4,14 +4,11 @@ declare(strict_types=1);
 
 namespace Kanvas\Workflow\Rules\Models;
 
-use Baka\Traits\SlugTrait;
 use Kanvas\Workflow\Models\BaseModel;
 use Kanvas\Workflow\Rules\Factories\ActionFactory;
 
 class Action extends BaseModel
 {
-    use SlugTrait;
-
     protected $table = 'actions';
 
     protected $guarded = [];
