2566c8d18ed35b9adb2e89dff1709627991d8f6f
Rafael White <rwhite@mctekk.com>
2025-02-17 14:29:28 -0400

    add user search grapql query

---
+++ app/GraphQL/Ecosystem/Queries/Users/UsersBuilder.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Ecosystem\Queries\Users;
+
+use Kanvas\Apps\Models\Apps;
+use Kanvas\Users\Models\Users;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+use GraphQL\Type\Definition\ResolveInfo;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Database\Eloquent\Builder;
+
+class UsersBuilder
+{
+    public function getAll(
+        mixed $root,
+        array $args,
+        GraphQLContext $context,
+        ResolveInfo $resolveInfo
+    ): Builder {
+        return Users::query();
+    }
+    
+}
