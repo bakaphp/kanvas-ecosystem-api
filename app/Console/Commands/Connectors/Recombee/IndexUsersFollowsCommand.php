4c6f45ac42e9466c9790d3ff576c378a231621cd
Rafael White <rwhite@mctekk.com>
2025-02-20 12:53:05 -0400

    feat: add command to index user follows for the recommendation engine

---
+++ app/Console/Commands/Connectors/Recombee/IndexUsersFollowsCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands\Connectors\Recombee;
+
+use Illuminate\Console\Command;
+use Kanvas\Apps\Models\Apps;
+use Kanvas\Companies\Models\Companies;
+use Kanvas\Connectors\PromptMine\Services\RecombeeIndexService;
+use Kanvas\Connectors\Recombee\Enums\ConfigurationEnum;
+use Kanvas\Social\Follows\Models\UsersFollows;
+use Kanvas\Users\Models\Users;
+
+class IndexUsersFollowsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'kanvas:recombee-index-users-follows {app_id}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Index users follows to the recommendation engine';
+
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $app = Apps::getById((int) $this->argument('app_id'));
+        // $company = Companies::find($this->argument('companies_id'));
+        // $this->overwriteAppService($app);
+
+        $query = UsersFollows::fromApp($app)
+        ->where('is_deleted', 0)
+        ->where('entity_namespace', Users::class)
+        ->orderBy('id', 'DESC');
+        $cursor = $query->cursor();
+        $totalTags = $query->count();
+
+        $this->output->progressStart($totalTags);
+        $usersFollowsIndex = new RecombeeIndexService(
+            $app,
+            $app->get(ConfigurationEnum::FOLLOWS_ENGINE_RECOMBEE_DATABASE->value),
+            $app->get(ConfigurationEnum::FOLLOWS_ENGINE_RECOMBEE_API_KEY->value)
+        );
+
+        foreach ($cursor as $userFollow) {
+            $result = $usersFollowsIndex->indexUsersFollows($userFollow);
+
+            $this->info('Users Follows ID: ' . $userFollow->getId() . ' indexed with result: ' . $result);
+            $this->output->progressAdvance();
+        }
+
+        $this->output->progressFinish();
+
+        return;
+    }
+}
