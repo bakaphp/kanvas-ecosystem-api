94c6dfa357a2b153fd0ededf2c87d30135490281
kaioken <max@mctekk.com>
2025-02-22 09:00:05 -0400

    Refact

--- src/Domains/Connectors/Recombee/Workflows/PushMessageToItemActivity.php
+++ src/Domains/Connectors/Recombee/Workflows/PushMessageToItemActivity.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Kanvas\Connectors\Google\Activities;
+namespace Kanvas\Connectors\Recombee\Workflows;
 
 use Baka\Contracts\AppInterface;
 use Illuminate\Database\Eloquent\Model;
