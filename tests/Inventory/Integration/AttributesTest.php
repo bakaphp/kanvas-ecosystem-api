ca8080ecfa846ebb7432ebcef3a01008a044400b
kaioken <max@mctekk.com>
2025-02-23 17:10:34 -0400

    fix: json attributes

--- tests/Inventory/Integration/AttributesTest.php
+++ tests/Inventory/Integration/AttributesTest.php
@@ -45,4 +45,40 @@ final class AttributesTest extends TestCase
 
         $this->assertEquals($product->getAttributeByName($attributeName)->value, $attributeValue);
     }
+
+    public function testGetAttributesByValue()
+    {
+        $app = app(Apps::class);
+        $user = auth()->user();
+        $company = $user->getCurrentCompany();
+        $setupInventory = new Setup($app, $user, $company);
+        $setupInventory->run();
+
+        $region = Regions::fromApp($app)->fromCompany($company)->first();
+
+        $sku = 'model_price_01';
+
+        $productData = new Product(
+            app: $app,
+            company: $company,
+            user: $user,
+            name: fake()->name,
+            sku: $sku,
+            warehouses: [[
+                'quantity' => 10,
+                'price' => 0.29,
+            ],
+            ]
+        );
+
+        $product = (new CreateProductAction($productData, $user))->execute();
+        $attributeName = 'test_attribute';
+        $attributeValue = 'test_value';
+        $product->addAttribute($attributeName, $attributeValue);
+
+        $product->variants->first()->addAttribute($attributeName, $attributeValue);
+
+        $this->assertEquals($product->getAttributeBySlug('test-attribute')->value, $attributeValue);
+        $this->assertEquals($product->variants->first()->getAttributeBySlug('test-attribute')->value, $attributeValue);
+    }
 }
