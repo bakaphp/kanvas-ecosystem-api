ec7fa2c5336bb99b5728ba3e9e53b2d774fa2d66
Rafael White <rwhite@mctekk.com>
2025-02-24 16:02:02 -0400

    refactor: simplify follow and unfollow methods by removing rating actions

--- app/GraphQL/Social/Mutations/Follows/FollowManagementMutation.php
+++ app/GraphQL/Social/Mutations/Follows/FollowManagementMutation.php
@@ -9,7 +9,6 @@ use Kanvas\Social\Follows\Actions\FollowAction;
 use Kanvas\Social\Follows\Actions\UnFollowAction;
 use Kanvas\Social\Follows\Models\UsersFollows;
 use Kanvas\Users\Repositories\UsersRepository;
-use Kanvas\Connectors\Recombee\Actions\AddRatingUserItemAction;
 
 class FollowManagementMutation
 {
@@ -29,11 +28,7 @@ class FollowManagementMutation
         //$action = new FollowAction(auth()->user(), $user);
         //$action->execute();
 
-        $follow = $user->follow($userToFollow) instanceof UsersFollows;
-
-        (new AddRatingUserItemAction($app, $user, $userToFollow->getId(), 1.0))->execute();
-
-        return $follow;
+        return $user->follow($userToFollow) instanceof UsersFollows;
     }
 
     /**
@@ -49,10 +44,6 @@ class FollowManagementMutation
             return false;
         }
 
-        $unFollow = $user->unFollow($userToUnFollow);
-
-        (new AddRatingUserItemAction($app, $user, $userToUnFollow->getId(), -1.0))->execute();
-
-        return $unFollow;
+        return $user->unFollow($userToUnFollow);
     }
 }
