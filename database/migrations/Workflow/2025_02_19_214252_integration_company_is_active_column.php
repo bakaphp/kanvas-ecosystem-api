4e5f269246376b4aeba6cfb7f1d6ff89963be63b
arfenis <arfen00@gmail.com>
2025-02-24 18:46:17 -0400

    add is_active column to integration company

---
+++ database/migrations/Workflow/2025_02_19_214252_integration_company_is_active_column.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('integration_companies', function (Blueprint $table) {
+            $table->tinyInteger('is_active')->default(1)->after('config');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('integration_companies', function (Blueprint $table) {
+            $table->dropColumn('is_active');
+        });
+    }
+};
