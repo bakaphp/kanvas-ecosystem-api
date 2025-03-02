503ebb518ee839ddbf665e5eb72fd837ad2f34ed
Maximo Castro Pou <kaioken@Maximos-MacBook-Pro-2.local>
2025-03-02 17:44:57 -0400

    fix

--- src/Domains/Souk/Enums/ConfigurationEnum.php
+++ src/Domains/Souk/Enums/ConfigurationEnum.php
@@ -11,5 +11,4 @@ enum ConfigurationEnum: string
     case SEND_NEW_ORDER_TO_OWNER_NOTIFICATION = 'souk_send_new_order_to_owner_notification';
     case USE_B2B_COMPANY_GROUP = 'USE_B2B_COMPANY_GROUP';
     case B2B_GLOBAL_COMPANY = 'B2B_GLOBAL_COMPANY';
-
 }
