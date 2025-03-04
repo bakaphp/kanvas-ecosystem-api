c817663162f591d2707d2d56fc02755e20a10571
FredPeal <frederickpeal@gmail.com>
2025-03-04 03:33:17 -0400

    refactor: add module seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Kanvas\Modules\Models\Module;
 
 class DatabaseSeeder extends Seeder
 {
@@ -30,6 +31,7 @@ class DatabaseSeeder extends Seeder
             CustomFieldsTypesSeeder::class,
             MessageActivityTypeSeeder::class,
             NotificationChannelsSeeder::class,
+            ModulesSeeder::class,
         ]);
     }
 }
