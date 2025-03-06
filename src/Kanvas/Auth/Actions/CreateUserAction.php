08e8cd3f5c980c3b823c13ba2efe6f1598d3b251
Maximo Castro Pou <kaioken@Maximos-MacBook-Pro-2.local>
2025-03-06 19:46:47 -0400

    fix: user signup validation

--- src/Kanvas/Auth/Actions/CreateUserAction.php
+++ src/Kanvas/Auth/Actions/CreateUserAction.php
@@ -127,7 +127,7 @@ class CreateUserAction
                 'lastname' => 'required|different:firstname',
             ]
         );
-    
+
         if ($validator->fails()) {
             throw new ValidationException($validator);
         }
