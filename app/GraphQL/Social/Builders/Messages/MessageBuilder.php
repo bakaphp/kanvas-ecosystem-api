1a8250204569c055ff3a50c22cbc92ab312fedb8
Rafael White <rwhite@mctekk.com>
2025-03-08 21:16:43 -0400

    add filter for public results in searchSuggestions

--- app/GraphQL/Social/Builders/Messages/MessageBuilder.php
+++ app/GraphQL/Social/Builders/Messages/MessageBuilder.php
@@ -199,6 +199,7 @@ class MessageBuilder
         $results = $index->search($args['search'], [
             'hitsPerPage' => 15,
             'attributesToRetrieve' => ['name', 'description'],
+            'filters' => 'is_public:1'
         ]);
 
         return $results['hits'];
