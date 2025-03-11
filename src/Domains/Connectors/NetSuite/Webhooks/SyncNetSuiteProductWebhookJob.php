0aa7cb815b1b7a93c88a9ad27b0a293995c9793d
Maximo Castro Pou <kaioken@Maximos-MacBook-Pro-2.local>
2025-03-11 10:27:00 -0400

    feat: add netsuite webhook

--- src/Domains/Connectors/NetSuite/Webhooks/SyncNetSuiteProductWebhookJob.php
+++ src/Domains/Connectors/NetSuite/Webhooks/SyncNetSuiteProductWebhookJob.php
@@ -7,15 +7,18 @@ namespace Kanvas\Connectors\NetSuite\Webhooks;
 use Kanvas\Companies\Models\Companies;
 use Kanvas\Connectors\NetSuite\Actions\PullNetSuiteProductPriceAction;
 use Kanvas\Workflow\Jobs\ProcessWebhookJob;
+use Override;
 
 class SyncNetSuiteProductWebhookJob extends ProcessWebhookJob
 {
+    #[Override]
     public function execute(): array
     {
         $barcode = $this->webhookRequest->payload['name'];
         $mainCompanyId = $this->receiver->app->get('B2B_MAIN_COMPANY_ID');
         $productSyncResult = [];
 
+        $successMessage = 'NetSuite Product Not Synced';
         if ($mainCompanyId) {
             $mainCompany = Companies::getById($mainCompanyId);
             $syncNetSuiteProduct = new PullNetSuiteProductPriceAction(
@@ -23,10 +26,11 @@ class SyncNetSuiteProductWebhookJob extends ProcessWebhookJob
                 $mainCompany
             );
             $productSyncResult = $syncNetSuiteProduct->execute($barcode);
+            $successMessage = 'NetSuite Product Synced';
         }
 
         return [
-            'message' => 'NetSuite Product Synced',
+            'message' => $successMessage,
             'barcode' => $barcode,
             'mainCompanyId' => $mainCompanyId,
             'productSyncResult' => $productSyncResult,
