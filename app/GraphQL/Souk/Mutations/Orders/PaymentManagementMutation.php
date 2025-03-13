6e1be2d070ab764647fe33d4ec56bfef10a11ba8
Maximo Castro Pou <kaioken@Maximos-MacBook-Pro-2.local>
2025-03-13 10:41:11 -0400

    feat: stripe payment intent

--- app/GraphQL/Souk/Mutations/Orders/PaymentManagementMutation.php
+++ app/GraphQL/Souk/Mutations/Orders/PaymentManagementMutation.php
@@ -9,8 +9,6 @@ use Kanvas\Apps\Models\Apps;
 use Kanvas\Companies\Models\CompaniesBranches;
 use Kanvas\Connectors\Stripe\Enums\ConfigurationEnum;
 use Kanvas\Exceptions\ValidationException;
-use Kanvas\Souk\Orders\Models\Order;
-use Kanvas\Souk\Services\B2BConfigurationService;
 use Stripe\PaymentIntent;
 use Stripe\Stripe;
 
@@ -30,7 +28,7 @@ class PaymentManagementMutation
         ];
     }
 
-    public function generateStripePaymentIntent(mixed $root, array $request): array
+    public function generatePaymentIntent(mixed $root, array $request): array
     {
         //$user = auth()->user();
         $app = app(Apps::class);
@@ -55,43 +53,6 @@ class PaymentManagementMutation
             'message' => [
                 'message' => 'Payment intent generated successfully',
                 'amount' => $amount,
-                'currency' => 'usd',
-            ],
-        ];
-    }
-
-    public function generatePaymentIntent(mixed $root, array $request): array
-    {
-        $user = auth()->user();
-        $app = app(Apps::class);
-        $orderId = $request['id'];
-        $company = B2BConfigurationService::getConfiguredB2BCompany($app, $user->getCurrentCompany());
-
-        $order = Order::getByIdFromCompanyApp($orderId, $company, $app);
-
-        if ($order->isFulfilled()) {
-            throw new ValidationException('Order is already fulfilled');
-        }
-
-        $stripeApiKey = $app->get(ConfigurationEnum::STRIPE_SECRET_KEY->value);
-        if (empty($stripeApiKey)) {
-            throw new ValidationException('Stripe is not configured for this app');
-        }
-
-        Stripe::setApiKey($stripeApiKey);
-
-        $totalAmount = $order->total_gross_amount * 100;
-        $intent = PaymentIntent::create([
-            'amount' => $totalAmount,
-            'currency' => 'usd',
-            ]);
-
-        return [
-            'status' => 'success',
-            'client_secret' => $intent->client_secret,
-            'message' => [
-                'message' => 'Payment intent generated successfully',
-                'amount' => $order->total_gross_amount,
                 'currency' => 'usd',
             ],
         ];
