720614f1e9594dc088691dd79e78effc2ee0bcae
freesgen <jesusant.guerrero@gmail.com>
2025-03-18 09:13:24 -0400

    feat: add support to multiple entities in user lists

--- app/GraphQL/Social/Mutations/UsersLists/UsersListsManagement.php
+++ app/GraphQL/Social/Mutations/UsersLists/UsersListsManagement.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\GraphQL\Social\Mutations\UsersLists;
 
+use Illuminate\Database\Eloquent\Model;
 use Kanvas\Apps\Models\Apps;
+use Kanvas\Inventory\Products\Models\Products;
 use Kanvas\Social\Messages\Models\Message;
 use Kanvas\Social\UsersLists\Actions\CreateUserListAction;
 use Kanvas\Social\UsersLists\DataTransferObject\UserList;
@@ -66,5 +68,42 @@ class UsersListsManagement
         $userList->items()->detach($message);
 
         return true;
+    }
+
+    public function addEntityToList(mixed $rootValue, array $req): bool
+    {
+        $userList = UserListRepository::getById((int)$req['entity']['users_lists_id'], auth()->user());
+        $entity = $this->getEntity($req['entity']['entity_type'], (int)$req['entity']['entity_id']);
+        $userList->entities()->create([
+            'entity_id' => $entity->id,
+            'entity_namespace' => $entity->getMorphClass(),
+            'description' => "",
+            'is_pin' => false
+        ]);
+        return true;
+    }
+
+    public function removeEntityFromList(mixed $rootValue, array $req): bool
+    {
+        $userList = UserListRepository::getById((int)$req['entity']['users_lists_id'], auth()->user());
+        $entity = $this->getEntity($req['entity']['entity_type'], (int)$req['entity']['entity_id']);
+
+        $userList->entities()->where('entity_id', $entity->id)->delete();
+
+        return true;
+    }
+
+    private function getEntity(string $entityType, int $entityId): Model
+    {
+        $entities = [
+            'message' => Message::class,
+            'product' => Products::class,
+        ];
+
+        if (! isset($entities[$entityType])) {
+            throw new \Exception('Invalid entity type');
+        }
+
+        return $entities[$entityType]::getById($entityId);
     }
 }
