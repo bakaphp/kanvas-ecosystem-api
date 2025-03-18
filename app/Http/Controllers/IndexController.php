6f94a8f673b673b7b5bc8d9fade5767a9f1433a5
Rafael White <rwhite@mctekk.com>
2025-03-18 19:11:11 -0400

    Add-deploy-to-queue-ec2 ## General Description
    Please include a summary of the changes and the related issue. Highlight key points of the PR, any relevant information, and reasons for making the change.
    
    ## Related Issue
    
    A link to the ticket or issue that this PR is related to.(if applicable)
    
    ## Checklist
    
    A checklist of things that should be done before merging this PR. Linked to the ticket or issue that this PR is related to.(if applicable)
    
    - [ ] I have performed a self-review of my code.
    - [ ] I have added tests to cover my changes.
    - [ ] I have updated the documentation (if needed).
    - [ ] My changes generate no new warnings.
    - [ ] My changes do not break any existing tests.
    
    ## Screenshots (if applicable)
    
    Provide screenshots of impactful changes you have made so that the reviewer can quickly understand the changes.

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,6 +6,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
+use Kanvas\Connectors\Google\Services\MapStaticApiService;
+use Kanvas\Connectors\SightEngine\Services\ContentModerationService;
 
 class IndexController extends BaseController
 {
@@ -16,6 +18,11 @@ class IndexController extends BaseController
      */
     public function index(): JsonResponse
     {
-        return response()->json(['Woot Kanvas Ecosystem']);
+        // $imageUrl = "https://s3.amazonaws.com/mc-canvas/8cCzR0Oq1wkwwA68JM5OwLST5JaOGX3YpPTYnozq.jpg";
+        $imageContentModerationService = (new ContentModerationService())->scanText("Im naked, come and contact me");
+
+        $imageUrl = MapStaticApiService::getImageFromCoordinates(18.463449, -66.117866);
+
+        return response()->json($imageUrl);
     }
 }
