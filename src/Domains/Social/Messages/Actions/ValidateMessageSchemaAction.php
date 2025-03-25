6d27f28af96b5af42540acf82469032c3c5442fe
Rafael White <rwhite@mctekk.com>
2025-03-25 11:23:14 -0400

    refactor: rename validation methods for clarity and consistency

--- src/Domains/Social/Messages/Actions/ValidateMessageSchemaAction.php
+++ src/Domains/Social/Messages/Actions/ValidateMessageSchemaAction.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Kanvas\Social\Messages\Actions;
 
-use Illuminate\Support\Facades\DB;
 use Kanvas\Social\Messages\Models\Message;
-use Kanvas\Social\Messages\Models\UserMessage;
 use Kanvas\Social\MessagesTypes\Models\MessageType;
 
 class ValidateMessageSchemaAction
@@ -25,17 +23,17 @@ class ValidateMessageSchemaAction
         $schema = json_decode($this->messageType->message_schema, true);
         $data = is_array($this->message->message) ? $this->message->message : json_decode($this->message->message, true);
 
-        return $this->validateJson($data, $schema);
+        return $this->validateSchema($data, $schema);
     }
 
-    private function validateJson($data, $schema, $parentKey = '')
+    private function validateSchema($data, $schema, $parentKey = '')
     {
         $errors = [];
         foreach ($schema['required'] as $key) {
             if (! isset($data[$key])) {
                 $errors[] = "Missing required field: {$parentKey}{$key}";
             } elseif (is_array($schema['types'][$key])) {
-                $errors = array_merge($errors, $this->validateJson($data[$key], $schema['types'][$key], "{$parentKey}{$key}."));
+                $errors = array_merge($errors, $this->validateSchema($data[$key], $schema['types'][$key], "{$parentKey}{$key}."));
             } elseif (gettype($data[$key]) !== $schema['types'][$key]) {
                 $errors[] = "Invalid type for {$parentKey}{$key}, expected " . $schema['types'][$key];
             }
@@ -43,7 +41,7 @@ class ValidateMessageSchemaAction
         foreach ($schema['optional'] as $key) {
             if (isset($data[$key])) {
                 if (is_array($schema['types'][$key])) {
-                    $errors = array_merge($errors, $this->validateJson($data[$key], $schema['types'][$key], "{$parentKey}{$key}."));
+                    $errors = array_merge($errors, $this->validateSchema($data[$key], $schema['types'][$key], "{$parentKey}{$key}."));
                 } elseif (gettype($data[$key]) !== $schema['types'][$key]) {
                     $errors[] = "Invalid type for {$parentKey}{$key}, expected " . $schema['types'][$key];
                 }
