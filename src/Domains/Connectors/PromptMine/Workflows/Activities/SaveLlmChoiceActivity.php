c7c2f33a18485145639ac80e2665de7d9e669dcd
Rafael White <rwhite@mctekk.com>
2025-03-26 15:21:23 -0400

    feat: add SaveLlmChoiceActivity to handle LLM choice saving

---
+++ src/Domains/Connectors/PromptMine/Workflows/Activities/SaveLlmChoiceActivity.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Social\Connectors\Promptmine\Workflows\Activities;
+
+use Baka\Contracts\AppInterface;
+use Illuminate\Database\Eloquent\Model;
+use Kanvas\Social\Messages\Actions\CheckMessageContentAction;
+use Kanvas\Workflow\Contracts\WorkflowActivityInterface;
+use Kanvas\Workflow\KanvasActivity;
+
+class SaveLlmChoiceActivity extends KanvasActivity implements WorkflowActivityInterface
+{
+    public function execute(Model $entity, AppInterface $app, array $params): array
+    {
+        $messageData = ! is_array($entity->message) ? json_decode($entity->message, true) : $entity->message;
+
+        if (! isset($messageData['ai_model'])) {
+            return [
+                'result' => false,
+                'message' => 'Message does not have an AI model',
+            ];
+        }
+        
+        $entity->user->set('llm_choice', $messageData['ai_model']);
+
+        return [
+            'message' => 'LLM choice saved',
+            'result' => true,
+            'user_id' => $entity->user->getId(),
+            'model' => $messageData['ai_model'],
+            'message_data' => $entity->message,
+            'message_id' => $entity->getId(),
+        ];
+    }
+}
