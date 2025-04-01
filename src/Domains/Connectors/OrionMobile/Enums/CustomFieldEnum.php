87bcd7f0446351197e39b47e51735b1116e8fb88
Maximo Castro Pou <kaioken@Maximos-MacBook-Pro-2.local>
2025-04-01 09:04:30 -0400

    feat: provider mobile

---
+++ src/Domains/Connectors/OrionMobile/Enums/CustomFieldEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Connectors\OrionMobile\Enums;
+
+enum CustomFieldEnum: string
+{
+    case ORION_MOBILE_PRODUCT_ID = 'orion_mobile_product_id';
+    case ORION_MOBILE_SOURCE_ID = 'orion_mobile_source_id';
+}
