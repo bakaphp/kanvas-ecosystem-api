b1db3d76922a282aecc95ed3f71f88c36074417b
arfenis <arfen00@gmail.com>
2025-04-01 15:27:52 -0400

    create migration to remove unique key

---
+++ database/migrations/Inventory/2025_04_01_184639_channel_unique_key_update.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('channels', function (Blueprint $table) {
+            if (Schema::hasColumn('channels', 'slug') && Schema::hasColumn('channels', 'companies_id')) {
+                $table->dropUnique(['slug', 'companies_id']);
+                $table->index(['slug', 'companies_id', 'apps_id']);
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
