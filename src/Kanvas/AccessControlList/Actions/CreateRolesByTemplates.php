7795070adeecc6cbf03c20ac6e7e9fe6e833bd13
FredPeal <frederickpeal@gmail.com>
2025-04-03 11:06:47 -0400

    refactor: contruct

--- src/Kanvas/AccessControlList/Actions/CreateRolesByTemplates.php
+++ src/Kanvas/AccessControlList/Actions/CreateRolesByTemplates.php
@@ -8,9 +8,17 @@ use Kanvas\AccessControlList\Templates\AdminRoleTemplate;
 use Kanvas\AccessControlList\Templates\UsersRoleTemplate;
 use Kanvas\AccessControlList\Templates\ModulesRepositories;
 use Bouncer;
+use Kanvas\Apps\Models\Apps;
+use Kanvas\AccessControlList\Enums\RolesEnums;
 
 class CreateRolesByTemplates
 {
+    public function __construct(
+        protected Apps $app
+    ) {
+        Bouncer::scope()->to(RolesEnums::getScope($app));
+    }
+
     /**
      * @var array
      */
@@ -20,18 +28,19 @@ class CreateRolesByTemplates
         UsersRoleTemplate::class,
     ];
 
-    public function execute(){
+    public function execute()
+    {
         foreach ($this->templates as $template) {
             $templateInstance = new $template();
             $role = $templateInstance->role;
             $denied = $templateInstance->denied;
             $allowed = $templateInstance->allowed;
-            if(empty($allowed)){
+            if (empty($allowed)) {
                 $allowed = ModulesRepositories::getAllAbilities();
             }
-            foreach($allowed as $key => $permissions){
-                foreach($permissions as $value){
-                    if(in_array($value, $denied)){
+            foreach ($allowed as $key => $permissions) {
+                foreach ($permissions as $value) {
+                    if (in_array($value, $denied)) {
                         continue;
                     }
                     Bouncer::allow($role)->to($value, $key);
