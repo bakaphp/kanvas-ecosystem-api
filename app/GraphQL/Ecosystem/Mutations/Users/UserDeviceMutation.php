83e4f06a3b974038a5e7895bde52d44e06536b22
Maximo Castro Pou <kaioken@Maximos-MacBook-Pro-2.local>
2025-04-05 23:35:48 -0400

    hotfix linked source

--- app/GraphQL/Ecosystem/Mutations/Users/UserDeviceMutation.php
+++ app/GraphQL/Ecosystem/Mutations/Users/UserDeviceMutation.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\GraphQL\Ecosystem\Mutations\Users;
 
+use Kanvas\Apps\Models\Apps;
 use Kanvas\Users\Models\Sources;
 use Kanvas\Users\Models\UserLinkedSources;
 
@@ -14,6 +15,7 @@ class UserDeviceMutation
      */
     public function register(mixed $root, array $req): bool
     {
+        $app = app(Apps::class);
         $req = $req['data'];
         $source = Sources::where('title', $req['source_site'])->firstOrFail();
         $user = auth()->user();
@@ -25,6 +27,7 @@ class UserDeviceMutation
         ]);
 
         // Update the attributes explicitly
+        $result->apps_id = $app->getId();
         $result->source_users_id = $user->getId();
         $result->source_username = $user->displayname . ' ' . $source->title;
         $result->is_deleted = 0;
@@ -37,12 +40,14 @@ class UserDeviceMutation
 
     public function remove(mixed $root, array $req): bool
     {
+        $app = app(Apps::class);
         $req = $req['data'];
         $source = Sources::where('title', $req['source_site'])->firstOrFail();
         $user = auth()->user();
 
         return (bool) UserLinkedSources::where('users_id', $user->getId())
             ->where('source_id', $source->getId())
+            ->whereIn('apps_id', [$app->getId(), null])
             ->where('source_users_id_text', $req['device_id'])
             ->update([
                 'is_deleted' => 1,
