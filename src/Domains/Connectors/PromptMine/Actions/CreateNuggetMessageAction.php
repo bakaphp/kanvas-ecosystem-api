79952c46b92d3973b6716fe3b1f90e15cea2efef
Rafael White <rwhite@mctekk.com>
2025-04-15 11:07:17 -0400

    fix: update tag handling in CreateNuggetMessageAction to use pluck for improved data retrieval

--- src/Domains/Connectors/PromptMine/Actions/CreateNuggetMessageAction.php
+++ src/Domains/Connectors/PromptMine/Actions/CreateNuggetMessageAction.php
@@ -35,7 +35,7 @@ class CreateNuggetMessageAction
             )
         ))->execute();
 
-        $nuggetMessage->addTags($this->parentMessage->tags->toArray());
+        $nuggetMessage->addTags($this->parentMessage->tags->pluck('name'));
         $this->parentMessage->total_children++;
         $this->parentMessage->save();
         return $nuggetMessage;
