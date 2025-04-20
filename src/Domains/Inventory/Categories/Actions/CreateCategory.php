119671d469de4fb2daf756b932bcfe96889c0bc3
FredPeal <frederickpeal@gmail.com>
2025-04-20 02:17:40 -0400

    fix: parent_id on category null

--- src/Domains/Inventory/Categories/Actions/CreateCategory.php
+++ src/Domains/Inventory/Categories/Actions/CreateCategory.php
@@ -42,7 +42,7 @@ class CreateCategory
         ], [
             'name' => $this->dto->name,
             'users_id' => $this->dto->user->getId(),
-            'parent_id' => $this->dto->parent_id,
+            'parent_id' => $this->dto->parent_id ?? null,
             'code' => $this->dto->code,
             'position' => $this->dto->position,
             'is_published' => $this->dto->is_published
