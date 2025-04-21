a0b02bec05c5bb49ef63a59d30ed079b823aff14
arfenis <arfen00@gmail.com>
2025-04-21 15:12:19 -0400

    create column to save rule_id

---
+++ database/migrations/Workflow/2025_04_21_180227_entity_integration_rule_column.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('entity_integration_history', function (Blueprint $table) {
+            $table->unsignedBigInteger('rules_id')->after('workflow_id')->nullable(true);
+
+            $table->index('rules_id', 'rules_id_index');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
