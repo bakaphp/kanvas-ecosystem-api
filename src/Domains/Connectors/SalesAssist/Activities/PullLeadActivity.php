5012f8942445c0c78e9c1400f100c6895d83ae35
Maximo Castro Pou <kaioken@Maximos-MacBook-Pro-2.local>
2025-04-21 14:49:39 -0400

    feat: pull sync

---
+++ src/Domains/Connectors/SalesAssist/Activities/PullLeadActivity.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Connectors\SalesAssist\Activities;
+
+use Baka\Contracts\AppInterface;
+use Illuminate\Database\Eloquent\Model;
+use Kanvas\Guild\Customers\Models\People;
+use Kanvas\Guild\Leads\Models\Lead;
+use Kanvas\Workflow\Contracts\WorkflowActivityInterface;
+use Kanvas\Workflow\KanvasActivity;
+use Override;
+
+class PullLeadActivity extends KanvasActivity implements WorkflowActivityInterface
+{
+    #[Override]
+    /**
+     * $entity <People>
+     */
+    public function execute(Model $entity, AppInterface $app, array $params): array
+    {
+        $leadId = $params['entity_id'] ?? null;
+
+        $lead = Lead::fromApp($app)->getById($leadId);
+        if ($lead === null) {
+            return [
+                'result' => false,
+                'message' => 'Lead not found with id ' . $leadId,
+            ];
+        }
+
+        return $lead->toArray();
+    }
+}
