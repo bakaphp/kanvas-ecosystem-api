58c160ff3b62f6c1d3336395721c27fb5f92dd17
Rafael White <rwhite@mctekk.com>
2025-04-21 12:40:42 -0400

    style: remove unnecessary blank line in execute method of SaveUserAppPreferencesAction

--- src/Kanvas/Users/Actions/SaveUserAppPreferencesAction.php
+++ src/Kanvas/Users/Actions/SaveUserAppPreferencesAction.php
@@ -21,7 +21,6 @@ class SaveUserAppPreferencesAction
         // activeUserAppSettings is an array of keys that are allowed to be set, this makes sure that no upwanted settings are stored. This array of keys comes from the app setting in_app_user_settings_keys
         $activeUserAppSettings = $this->app->get('in_app_user_settings_keys');
         foreach ($this->$activeUserAppSettings->toArray() as $key) {
-
             if (! array_key_exists($key, $this->preferences)) {
                 continue;
             }
