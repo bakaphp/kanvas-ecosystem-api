433d259f5f54258e1095e3afa152525c163b4763
Maximo Castro Pou <kaioken@Maximos-MacBook-Pro-2.local>
2025-04-21 14:58:18 -0400

    feat: pull sync

--- src/Domains/Connectors/SalesAssist/Activities/PullLeadActivity.php
+++ src/Domains/Connectors/SalesAssist/Activities/PullLeadActivity.php
@@ -6,7 +6,6 @@ namespace Kanvas\Connectors\SalesAssist\Activities;
 
 use Baka\Contracts\AppInterface;
 use Illuminate\Database\Eloquent\Model;
-use Kanvas\Guild\Customers\Models\People;
 use Kanvas\Guild\Leads\Models\Lead;
 use Kanvas\Workflow\Contracts\WorkflowActivityInterface;
 use Kanvas\Workflow\KanvasActivity;
@@ -16,7 +15,7 @@ class PullLeadActivity extends KanvasActivity implements WorkflowActivityInterfa
 {
     #[Override]
     /**
-     * $entity <People>
+     * $entity <Lead>
      */
     public function execute(Model $entity, AppInterface $app, array $params): array
     {
