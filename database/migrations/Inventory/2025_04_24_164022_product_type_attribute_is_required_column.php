d75f326a1cae469fa12a6db25d8c9d47e4be3070
arfenis <arfen00@gmail.com>
2025-04-24 15:43:06 -0400

    add migration to move is_required to product_attribute_type

---
+++ database/migrations/Inventory/2025_04_24_164022_product_type_attribute_is_required_column.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products_types_attributes', function (Blueprint $table) {
+            $table->boolean('is_required')->after('to_variant')->default(false);
+            $table->index('is_required');
+        });
+
+        Schema::table('attributes', function (Blueprint $table) {
+            $table->dropColumn('is_required');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
