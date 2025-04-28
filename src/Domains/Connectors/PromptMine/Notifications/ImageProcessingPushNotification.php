ba164251c1df12435ac87d128cfc83bca401a7aa
Rafael White <rwhite@mctekk.com>
2025-04-28 14:45:17 -0400

    fix: reorder use statements in ImageProcessingPushNotification for improved clarity

--- src/Domains/Connectors/PromptMine/Notifications/ImageProcessingPushNotification.php
+++ src/Domains/Connectors/PromptMine/Notifications/ImageProcessingPushNotification.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Kanvas\Connectors\PromptMine\Notifications;
 
-use Kanvas\Social\Messages\Models\Message;
 use Kanvas\Connectors\PromptMine\Enums\NotificationTemplateEnum;
+use Kanvas\Social\Messages\Models\Message;
 use Kanvas\Social\Messages\Notifications\CustomMessageNotification;
 use Kanvas\Templates\Enums\EmailTemplateEnum;
 use Kanvas\Users\Models\Users;
