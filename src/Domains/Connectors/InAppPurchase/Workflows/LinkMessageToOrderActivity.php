397a726e9fcab8fe36b354559f74f5e0c9336222
Rafael White <rwhite@mctekk.com>
2025-04-29 10:35:58 -0400

    refactor: Remove LockPremiumPromptsActivity class and update LinkMessageToOrderActivity for improved message handling

--- src/Domains/Connectors/InAppPurchase/Workflows/LinkMessageToOrderActivity.php
+++ src/Domains/Connectors/InAppPurchase/Workflows/LinkMessageToOrderActivity.php
@@ -17,15 +17,15 @@ use Kanvas\SystemModules\Repositories\SystemModulesRepository;
 use Kanvas\Users\Models\Users;
 use Kanvas\Workflow\Contracts\WorkflowActivityInterface;
 use Kanvas\Workflow\KanvasActivity;
+use Kanvas\Enums\AppSettingsEnums;
+use Kanvas\Companies\Models\CompaniesBranches;
+use Kanvas\Exceptions\ModelNotFoundException;
+use Kanvas\Workflow\Enums\IntegrationsEnum;
 use Override;
 
 class LinkMessageToOrderActivity extends KanvasActivity implements WorkflowActivityInterface
 {
     public $tries = 3;
-
-    /**
-     * @param Model<Order> $order
-     */
     #[Override]
     public function execute(Model $order, AppInterface $app, array $params): array
     {
@@ -38,57 +38,75 @@ class LinkMessageToOrderActivity extends KanvasActivity implements WorkflowActiv
             ];
         }
 
-        $message = Message::fromApp($app)->where('id', $order->get('message_id'))->first();
-        if (! $message) {
-            return [
-                'message' => 'No message found',
-                'order' => $order->id,
-            ];
-        }
-        $orderSystemModule = SystemModulesRepository::getByModelName(Order::class);
-        (new CreateAppModuleMessageAction($message, $orderSystemModule, $order->getId()))->execute();
-
-        $order->metadata = array_merge(
-            $order->metadata,
-            ['message_id' => $message->getId(), 'message' => $message->message]
-        );
-        $order->saveOrFail();
-        $user = $order->user;
-
-        $newPurchaseMessageChannel = new CreateChannelAction(
-            new Channel(
-                apps: $app,
-                companies: $message->company,
-                users: $order->user,
-                entity_id: $user->getId(),
-                entity_namespace: Users::class,
-                name: 'Purchase Message',
-                description: 'Purchase Message Channel',
-                slug: 'PMC-' . $user->uuid
-            ),
-        );
-
-        $purchaseChannel = $newPurchaseMessageChannel->execute();
-        $purchaseChannel->addMessage($message, $user);
-
-        $user->set('purchase_channel', $purchaseChannel->uuid);
+        $defaultAppCompanyBranch = $app->get(AppSettingsEnums::GLOBAL_USER_REGISTRATION_ASSIGN_GLOBAL_COMPANY->getValue());
 
         try {
-            $messageInteractionService = new MessageInteractionService($message);
-            $messageInteractionService->purchase($user);
-        } catch (UniqueConstraintViolationException $e) {
-            $warning = [
-                'msg' => 'This order has been linked to a message and a channel',
-                'exception' => $e->getMessage(),
-            ];
+            $branch = CompaniesBranches::getById($defaultAppCompanyBranch);
+            $company = $branch->company;
+        } catch (ModelNotFoundException $e) {
+            $company = $order->company;
         }
 
-        return [
-            'order' => $order->id,
-            'message' => $message->id,
-            'channel' => $purchaseChannel->id,
-            'channel_name' => $purchaseChannel->name,
-            'warning' => $warning,
-        ];
+        return $this->executeIntegration(
+            entity: $order,
+            app: $app,
+            integration: IntegrationsEnum::PROMPT_MINE,
+            integrationOperation: function ($order) use ($app) {
+
+                $message = Message::fromApp($app)->where('id', $order->get('message_id'))->first();
+                if (! $message) {
+                    return [
+                        'message' => 'No message found',
+                        'order' => $order->id,
+                    ];
+                }
+                $orderSystemModule = SystemModulesRepository::getByModelName(Order::class);
+                (new CreateAppModuleMessageAction($message, $orderSystemModule, $order->getId()))->execute();
+
+                $order->metadata = array_merge(
+                    $order->metadata,
+                    ['message_id' => $message->getId(), 'message' => $message->message]
+                );
+                $order->saveOrFail();
+                $user = $order->user;
+
+                $newPurchaseMessageChannel = new CreateChannelAction(
+                    new Channel(
+                        apps: $app,
+                        companies: $message->company,
+                        users: $order->user,
+                        entity_id: $user->getId(),
+                        entity_namespace: Users::class,
+                        name: 'Purchase Message',
+                        description: 'Purchase Message Channel',
+                        slug: 'PMC-' . $user->uuid
+                    ),
+                );
+
+                $purchaseChannel = $newPurchaseMessageChannel->execute();
+                $purchaseChannel->addMessage($message, $user);
+
+                $user->set('purchase_channel', $purchaseChannel->uuid);
+
+                try {
+                    $messageInteractionService = new MessageInteractionService($message);
+                    $messageInteractionService->purchase($user);
+                } catch (UniqueConstraintViolationException $e) {
+                    $warning = [
+                        'msg' => 'This order has been linked to a message and a channel',
+                        'exception' => $e->getMessage(),
+                    ];
+                }
+                
+                return [
+                    'order' => $order->id,
+                    'message' => $message->id,
+                    'channel' => $purchaseChannel->id,
+                    'channel_name' => $purchaseChannel->name,
+                    'warning' => $warning,
+                ];
+            },
+            company: $company,
+        );
     }
 }
