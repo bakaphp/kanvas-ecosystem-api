9763368f5060065d6e2aaceb7aabea04c6026234
Maximo Castro Pou <kaioken@Maximos-MacBook-Pro-2.local>
2025-05-01 11:50:11 -0400

    feat: update product description

--- src/Domains/Inventory/Products/Observers/ProductsObserver.php
+++ src/Domains/Inventory/Products/Observers/ProductsObserver.php
@@ -8,24 +8,6 @@ use Kanvas\Inventory\Products\Models\Products;
 
 class ProductsObserver
 {
-    public function saving(Products $product): void
-    {
-        // If description is empty but html_description has content, update description
-        // with the stripped HTML content
-        if (empty($product->description) && ! empty($product->html_description)) {
-            $html = $product->html_description;
-            $html = str_replace(['</p>', '</div>', '<br>', '<br />'], "\n", $html);
-
-            $plainText = \Illuminate\Support\Str::of($html)
-                ->stripTags()
-                ->replaceMatches('/\n\s+\n/', "\n\n") // Normalize whitespace between paragraphs
-                ->replaceMatches('/[\r\n]{3,}/', "\n\n") // Limit consecutive line breaks
-                ->trim();
-
-            $product->description = (string) $plainText;
-        }
-    }
-
     public function saved(Products $product): void
     {
         if ($product->wasChanged('products_types_id') && $product->productsTypes()->exists()) {
