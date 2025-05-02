35318252b56ede698138cdb66b24b6dda15f3ec5
FredPeal <frederickpeal@gmail.com>
2025-05-02 16:33:27 -0400

    refactor: order has people and order_number

---
+++ app/GraphQL/Souk/Handlers/HasPeopleHandler.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+namespace App\GraphQL\Souk\Handlers;
+
+use Illuminate\Database\Eloquent\Model;
+use Kanvas\Guild\Customers\Models\People;
+use Nuwave\Lighthouse\WhereConditions\WhereConditionsHandler;
+
+final class HasPeopleHandler extends WhereConditionsHandler
+{
+    /**
+     * @param  array<string, mixed>  $whereConditions
+     */
+    public function __invoke(
+        object $builder,
+        array $whereConditions,
+        ?Model $model = null,
+        string $boolean = 'and',
+    ): void {
+        $peopleQuery = People::query();
+        if ($column = $whereConditions['column'] ?? null) {
+            $this->assertValidColumnReference($column);
+            $this->operator->applyConditions($peopleQuery, $whereConditions, $boolean);
+        }
+        // @to do: add nested where conditions
+        $peopleQuery->from((new People())->getFullTableName());
+
+        if (array_key_exists('AND', $whereConditions)) {
+            $this->nestedConditions($peopleQuery, $whereConditions['AND'][0], 'and');
+        } elseif (array_key_exists('OR', $whereConditions)) {
+            $this->nestedConditions($peopleQuery, $whereConditions['OR'][0], 'or');
+        }
+        $peopleQuery->whereColumn('id', 'orders.shipping_address_id');
+        $builder->whereExists($peopleQuery);
+    }
+}
