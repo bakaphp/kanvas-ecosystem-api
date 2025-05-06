95ec54c79f47a1db0c32f7b7fc87104555486f9b
Maximo Castro Pou <kaioken@Maximos-MacBook-Pro-2.local>
2025-05-06 17:30:41 -0400

    feat: iplus order issue

--- src/Domains/Connectors/IPlus/Workflows/Activities/SyncOrderWithIPlusActivities.php
+++ src/Domains/Connectors/IPlus/Workflows/Activities/SyncOrderWithIPlusActivities.php
@@ -6,6 +6,8 @@ namespace Kanvas\Connectors\IPlus\Workflows\Activities;
 
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Connectors\IPlus\Actions\SaveOrderToIPlusAction;
+use Kanvas\Connectors\IPlus\Actions\SavePeopleToIPlusAction;
+use Kanvas\Connectors\IPlus\Enums\CustomFieldEnum;
 use Kanvas\Souk\Orders\Models\Order;
 use Kanvas\Workflow\Enums\IntegrationsEnum;
 use Kanvas\Workflow\KanvasActivity;
@@ -21,13 +23,26 @@ class SyncOrderWithIPlusActivities extends KanvasActivity
             app: $app,
             integration: IntegrationsEnum::IPLUS,
             integrationOperation: function ($order, $app, $integrationCompany, $additionalParams) use ($params) {
-                $createOrder = new SaveOrderToIPlusAction($order);
-                $response = $createOrder->execute();
+                /*    $createOrder = new SaveOrderToIPlusAction($order);
+                   $response = $createOrder->execute(); */
+
+                if ($order->get(CustomFieldEnum::I_PLUS_CUSTOMER_ID->value)) {
+                    return [
+                        'status' => 'success',
+                        'message' => 'Order already synced with IPlus',
+                        'response' => $order->get(CustomFieldEnum::I_PLUS_CUSTOMER_ID->value),
+                    ];
+                }
+
+                $createPeopleAction = new SavePeopleToIPlusAction($order->people);
+                $responseClientId = $createPeopleAction->execute();
+
+                $order->set(CustomFieldEnum::I_PLUS_CUSTOMER_ID->value, $responseClientId);
 
                 return [
                     'status' => 'success',
-                    'message' => 'Order synced with IPlus',
-                    'response' => $response,
+                    'message' => 'Order synced with IPlus Customer',
+                    'response' => $responseClientId,
                 ];
             },
             company: $order->company,
