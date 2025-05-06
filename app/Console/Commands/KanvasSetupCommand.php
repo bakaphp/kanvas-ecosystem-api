4da1f0c1ddeebd4ba0bfa7637ce4378abe003cf6
FredPeal <frederickpeal@gmail.com>
2025-05-06 01:12:28 -0400

    fix: kanvas setup command

--- app/Console/Commands/KanvasSetupCommand.php
+++ app/Console/Commands/KanvasSetupCommand.php
@@ -55,6 +55,7 @@ class KanvasSetupCommand extends Command
             'kanvas:filesystem-setup',
             'kanvas:create-workflow-status',
             'kanvas:update-abilities',
+            'kanvas:update-abilities-templates',
         ];
 
         foreach ($commands as $command) {
