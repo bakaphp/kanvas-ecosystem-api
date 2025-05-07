82e517a3cb25049a1c0a232a8bd9ba984da6031d
Maximo Castro Pou <kaioken@Maximos-MacBook-Pro-2.local>
2025-05-07 12:00:52 -0400

    fix: inventory type mapping

--- src/Domains/Inventory/Attributes/DataTransferObject/Attributes.php
+++ src/Domains/Inventory/Attributes/DataTransferObject/Attributes.php
@@ -10,6 +10,7 @@ use Baka\Support\Str;
 use Baka\Users\Contracts\UserInterface;
 use Exception;
 use Kanvas\Companies\Models\Companies;
+use Kanvas\Enums\AppEnums;
 use Kanvas\Inventory\Attributes\Models\AttributesTypes as AttributesTypesModel;
 use Kanvas\Inventory\Attributes\Repositories\AttributesTypesRepository;
 use Spatie\LaravelData\Data;
@@ -34,7 +35,9 @@ class Attributes extends Data
         try {
             $attributeType = isset($request['attribute_type']['id']) ? AttributesTypesRepository::getById((int) $request['attribute_type']['id'], $user->getCurrentCompany(), $app) : null;
         } catch (Exception $e) {
-            $attributeType = AttributesTypesModel::query()->where('companies_id', 0)->where('apps_id', 0)->firstOrFail();
+            $attributeType = AttributesTypesModel::where('companies_id', AppEnums::GLOBAL_COMPANY_ID->getValue())
+                ->where('apps_id', AppEnums::LEGACY_APP_ID->getValue())
+                ->firstOrFail();
         }
 
         return new self(
