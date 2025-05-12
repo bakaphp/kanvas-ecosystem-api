220ba9d604aca14098a7a7bf0345ae83307e82bb
Rafael White <rwhite@mctekk.com>
2025-05-12 12:42:39 -0400

    refactor: streamline user app preferences saving logic in SaveUserAppPreferencesAction

--- src/Kanvas/Users/Actions/SaveUserAppPreferencesAction.php
+++ src/Kanvas/Users/Actions/SaveUserAppPreferencesAction.php
@@ -6,6 +6,7 @@ namespace Kanvas\Users\Actions;
 
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Users\Models\Users;
+use Kanvas\Users\Models\UserConfig;
 
 class SaveUserAppPreferencesAction
 {
@@ -13,18 +14,30 @@ class SaveUserAppPreferencesAction
         private Users $user,
         private Apps $app,
         private array $preferences = [],
-    ) {
-    }
+    ) {}
 
     public function execute(): void
     {
         // activeUserAppSettings is an array of keys that are allowed to be set, this makes sure that no upwanted settings are stored. This array of keys comes from the app setting in_app_user_settings_keys
         $activeUserAppSettings = $this->app->get('in_app_user_settings_keys');
+        $savedAppPreferences = [];
         foreach ($activeUserAppSettings as $setting) {
+            // check if the setting is in the preferences array, if not con
             if (! array_key_exists($setting, $this->preferences)) {
                 continue;
             }
-            $this->user->set($setting, $this->preferences[$setting]);
+            $savedAppPreferences[$setting] = $this->preferences[$setting];
         }
+
+        UserConfig::updateOrCreate(
+            [
+                'users_id' => $this->user->getId(),
+                'name' => 'user_app_' . $this->app->getId() . '_preferences',
+            ],
+            [
+                'value' => $savedAppPreferences,
+                'is_public' => 1,
+            ],
+        );
     }
 }
