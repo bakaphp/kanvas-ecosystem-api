d80bc3302ecd5b8ca14ecb54f73caea9f82ee00c
Rafael White <rwhite@mctekk.com>
2025-05-14 14:00:31 -0400

    refactor: remove eager loading of message children in recommendations query

--- src/Domains/Connectors/Recombee/Actions/GenerateRecommendForYourFeedAction.php
+++ src/Domains/Connectors/Recombee/Actions/GenerateRecommendForYourFeedAction.php
@@ -74,7 +74,6 @@ class GenerateRecommendForYourFeedAction
 
         $messageTypeId = $this->app->get('social-user-message-filter-message-type');
         $builder = Message::fromApp($this->app)
-            ->with('children')
             ->whereIn('id', $entityIds)
             ->where('is_public', 1)
             ->where('is_deleted', 0)
