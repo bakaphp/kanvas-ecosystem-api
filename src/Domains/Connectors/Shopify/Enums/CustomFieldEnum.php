c2f90d132946a30d2cc9991b8b0fa28d573a4db7
Maximo Castro Pou <kaioken@Maximos-MacBook-Pro-2.local>
2025-05-30 17:10:51 -0400

    feat: shopify compliance webhook

--- src/Domains/Connectors/Shopify/Enums/CustomFieldEnum.php
+++ src/Domains/Connectors/Shopify/Enums/CustomFieldEnum.php
@@ -8,7 +8,10 @@ enum CustomFieldEnum: string
 {
     case SHOPIFY_API_CREDENTIAL = 'SHOPIFY_CREDENTIAL';
     case SHOPIFY_API_KEY = 'SHOPIFY_API_KEY';
+    case SHOPIFY_API_KEY_V2 = 'shopify-api-key';
     case SHOPIFY_API_SECRET = 'SHOPIFY_API_SECRET';
+    case SHOPIFY_API_SECRET_V2 = 'shopify-api-secret';
+    case SHOPIFY_WEBHOOK_SECRET = 'shopify_webhook_secret';
     case SHOP_URL = 'SHOPIFY_HOSTNAME';
     case SHOPIFY_PRODUCT_ID = 'SHOPIFY_PRODUCT_ID';
     case SHOPIFY_VARIANT_ID = 'SHOPIFY_VARIANT_ID';
