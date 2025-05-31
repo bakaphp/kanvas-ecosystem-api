f8b82031c74026842c4ffe36531e2f436a5b99f4
Maximo Castro Pou <kaioken@Maximos-MacBook-Pro-2.local>
2025-05-30 22:28:03 -0400

    hotfix: unauthorize webhook

--- src/Domains/Workflow/Jobs/ProcessWebhookJob.php
+++ src/Domains/Workflow/Jobs/ProcessWebhookJob.php
@@ -14,10 +14,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Kanvas\Workflow\Models\ReceiverWebhook;
 use Kanvas\Workflow\Models\ReceiverWebhookCall;
+use Throwable;
 
 use function Sentry\captureException;
-
-use Throwable;
 
 abstract class ProcessWebhookJob implements ShouldQueue
 {
