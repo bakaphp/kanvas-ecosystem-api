25ef3c02dc68695b0c456060209a58da3061c1c1
FredPeal <frederickpeal@gmail.com>
2025-06-02 22:52:26 -0400

    refactor: check if is array

--- src/Domains/Inventory/Products/Models/Products.php
+++ src/Domains/Inventory/Products/Models/Products.php
@@ -465,12 +465,16 @@ class Products extends BaseModel implements EntityIntegrationInterface, EntityIm
 
         $attributes = $this->searchableAttributes();
         foreach ($attributes as $attribute) {
-            $product['attributes'][$attribute['name']] = (string)$attribute['value'];
+            $product['attributes'][$attribute['name']] = is_array($attribute['value'])
+                ? $attribute['value']
+                : (string) $attribute['value'];
         }
 
         $customFields = $this->getAllCustomFields();
         foreach ($customFields as $key => $value) {
-            $product['custom_fields'][$key] = (string)$value;
+            $product['custom_fields'][$key] = is_array($value)
+                ? $value
+                : (string) $value;
         }
 
         return $product;
