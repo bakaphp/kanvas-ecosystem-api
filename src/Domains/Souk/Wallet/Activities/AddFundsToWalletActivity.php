c9f79f0f9683b09b5b29b7940292606cf90a792d
Maximo Castro Pou <kaioken@Maximos-MacBook-Pro-2.local>
2025-06-05 13:46:28 -0400

    feat: add chat history to redis

--- src/Domains/Souk/Wallet/Activities/AddFundsToWalletActivity.php
+++ src/Domains/Souk/Wallet/Activities/AddFundsToWalletActivity.php
@@ -7,6 +7,7 @@ namespace Kanvas\Souk\Wallet\Activities;
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Souk\Orders\Models\Order;
 use Kanvas\Souk\Wallet\Actions\AddFundsToWalletAction;
+use Kanvas\Souk\Wallet\Enums\ConfigurationEnum;
 use Kanvas\Workflow\Enums\IntegrationsEnum;
 use Kanvas\Workflow\KanvasActivity;
 
@@ -15,6 +16,13 @@ class AddFundsToWalletActivity extends KanvasActivity
     public function execute(Order $order, Apps $app, array $params): array
     {
         $this->overwriteAppService($app);
+        $isAddingWalletFundTransaction = false;
+        foreach ($order->items as $item) {
+            if ($item->variant->getAttributeBySlug(ConfigurationEnum::PRODUCT_TYPE_WALLET_COIN_SLUG->value)?->value === null) {
+                continue;
+            }
+            $isAddingWalletFundTransaction = true;
+        }
 
         $userCompany = $order->getMetadata('user_company_id');
 
@@ -30,7 +38,15 @@ class AddFundsToWalletActivity extends KanvasActivity
             entity: $order,
             app: $app,
             integration: IntegrationsEnum::INTERNAL,
-            integrationOperation: function ($order, $app, $integrationCompany, $additionalParams) use ($params) {
+            integrationOperation: function ($order, $app, $integrationCompany, $additionalParams) use ($params, $isAddingWalletFundTransaction) {
+                if (! $isAddingWalletFundTransaction) {
+                    return [
+                        'result' => false,
+                        'message' => 'No wallet fund transaction found in order items.',
+                        'order_id' => $order->getId(),
+                    ];
+                }
+
                 return new AddFundsToWalletAction(
                     order: $order,
                 )->execute();
