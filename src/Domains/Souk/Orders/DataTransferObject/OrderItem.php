2ebf65e7e59df9456e17376ebc46c7479153032f
arfenis <arfen00@gmail.com>
2025-06-10 19:04:11 -0400

    use the getbyid exclude company method

--- src/Domains/Souk/Orders/DataTransferObject/OrderItem.php
+++ src/Domains/Souk/Orders/DataTransferObject/OrderItem.php
@@ -8,8 +8,9 @@ use Baka\Contracts\AppInterface;
 use Baka\Contracts\CompanyInterface;
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Currencies\Models\Currencies;
-use Kanvas\Inventory\Regions\Models\Regions;
 use Kanvas\Inventory\Variants\Models\Variants;
+use Kanvas\Regions\Models\Regions;
+use Kanvas\Souk\Enums\ConfigurationEnum;
 use Spatie\LaravelData\Data;
 
 class OrderItem extends Data
@@ -30,7 +31,12 @@ class OrderItem extends Data
 
     public static function viaRequest(AppInterface $app, CompanyInterface $company, Regions $region, array $request): self
     {
-        $variant = Variants::getByIdFromCompanyApp($request['variant_id'], $company, $app);
+        if ($app->get(ConfigurationEnum::B2B_GLOBAL_COMPANY->value)) {
+            $variant = Variants::getById($request['variant_id'], $app);
+        } else {
+            $variant = Variants::getByIdFromCompanyApp($request['variant_id'], $company, $app);
+        }
+
         $warehouse = $region->warehouses()->firstOrFail(); //@todo get product warehouse with  stock
         $price = $variant->getPrice($warehouse);
 
