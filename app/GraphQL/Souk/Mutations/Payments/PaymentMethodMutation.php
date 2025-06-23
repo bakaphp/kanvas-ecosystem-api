70ef382578eced537e4744d352dfe4a68b186190
freesgen <jesusant.guerrero@gmail.com>
2025-06-23 17:16:43 -0400

    fix: update portal fixes

--- app/GraphQL/Souk/Mutations/Payments/PaymentMethodMutation.php
+++ app/GraphQL/Souk/Mutations/Payments/PaymentMethodMutation.php
@@ -25,6 +25,8 @@ class PaymentMethodMutation
             // TODO: move this to a provider to avoid hardcoding here
             if ($input['processor']) {
                 $processor = app("payment.{$input['processor']}");
+                $input['brand'] = $this->guessCardBrand($input['number']);
+                $input['state'] = $input['country'] == 'DO' ? 'DN' : $input['state'];
                 $paymentMethod = $processor->addCardFromRequest($input, $user);
             } else {
                 $paymentMethod = new PaymentMethod(
