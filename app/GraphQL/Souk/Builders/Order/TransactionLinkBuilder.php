723f743d29b6c1d4afb8a9c228e0ca75ee5e9b9b
FredPeal <frederickpeal@gmail.com>
2025-06-23 18:55:59 -0400

    refactor: transaction stripe link

---
+++ app/GraphQL/Souk/Builders/Order/TransactionLinkBuilder.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Souk\Builders\Order;
+
+use Kanvas\Souk\Orders\Models\Order;
+
+class TransactionLinkBuilder
+{
+    // @todo: create order links by provider, currently is just for stripe
+    public function getLink(Order $order): string
+    {
+        $url = 'https://dashboard.stripe.com';
+        if (config('app.env') != 'production') {
+            $url .= '/test';
+        }
+        if ($order->checkout_token && strlen($order->checkout_token)) {
+            $paymentIntentId = explode('_secret_', $order->checkout_token)[0];
+            $url .= "/payments/$paymentIntentId";
+        }
+        return $url;
+    }
+}
