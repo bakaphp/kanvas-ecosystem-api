81a4d3d5e70ba1bab86cd69b70f3de7d6612779b
arfenis <arfen00@gmail.com>
2025-07-04 18:36:48 -0400

    Use base Order creation instead of from cart

--- src/Domains/Souk/Orders/Actions/CreateOrderFromCartAction.php
+++ src/Domains/Souk/Orders/Actions/CreateOrderFromCartAction.php
@@ -4,45 +4,12 @@ declare(strict_types=1);
 
 namespace Kanvas\Souk\Orders\Actions;
 
-use Baka\Contracts\AppInterface;
-use Baka\Support\Str;
-use Baka\Users\Contracts\UserInterface;
-use Joelwmale\Cart\Cart;
-use Kanvas\Apps\Models\Apps;
-use Kanvas\Companies\Models\Companies;
 use Kanvas\Connectors\Stripe\Services\StripePaymentService;
-use Kanvas\Currencies\Models\Currencies;
 use Kanvas\Exceptions\ValidationException;
-use Kanvas\Guild\Customers\DataTransferObject\Address;
-use Kanvas\Guild\Customers\Enums\AddressTypeEnum;
-use Kanvas\Guild\Customers\Models\AddressType;
-use Kanvas\Guild\Customers\Models\People;
-use Kanvas\Inventory\Variants\Models\Variants;
-use Kanvas\Regions\Models\Regions;
-use Kanvas\Souk\Orders\DataTransferObject\Order;
-use Kanvas\Souk\Orders\DataTransferObject\OrderCustomer;
-use Kanvas\Souk\Orders\DataTransferObject\OrderItem;
 use Kanvas\Souk\Orders\Models\Order as ModelsOrder;
-use Kanvas\Souk\Payments\DataTransferObject\CreditCardBilling;
-use Kanvas\Users\Actions\SendUserNotificationAction;
-use Spatie\LaravelData\DataCollection;
 
-class CreateOrderFromCartAction
+class CreateOrderFromCartAction extends CreateBaseOrderAction
 {
-    public function __construct(
-        protected Cart $cart,
-        protected Companies $company,
-        protected Regions $region,
-        protected OrderCustomer $orderCustomer,
-        protected People $people,
-        protected UserInterface $user,
-        protected Apps $app,
-        protected ?CreditCardBilling $billingAddress,
-        protected ?Address $shippingAddress,
-        protected ?array $request,
-        protected ?ModelsOrder $parent = null,
-    ) {
-    }
 
     public function execute(): ModelsOrder
     {
@@ -61,126 +28,8 @@ class CreateOrderFromCartAction
             throw new ValidationException($validation['error'], $validation['status']);
         }
 
-        if ($this->billingAddress !== null) {
-            $billing = $this->people->addAddress(new Address(
-                address: $this->billingAddress->address,
-                address_2: $this->billingAddress->address2,
-                city: $this->billingAddress->city,
-                state: $this->billingAddress->state,
-                country: $this->billingAddress->country,
-                zip: $this->billingAddress->zip,
-                address_type_id: AddressType::getByName(AddressTypeEnum::BILLING->value, $this->app)->getId()
-            ));
-        }
-
-        if ($this->shippingAddress !== null) {
-            $shipping = $this->people->addAddress(new Address(
-                address: $this->shippingAddress->address,
-                address_2: $this->shippingAddress->address_2,
-                city: $this->shippingAddress->city,
-                state: $this->shippingAddress->state,
-                country: $this->shippingAddress->country,
-                zip: $this->shippingAddress->zip,
-                address_type_id: AddressType::getByName(AddressTypeEnum::SHIPPING->value, $this->app)->getId()
-            ));
-        }
-
-        $hasItemsInCart = ! $this->cart->isEmpty(); //&& $this->cart->getTotal() > 0;
-        if ($hasItemsInCart) {
-            $total = $this->cart->getTotal();
-            $totalTax = ($this->cart->getTotal()) - ($this->cart->getSubTotal());
-            $totalDiscount = 0.0;
-            $lineItems = $this->cart->getContent()->toArray();
-        } else {
-            $total = 0;
-            $totalTax = 0;
-            $totalDiscount = 0;
-            $lineItems = [];
-
-            foreach ($this->request['input']['items'] as $key => $lineItem) {
-                $lineItems[$key] = OrderItem::viaRequest($this->app, $this->company, $this->region, $lineItem);
-                $total += $lineItems[$key]->getTotal();
-                $totalTax += $lineItems[$key]->getTotalTax();
-                $totalDiscount = $lineItems[$key]->getTotalDiscount();
-            }
-
-            $lineItems = OrderItem::collect($lineItems, DataCollection::class);
-        }
-
-        $items = $hasItemsInCart ? $this->getOrderItems($lineItems, $this->app) : $lineItems;
-
-        $order = new Order(
-            app: $this->app,
-            region: $this->region,
-            company: $this->company,
-            people: $this->people,
-            user: $this->user ?? $this->company->user,
-            email: $this->orderCustomer->email,
-            phone: $this->orderCustomer->phone,
-            token: Str::random(32),
-            shippingAddress: $shipping ?? null,
-            billingAddress: $billing ?? null,
-            total: (float) $total,
-            taxes: (float) $totalTax,
-            totalDiscount: $totalDiscount,
-            totalShipping: 0.0,
-            status: 'completed',
-            orderNumber: '',
-            shippingMethod: null,
-            currency: $this->region->currency,
-            fulfillmentStatus: 'pending',
-            items: $items,
-            orderType: $this->request['input']['order_type'] ?? null,
-            metadata: $this->request['input']['metadata'] ?? [],
-            weight: 0.0,
-            checkoutToken: '',
-            paymentGatewayName: ['manual'],
-            languageCode: null,
-            reference: $this->request['input']['reference'] ?? '',
-            paymentStatus: 'unpaid',
-            parent: $this->parent,
-        );
-
-        $order = (new CreateOrderAction($order))->execute();
-
-        //@todo remove this we already have it on create order action
-        new SendUserNotificationAction(
-            $order->app,
-            $this->company,
-            $order->user
-        )->execute('admin-new-order', $order->toArray());
-
-        $this->cart->clear();
+        $order = parent::execute();
 
         return $order;
-    }
-
-    protected function getOrderItems(array $cartContent, AppInterface $app): DataCollection
-    {
-        $orderItems = [];
-
-        foreach ($cartContent as $lineItem) {
-            $variant = Variants::getById($lineItem['id']);
-
-            //this shouldn't happen but just in case
-            if (! $variant) {
-                continue;
-            }
-
-            $orderItems[] = new OrderItem(
-                app: $app,
-                variant: $variant,
-                name: (string) $lineItem['name'],
-                sku: (string) ($variant->sku ?? $lineItem['id']),
-                quantity: (float) $lineItem['quantity'],
-                price: (float) $lineItem['price'],
-                tax: (float) ($lineItem['tax'] ?? 0),
-                discount: (float) ($lineItem['total_discount'] ?? 0),
-                currency: Currencies::getByCode('USD'),
-                quantityShipped: 0
-            );
-        }
-
-        return OrderItem::collect($orderItems, DataCollection::class);
     }
 }
