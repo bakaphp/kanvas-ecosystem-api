250019f2dd81bfa3900b006891848b866b1381c0
Maximo Castro Pou <max@mctekk.com>
2025-07-07 10:15:49 -0400

    fix: message type

--- src/Domains/Social/Messages/Observers/MessageObserver.php
+++ src/Domains/Social/Messages/Observers/MessageObserver.php
@@ -7,7 +7,6 @@ namespace Kanvas\Social\Messages\Observers;
 use Kanvas\Social\Messages\Actions\CheckMessagePostLimitAction;
 use Kanvas\Social\Messages\Models\Message;
 use Kanvas\Social\Messages\Validations\MessageSchemaValidator;
-use Kanvas\Social\MessagesTypes\Repositories\MessagesTypesRepository;
 use Kanvas\Workflow\Enums\WorkflowEnum;
 
 class MessageObserver
