ecafe97d263276a9cd3bd3d734166f7bacfeb901
arfenis <arfen00@gmail.com>
2025-07-08 10:17:45 -0400

    Add non payment flag validation

--- src/Domains/Souk/Orders/Actions/CreateOrderFromCartAction.php
+++ src/Domains/Souk/Orders/Actions/CreateOrderFromCartAction.php
@@ -6,25 +6,28 @@ namespace Kanvas\Souk\Orders\Actions;
 
 use Kanvas\Connectors\Stripe\Services\StripePaymentService;
 use Kanvas\Exceptions\ValidationException;
+use Kanvas\Souk\Enums\ConfigurationEnum;
 use Kanvas\Souk\Orders\Models\Order as ModelsOrder;
 
 class CreateOrderFromCartAction extends CreateBaseOrderAction
 {
     public function execute(): ModelsOrder
     {
-        $paymentIntentId = $this->request['input']['metadata']['paymentIntent']['client_secret']
-            ?? $this->request['payment_intent_id']
-            ?? null;
+        if (!$this->app->get(ConfigurationEnum::ALLOW_NO_PAYMENT_ORDER->value)) {
+            $paymentIntentId = $this->request['input']['metadata']['paymentIntent']['client_secret']
+                ?? $this->request['payment_intent_id']
+                ?? null;
 
-        if (! $paymentIntentId) {
-            throw new ValidationException('Payment Intent not provided');
-        }
-        $stripe = new StripePaymentService($this->app);
-        $stripeIntent = $this->request['input']['metadata']['paymentIntent']['client_secret'];
-        $validation = $stripe->validatePaymentIntent($stripeIntent);
+            if (! $paymentIntentId) {
+                throw new ValidationException('Payment Intent not provided');
+            }
+            $stripe = new StripePaymentService($this->app);
+            $stripeIntent = $this->request['input']['metadata']['paymentIntent']['client_secret'];
+            $validation = $stripe->validatePaymentIntent($stripeIntent);
 
-        if (! $validation['valid']) {
-            throw new ValidationException($validation['error'], $validation['status']);
+            if (! $validation['valid']) {
+                throw new ValidationException($validation['error'], $validation['status']);
+            }
         }
 
         $order = parent::execute();
