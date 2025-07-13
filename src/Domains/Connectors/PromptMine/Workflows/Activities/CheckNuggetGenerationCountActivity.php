d685305cbac5d92a4af3a604b49917cf97f611cc
Rafael White <rwhite@mctekk.com>
2025-07-13 18:38:16 -0400

    refactor: improve company retrieval and integration operation in CheckNuggetGenerationCountActivity

--- src/Domains/Connectors/PromptMine/Workflows/Activities/CheckNuggetGenerationCountActivity.php
+++ src/Domains/Connectors/PromptMine/Workflows/Activities/CheckNuggetGenerationCountActivity.php
@@ -9,6 +9,11 @@ use Kanvas\Connectors\PromptMine\Actions\CheckNuggetGenerationCountAction;
 use Kanvas\Social\Messages\Models\Message;
 use Kanvas\Workflow\Enums\IntegrationsEnum;
 use Kanvas\Workflow\KanvasActivity;
+use Kanvas\Exceptions\ModelNotFoundException;
+use Illuminate\Database\Eloquent\Model;
+use Kanvas\Enums\AppSettingsEnums;
+use Kanvas\Companies\Models\CompaniesBranches;
+use Kanvas\Companies\Models\Companies;
 
 class CheckNuggetGenerationCountActivity extends KanvasActivity
 {
@@ -18,10 +23,13 @@ class CheckNuggetGenerationCountActivity extends KanvasActivity
     {
         $this->overwriteAppService($app);
 
+        $company = $this->getCompany($app, $message->company);
+
         if (! $app->get('check-free-generation-count') || ! $app->get('free-generation-check-message-type') || $message->parent_id === null) {
             return [
                 'result' => true,
                 'message' => 'Free generation check is not enabled or message does not have a parent',
+                'message_id' => $message->getId()
             ];
         }
 
@@ -29,15 +37,32 @@ class CheckNuggetGenerationCountActivity extends KanvasActivity
             entity: $message,
             app: $app,
             integration: IntegrationsEnum::PROMPT_MINE,
-            integrationOperation: function ($message, $app, $integrationCompany, $additionalParams) {
+            integrationOperation: function ($message) {
                 $checkNuggetGeneration = (new CheckNuggetGenerationCountAction($message))->execute();
 
                 return [
+                    'message_id' => $message->getId(),
                     'message' => 'Nugget generation count check completed successfully',
                     'result' => $checkNuggetGeneration,
                 ];
             },
-            company: $message->company,
+            company: $company,
         );
+    }
+
+    /**
+     * Get the company for this workflow
+     */
+    protected function getCompany(AppInterface $app, Model $entity): Companies
+    {
+        $defaultAppCompanyBranch = $app->get(AppSettingsEnums::GLOBAL_USER_REGISTRATION_ASSIGN_GLOBAL_COMPANY->getValue());
+
+        try {
+            $branch = CompaniesBranches::getById($defaultAppCompanyBranch);
+
+            return $branch->company;
+        } catch (ModelNotFoundException $e) {
+            return $entity->company;
+        }
     }
 }
