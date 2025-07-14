6e072b637d7f6919907bc33d918456e991b43eeb
Maximo Castro Pou <max@mctekk.com>
2025-07-14 15:19:44 -0400

    refact: payment intent

--- src/Domains/Souk/Orders/Actions/CreateOrderFromCartAction.php
+++ src/Domains/Souk/Orders/Actions/CreateOrderFromCartAction.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Kanvas\Souk\Orders\Actions;
 
+use Baka\Support\Str;
 use Kanvas\Connectors\Stripe\Services\StripePaymentService;
 use Kanvas\Exceptions\ValidationException;
 use Kanvas\Souk\Enums\ConfigurationEnum;
@@ -16,9 +17,17 @@ class CreateOrderFromCartAction extends CreateBaseOrderAction
     public function execute(): ModelsOrder
     {
         if (! $this->app->get(ConfigurationEnum::ALLOW_NO_PAYMENT_ORDER->value)) {
-            $paymentIntentId = $this->request['input']['metadata']['paymentIntent']['client_secret']
+            $paymentIntentId = $this->request['input']['metadata']['paymentIntent']['id']
                 ?? $this->request['input']['payment_intent_id']
+                ?? $this->request['input']['metadata']['paymentIntent']['client_secret'] //remove later
                 ?? null;
+
+            /**
+             * @todo remove later
+             */
+            if (Str::contains($paymentIntentId, '_secret_')) {
+                $paymentIntentId = explode('_secret_', $paymentIntentId ?? '')[0]; // Gets "pi_3RAClYDdrFkcUBzl0vNHHnFD"
+            }
 
             if (! $paymentIntentId) {
                 throw new ValidationException('Payment Intent not provided');
