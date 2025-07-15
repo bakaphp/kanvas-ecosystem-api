f302d027333e8e232274f89ea5d61dd08acd3d6e
Maximo Castro Pou <max@mctekk.com>
2025-07-15 09:06:12 -0400

    hotfix: lead channel description

--- src/Domains/Guild/Leads/Observers/LeadObserver.php
+++ src/Domains/Guild/Leads/Observers/LeadObserver.php
@@ -77,7 +77,7 @@ class LeadObserver
                         (string)$lead->getKey(),
                         Lead::class,
                         'Default Channel',
-                        $lead->description ?? '',
+                        $lead->description ?? $lead->uuid->toString(),
                         $lead->uuid->toString()
                     )
                 )
