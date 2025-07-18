bdb4540a17862663bb1e517e3249d69fa6402d7e
Maximo Castro Pou <max@mctekk.com>
2025-07-18 08:45:52 -0400

    feat: upload people lead

--- tests/GraphQL/Souk/ImportOrderItemsCsvTest.php
+++ tests/GraphQL/Souk/ImportOrderItemsCsvTest.php
@@ -257,13 +257,9 @@ class ImportOrderItemsCsvTest extends TestCase
         ];
 
         $response = $this->multipartGraphQL($operations, $map, $file);
-        $response->assertJson([
-            'data' => [
-                'importOrderCsv' => [
-                    'message' => "Not enough stock for product {$variantResponse['name']} ({$variantResponse['ean']}), Not enough stock for product {$variantResponse2['name']} ({$variantResponse2['ean']})",
-                    'status' => 'error',
-                ],
-            ],
+        $response->assertJsonFragment([
+            'message' => "Not enough stock for product {$variantResponse2['name']}",
+            'status' => 'error',
         ]);
     }
 
