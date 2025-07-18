f27c0209b7f71364170d3181daa5e4abfec6053c
arfenis <arfen00@gmail.com>
2025-07-18 17:32:55 -0400

    add new query for 2025 version

--- app/GraphQL/Ecosystem/Queries/ActivityLog/ActivityLogQuery.php
+++ app/GraphQL/Ecosystem/Queries/ActivityLog/ActivityLogQuery.php
@@ -7,10 +7,28 @@ namespace App\GraphQL\Ecosystem\Queries\ActivityLog;
 use Illuminate\Database\Eloquent\Builder;
 use Kanvas\Activities\Models\Activity as ModelsActivity;
 use Kanvas\Apps\Models\Apps;
+use Kanvas\SystemModules\Models\SystemModules;
+use Spatie\Activitylog\Models\Activity;
 
 class ActivityLogQuery
 {
     public function getAllActivityLogs(mixed $root, array $query): Builder
+    {
+        $user = auth()->user();
+        $company = $user->getCurrentCompany();
+        $app = app(Apps::class);
+
+        $systemModules = SystemModules::getByUuid($query['system_module_uuid'], $app);
+        $module = $systemModules->model_name;
+
+        $entity = $module::getById($query['entity_id'], $app);
+
+        return Activity::query()
+            ->where('subject_type', $module)
+            ->where('subject_id', $entity->getKey());
+    }
+
+    public function getAllAppCompanyActivityLogs(mixed $root, array $query): Builder
     {
         $user = auth()->user();
         $company = $user->getCurrentCompany();
