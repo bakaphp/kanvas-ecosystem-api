b9d21475d39357072f5d65480846d089daf0a97f
Maximo Castro Pou <max@mctekk.com>
2025-07-28 12:01:04 -0400

    feat: variant discount price

--- src/Domains/Inventory/Enums/AppEnums.php
+++ src/Domains/Inventory/Enums/AppEnums.php
@@ -10,15 +10,15 @@ enum AppEnums implements EnumsInterface
 {
     case PRODUCT_VARIANTS_SEARCH_INDEX;
     case PRODUCT_SEARCH_INDEX;
+    case CAN_USE_COMMERCE_DISCOUNT_PRICE
 
-    /**
-     * Get value.
-     */
+    #[Override]
     public function getValue(): mixed
     {
         return match ($this) {
             self::PRODUCT_VARIANTS_SEARCH_INDEX => 'products_variants_company_',
             self::PRODUCT_SEARCH_INDEX => 'products_company_',
+            self::CAN_USE_COMMERCE_DISCOUNT_PRICE => 'can_use_commerce_discount_price',
         };
     }
 }
