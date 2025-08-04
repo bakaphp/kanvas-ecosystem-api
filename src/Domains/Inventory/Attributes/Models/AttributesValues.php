c0958039a85d9bb04911f5cc37755b28f69394d3
arfenis <arfen00@gmail.com>
2025-08-04 14:49:26 -0400

    method to find or create translate values

--- src/Domains/Inventory/Attributes/Models/AttributesValues.php
+++ src/Domains/Inventory/Attributes/Models/AttributesValues.php
@@ -9,6 +9,7 @@ use Baka\Traits\NoCompanyRelationshipTrait;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Kanvas\Inventory\Models\BaseModel;
 use Kanvas\Languages\Traits\HasTranslationsDefaultFallback;
+use Nevadskiy\Tree\AsTree;
 
 /**
  * Class Attributes.
@@ -19,6 +20,7 @@ use Kanvas\Languages\Traits\HasTranslationsDefaultFallback;
  */
 class AttributesValues extends BaseModel
 {
+    use AsTree;
     use NoAppRelationshipTrait;
     use NoCompanyRelationshipTrait;
     use HasTranslationsDefaultFallback;
@@ -33,5 +35,31 @@ class AttributesValues extends BaseModel
     public function attribute(): BelongsTo
     {
         return $this->belongsTo(Attributes::class, 'attributes_id');
+    }
+
+    /**
+     * Find or create for translatable models
+     */
+    public static function firstOrNewTranslatable(array $attributes, string $translatableField, $translatableValue, $locale = null)
+    {
+        $locale = $locale ?? app()->getLocale();
+
+        $query = static::where($attributes)
+            ->where("{$translatableField}->{$locale}", $translatableValue);
+
+        $instance = $query->first();
+
+        if (!$instance) {
+            $instance = new static($attributes);
+            $instance->setTranslation($translatableField, $locale, $translatableValue);
+        }
+
+        return $instance;
+    }
+
+    public function setHasChildren(bool $value): void
+    {
+        $this->has_children = $value;
+        $this->saveOrFail();
     }
 }
