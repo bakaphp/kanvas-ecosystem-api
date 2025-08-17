4a06d153c0f26a140987fd27c1f1250181b56e59
Maximo Castro Pou <max@mctekk.com>
2025-08-17 18:31:07 -0400

    feat: expo notification channel

--- src/Kanvas/Notifications/Enums/NotificationChannelEnum.php
+++ src/Kanvas/Notifications/Enums/NotificationChannelEnum.php
@@ -7,6 +7,7 @@ namespace Kanvas\Notifications\Enums;
 use Kanvas\Exceptions\ValidationException;
 use Kanvas\Notifications\Channels\KanvasDatabase;
 use Kanvas\Notifications\Channels\OneSignalNotificationChannel;
+use NotificationChannels\Expo\ExpoChannel;
 
 enum NotificationChannelEnum: int
 {
@@ -15,6 +16,7 @@ enum NotificationChannelEnum: int
     case DATABASE = 3;
     case REALTIME = 4;
     case SMS = 5;
+    case EXPO = 6;
 
     public static function getIdFromString(string $channel): ?int
     {
@@ -24,6 +26,7 @@ enum NotificationChannelEnum: int
             'REALTIME' => self::REALTIME->value,
             'SMS' => self::SMS->value,
             'DATABASE' => self::DATABASE->value,
+            'EXPO' => self::EXPO->value,
             default => throw new ValidationException('Invalid channel ' . $channel),
         };
     }
@@ -34,6 +37,7 @@ enum NotificationChannelEnum: int
             'EMAIL' => 'mail',
             'MAIL' => 'mail',
             'PUSH' => OneSignalNotificationChannel::class,
+            'EXPO' => ExpoChannel::class,
             'DATABASE' => KanvasDatabase::class,
             default => throw new ValidationException('Invalid channel ' . $slug),
         };
@@ -45,6 +49,7 @@ enum NotificationChannelEnum: int
             'mail' => self::MAIL->value,
             'push' => self::PUSH->value,
             OneSignalNotificationChannel::class => self::PUSH->value,
+            ExpoChannel::class => self::EXPO->value,
             KanvasDatabase::class => self::DATABASE->value,
             'database' => self::DATABASE->value,
             default => throw new ValidationException('Invalid channel ' . $class),
