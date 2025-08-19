9cda7302ae7223be455f376e79853423eb8a55b5
FredPeal <frederickpeal@gmail.com>
2025-08-19 04:39:59 -0400

    refactor: search job

---
+++ src/Domains/Connectors/ScrapingDog/Jobs/SearchJob.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Connectors\ScrapingDog\Jobs;
+
+use Baka\Traits\KanvasJobsTrait;
+use Illuminate\Support\Facades\Cache;
+use Kanvas\Companies\Models\CompaniesBranches;
+use Kanvas\Connectors\ScrapingDog\Enums\ConfigEnum;
+use Kanvas\Connectors\ScrapingDog\Repositories\ScrapingDogRepository;
+use Kanvas\Inventory\Channels\Models\Channels;
+use Kanvas\Inventory\Warehouses\Models\Warehouses;
+use Kanvas\Workflow\Jobs\ProcessWebhookJob;
+use Override;
+
+class SearchJob extends ProcessWebhookJob
+{
+    use KanvasJobsTrait;
+    public Channels $channel;
+    public Warehouses $warehouse;
+    public CompaniesBranches $companiesBranches;
+
+    #[Override]
+    public function execute(): array
+    {
+        $search = $this->webhookRequest->payload['search'] ?? '';
+
+        $repository = new ScrapingDogRepository($this->receiver->app);
+
+        $ttl = $this->receiver->app->get(ConfigEnum::TTL_SEARCH->value);
+        $key = $search . ':' . $this->receiver->app->getId();
+
+        return Cache::remember($key, $ttl, function () use ($repository, $search) {
+            return $repository->getSearch($search)['results'];
+        });
+    }
+}
