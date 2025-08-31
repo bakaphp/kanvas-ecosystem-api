b22dae4eaeccc3de85f97d4d8e1254b0f51c6541
Maximo Castro Pou <max@mctekk.com>
2025-08-31 10:57:26 -0400

    refact: add wallet actions

--- src/Domains/Souk/Wallet/Actions/AddFundsToCompanyWalletAction.php
+++ src/Domains/Souk/Wallet/Actions/AddFundsToCompanyWalletAction.php
@@ -6,27 +6,17 @@ namespace Kanvas\Souk\Wallet\Actions;
 
 use Bavix\Wallet\Models\Transaction;
 use Exception;
+use Illuminate\Database\Eloquent\Model;
 use Kanvas\Companies\Models\Companies;
-use Kanvas\Souk\Orders\Models\Order;
-use Kanvas\Souk\Wallet\Enums\ConfigurationEnum;
 use Kanvas\Users\Repositories\UsersRepository;
+use Override;
 
-class AddFundsToCompanyWalletAction
+class AddFundsToCompanyWalletAction extends AddFundsToWalletActionBase
 {
-    public function __construct(
-        protected Order $order,
-    ) {
-    }
-
+    #[Override]
     public function execute(): Transaction
     {
-        $userCompany = $this->order->getMetadata('user_company_id');
-        if (! $userCompany) {
-            throw new Exception('User company not found in order metadata.');
-        }
-
-        ///$company = Companies::getById($userCompany); hotfix while we figure it out
-        $company = $this->order->user->getCurrentCompany();
+        $company = $this->getCompany();
 
         UsersRepository::belongsToThisApp(
             $this->order->user,
@@ -34,34 +24,26 @@ class AddFundsToCompanyWalletAction
             $company
         );
 
-        $tag = ConfigurationEnum::WALLET_DEFAULT_NAME->value;
-        $wallet = $company->createAppWallet($this->order->app, ['name' => $tag]);
-        $total = 0;
-        foreach ($this->order->items as $item) {
-            if ($item->variant->getAttributeBySlug(ConfigurationEnum::PRODUCT_TYPE_WALLET_COIN_SLUG->value)?->value === null) {
-                continue;
-            }
+        return $this->processTransaction();
+    }
 
-            $total += (float) ($item->variant->getAttributeBySlug(ConfigurationEnum::PRODUCT_TYPE_WALLET_COIN_AMOUNT->value)?->value ?? $item->getPrice());
+    #[Override]
+    protected function getWalletHolder(): Model
+    {
+        return $this->getCompany();
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function getCompany(): Companies
+    {
+        $userCompany = $this->order->getMetadata('user_company_id');
+        if (! $userCompany) {
+            throw new Exception('User company not found in order metadata.');
         }
-        if ($total <= 0) {
-            throw new Exception('Total amount to deposit must be greater than zero.');
-        }
 
-        $transaction = $wallet->depositFloat($total);
-        $transaction->meta = [
-            'order_id' => $this->order->getId(),
-            'variants' => $this->order->items->map(function ($item) {
-                return [
-                    'id' => $item->variant->getId(),
-                    'name' => $item->variant->name,
-                    'price' => $item->getPrice(),
-                    'quantity' => $item->quantity,
-                ];
-            })->toArray(),
-        ];
-        $transaction->saveOrFail();
-
-        return $transaction;
+        ///$company = Companies::getById($userCompany); hotfix while we figure it out
+        return $this->order->user->getCurrentCompany();
     }
 }
--- src/Domains/Souk/Wallet/Actions/AddFundsToUserWalletAction.php
+++ src/Domains/Souk/Wallet/Actions/AddFundsToUserWalletAction.php
@@ -5,49 +5,20 @@ declare(strict_types=1);
 namespace Kanvas\Souk\Wallet\Actions;
 
 use Bavix\Wallet\Models\Transaction;
-use Exception;
-use Kanvas\Souk\Orders\Models\Order;
-use Kanvas\Souk\Wallet\Enums\ConfigurationEnum;
+use Illuminate\Database\Eloquent\Model;
+use Override;
 
-class AddFundsToUserWalletAction
+class AddFundsToUserWalletAction extends AddFundsToWalletActionBase
 {
-    public function __construct(
-        protected Order $order,
-    ) {
-    }
-
+    #[Override]
     public function execute(): Transaction
     {
-        $user = $this->order->user;
+        return $this->processTransaction();
+    }
 
-        $tag = ConfigurationEnum::WALLET_DEFAULT_NAME->value;
-        $wallet = $user->createAppWallet($this->order->app, ['name' => $tag]);
-        $total = 0;
-        foreach ($this->order->items as $item) {
-            if ($item->variant->getAttributeBySlug(ConfigurationEnum::PRODUCT_TYPE_WALLET_COIN_SLUG->value)?->value === null) {
-                continue;
-            }
-
-            $total += (float) ($item->variant->getAttributeBySlug(ConfigurationEnum::PRODUCT_TYPE_WALLET_COIN_AMOUNT->value)?->value ?? $item->getPrice());
-        }
-        if ($total <= 0) {
-            throw new Exception('Total amount to deposit must be greater than zero.');
-        }
-
-        $transaction = $wallet->depositFloat($total);
-        $transaction->meta = [
-            'order_id' => $this->order->getId(),
-            'variants' => $this->order->items->map(function ($item) {
-                return [
-                    'id' => $item->variant->getId(),
-                    'name' => $item->variant->name,
-                    'price' => $item->getPrice(),
-                    'quantity' => $item->quantity,
-                ];
-            })->toArray(),
-        ];
-        $transaction->saveOrFail();
-
-        return $transaction;
+    #[Override]
+    protected function getWalletHolder(): Model
+    {
+        return $this->order->user;
     }
 }
