3eee2c25d94c3aad1c8d08f0a48fe6af44943558
Rafael White <rwhite@mctekk.com>
2025-09-01 16:43:54 -0400

    fix format

--- app/Console/Commands/Connectors/PromptMine/ChangeMediaUrlCommand.php
+++ app/Console/Commands/Connectors/PromptMine/ChangeMediaUrlCommand.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace App\Console\Commands\Connectors\PromptMine;
 
+use finfo;
 use Illuminate\Console\Command;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Companies\Models\Companies;
-use Kanvas\Social\Messages\Models\Message;
-use Kanvas\Social\MessagesTypes\Models\MessageType;
 use Kanvas\Filesystem\Services\FilesystemServices;
 use Kanvas\Filesystem\Services\ImageOptimizerService;
-use Illuminate\Http\UploadedFile;
-use finfo;
+use Kanvas\Social\Messages\Models\Message;
+use Kanvas\Social\MessagesTypes\Models\MessageType;
 use Kanvas\Users\Models\Users;
 
 class ChangeMediaUrlCommand extends Command
@@ -63,16 +63,16 @@ class ChangeMediaUrlCommand extends Command
                 foreach ($messages as $message) {
                     DB::beginTransaction();
                     try {
-                        echo ('Updating Message ID ' . $message->getId() . PHP_EOL);
+                        echo('Updating Message ID ' . $message->getId() . PHP_EOL);
                         Log::info(sprintf('Updating Message ID %d:', $message->getId()));
 
                         $this->toCdnUrl($message);
                         DB::commit();
-                        echo ('Successfully updated Message ID ' . $message->getId() . PHP_EOL);
+                        echo('Successfully updated Message ID ' . $message->getId() . PHP_EOL);
                         Log::info(sprintf('Successfully updated Message ID %d:', $message->getId()));
                     } catch (\Throwable $e) {
                         DB::rollBack();
-                        echo ('Failed to update Message ID ' . $message->getId() . ': ' . $e->getMessage() . PHP_EOL);
+                        echo('Failed to update Message ID ' . $message->getId() . ': ' . $e->getMessage() . PHP_EOL);
                         Log::error(sprintf('Failed to update Message ID %d: %s', $message->getId(), $e->getMessage()));
                     }
                 }
@@ -84,13 +84,13 @@ class ChangeMediaUrlCommand extends Command
         $messageData = is_array($message->message) ? $message->message : json_decode($message->message, true);
         match ($messageData['type']) {
             'video-format' => $messageData['video'] = (function () use ($messageData, $message) {
-                if (!isset($messageData['video']) || strpos($messageData['video'], self::OLD_MEDIA_URL) === false) {
+                if (! isset($messageData['video']) || strpos($messageData['video'], self::OLD_MEDIA_URL) === false) {
                     $messageData['video'] = $this->uploadToS3($messageData['video'], $messageData['type'], $message->user, $message->app, $message->company);
                 }
                 return str_replace(self::OLD_MEDIA_URL, self::CDN_URL, $messageData['video']);
             })(),
             'image-format' => $messageData['image'] = (function () use ($messageData, $message) {
-                if (!isset($messageData['image']) || strpos($messageData['image'], self::OLD_MEDIA_URL) === false) {
+                if (! isset($messageData['image']) || strpos($messageData['image'], self::OLD_MEDIA_URL) === false) {
                     $messageData['image'] = $this->uploadToS3($messageData['image'], $messageData['type'], $message->user, $message->app, $message->company);
                 }
                 return str_replace(self::OLD_MEDIA_URL, self::CDN_URL, $messageData['image']);
