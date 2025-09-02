452dffd8118ab90aa88ec8ed0f35a2723e9a1f5b
FredPeal <frederickpeal@gmail.com>
2025-09-02 19:58:31 -0400

    refactor: hand off notification

--- src/Domains/Intelligence/Workflows/HandOffActivity.php
+++ src/Domains/Intelligence/Workflows/HandOffActivity.php
@@ -2,12 +2,13 @@
 
 declare(strict_types=1);
 
-namespace Kanvas\Connectors\WaSender\Workflows;
+namespace Kanvas\Connectors\Intelligence\Workflows;
 
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Guild\Leads\Models\Lead;
 use Kanvas\Guild\Leads\Models\LeadRotation;
 use Kanvas\Intelligence\Enums\ConfigurationEnum;
+use Kanvas\Intelligence\Notifications\HandOffNotification;
 use Kanvas\Workflow\KanvasActivity;
 
 class HandOffActivity extends KanvasActivity
@@ -17,8 +18,19 @@ class HandOffActivity extends KanvasActivity
         $this->overwriteAppService($app);
         $lead->set(ConfigurationEnum::AGENT_HAND_OFF->value, 1);
         if ($rotation = LeadRotation::getById($params['rotation_id'], $app)) {
-            $lead->leads_owner_id = $rotation->getAgent();
+            $leadOwner = $rotation->getAgent();
+            $lead->leads_owner_id = $leadOwner->getId();
             $lead->saveOrFail();
+            $leadOwner->notify(
+                new HandOffNotification(
+                    lead: $lead,
+                    templateName: $params['template_name'],
+                    data: [
+                        'lead' => $lead,
+                        'agent' => $leadOwner,
+                    ]
+                )
+            );
         }
     }
 }
