b88577d194954b676acc817b6c3f14f7f025e79d
FredPeal <frederickpeal@gmail.com>
2025-09-02 22:02:23 -0400

    refactor: try and catch

--- src/Domains/Intelligence/Workflows/HandOffActivity.php
+++ src/Domains/Intelligence/Workflows/HandOffActivity.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Kanvas\Connectors\Intelligence\Workflows;
 
+use Exception;
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Guild\Leads\Models\Lead;
 use Kanvas\Guild\Leads\Models\LeadRotation;
@@ -24,13 +25,17 @@ class HandOffActivity extends KanvasActivity
             integration: IntegrationsEnum::INTERNAL,
             integrationOperation: function ($lead, $app, $integrationCompany, $additionalParams) use ($params) {
                 $lead->set(ConfigurationEnum::AGENT_HAND_OFF->value, 1);
-                if ($rotation = LeadRotation::getById($params['rotation_id'], $app)) {
-                    $leadOwner = $rotation->getAgent();
-                    $lead->leads_owner_id = $leadOwner->getId();
-                    $lead->saveOrFail();
-                } else {
+
+                try {
+                    if ($rotation = LeadRotation::getById($params['rotation_id'], $app)) {
+                        $leadOwner = $rotation->getAgent();
+                        $lead->leads_owner_id = $leadOwner->getId();
+                        $lead->saveOrFail();
+                    }
+                } catch (Exception $e) {
                     $leadOwner = $lead->owner;
                 }
+
                 $leadOwner->notify(
                     new HandOffNotification(
                         lead: $lead,
