fae5206ad3591233fb91d69451ff5f4ff3dd8ff0
FredPeal <frederickpeal@gmail.com>
2025-09-02 20:34:48 -0400

    refactor: use internal integration

--- src/Domains/Intelligence/Workflows/HandOffActivity.php
+++ src/Domains/Intelligence/Workflows/HandOffActivity.php
@@ -9,28 +9,39 @@ use Kanvas\Guild\Leads\Models\Lead;
 use Kanvas\Guild\Leads\Models\LeadRotation;
 use Kanvas\Intelligence\Enums\ConfigurationEnum;
 use Kanvas\Intelligence\Notifications\HandOffNotification;
+use Kanvas\Workflow\Enums\IntegrationsEnum;
 use Kanvas\Workflow\KanvasActivity;
 
 class HandOffActivity extends KanvasActivity
 {
-    public function execute(Lead $lead, Apps $app, array $params): void
+    public function execute(Lead $lead, Apps $app, array $params): array
     {
         $this->overwriteAppService($app);
-        $lead->set(ConfigurationEnum::AGENT_HAND_OFF->value, 1);
-        if ($rotation = LeadRotation::getById($params['rotation_id'], $app)) {
-            $leadOwner = $rotation->getAgent();
-            $lead->leads_owner_id = $leadOwner->getId();
-            $lead->saveOrFail();
-            $leadOwner->notify(
-                new HandOffNotification(
-                    lead: $lead,
-                    templateName: $params['template_name'],
-                    data: [
-                        'lead' => $lead,
-                        'agent' => $leadOwner,
-                    ]
-                )
-            );
-        }
+
+        return $this->executeIntegration(
+            entity: $lead,
+            app: $app,
+            integration: IntegrationsEnum::INTERNAL,
+            integrationOperation: function ($lead, $app, $integrationCompany, $additionalParams) use ($params) {
+                $lead->set(ConfigurationEnum::AGENT_HAND_OFF->value, 1);
+                if ($rotation = LeadRotation::getById($params['rotation_id'], $app)) {
+                    $leadOwner = $rotation->getAgent();
+                    $lead->leads_owner_id = $leadOwner->getId();
+                    $lead->saveOrFail();
+                } else {
+                    $leadOwner = $lead->owner;
+                }
+                $leadOwner->notify(
+                    new HandOffNotification(
+                        lead: $lead,
+                        templateName: $params['template_name'] ?? 'lead_handoff',
+                        data: [
+                                'lead' => $lead,
+                                'agent' => $leadOwner,
+                            ]
+                    )
+                );
+            }
+        );
     }
 }
