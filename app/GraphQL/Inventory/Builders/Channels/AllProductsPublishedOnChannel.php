9bf1080e77118f666ad53e35968e784eb5a272b8
arfenis <arfen00@gmail.com>
2025-09-04 16:06:41 -0400

    Allow to search and filter the data from scout

--- app/GraphQL/Inventory/Builders/Channels/AllProductsPublishedOnChannel.php
+++ app/GraphQL/Inventory/Builders/Channels/AllProductsPublishedOnChannel.php
@@ -6,17 +6,14 @@ namespace App\GraphQL\Inventory\Builders\Channels;
 
 use GraphQL\Type\Definition\ResolveInfo;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Companies\Models\CompaniesBranches;
-use Kanvas\Enums\AppEnums;
 use Kanvas\Inventory\Channels\Models\Channels;
 use Kanvas\Inventory\Products\Models\Products;
 use Kanvas\Inventory\Products\Traits\SearchProductWorkflowTrait;
 use Kanvas\Inventory\Regions\Models\Regions;
 use Kanvas\Inventory\Variants\Models\Variants as ModelsVariants;
 use Kanvas\Inventory\Variants\Models\VariantsChannels;
-use Kanvas\Users\Repositories\UsersRepository;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class AllProductsPublishedOnChannel
@@ -31,18 +28,14 @@ class AllProductsPublishedOnChannel
     ): Builder {
         $channelUuid = $args['id'];
 
-        $channelUuid = $args['id'];
-
         $channel = Channels::getByUuid($channelUuid);
         $variants = new ModelsVariants();
         $variantsChannel = new VariantsChannels();
         $app = app(Apps::class);
         $companyBranch = app(CompaniesBranches::class);
         $region = Regions::getDefault($companyBranch->company, $app);
-        if (! $userId = $app->get(AppEnums::fromName('DEFAULT_PUBLIC_SEARCH_USER_ID'))) {
-            throw new ModelNotFoundException('User default search not configured');
-        }
-        $user = UsersRepository::getUserOfAppById($userId, $app);
+
+        $user = auth()->user();
         $this->fireSearch(
             $app,
             $user,
@@ -51,14 +44,35 @@ class AllProductsPublishedOnChannel
             $args['search'] ?? ''
         );
         $variantsChannelTable = $variantsChannel->getTable();
-        $query = Products::query()
-                ->join($variants->getTable(), $variants->getTable() . '.products_id', '=', 'products.id')
-                ->join($variantsChannelTable, $variantsChannelTable . '.products_variants_id', '=', $variants->getTable() . '.id')
+        $variantsTable = $variants->getTable();
+
+        if (! empty($args['search'])) {
+
+            $productIds = Products::search($args['search'])->keys();
+            $query = Products::query()
+                ->whereIn('products.id', $productIds)
+                ->join($variantsTable, $variantsTable . '.products_id', '=', 'products.id')
+                ->join($variantsChannelTable, $variantsChannelTable . '.products_variants_id', '=', $variantsTable . '.id')
                 ->where($variantsChannelTable . '.channels_id', $channel->getId())
                 ->where($variantsChannelTable . '.is_deleted', 0)
                 ->where($variantsChannelTable . '.is_published', 1)
                 ->select('products.*')
                 ->distinct();
+
+            return $query;
+        } else {
+
+            $query = Products::query()
+                ->join($variantsTable, $variantsTable . '.products_id', '=', 'products.id')
+                ->join($variantsChannelTable, $variantsChannelTable . '.products_variants_id', '=', $variantsTable . '.id')
+                ->where($variantsChannelTable . '.channels_id', $channel->getId())
+                ->where($variantsChannelTable . '.is_deleted', 0)
+                ->where($variantsChannelTable . '.is_published', 1)
+                ->select('products.*')
+                ->distinct();
+
+            return $query;
+        }
 
         return $query;
     }
