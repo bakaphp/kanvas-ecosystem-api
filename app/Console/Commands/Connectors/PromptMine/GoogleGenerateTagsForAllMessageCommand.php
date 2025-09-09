9c7518e5d7613a8018a8bb6a139c501631e63d2f
Rafael White <rwhite@mctekk.com>
2025-09-09 12:06:29 -0400

    fix format

--- app/Console/Commands/Connectors/PromptMine/GoogleGenerateTagsForAllMessageCommand.php
+++ app/Console/Commands/Connectors/PromptMine/GoogleGenerateTagsForAllMessageCommand.php
@@ -61,7 +61,7 @@ class GoogleGenerateTagsForAllMessageCommand extends Command
             $generateMessageTagAction = new GenerateMessageTagAction($message);
             $messageTags = $generateMessageTagAction->execute(
                 textLookupKey: 'ai_nugged.nugget',
-                totalTags:3,
+                totalTags: 3,
                 tags: $allTagsWithIgnore
             );
 
