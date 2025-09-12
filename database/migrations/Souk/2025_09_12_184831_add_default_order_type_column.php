7b2fc07cb94f7253632f2a351f1313914f5a9cf7
arfenis <arfen00@gmail.com>
2025-09-12 17:13:45 -0400

    stylo

--- database/migrations/Souk/2025_09_12_184831_add_default_order_type_column.php
+++ database/migrations/Souk/2025_09_12_184831_add_default_order_type_column.php
@@ -24,6 +24,5 @@ return new class () extends Migration {
      */
     public function down(): void
     {
-
     }
 };
--- database/migrations/Souk/2025_09_12_205137_remove_order_type_unique.php
+++ database/migrations/Souk/2025_09_12_205137_remove_order_type_unique.php
@@ -21,6 +21,5 @@ return new class () extends Migration {
      */
     public function down(): void
     {
-
     }
 };
