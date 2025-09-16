fd65ef48621a92b40731a4c229bf13817f95db6f
FredPeal <frederickpeal@gmail.com>
2025-09-16 02:51:59 -0400

    refactor: option lead type

--- app/Console/Commands/Intelligence/CreateFakeLeadContextCommand.php
+++ app/Console/Commands/Intelligence/CreateFakeLeadContextCommand.php
@@ -10,6 +10,7 @@ use Kanvas\Companies\Enums\ConfigurationEnum;
 use Kanvas\Companies\Models\Companies;
 use Kanvas\Connectors\SalesAssist\Enums\LeadCustomFieldEnum;
 use Kanvas\Guild\Leads\Models\Lead;
+use Kanvas\Guild\Leads\Models\LeadType;
 use Kanvas\Workflow\Enums\WorkflowEnum;
 
 class CreateFakeLeadContextCommand extends Command
@@ -19,14 +20,14 @@ class CreateFakeLeadContextCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'kanvas:create-fake-lead-context {app_id} {company_id} {lead_type_id} {pipeline_stage_id}';
+    protected $signature = 'kanvas:create-fake-lead-context {app_id} {company_id} {--lead_type_id=null} {--pipeline_stage_id=null}';
 
     public function handle(): void
     {
         $appId = $this->argument('app_id');
         $companyId = $this->argument('company_id');
-        $leadTypeId = $this->argument('lead_type_id');
-        $pipelineStageId = $this->argument('pipeline_stage_id');
+        $leadTypeId = $this->option('lead_type_id');
+        $pipelineStageId = $this->option('pipeline_stage_id');
         $company = Companies::getById((int) $companyId);
         $app = Apps::getById((int) $appId);
         $company->set(ConfigurationEnum::WORKING_DAYS->value, [
@@ -44,7 +45,19 @@ class CreateFakeLeadContextCommand extends Command
                 ->withAppId($app->getId())
                 ->withCompanyId($company->getId())
                 ->create();
+        if ($leadTypeId === 'null') {
+            $leadType = LeadType::firstOrCreate([
+                'name' => 'Internet ',
+                'companies_id' => $company->getId(),
+                'apps_id' => $app->getId(),
+            ], [
+                'description' => 'Internet Lead',
+            ]);
+            $leadTypeId = $leadType->id;
+        }
         $lead->leads_types_id = $leadTypeId;
+        $lead->saveOrFail();
+        $lead->pipeline_stage_id = $pipelineStageId ?? null;
         $lead->saveOrFail();
         $lead->set(LeadCustomFieldEnum::VEHICLE_OF_INTEREST->value, [
             'isNew' => true,
@@ -58,6 +71,6 @@ class CreateFakeLeadContextCommand extends Command
             'price' => 38995,
         ]);
         $lead->refresh();
-        $lead->fireWorkflow(WorkflowEnum::FAKE_CONTEXT);
+        $lead->fireWorkflow(WorkflowEnum::FAKE_CONTEXT->value);
     }
 }
