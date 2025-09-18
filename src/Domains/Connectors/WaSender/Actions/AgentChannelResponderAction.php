c5ad676c3722515e345f5a42e684f1fd5e3d316d
Maximo Castro Pou <max@mctekk.com>
2025-09-18 13:47:07 -0400

    feat: download leads from elad

--- src/Domains/Connectors/WaSender/Actions/AgentChannelResponderAction.php
+++ src/Domains/Connectors/WaSender/Actions/AgentChannelResponderAction.php
@@ -149,10 +149,10 @@ class AgentChannelResponderAction
 
     protected function hijackMessagePhone(string $channelId): string
     {
-        if ($this->message->app->get('allow_session_hijack', false)
-          && $this->message->app->get('overwrite_phone_number') !== null
+        if ($this->agent->app->get('allow_session_hijack', false)
+          && $this->agent->app->get('overwrite_phone_number') !== null
         ) {
-            $overwriteConfig = $this->message->app->get('overwrite_phone_number');
+            $overwriteConfig = $this->agent->app->get('overwrite_phone_number');
             $originalRemoteJid = $channelId;
 
             if (isset($overwriteConfig[$originalRemoteJid])) {
