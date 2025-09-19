8949073cffe54deda3b9c3afa3370e9e61c7e1b9
FredPeal <frederickpeal@gmail.com>
2025-09-19 17:28:46 -0400

    hotfix: token 7000

--- src/Domains/Intelligence/Workflows/LeadAgentFirstMessageOutreachActivity.php
+++ src/Domains/Intelligence/Workflows/LeadAgentFirstMessageOutreachActivity.php
@@ -87,7 +87,10 @@ class LeadAgentFirstMessageOutreachActivity extends KanvasActivity
                     && $lead->company->get('overwrite_phone_number') !== null) {
                     $overwriteConfig = $lead->company->get('overwrite_phone_number');
                     $overwriteConfig = array_flip($overwriteConfig);
-                    $originalRemoteJid = $cellPhone . '@s.whatsapp.net';
+                    $originalRemoteJid = match ($lead->get(LeadsEnumsConfigurationEnum::AGENT_COMMUNICATION_CHANNEL->value)) {
+                        'whatsapp' => $cellPhone = $cellPhone . '@s.whatsapp.net',
+                        'sms' => '+' . $cellPhone
+                    };
 
                     if (isset($overwriteConfig[$originalRemoteJid])) {
                         unset($params['disable_sending']);
