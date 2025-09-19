93f2ef0ead6ebbd5aa8c8f82e6b2396c104f5b9c
freesgen <jesusant.guerrero@gmail.com>
2025-09-19 11:58:37 -0400

    fix: lint fixes

--- app/GraphQL/Event/Builders/Events/EventOrderBuilder.php
+++ app/GraphQL/Event/Builders/Events/EventOrderBuilder.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\GraphQL\Event\Builders\Events;
 
 use Illuminate\Database\Eloquent\Builder;
+use Kanvas\Apps\Models\Apps;
 use Kanvas\Event\Events\Models\Event;
 use Kanvas\Souk\Orders\Models\Order;
 
@@ -17,9 +18,10 @@ class EventOrderBuilder
     {
         /** @var Event $event */
         $event = $root;
+        $app = app(Apps::class);
 
-        return Order::fromApp()
-            ->fromCompany()
+        return Order::fromApp($app)
+            ->fromCompany($event->company)
             ->where('resources_type', get_class($event))
             ->where('resources_id', $event->getId());
     }
