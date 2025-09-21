29ae829dfedbe50300728528baed9c5dc401d9ef
FredPeal <frederickpeal@gmail.com>
2025-09-21 19:00:00 -0400

    refactor: todo: name

--- src/Domains/Connectors/SalesAssist/Webhooks/CreateLeadFromADFWebhookJob.php
+++ src/Domains/Connectors/SalesAssist/Webhooks/CreateLeadFromADFWebhookJob.php
@@ -7,7 +7,9 @@ namespace Kanvas\Connectors\SalesAssist\Webhooks;
 use Kanvas\Connectors\SalesAssist\Actions\CreateLeadFromADFAction;
 use Kanvas\Workflow\Jobs\ProcessWebhookJob;
 use Override;
-
+/**
+ * @todo: change the name of this class to something more generic like LinkADFToLead
+ */
 class CreateLeadFromADFWebhookJob extends ProcessWebhookJob
 {
     #[Override]
