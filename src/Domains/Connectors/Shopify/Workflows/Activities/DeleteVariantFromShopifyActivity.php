dad70743537b6e4ccc6354b1e6386105a4eef2f2
arfenis <arfen00@gmail.com>
2025-09-22 15:14:56 -0400

    send param rule to delete

--- src/Domains/Connectors/Shopify/Workflows/Activities/DeleteVariantFromShopifyActivity.php
+++ src/Domains/Connectors/Shopify/Workflows/Activities/DeleteVariantFromShopifyActivity.php
@@ -26,6 +26,7 @@ class DeleteVariantFromShopifyActivity extends KanvasActivity
         $status = Status::where('slug', StatusEnum::ACTIVE->value)
         ->where('apps_id', 0)
         ->first();
+        $rule = $params['rule'] ?? null;
 
         foreach ($variant->warehouses as $warehouse) {
             $integrationCompany = IntegrationsCompany::getByIntegration(
@@ -68,7 +69,7 @@ class DeleteVariantFromShopifyActivity extends KanvasActivity
                     response: $historyResponse ?? null,
                     exception: $exception,
                     workflowId: $this->workflowId(),
-                    rule: null
+                    rule: $rule
                 );
 
                 (new AddEntityIntegrationHistoryAction(
