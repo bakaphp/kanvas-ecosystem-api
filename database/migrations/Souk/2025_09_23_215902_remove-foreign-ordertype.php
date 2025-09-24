897eb9b4e3c367e2c0e8fed1324981b60e2a7b0d
arfenis <arfen00@gmail.com>
2025-09-23 21:42:12 -0400

    migration to remove foreign for global status

---
+++ database/migrations/Souk/2025_09_23_215902_remove-foreign-ordertype.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('order_statuses', function (Blueprint $table) {
+            $table->dropForeign(['order_types_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+    }
+};
