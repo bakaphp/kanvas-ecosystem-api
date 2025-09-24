ab41b21b60706e635729e0d73f51baf94fca3044
arfenis <arfen00@gmail.com>
2025-09-23 21:43:40 -0400

    stylo

--- app/Console/Commands/Souk/Orders/CreateOrderStatusCommand.php
+++ app/Console/Commands/Souk/Orders/CreateOrderStatusCommand.php
@@ -6,7 +6,6 @@ namespace App\Console\Commands\Souk\Orders;
 
 use Baka\Support\Str;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\DB;
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Companies\Models\Companies;
 use Kanvas\Exceptions\ModelNotFoundException;
