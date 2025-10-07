e24c41768a238f0820063e27764eadb05fc7aacf
Maximo Castro Pou <max@mctekk.com>
2025-10-07 09:47:25 -0400

    feat: push participant activity

--- app/Console/Commands/Workflows/KanvasWorkflowSynActionCommand.php
+++ app/Console/Commands/Workflows/KanvasWorkflowSynActionCommand.php
@@ -17,6 +17,7 @@ use Kanvas\Connectors\EchoPay\Workflows\Activities\ProcessPaymentActivity;
 use Kanvas\Connectors\Elead\Workflow\AddLeadCommentFromAgentMessageActivity;
 use Kanvas\Connectors\Elead\Workflow\PushLeadActivity as WorkflowPushLeadActivity;
 use Kanvas\Connectors\Elead\Workflow\PushLeadNotesActivity as WorkflowPushLeadNotesActivity;
+use Kanvas\Connectors\Elead\Workflow\PushParticipantActivity;
 use Kanvas\Connectors\Elead\Workflow\PushPeopleActivity as WorkflowPushPeopleActivity;
 use Kanvas\Connectors\ESim\WorkflowActivities\CreateOrderInESimActivity;
 use Kanvas\Connectors\ESim\WorkflowActivities\UpdateOrderStripePaymentActivity;
@@ -267,6 +268,7 @@ class KanvasWorkflowSynActionCommand extends Command
             WorkflowsAgentChannelResponderActivity::class,
             AgentProcessEmailWebhookJob::class,
             StripePaymentLinkWebhookJob::class,
+            PushParticipantActivity::class,
         ];
 
         $createdActions = [];
