1d37c6eb643a621324ba6a95a91fccfafa66584f
Rafael White <rwhite@mctekk.com>
2025-10-09 16:42:20 -0400

    fix: remove unused GuzzleHttp Proxy import from PromptImageFilterActivity

--- src/Domains/Connectors/PromptMine/Workflows/Activities/PromptImageFilterActivity.php
+++ src/Domains/Connectors/PromptMine/Workflows/Activities/PromptImageFilterActivity.php
@@ -8,7 +8,6 @@ use Baka\Contracts\AppInterface;
 use Baka\Support\Str;
 use Exception;
 use finfo;
-use GuzzleHttp\Handler\Proxy;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\UploadedFile;
