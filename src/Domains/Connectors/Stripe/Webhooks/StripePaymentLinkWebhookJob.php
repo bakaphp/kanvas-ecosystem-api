15770d1c879df5119b4e6eaa17dc9263f9a09814
Maximo Castro Pou <max@mctekk.com>
2025-10-14 00:30:05 -0400

    fix: stripe webhook v3

--- src/Domains/Connectors/Stripe/Webhooks/StripePaymentLinkWebhookJob.php
+++ src/Domains/Connectors/Stripe/Webhooks/StripePaymentLinkWebhookJob.php
@@ -5,12 +5,16 @@ declare(strict_types=1);
 namespace Kanvas\Connectors\Stripe\Webhooks;
 
 use Kanvas\ActionEngine\Engagements\Actions\CreateEngagementAction;
+use Kanvas\ActionEngine\Engagements\Actions\MessageNotificationTextAction;
 use Kanvas\ActionEngine\Engagements\DataTransferObject\Engagement;
+use Kanvas\ActionEngine\Engagements\Models\Engagement as ModelsEngagement;
 use Kanvas\ActionEngine\Engagements\Repositories\EngagementRepository;
 use Kanvas\ActionEngine\Enums\ActionStatusEnum;
-use Kanvas\Guild\Leads\Models\Lead;
+use Kanvas\Notifications\Channels\OneSignalNotificationChannel;
+use Kanvas\Notifications\Templates\Blank;
 use Kanvas\Social\Messages\Models\Message;
 use Kanvas\Workflow\Jobs\ProcessWebhookJob;
+use NotificationChannels\Expo\ExpoChannel;
 use Override;
 
 class StripePaymentLinkWebhookJob extends ProcessWebhookJob
@@ -40,11 +44,36 @@ class StripePaymentLinkWebhookJob extends ProcessWebhookJob
     {
         $session = $payload['data']['object'];
         $metadata = $session['metadata'] ?? [];
-        $messageId = $metadata['message_id'] ?? null;
-        $leadId = $metadata['leads_id'] ?? null;
+        //$messageId = $metadata['message_id'] ?? null;
+        //$leadId = $metadata['leads_id'] ?? null;
 
-        $lead = Lead::getById((int)$leadId, $this->webhookRequest->receiverWebhook->app);
-        $message = Message::getById((int)$messageId, $this->webhookRequest->receiverWebhook->app);
+        $message = Message::getByCustomField(
+            'stripe_payment_link_id',
+            $session['payment_link'] ?? null,
+            $this->webhookRequest->receiverWebhook->company
+        );
+
+        if (! $message) {
+            return [
+                'message' => 'No message found for payment link id: ' . ($session['payment_link'] ?? 'null'),
+                'response' => null,
+            ];
+        }
+
+        $engagement = ModelsEngagement::fromApp($this->webhookRequest->receiverWebhook->app)
+            ->fromCompany($this->webhookRequest->receiverWebhook->company)
+            ->where('message_id', $message->getId())
+            ->first();
+
+        if (! $engagement) {
+            return [
+                'message' => 'No engagement found for message id: ' . $message->getId(),
+                'response' => null,
+            ];
+        }
+
+        $lead = $engagement->lead;
+        //$message = Message::getById((int)$messageId, $this->webhookRequest->receiverWebhook->app);
         $action = $message->message['verb'] ?? null;
 
         $firstEngagement = EngagementRepository::findEngagementForLeadBuilder(
@@ -54,6 +83,8 @@ class StripePaymentLinkWebhookJob extends ProcessWebhookJob
             'DESC'
         );
 
+        $taskId = $lead->get('check_list_status') ?? $lead->company->get('default_checklist_id');
+
         // Create engagement data manually
         $engagementData = new Engagement(
             app: $lead->app,
@@ -61,12 +92,12 @@ class StripePaymentLinkWebhookJob extends ProcessWebhookJob
             user: $lead->owner,
             lead: $lead,
             action: $action,
-            requestId: $firstEngagement->exists() ? (string)$firstEngagement->first()->request_id : null,
+            requestId: $firstEngagement->exists() ? (string)$firstEngagement->first()->entity_uuid : $engagement->entity_uuid,
             source: 'stripe',
             status: ActionStatusEnum::SUBMITTED,
             people: $lead->people,
             receiverId: $lead->receiver?->getId(),
-            taskId: $lead->get('check_list_status') ?? $lead->company->get('default_checklist_id'),
+            taskId: is_array($taskId) && isset($taskId['activeTaskListId']) ? (int)$taskId['activeTaskListId'] : (int)$taskId,
             via: 'webhook',
             data: $payload,
             //formType: $metadata['form_type'] ?? null,
@@ -76,6 +107,27 @@ class StripePaymentLinkWebhookJob extends ProcessWebhookJob
         );
 
         $submittedEngagement = new CreateEngagementAction($engagementData)->execute();
+        $submittedMessage = Message::getById($submittedEngagement->message_id, $this->webhookRequest->receiverWebhook->app);
+        $submittedMessage->parent_id = $message->getId();
+        $submittedMessage->saveOrFail();
+
+        $notificationMessage = new MessageNotificationTextAction($submittedEngagement, $submittedMessage)->notificationText();
+        $notification = new Blank(
+            'new-push-default',
+            [
+                'title' => $engagement->companyAction->action->name,
+                'message' => $notificationMessage,
+                'destination_id' => $engagement->leads_id,
+                'destination_slug' => $message->slug,
+                'destination_type' => 'ENGAGEMENT',
+                'destination_event' => 'CREATED',
+                'notification_type' => null,
+                'user_id' => $engagement->user_id,
+            ],
+            [OneSignalNotificationChannel::class, ExpoChannel::class],
+            $engagement,
+        );
+        $lead->owner->notify($notification);
 
         return [
             'message' => 'Checkout session completed processed successfully.',
