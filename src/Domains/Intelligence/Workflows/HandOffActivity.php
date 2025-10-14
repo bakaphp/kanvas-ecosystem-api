572fbe6126f9a92a62f25213c56a0db6884a64ec
Maximo Castro Pou <max@mctekk.com>
2025-10-14 02:04:18 -0400

    fix: stripe webhook v3

--- src/Domains/Intelligence/Workflows/HandOffActivity.php
+++ src/Domains/Intelligence/Workflows/HandOffActivity.php
@@ -6,6 +6,7 @@ namespace Kanvas\Intelligence\Workflows;
 
 use Exception;
 use Kanvas\Apps\Models\Apps;
+use Kanvas\Guild\Leads\Enums\ConfigurationEnum as EnumsConfigurationEnum;
 use Kanvas\Guild\Leads\Models\Lead;
 use Kanvas\Guild\Leads\Models\LeadRotation;
 use Kanvas\Intelligence\Enums\ConfigurationEnum;
@@ -44,6 +45,30 @@ class HandOffActivity extends KanvasActivity
                 }
 
                 $leadOwner = $leadOwner ?? $lead->owner ?? $lead->user;
+
+                $handOffType = $params['handoff_type'] ?? 'human';
+                $comunicationChannel = $lead->get(EnumsConfigurationEnum::AGENT_COMMUNICATION_CHANNEL->value) ?? 'sms';
+
+                if (strtolower($handOffType) === 'compliance_internal') {
+                    $contactInfo = match (strtolower($comunicationChannel)) {
+                        'sms' => $lead->people->getCellPhones(),
+                        'email' => $leadOwner->getEmails(),
+                        default => null
+                    };
+
+                    if ($contactInfo === null) {
+                        return [
+                            'success' => false,
+                            'message' => 'No contact info found for the lead or the agent',
+                        ];
+                    }
+
+                    if ($contactInfo->count()) {
+                        foreach ($contactInfo as $contact) {
+                            $contact->optOut();
+                        }
+                    }
+                }
 
                 $handOffNotification = new HandOffNotification(
                     lead: $lead,
