6f0c9e22ac0cc2cbd329fd49a2428d6a894a9fcc
FredPeal <frederickpeal@gmail.com>
2025-10-16 02:19:24 -0400

    feat: notifiable all users in channel

---
+++ src/Domains/Social/Channels/Actions/NotifiableUsersChannelAction.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Social\Channels\Actions;
+
+use Illuminate\Support\Facades\Notification;
+use Kanvas\Notifications\Channels\OneSignalNotificationChannel;
+use Kanvas\Social\Channels\Models\Channel;
+use Kanvas\Social\Messages\Models\Message;
+use Kanvas\Social\Messages\Notifications\NewMessageNotification;
+
+class NotifiableUsersChannelAction
+{
+    public function __construct(
+        public Channel $channel,
+        public Message $message,
+    ) {
+    }
+
+    public function execute(): void
+    {
+        $users = $this->channel->users()
+            ->where('users.id', '!=', $this->message->users_id)
+            ->get();
+        $notification = new NewMessageNotification(
+            message: $this->message,
+            data: [
+                'message' => $this->message->message,
+            ],
+            via: [OneSignalNotificationChannel::class]
+        );
+        Notification::send($users, $notification);
+    }
+}
