5a2ec80f97a0babf690c24e39b647e03f95a0285
arfenis <arfen00@gmail.com>
2025-10-16 23:43:36 -0400

    Add dto for filesystem update

---
+++ src/Kanvas/Filesystem/DataTransferObject/FilesystemEntityUpdate.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Filesystem\DataTransferObject;
+
+use Spatie\LaravelData\Data;
+
+class FilesystemEntityUpdate extends Data
+{
+    public function __construct(
+        public string $fieldName,
+        public float $weight = 0,
+    ) {
+    }
+
+    public static function viaRequest(array $data): self
+    {
+        return new self(
+            $data['field_name'],
+            $data['weight'] ?? 0
+        );
+    }
+}
