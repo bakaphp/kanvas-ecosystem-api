debac6c0aa16f21898e8e5f580b345e5247a4def
Maximo Castro Pou <max@mctekk.com>
2025-10-22 11:07:31 -0400

    fix: followup

--- src/Domains/Intelligence/PipelinesStages/Actions/CreateMessageFollowUpAction.php
+++ src/Domains/Intelligence/PipelinesStages/Actions/CreateMessageFollowUpAction.php
@@ -56,7 +56,7 @@ class CreateMessageFollowUpAction
 
         $messageType = MessageType::firstOrCreate([
             'apps_id' => $this->session->apps_id,
-            'languages_id' => 1,
+            //'languages_id' => 1,
             //'name' => 'AI Generated Message',
             'name' => 'twilio-sms',
             'verb' => 'twilio-sms',
