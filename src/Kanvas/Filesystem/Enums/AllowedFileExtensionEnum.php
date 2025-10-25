949d4274d06b17c4adf25ea611843a09a58f2c70
Maximo Castro Pou <max@mctekk.com>
2025-10-25 19:21:49 -0400

    fix

--- src/Kanvas/Filesystem/Enums/AllowedFileExtensionEnum.php
+++ src/Kanvas/Filesystem/Enums/AllowedFileExtensionEnum.php
@@ -12,7 +12,7 @@ enum AllowedFileExtensionEnum
     public function getAllowedExtensions(): array
     {
         return match ($this) {
-            self::ONLY_IMAGES => ['jpg', 'jpeg', 'png', 'gif', 'svg', 'webp', 'json', 'pdf', 'txt', 'text'],
+            self::ONLY_IMAGES => ['jpg', 'jpeg', 'png', 'gif', 'svg', 'webp', 'json', 'pdf', 'txt', 'text', 'heic'],
             self::WORK_FILES => ['jpg', 'jpeg', 'png', 'gif', 'svg', 'webp', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'txt', 'pdf', 'csv'],
         };
     }
