e25d1b4204a03d48598a0b3ca36d004b95c4283c
freesgen <jesusant.guerrero@gmail.com>
2025-10-28 18:48:49 -0400

    fix: update variant filters

--- src/Domains/Souk/Orders/Helpers/DateHelper.php
+++ src/Domains/Souk/Orders/Helpers/DateHelper.php
@@ -4,26 +4,22 @@ declare(strict_types=1);
 
 namespace Kanvas\Souk\Orders\Helpers;
 
-use DateTime;
 use Illuminate\Support\Carbon;
 
 class DateHelper
 {
     /**
      * Generate a list of dates between start and end dates
-     *
-     * @param Carbon|string $start
-     * @param Carbon|string $end
      */
-    public static function generateDateList($start, $end): array
+    public static function generateDateList(Carbon $start, Carbon $end): array
     {
         $dates = [];
-        $startDate = new DateTime($start);
-        $endDate = new DateTime($end);
+        $startDate = $start->copy();
+        $endDate = $end->copy();
 
         while ($startDate <= $endDate) {
             $dates[] = "'" . $startDate->format('Y-m-d') . "'";
-            $startDate->modify('+1 day');
+            $startDate->addDay();
         }
 
         return $dates;
