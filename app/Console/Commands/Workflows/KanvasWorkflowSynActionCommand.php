508fe0984be64edf338365cc05221e0402464b64
Maximo Castro Pou <max@mctekk.com>
2025-10-28 00:50:01 -0400

    feat: kanvas workflow syn action command

--- app/Console/Commands/Workflows/KanvasWorkflowSynActionCommand.php
+++ app/Console/Commands/Workflows/KanvasWorkflowSynActionCommand.php
@@ -67,6 +67,7 @@ use Kanvas\Connectors\PromptMine\Workflows\Activities\SaveLlmChoiceActivity;
 use Kanvas\Connectors\QuickBooks\Workflows\PushOrderToInvoiceActivity;
 use Kanvas\Connectors\RainForest\Workflows\Activities\ImportProductActivity;
 use Kanvas\Connectors\Recombee\Workflows\PushMessageToItemActivity;
+use Kanvas\Connectors\Recombee\Workflows\PushProductToItemActivity;
 use Kanvas\Connectors\Recombee\Workflows\PushUserInteractionToEventActivity;
 use Kanvas\Connectors\SalesAssist\Activities\AttachFileToChecklistItemActivity;
 use Kanvas\Connectors\SalesAssist\Activities\ConvertMessageImagesToPdfActivity;
@@ -271,6 +272,7 @@ class KanvasWorkflowSynActionCommand extends Command
             AgentProcessEmailWebhookJob::class,
             StripePaymentLinkWebhookJob::class,
             PushParticipantActivity::class,
+            PushProductToItemActivity::class,
         ];
 
         $createdActions = [];
