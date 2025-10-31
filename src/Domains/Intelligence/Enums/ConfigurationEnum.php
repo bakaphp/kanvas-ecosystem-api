9caf523a21f81c4ec398b0f5a2418ad6562dce49
Maximo Castro Pou <max@mctekk.com>
2025-10-31 01:22:39 -0400

    feat: add agent channel responder activity

--- src/Domains/Intelligence/Enums/ConfigurationEnum.php
+++ src/Domains/Intelligence/Enums/ConfigurationEnum.php
@@ -22,4 +22,5 @@ enum ConfigurationEnum: string
     case LEAD_CONTEXT_INFO = 'lead_ai_agent_context_info';
     case LAST_MESSAGE_TIME = 'last_message_time';
     case LAST_MESSAGE = 'last_message';
+    case MUTE_AI_AGENT = 'mute_ai_agent';
 }
