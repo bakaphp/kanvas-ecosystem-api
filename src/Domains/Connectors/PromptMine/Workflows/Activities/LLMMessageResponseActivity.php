1dbf004722a692b14552aa15d6e4992d1640bdf1
Rafael White <rwhite@mctekk.com>
2025-11-07 19:32:18 -0400

    test

--- src/Domains/Connectors/PromptMine/Workflows/Activities/LLMMessageResponseActivity.php
+++ src/Domains/Connectors/PromptMine/Workflows/Activities/LLMMessageResponseActivity.php
@@ -358,7 +358,7 @@ class LLMMessageResponseActivity extends KanvasActivity
                     $previousChatResponseMessage = $message->message['prompt'];
                     $previousChatImage = $this->getLastAssistantResponse($chatHistory);
                     $params['previousImageUrl'] = array_key_exists('content', $previousChatImage) ? $previousChatImage['content'] : null;
-                    unset($chatHistory[$previousChatImage['original_index']]);
+                    // unset($chatHistory[$previousChatImage['original_index']]);
                 } else {
                     $previousChatResponseMessage = $previousChatResponse->message['prompt'] ?? null;
                     $previousChatMessageChildren = $previousChatResponse->children()?->first();
@@ -445,7 +445,7 @@ class LLMMessageResponseActivity extends KanvasActivity
 
         // Add the new user message to the conversation
         $messages = $chatHistory;
-        if (! $message->message['is_regeneration']) {
+        if (array_key_exists('is_regeneration', $message->message) && ! $message->message['is_regeneration']) {
             $messages[] = [
             'role' => 'user',
             'content' => $prompt,
