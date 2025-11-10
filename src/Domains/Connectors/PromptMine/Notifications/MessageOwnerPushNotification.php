654cf1f49fcd086fe7e483e4e23bc3d5a17719d0
Maximo Castro Pou <max@mctekk.com>
2025-11-10 08:26:02 -0400

    feat: add payment management mutation

--- src/Domains/Connectors/PromptMine/Notifications/MessageOwnerPushNotification.php
+++ src/Domains/Connectors/PromptMine/Notifications/MessageOwnerPushNotification.php
@@ -24,7 +24,7 @@ class MessageOwnerPushNotification extends CustomMessageNotification
         unset($metadata['ai_image']);
         unset($metadata['prompt']);
         unset($metadata['ai_nugget']['nugget']);
-        
+
         $data = [
             'email_template' => $templates['email_template'] ?? null,
             'push_template' => $templates['push_template'] ?? null,
