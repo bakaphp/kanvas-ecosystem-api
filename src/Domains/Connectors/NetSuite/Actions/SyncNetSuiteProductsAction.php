91b02cafd8af0f630c0321f4a4bbaf1becd064d9
freesgen <jesusant.guerrero@gmail.com>
2025-11-17 09:46:58 -0400

    feat: allow legacy search

--- src/Domains/Connectors/NetSuite/Actions/SyncNetSuiteProductsAction.php
+++ src/Domains/Connectors/NetSuite/Actions/SyncNetSuiteProductsAction.php
@@ -13,7 +13,6 @@ use Kanvas\Connectors\NetSuite\Services\NetSuiteCustomerService;
 use Kanvas\Connectors\NetSuite\Services\NetSuiteProductSearchService;
 use Kanvas\Connectors\NetSuite\Services\NetSuiteProductService;
 use Kanvas\Inventory\Variants\Models\Variants;
-use Kanvas\Inventory\Variants\Models\VariantsWarehouses;
 use NetSuite\Classes\InventoryItem;
 
 /**
@@ -24,6 +23,8 @@ class SyncNetSuiteProductsAction
 {
     protected NetSuiteCustomerService $service;
     protected NetSuiteProductService $productService;
+    protected NetSuiteProductSearchService $searchService;
+    protected bool $shouldUseLegacySearch = false;
 
     public function __construct(
         protected AppInterface $app,
@@ -32,6 +33,8 @@ class SyncNetSuiteProductsAction
     ) {
         $this->service = new NetSuiteCustomerService($app, $mainAppCompany);
         $this->productService = new NetSuiteProductService($app, $mainAppCompany);
+        $this->shouldUseLegacySearch = $this->app->get(ConfigurationEnum::NET_SUITE_USE_LEGACY_PRODUCT_SEARCH->value);
+        $this->searchService = new NetSuiteProductSearchService($app, $mainAppCompany);
     }
 
     public function execute(): array
@@ -50,6 +53,7 @@ class SyncNetSuiteProductsAction
         $setMinimumQuantity = $this->app->get(ConfigurationEnum::NET_SUITE_MINIMUM_PRODUCT_QUANTITY->value);
         $defaultWarehouse = $this->mainAppCompany->get(ConfigurationEnum::NET_SUITE_DEFAULT_WAREHOUSE->value);
         $missed = [];
+
         foreach ($listOrProductVariantsBarCodeIds as $bardCodeId) {
             $config = null;
             $variant = Variants::fromApp($this->app)
@@ -59,7 +63,7 @@ class SyncNetSuiteProductsAction
 
             if (! $variant) {
                 $missed[] = $bardCodeId->item->name;
-
+                $totalProcessed++;
                 continue;
             }
 
@@ -67,42 +71,41 @@ class SyncNetSuiteProductsAction
 
             $searchNetsuiteProductInfo = $this->productService->searchProductByItemNumber($variant->barcode);
             $netsuiteProductInfo = $this->productService->getProductById($searchNetsuiteProductInfo[0]->internalId);
-            $productSearchService = new NetSuiteProductSearchService($this->app, $this->mainAppCompany);
             /**
              * @todo , this logic to update the quantity and price should be moved to a dedicated action / workflow
              */
 
             try {
-                $warehouseOptions = $this->getWarehouseOptions($netsuiteProductInfo, $variantWarehouse, $defaultWarehouse);
+                $warehouseOptions = $this->getWarehouseOptions($netsuiteProductInfo);
                 $locationId = $variantWarehouse->get(CustomFieldEnum::NET_SUITE_LOCATION_ID->value) ?? $defaultWarehouse;
-                $searchProduct = $productSearchService->searchProductByItemNumber($variant->barcode, $locationId);
 
-                if (count($searchProduct) === 0) {
+                $product = $this->shouldUseLegacySearch
+                ? $this->legacySearchProductByItemNumber($netsuiteProductInfo, $locationId)
+                : $this->customSearchProductByItemNumber($variant->barcode, $locationId);
+
+                if (empty($product)) {
                     $missed[] = $bardCodeId->item->name;
+                    $totalProcessed++;
                     continue;
                 }
 
-                $mapPrice = (float) $searchProduct[0]['mapPrice'];
-                $colorCode = $searchProduct[0]['colorCode'];
-                $minimumQuantity = $searchProduct[0]['minimumQuantity'];
-                $quantityAvailable = $searchProduct[0]['quantityAvailable'];
-
+                $minimumQuantity = $product['minimum_quantity'];
 
                 $config = [
-                    'map_price' => $mapPrice,
+                    'map_price' => $product["map_price"],
                     ...($minimumQuantity && $setMinimumQuantity ? ['minimum_quantity' => $minimumQuantity] : []),
                 ];
 
-                $variantWarehouse->quantity = $quantityAvailable;
+                $variantWarehouse->quantity = $product["quantity_available"];
                 $variantWarehouse->price = $warehouseOptions['price'] ?? 0;
 
-                $variantWarehouse->config = $config ?? null;
+                $variantWarehouse->config = $config;
                 $variantWarehouse->saveOrFail();
 
                 $variant->addAttributes($this->mainAppCompany->user, [
                     [
                         'name' => 'color_code',
-                        'value' => $colorCode,
+                        'value' => $product['color_code'],
                     ],
                 ]);
             } catch (Exception $e) {
@@ -134,5 +137,45 @@ class SyncNetSuiteProductsAction
         }
 
         return $config;
+    }
+
+    private function legacySearchProductByItemNumber(
+        InventoryItem $netsuiteProductInfo,
+        ?string $locationId = null
+    ): array {
+        $mapPrice = (float) $this->productService->getCustomField($netsuiteProductInfo, CustomFieldEnum::NET_SUITE_MAP_PRICE_CUSTOM_FIELD->value);
+        $colorCode = $this->productService->getCustomField($netsuiteProductInfo, CustomFieldEnum::NET_SUITE_COLOR_CODE_CUSTOM_FIELD->value);
+        $quantity = 0;
+
+        try {
+            $quantity = $this->productService->getInventoryQuantityByLocation($netsuiteProductInfo, $locationId);
+        } catch (Exception) {
+            return [];
+        }
+
+        return [
+            'map_price' => $mapPrice,
+            'color_code' => $colorCode,
+            'minimum_quantity' => $netsuiteProductInfo->minimumQuantity,
+            'quantity_available' => $quantity,
+
+        ];
+    }
+
+
+    private function customSearchProductByItemNumber(string $itemNumber, ?string $locationId = null): array
+    {
+        $searchProduct = $this->searchService->searchProductByItemNumber($itemNumber, $locationId);
+
+        if (count($searchProduct) === 0) {
+            return [];
+        }
+
+        return [
+            'map_price' => (float) $searchProduct[0]['mapPrice'],
+            'color_code' => $searchProduct[0]['colorCode'],
+            'minimum_quantity' => $searchProduct[0]['minimumQuantity'],
+            'quantity_available' => $searchProduct[0]['quantityAvailable'],
+        ];
     }
 }
