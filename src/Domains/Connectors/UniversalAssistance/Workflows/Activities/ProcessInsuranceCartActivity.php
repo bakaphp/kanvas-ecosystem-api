def1fbc9bd45a831168791999a21cd7fae1c250f
Manuel Pellerano <pelleranomanuel@gmail.com>
2025-11-24 19:03:04 -0400

    extra space

--- src/Domains/Connectors/UniversalAssistance/Workflows/Activities/ProcessInsuranceCartActivity.php
+++ src/Domains/Connectors/UniversalAssistance/Workflows/Activities/ProcessInsuranceCartActivity.php
@@ -271,7 +271,7 @@ class ProcessInsuranceCartActivity extends KanvasActivity
                             }
                         }
                     }
-                    
+
                     // If we found data with this orderKey, break out of the loop
                     if (! empty($insuranceData)) {
                         break;
