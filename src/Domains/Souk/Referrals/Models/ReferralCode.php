401790846f8717d72bd1dec1d48e21380630c8ac
Maximo Castro Pou <max@mctekk.com>
2025-12-01 10:41:14 -0400

    feat: add referral code

--- src/Domains/Souk/Referrals/Models/ReferralCode.php
+++ src/Domains/Souk/Referrals/Models/ReferralCode.php
@@ -6,6 +6,7 @@ namespace Kanvas\Souk\Referrals\Models;
 
 use Baka\Casts\Json;
 use Baka\Traits\UuidTrait;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Kanvas\Souk\Discounts\Models\Discount;
@@ -13,6 +14,8 @@ use Kanvas\Souk\Loyalty\Models\LoyaltyProgram;
 use Kanvas\Souk\Models\BaseModel;
 use Kanvas\Souk\Referrals\Factories\ReferralCodeFactory;
 use Override;
+
+use function Symfony\Component\Clock\now;
 
 /**
  * @property int $apps_id
@@ -122,6 +125,19 @@ class ReferralCode extends BaseModel
         $this->increment('current_uses');
 
         return $this;
+    }
+
+    public function scopeNotExpired($query): Builder
+    {
+        return $query->where(function ($q) {
+            $q->whereNull('expires_at')
+              ->orWhere('expires_at', '>', now());
+        });
+    }
+
+    public function isExpired(): bool
+    {
+        return $this->expires_at > now();
     }
 
     #[Override]
