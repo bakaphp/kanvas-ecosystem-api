53a011dcf4f9b083b5c975f70c30c24c1ed799ed
Maximo Castro Pou <max@mctekk.com>
2025-12-03 10:58:20 -0400

    feat: loyalty

--- app/Console/Commands/Workflows/KanvasWorkflowSynActionCommand.php
+++ app/Console/Commands/Workflows/KanvasWorkflowSynActionCommand.php
@@ -130,7 +130,9 @@ use Kanvas\Social\Messages\Workflows\Activities\MessageOwnerInteractionNotifierA
 use Kanvas\Social\Messages\Workflows\Activities\MessageReportNotificationActivity;
 use Kanvas\Social\Messages\Workflows\Activities\OptimizeImageFromMessageActivity;
 use Kanvas\Souk\Affiliates\Activities\CreateAffiliateConversionActivity;
+use Kanvas\Souk\Loyalty\Activities\ProcessOrderLoyaltyActivity;
 use Kanvas\Souk\Orders\Activities\B2BUpdateCompanyOrderActivity;
+use Kanvas\Souk\Referrals\Activities\ProcessUserSignupLoyaltyActivity;
 use Kanvas\Souk\Referrals\Activities\UserReferralCodeActivity;
 use Kanvas\Souk\Wallet\Activities\AddFundsToWalletActivity;
 use Kanvas\Souk\Wallet\Activities\PayFromWalletActivity;
@@ -281,6 +283,8 @@ class KanvasWorkflowSynActionCommand extends Command
             PushProductToItemActivity::class,
             UserReferralCodeActivity::class,
             CreateAffiliateConversionActivity::class,
+            ProcessUserSignupLoyaltyActivity::class,
+            ProcessOrderLoyaltyActivity::class,
         ];
 
         $createdActions = [];
