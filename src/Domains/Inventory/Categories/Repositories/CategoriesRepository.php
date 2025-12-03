fe923afd85cf0b10e1caa6f7c02644d85f2e0981
arfenis <arfen00@gmail.com>
2025-12-02 22:08:27 -0400

    Add method to getByResource

--- src/Domains/Inventory/Categories/Repositories/CategoriesRepository.php
+++ src/Domains/Inventory/Categories/Repositories/CategoriesRepository.php
@@ -6,6 +6,7 @@ namespace Kanvas\Inventory\Categories\Repositories;
 
 use Baka\Traits\SearchableTrait;
 use Kanvas\Inventory\Categories\Models\Categories;
+use Kanvas\SystemModules\Models\SystemModules;
 
 class CategoriesRepository
 {
@@ -14,5 +15,23 @@ class CategoriesRepository
     public static function getModel(): Categories
     {
         return new Categories();
+    }
+
+    public static function getByResource(string $categoryId, string $resource): Categories
+    {
+        $systemModule = SystemModules::where('model_name', $resource)
+            ->fromPublicApp()
+            ->firstOrFail();
+
+        $systemModuleId = $systemModule->id;
+
+        $category = Categories::query()
+            ->fromApp()
+            ->fromCompany()
+            ->fromResource($systemModuleId)
+            ->where('id', $categoryId)
+            ->firstOrFail();
+
+        return $category;
     }
 }
