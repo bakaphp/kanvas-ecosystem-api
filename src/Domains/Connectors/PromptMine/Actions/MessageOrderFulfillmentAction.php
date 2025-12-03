c366d21903fb204de6d790a252a8f5c26d09b188
Rafael White <rwhite@mctekk.com>
2025-12-03 12:34:29 -0400

    fix issue with credits not being debitted from account

--- src/Domains/Connectors/PromptMine/Actions/MessageOrderFulfillmentAction.php
+++ src/Domains/Connectors/PromptMine/Actions/MessageOrderFulfillmentAction.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Kanvas\Connectors\PromptMine\Actions;
 
+use Illuminate\Support\Facades\Log;
 use Kanvas\Inventory\Variants\Models\Variants;
 use Kanvas\Social\Messages\Models\Message;
 use Kanvas\Users\Models\Users;
@@ -33,6 +34,8 @@ class MessageOrderFulfillmentAction
 
         $orderCredit = $this->user->get('order_credits', []);
 
+         \Illuminate\Support\Facades\Log::info('Initial order credits', ['user_id' => $this->user->getId(), 'order_credits' => $orderCredit]);
+
         if (isset($orderCredit[$aiIndex][$modelIndex]) && $orderCredit[$aiIndex][$modelIndex] > 0) {
             $orderCredit[$aiIndex][$modelIndex] -= 1;
 
@@ -57,6 +60,8 @@ class MessageOrderFulfillmentAction
                     $orderCredit[$aiIndex][$modelIndex] += 1;
                 }
             }
+
+            \Illuminate\Support\Facades\Log::info('Updating user order credits', ['user_id' => $this->user->getId(), 'order_credits' => $orderCredit]);
 
             $this->user->set('order_credits', $orderCredit, true);
         }
