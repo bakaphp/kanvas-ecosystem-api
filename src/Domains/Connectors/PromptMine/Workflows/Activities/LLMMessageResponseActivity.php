cddb46d2b9128cf98f10217472291d7b3e6ad2b2
Rafael White <rwhite@mctekk.com>
2025-12-05 15:05:03 -0400

    if skipped messages greater than and nsfw flag then

--- src/Domains/Connectors/PromptMine/Workflows/Activities/LLMMessageResponseActivity.php
+++ src/Domains/Connectors/PromptMine/Workflows/Activities/LLMMessageResponseActivity.php
@@ -444,7 +444,7 @@ class LLMMessageResponseActivity extends KanvasActivity
             $channel = $message->channels?->first();
             $previousChatResponse = $channel !== null ? $channel->getPreviousMessage($message) : null;
 
-            if ($previousChatResponse !== null) {
+            if ($previousChatResponse !== null && ! $previousChatResponse->isRoot()) {
                 $previousChatChildMessage = $previousChatResponse->children()?->first();
                 //We need to make sure previous response is not nsfw or error in image creation(for some reason nsfw flag also works for other errors)
                 while ((isset($previousChatChildMessage->message['nsfw_flag']) && $previousChatChildMessage->message['nsfw_flag'])
@@ -454,6 +454,12 @@ class LLMMessageResponseActivity extends KanvasActivity
                     $previousChatResponse = $channel->getPreviousMessage($previousChatResponse);
                     $previousChatChildMessage = $previousChatResponse->children()?->first();
                     $messagesSkipped++;
+                }
+
+                if ($previousChatResponse->isRoot()
+                    && (isset($previousChatChildMessage->message['nsfw_flag']) && $previousChatChildMessage->message['nsfw_flag'])
+                    && (isset($previousChatChildMessage->message['nsfw_reason']) && ! is_null($previousChatChildMessage->message['nsfw_reason']))) {
+                    $previousChatResponse = null;
                 }
             }
 
