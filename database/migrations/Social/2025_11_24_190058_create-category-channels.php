f28dd9440dd1de0d0c11fcf1a58f5246696e2c6e
Maximo Castro Pou <max@mctekk.com>
2025-12-05 08:50:16 -0400

    feat: social channel

--- database/migrations/Social/2025_11_24_190058_create-category-channels.php
+++ database/migrations/Social/2025_11_24_190058_create-category-channels.php
@@ -10,15 +10,15 @@ return new class () extends Migration {
      */
     public function up(): void
     {
-        Schema::create('channel_categories', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->integer('companies_id')->index();
-            $table->integer('apps_id')->index();
-            $table->boolean('is_deleted')->default(0);
-            $table->timestamp('created_at')->index()->useCurrent();
-            $table->datetime('updated_at')->nullable()->index();
-        });
+        /*  Schema::create('channel_categories', function (Blueprint $table) {
+             $table->id();
+             $table->string('name');
+             $table->integer('companies_id')->index();
+             $table->integer('apps_id')->index();
+             $table->boolean('is_deleted')->default(0);
+             $table->timestamp('created_at')->index()->useCurrent();
+             $table->datetime('updated_at')->nullable()->index();
+         }); */
     }
 
     /**
