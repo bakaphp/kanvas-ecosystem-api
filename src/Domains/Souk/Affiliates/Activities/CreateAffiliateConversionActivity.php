ec5464c545f223f70a756421b7530787fb75263f
Maximo Castro Pou <max@mctekk.com>
2025-12-05 12:35:22 -0400

    fix: affiliate conversion

--- src/Domains/Souk/Affiliates/Activities/CreateAffiliateConversionActivity.php
+++ src/Domains/Souk/Affiliates/Activities/CreateAffiliateConversionActivity.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Kanvas\Souk\Affiliates\Activities;
 
+use Illuminate\Database\Eloquent\Builder;
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Souk\Affiliates\Actions\CreateAffiliateConversionAction;
+use Kanvas\Souk\Affiliates\Models\AffiliateLink;
 use Kanvas\Souk\Orders\Models\Order;
 use Kanvas\Workflow\Enums\IntegrationsEnum;
 use Kanvas\Workflow\KanvasActivity;
@@ -22,10 +24,22 @@ class CreateAffiliateConversionActivity extends KanvasActivity
             entity: $order,
             app: $app,
             integration: IntegrationsEnum::INTERNAL,
-            integrationOperation: function (Order $order) {
+            additionalParams: $params,
+            integrationOperation: function (Order $order, $app, $integrationCompany, $additionalParams): array {
                 $affiliateId = $order->get('affiliate_id') ?? $order->metadata['affiliate_id'] ?? null;
+                $affiliateLink = $order->get('affiliate_link_code') ?? $order->metadata['affiliate_link_code'] ?? null;
 
-                if ($affiliateId === null) {
+                $affiliateLink = AffiliateLink::fromApp($app)
+                    ->where('companies_id', $order->companies_id)
+                    ->where(function (Builder $query) use ($affiliateLink, $affiliateId) {
+                        $query->where('code', $affiliateLink)
+                            ->orWhere('code', $affiliateId);
+                    })
+                    ->first();
+
+                //$affiliateId = $affiliateLink?->affiliates_id ?? $affiliateId;
+
+                if ($affiliateLink === null) {
                     return $this->failWorkflow([
                         'result' => false,
                         'message' => 'Order does not have affiliate_id in custom_fields or metadata',
@@ -33,8 +47,11 @@ class CreateAffiliateConversionActivity extends KanvasActivity
                     ]);
                 }
 
-                $action = new CreateAffiliateConversionAction($order);
-                $conversion = $action->execute();
+                $order->set('affiliate_id', $affiliateLink->affiliates_id);
+                $order->set('affiliate_link_id', $affiliateLink->id);
+                $order->addMetadata('affiliate_id', $affiliateLink->affiliates_id);
+
+                $conversion = new CreateAffiliateConversionAction($order)->execute();
 
                 return [
                     'result' => true,
