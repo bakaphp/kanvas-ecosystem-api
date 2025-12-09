56bac87f6260d72ee76b85112f1502822404bdad
Maximo Castro Pou <max@mctekk.com>
2025-12-09 01:08:35 -0400

    feat: twilio webhook

--- src/Domains/Connectors/Internal/Activities/GeneratePdfActivity.php
+++ src/Domains/Connectors/Internal/Activities/GeneratePdfActivity.php
@@ -16,6 +16,7 @@ use Kanvas\ActionEngine\Tasks\Models\TaskListItem;
 use Kanvas\Filesystem\Services\PdfService;
 use Kanvas\Social\Messages\Models\Message;
 use Kanvas\Workflow\Contracts\WorkflowActivityInterface;
+use Kanvas\Workflow\Enums\IntegrationsEnum;
 use Kanvas\Workflow\KanvasActivity;
 use Override;
 
@@ -30,76 +31,94 @@ class GeneratePdfActivity extends KanvasActivity implements WorkflowActivityInte
         $pdfFileName = $params['pdf_file_name'] ?? null;
         $errorMessage = null;
 
-        if ($pdfTemplate === null) {
-            return [
-                'message' => 'No template configured to generate pdf',
-                'entity_id' => $entity->getId(),
-            ];
-        }
-
-        if ($pdfFileName === null) {
-            return [
-                'message' => 'No file name configured to generate pdf',
-                'entity_id' => $entity->getId(),
-            ];
-        }
-
-        $pdfData = array_merge([
-            'app' => $app,
-        ], $params);
-
-        $pdfFile = PdfService::generatePdfFromTemplate(
-            $app,
-            $entity->user,
-            $pdfTemplate,
-            $entity,
-            $pdfData
-        );
-
-        $entity->addFile($pdfFile, $pdfFileName);
-
-        //@todo any better way to do this?
-        if ($entity instanceof Message && $entity->parent) {
-            $entity->parent->addFile($pdfFile, $pdfFileName);
-        }
-
-        /**
-         * @todo MOVE THIS TO ITS OWN ACTIVITY
-         */
-        if ($entity instanceof Message && isset($entity->message['checkListId'])) {
-            try {
-                $action = Action::getBySlug($entity->message['verb'], $entity->company);
-                $companyAction = CompanyAction::getByAction($action, $entity->company, $app);
-                $engagement = Engagement::getByMessageId($entity->getId());
-
-                $companyTaskList = TaskListItem::query()->where('companies_action_id', $companyAction->getId())
-                    ->where('task_list_id', $entity->message['checkListId'])
-                    ->where('is_deleted', 0)
-                    ->first();
-
-                //$entity->message['checkListId'] = $entity->message['checkListId'];
-                if ($companyTaskList) {
-                    new ChangeTaskEngagementItemStatusAction(
-                        taskListItem: $companyTaskList,
-                        lead: $engagement->lead,
-                        status: TaskStatusEnum::COMPLETED->value,
-                        user: $engagement->user,
-                        app: $app,
-                        company: $engagement->company,
-                        message: $entity
-                    )->execute();
+        return $this->executeIntegration(
+            entity: $entity,
+            app: $app,
+            integration: IntegrationsEnum::INTERNAL,
+            integrationOperation: function ($buyerCompany, $app, $integrationCompany, $additionalParams) use ($pdfTemplate, $pdfFileName, $entity, $params): array {
+                if ($pdfTemplate === null) {
+                    return [
+                        'message' => 'No template configured to generate pdf',
+                        'entity_id' => $entity->getId(),
+                    ];
                 }
-            } catch (Exception $e) {
-                $errorMessage = $e->getMessage();
-            }
-        }
 
-        return [
-            'message' => 'Pdf generated successfully',
-            'entity_id' => $entity->getId(),
-            'file_id' => $pdfFile->getId(),
-            'file_url' => $pdfFile->url,
-            'error' => $errorMessage,
-        ];
+                if ($pdfFileName === null) {
+                    return [
+                        'message' => 'No file name configured to generate pdf',
+                        'entity_id' => $entity->getId(),
+                    ];
+                }
+
+                $pdfData = array_merge([
+                    'app' => $app,
+                ], $params);
+
+                $pdfFile = PdfService::generatePdfFromTemplate(
+                    $app,
+                    $entity->user,
+                    $pdfTemplate,
+                    $entity,
+                    $pdfData
+                );
+
+                $entity->addFile($pdfFile, $pdfFileName);
+
+                //@todo any better way to do this?
+                if ($entity instanceof Message && $entity->parent) {
+                    $entity->parent->addFile($pdfFile, $pdfFileName);
+                }
+
+                /**
+                 * @todo MOVE THIS TO ITS OWN ACTIVITY
+                 */
+                if ($entity instanceof Message && isset($entity->message['checkListId'])) {
+                    try {
+                        $action = Action::getBySlug($entity->message['verb'], $entity->company);
+                        $companyAction = CompanyAction::getByAction($action, $entity->company, $app);
+                        $engagement = Engagement::getByMessageId($entity->getId());
+
+                        $companyTaskList = TaskListItem::query()->where('companies_action_id', $companyAction->getId())
+                            ->where('task_list_id', $entity->message['checkListId'])
+                            ->where('is_deleted', 0)
+                            ->first();
+
+                        //$entity->message['checkListId'] = $entity->message['checkListId'];
+                        if ($companyTaskList) {
+                            new ChangeTaskEngagementItemStatusAction(
+                                taskListItem: $companyTaskList,
+                                lead: $engagement->lead,
+                                status: TaskStatusEnum::COMPLETED->value,
+                                user: $engagement->user,
+                                app: $app,
+                                company: $engagement->company,
+                                message: $entity
+                            )->execute();
+                        }
+                    } catch (Exception $e) {
+                        $errorMessage = $e->getMessage() . $e->getTraceAsString();
+                    }
+                }
+
+                if ($errorMessage !== null) {
+                    return $this->failWorkflow([
+                        'message' => 'Pdf generated with errors',
+                        'entity_id' => $entity->getId(),
+                        'file_id' => $pdfFile->getId(),
+                        'file_url' => $pdfFile->url,
+                        'error' => $errorMessage,
+                    ]);
+                }
+
+                return [
+                    'message' => 'Pdf generated successfully',
+                    'entity_id' => $entity->getId(),
+                    'file_id' => $pdfFile->getId(),
+                    'file_url' => $pdfFile->url,
+                    'error' => $errorMessage,
+                ];
+            },
+            company: $entity->company
+        );
     }
 }
