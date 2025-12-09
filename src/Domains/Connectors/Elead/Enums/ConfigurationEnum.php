8e3e5b21acedd20081d0fbacde7916de32e3d3ef
FredPeal <frederickpeal@gmail.com>
2025-12-08 22:50:05 -0400

    refactor: pull user by employee activity

--- src/Domains/Connectors/Elead/Enums/ConfigurationEnum.php
+++ src/Domains/Connectors/Elead/Enums/ConfigurationEnum.php
@@ -4,9 +4,19 @@ declare(strict_types=1);
 
 namespace Kanvas\Connectors\Elead\Enums;
 
+use Baka\Users\Contracts\UserInterface;
+use Kanvas\Companies\Models\Companies;
+
 enum ConfigurationEnum: string
 {
     case ELEAD_API_KEY = 'ELEAD_API_KEY';
     case ELEAD_API_SECRET = 'ELEAD_API_SECRET';
     case ELEAD_DEV_MODE = 'ELEAD_DEV_MODE';
+    case COMPANY = 'ELEAD_COMPANY';
+    case USER = 'ELEAD_USER';
+
+    public static function getUserKey(Companies $company, UserInterface $user): string
+    {
+        return self::USER->value . '_' . $company->getId() . '_' . $user->getId();
+    }
 }
