aa3a2e70b37675f3065947c424a955b68d24a589
Maximo Castro Pou <max@mctekk.com>
2025-12-16 15:36:54 -0400

    fix: channel id fix

--- src/Domains/Connectors/Zoho/Jobs/UpdateLeadFromZohoDealWebhookJob.php
+++ src/Domains/Connectors/Zoho/Jobs/UpdateLeadFromZohoDealWebhookJob.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Kanvas\Connectors\Zoho\Jobs;
 
 use Kanvas\Connectors\Zoho\Client;
+use Kanvas\Connectors\Zoho\Enums\CustomFieldEnum;
 use Kanvas\Guild\Leads\Models\Lead;
+use Kanvas\Guild\Leads\Models\LeadStatus;
 use Kanvas\Workflow\Jobs\ProcessWebhookJob;
 use Override;
 
@@ -44,8 +46,10 @@ class UpdateLeadFromZohoDealWebhookJob extends ProcessWebhookJob
         $dealDescription = $zohoDealInfo['Description'] ?? 'No Description';
         $agentNotes = $zohoDealInfo['Agent_Notes'] ?? 'No Agent Notes';
         $userWritingNotes = $zohoDealInfo['Underwriter_Notes1'] ?? 'No User Writing Notes';
+        $leadId = $zohoDealInfo['ZCRM_Lead_ID'] ?? null;
+        $dealStage = $zohoDealInfo['Stage'] ?? null;
 
-        if (empty($zohoDealInfo['SubID'])) {
+        if ($leadId === null) {
             return [
                 'message' => 'Zoho Deal SubID not found',
                 'zoho_data' => $zohoDealInfo,
@@ -56,15 +60,40 @@ class UpdateLeadFromZohoDealWebhookJob extends ProcessWebhookJob
          * @todo  make this dynamic to find the lead by other custom fields
          */
         $lead = Lead::getByCustomField(
-            'SubID',
-            (string) ($zohoDealInfo['SubID'] ?? ''),
+            CustomFieldEnum::ZOHO_LEAD_ID->value,
+            (string) $leadId,
             $this->receiver->company
         );
+
+        if (! $lead) {
+            return [
+                'message' => 'Lead not found for Zoho Lead ID',
+                'zoho_data' => $zohoDealInfo,
+            ];
+        }
+
+        if ($lead->company->getId() !== $this->receiver->company->getId() || $lead->app->getId() !== $this->receiver->app->getId()) {
+            return [
+                'message' => 'Lead company does not match receiver company',
+                'zoho_data' => $zohoDealInfo,
+            ];
+        }
 
         if ($lead) {
             $lead->set('agent_notes', $agentNotes);
             $lead->set('uw_notes', $userWritingNotes);
             $lead->description = $dealDescription;
+
+            $leadStatus = LeadStatus::firstOrCreate(
+                [
+                    'name' => $dealStage,
+                ],
+                [
+                    'is_default' => 0,
+                ]
+            );
+
+            $lead->leads_status_id = $leadStatus->id;
             $lead->saveOrFail();
         }
 
