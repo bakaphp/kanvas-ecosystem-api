ab1bcee48a1a51217a797ac7fa664493139a8d89
Rafael White <rwhite@mctekk.com>
2026-01-06 16:28:58 -0400

    fix: remove unused imports from TagsObserver class

--- src/Domains/Social/Tags/Observers/TagsObserver.php
+++ src/Domains/Social/Tags/Observers/TagsObserver.php
@@ -4,11 +4,6 @@ declare(strict_types=1);
 
 namespace Kanvas\Social\Tags\Observers;
 
-use Kanvas\Social\Messages\Actions\CheckMessagePostLimitAction;
-use Kanvas\Social\Messages\Models\Message;
-use Kanvas\Social\Messages\Validations\MessageSchemaValidator;
-use Kanvas\Workflow\Enums\WorkflowEnum;
-
 class TagsObserver
 {
 }
