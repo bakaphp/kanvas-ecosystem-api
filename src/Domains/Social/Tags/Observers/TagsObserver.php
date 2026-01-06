26269354953ee26062573c1d5f25c2c43ebd731b
Rafael White <rwhite@mctekk.com>
2026-01-06 16:28:21 -0400

    feat: add TagsObserver class for handling tag-related events

---
+++ src/Domains/Social/Tags/Observers/TagsObserver.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Social\Tags\Observers;
+
+use Kanvas\Social\Messages\Actions\CheckMessagePostLimitAction;
+use Kanvas\Social\Messages\Models\Message;
+use Kanvas\Social\Messages\Validations\MessageSchemaValidator;
+use Kanvas\Workflow\Enums\WorkflowEnum;
+
+class TagsObserver
+{
+}
