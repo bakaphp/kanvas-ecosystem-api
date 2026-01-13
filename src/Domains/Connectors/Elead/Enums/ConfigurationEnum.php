860f99e974a450ed02cfdaffa2b895c11ee5dae9
FredPeal <frederickpeal@gmail.com>
2026-01-13 10:30:45 -0400

    refactor: same crm-sa-api

--- src/Domains/Connectors/Elead/Enums/ConfigurationEnum.php
+++ src/Domains/Connectors/Elead/Enums/ConfigurationEnum.php
@@ -13,10 +13,10 @@ enum ConfigurationEnum: string
     case ELEAD_API_SECRET = 'ELEAD_API_SECRET';
     case ELEAD_DEV_MODE = 'ELEAD_DEV_MODE';
     case COMPANY = 'ELEAD_COMPANY';
-    case USER = 'ELEAD_USER';
+    case USER = 'ELEADS_SOLUTION_USER';
 
     public static function getUserKey(Companies $company, UserInterface $user): string
     {
-        return self::USER->value . '_' . $company->getId() . '_' . $user->getId();
+        return self::USER . '_' . $company->getId();
     }
 }
