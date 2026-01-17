852cd7da80f592173dcdb04c94aa2089985c90ed
Maximo Castro Pou <max@mctekk.com>
2026-01-16 22:19:22 -0400

    fix: drivecentric lead pull action

--- src/Domains/Guild/Customers/Actions/SyncPeopleByThirdPartyCustomFieldAction.php
+++ src/Domains/Guild/Customers/Actions/SyncPeopleByThirdPartyCustomFieldAction.php
@@ -43,7 +43,7 @@ class SyncPeopleByThirdPartyCustomFieldAction
             }
 
             $createPeople->runWorkflow = $this->people->runWorkflow ?? true;
-            
+
             return $createPeople->execute();
         });
     }
