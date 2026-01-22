b4191f6cf9ccb5b78f534287fb06e11d718003c6
FredPeal <frederickpeal@gmail.com>
2026-01-22 12:54:11 -0400

    refactor: change to is_service_lead

--- src/Domains/Connectors/WaSender/Webhooks/ProcessWaSenderWebhookJob.php
+++ src/Domains/Connectors/WaSender/Webhooks/ProcessWaSenderWebhookJob.php
@@ -181,7 +181,7 @@ class ProcessWaSenderWebhookJob extends ProcessWebhookJob
                             LeadsEnumsConfigurationEnum::FIRST_MESSAGE->value,
                             $messageBody
                         );
-                        $lead->set('lead_from_whatsapp', 1);
+                        $lead->set('is_service_lead', 1);
                     }
 
                     //status = 2 , means user delivery, status = 1 means api delivery
