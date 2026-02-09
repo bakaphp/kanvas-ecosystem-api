ef3841cb3bda8be2a66ec5d8cd87f4154599b8ba
freesgen <jesusant.guerrero@gmail.com>
2026-02-09 18:19:11 -0400

    add a transitional state

--- src/Domains/Souk/Payments/Enums/PaymentStatusEnum.php
+++ src/Domains/Souk/Payments/Enums/PaymentStatusEnum.php
@@ -5,9 +5,13 @@ namespace Kanvas\Souk\Payments\Enums;
 enum PaymentStatusEnum: string
 {
     case PENDING = 'pending';
+    // transitional statuses
     case WAITING_DEVICE_DATA = 'waiting_device_data';
     case PENDING_AUTHORIZATION = 'pending_authorization';
+    case PROCESSING = 'processing';
+    // authorized
     case AUTHORIZED = 'authorized';
+    // final states
     case PAID = 'paid';
     case FAILED = 'failed';
     case REVERSED = 'refunded';
