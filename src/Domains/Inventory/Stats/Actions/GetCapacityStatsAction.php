9b81e560f761e2b0ca0a318499be42ba8ff6afb2
freesgen <jesusant.guerrero@gmail.com>
2026-02-09 00:13:27 -0400

    feat: add payment

--- src/Domains/Inventory/Stats/Actions/GetCapacityStatsAction.php
+++ src/Domains/Inventory/Stats/Actions/GetCapacityStatsAction.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Kanvas\Inventory\Stats\Actions;
 
-use Illuminate\Support\Facades\DB;
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Companies\Models\Companies;
 use Kanvas\Inventory\Stats\DataTransferObject\CapacityStats;
