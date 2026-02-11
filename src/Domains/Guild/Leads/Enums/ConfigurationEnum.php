3d856ea6ae71a2c83f73dd21c674c5ef29781298
FredPeal <frederickpeal@gmail.com>
2026-02-11 02:55:16 -0400

    refactor: follow up in preferred channel

--- src/Domains/Guild/Leads/Enums/ConfigurationEnum.php
+++ src/Domains/Guild/Leads/Enums/ConfigurationEnum.php
@@ -14,4 +14,5 @@ enum ConfigurationEnum: string
     case IS_ENGAGEMENT = 'guild_is_engagement';
     case CONTACTED = 'guild_lead_contacted';
     case FOLLOW_UP_LEAD_TYPE = 'guild_follow_up_lead_type';
+    case PREFERRED_CHANNEL = 'guild_preferred_channel';
 }
