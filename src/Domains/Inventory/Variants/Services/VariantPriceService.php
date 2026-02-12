ceb6f834741704cc2a3f319ffe39215b09a92b9f
Manuel Pellerano <pelleranomanuel@gmail.com>
2026-02-12 11:58:24 -0400

    filter by app

--- src/Domains/Inventory/Variants/Services/VariantPriceService.php
+++ src/Domains/Inventory/Variants/Services/VariantPriceService.php
@@ -144,7 +144,7 @@ class VariantPriceService
     private function getCurrencyFromWarehouse(Variants $variant, ?int $warehouseId = null): Currencies
     {
         if ($warehouseId) {
-            $warehouse = Warehouses::find($warehouseId);
+            $warehouse = Warehouses::where('apps_id', $this->app->getId())->find($warehouseId);
             if ($warehouse && $warehouse->region && $warehouse->region->currency) {
                 return $warehouse->region->currency;
             }
