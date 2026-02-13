20e958bfb6e748d9981805b9c473cf7d230441fd
Maximo Castro Pou <max@mctekk.com>
2026-02-12 23:49:09 -0400

    feat: add rules crud

---
+++ src/Domains/Workflow/Rules/Actions/UpdateRuleAction.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Workflow\Rules\Actions;
+
+use Illuminate\Support\Facades\DB;
+use Kanvas\Workflow\Rules\DataTransferObject\RuleActionData;
+use Kanvas\Workflow\Rules\DataTransferObject\RuleConditionData;
+use Kanvas\Workflow\Rules\DataTransferObject\Rule as RuleData;
+use Kanvas\Workflow\Rules\Models\Rule;
+use Kanvas\Workflow\Rules\Models\RuleAction;
+use Kanvas\Workflow\Rules\Models\RuleWorkflowAction;
+
+class UpdateRuleAction
+{
+    public function __construct(
+        protected readonly Rule $rule,
+        protected readonly RuleData $data,
+    ) {
+    }
+
+    public function execute(): Rule
+    {
+        return DB::connection('workflow')->transaction(function (): Rule {
+            $this->rule->update([
+                'name' => $this->data->name,
+                'description' => $this->data->description,
+                'rules_types_id' => $this->data->ruleType->getId(),
+                'systems_modules_id' => $this->data->systemModule->getId(),
+                'companies_id' => $this->data->company->getId(),
+                'pattern' => $this->data->pattern,
+                'params' => $this->data->params,
+                'is_async' => $this->data->is_async,
+            ]);
+
+            if ($this->data->conditions !== null && $this->data->conditions->count() > 0) {
+                $this->rule->getRulesConditions()->update(['is_deleted' => 1]);
+
+                /** @var RuleConditionData $condition */
+                foreach ($this->data->conditions as $condition) {
+                    $this->rule->getRulesConditions()->create([
+                        'attribute_name' => $condition->attribute_name,
+                        'operator' => $condition->operator->value,
+                        'value' => $condition->value,
+                        'is_deleted' => 0,
+                    ]);
+                }
+            }
+
+            if ($this->data->actions !== null && $this->data->actions->count() > 0) {
+                $this->rule->workflowActivities()->update(['is_deleted' => 1]);
+
+                $weight = 0;
+                /** @var RuleActionData $actionData */
+                foreach ($this->data->actions as $actionData) {
+                    $ruleWorkflowAction = RuleWorkflowAction::firstOrCreate([
+                        'system_modules_id' => $this->data->systemModule->getId(),
+                        'actions_id' => $actionData->action->getId(),
+                    ], [
+                        'is_deleted' => 0,
+                    ]);
+
+                    RuleAction::create([
+                        'rules_id' => $this->rule->getId(),
+                        'rules_workflow_actions_id' => $ruleWorkflowAction->getId(),
+                        'weight' => $actionData->weight ?? $weight,
+                        'is_deleted' => 0,
+                    ]);
+
+                    $weight++;
+                }
+            }
+
+            return $this->rule->refresh();
+        });
+    }
+}
