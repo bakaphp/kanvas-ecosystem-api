b2a9135062dc51dc0fd2246aae0ee35f9ec19099
FredPeal <frederickpeal@gmail.com>
2026-03-05 11:55:39 -0400

    hotfix: workflow enum

--- src/Domains/Workflow/Enums/WorkflowEnum.php
+++ src/Domains/Workflow/Enums/WorkflowEnum.php
@@ -47,6 +47,7 @@ enum WorkflowEnum: string
     case AFTER_LEGACY_FEED = 'after-legacy-feed';
     case SUBSCRIPTION_LIFECYCLE = 'subscription-lifecycle';
     case FOLLOW_UP_PROMPT = 'follow-up-prompt';
+    case DEBUG = 'debug';
 
     /**
      * Get the enum case by its value.
