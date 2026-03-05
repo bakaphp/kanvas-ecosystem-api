2720e05f2128ae3c0551415b98e63b6e56965e72
Maximo Castro Pou <max@mctekk.com>
2026-03-05 17:38:41 -0400

    refactor: update wordpress download inventory action to use the new api

--- app/GraphQL/Ecosystem/Mutations/CustomFields/CustomFieldMutation.php
+++ app/GraphQL/Ecosystem/Mutations/CustomFields/CustomFieldMutation.php
@@ -4,9 +4,20 @@ declare(strict_types=1);
 
 namespace App\GraphQL\Ecosystem\Mutations\CustomFields;
 
+use Baka\Contracts\AppInterface;
+use Baka\Users\Contracts\UserInterface;
+use Exception;
+use Kanvas\Apps\Models\Apps;
 use Kanvas\CustomFields\DataTransferObject\CustomFieldInput;
 use Kanvas\CustomFields\Models\AppsCustomFields;
 use Kanvas\SystemModules\Repositories\SystemModulesRepository;
+use Kanvas\Users\Models\Users;
+
+use function Sentry\captureException;
+
+use Sentry\State\Scope;
+
+use function Sentry\withScope;
 
 class CustomFieldMutation
 {
@@ -20,6 +31,18 @@ class CustomFieldMutation
         $entity = SystemModulesRepository::getEntityFromInput($customFieldInput, auth()->user());
 
         if (method_exists($entity, 'set')) {
+            if ($entity instanceof Users || $entity instanceof UserInterface) {
+                $customFields = $this->filterPublicCustomFields(
+                    app: app(Apps::class),
+                    user: $entity,
+                    customFields: [$customFieldInput->name => $customFieldInput->data]
+                );
+
+                if (empty($customFields)) {
+                    return false;
+                }
+            }
+
             $customField = $entity->set(
                 $customFieldInput->name,
                 $customFieldInput->data
@@ -81,5 +104,70 @@ class CustomFieldMutation
         }
 
         return false;
+    }
+
+    protected function filterPublicCustomFields(AppInterface $app, UserInterface $user, array $customFields): array
+    {
+        $blockedCustomFields = $app->get('blocked_user_custom_fields');
+
+        if (! $blockedCustomFields || ! is_array($blockedCustomFields)) {
+            return $customFields;
+        }
+
+        $filteredFields = [];
+        $attemptedBlockedFields = [];
+
+        // Detect format: check if first element has 'name' key (array format)
+        $isArrayFormat = isset($customFields[0]) && is_array($customFields[0]) && isset($customFields[0]['name']);
+
+        if ($isArrayFormat) {
+            // Array format: [['name' => 'field', 'data' => 'value']]
+            foreach ($customFields as $field) {
+                if (! is_array($field)) {
+                    continue;
+                }
+
+                $fieldName = $field['name'] ?? null;
+                if (! $fieldName || ! is_string($fieldName)) {
+                    continue;
+                }
+
+                if (in_array($fieldName, $blockedCustomFields, true)) {
+                    $attemptedBlockedFields[] = $fieldName;
+                } else {
+                    $filteredFields[] = $field;
+                }
+            }
+        } else {
+            // Key-value format: ['field' => 'value']
+            foreach ($customFields as $fieldName => $fieldValue) {
+                if (! is_string($fieldName)) {
+                    continue;
+                }
+
+                if (in_array($fieldName, $blockedCustomFields, true)) {
+                    $attemptedBlockedFields[] = $fieldName;
+                } else {
+                    $filteredFields[$fieldName] = $fieldValue;
+                }
+            }
+        }
+
+        if (! empty($attemptedBlockedFields)) {
+            withScope(function (Scope $scope) use ($attemptedBlockedFields, $blockedCustomFields, $user, $app): void {
+                $scope->setContext('custom_fields_violation', [
+                    'user_id' => $user->getId(),
+                    'app_id' => $app->getId(),
+                    'blocked_fields' => $attemptedBlockedFields,
+                    'configured_blocked_fields' => $blockedCustomFields,
+                ]);
+
+                captureException(new Exception(
+                    'User attempted to set blocked custom fields: ' . implode(', ', $attemptedBlockedFields)
+                ));
+            });
+        }
+
+        return $filteredFields;
     }
 }
