dac9f7d248f6adbf586792c15bea68fa13029071
Maximo Castro Pou <max@mctekk.com>
2026-03-11 08:51:40 -0400

    hotfix: issue

--- src/Domains/Intelligence/Workflows/HandOffActivity.php
+++ src/Domains/Intelligence/Workflows/HandOffActivity.php
@@ -84,6 +84,10 @@ class HandOffActivity extends KanvasActivity
                     $lead->saveOrFail();
                 }
 
+                if ($handOffType === 'compliance_internal') {
+                    $lead->people->optOutPhoneContacts();
+                }
+
                 //$communicationChannel = $lead->get(EnumsConfigurationEnum::AGENT_COMMUNICATION_CHANNEL->value) ?? 'sms';
                 $lead->set(ConfigurationEnum::AGENT_HAND_OFF->value, 1);
                 $lead->set(ConfigurationEnum::AGENT_HAND_OFF_TYPE->value, $handOffType);
