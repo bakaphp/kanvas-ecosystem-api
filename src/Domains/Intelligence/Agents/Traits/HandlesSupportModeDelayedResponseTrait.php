b1609ce845da7b7f5fc945ad371c5b3f3f9adb83
FredPeal <frederickpeal@gmail.com>
2026-03-19 12:12:44 -0400

    fix: handle support

--- src/Domains/Intelligence/Agents/Traits/HandlesSupportModeDelayedResponseTrait.php
+++ src/Domains/Intelligence/Agents/Traits/HandlesSupportModeDelayedResponseTrait.php
@@ -33,10 +33,15 @@ trait HandlesSupportModeDelayedResponseTrait
         $hasHumanMessage = $channel->messages()
             ->where('message->from_human', true)
             ->exists();
-        if ($lead->get('ai_mode') !== IntelligenceModeEnum::SUPPORT->value || ! $isWithinWorkingHours) {
-            if (! $hasHumanMessage) {
-                return null;
-            }
+
+        if (! $isWithinWorkingHours && ! $hasHumanMessage) {
+            return null;
+        }
+
+        $supportMode = $lead->get('ai_mode') == IntelligenceModeEnum::SUPPORT->value;
+
+        if (! $supportMode) {
+            return null;
         }
 
         if (UnrespondedLeadAgentMessageCache::exists($lead, $channel)) {
