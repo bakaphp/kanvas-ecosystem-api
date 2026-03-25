698ba2964535e94719bd278b28e33cfa8427d4a3
FredPeal <frederickpeal@gmail.com>
2026-03-25 15:46:49 -0400

    refactor: resolve conflicts

--- src/Domains/Social/Messages/Repositories/MessagesRepository.php
+++ src/Domains/Social/Messages/Repositories/MessagesRepository.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace Kanvas\Social\Messages\Repositories;
 
 use Baka\Contracts\AppInterface;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Kanvas\Apps\Models\Apps;
 use Kanvas\Companies\Models\Companies;
 use Kanvas\Guild\Leads\Models\Lead;
+use Kanvas\Social\Messages\Models\AppModuleMessage;
 use Kanvas\Social\Messages\Models\Message;
 use Kanvas\Social\MessagesTypes\Models\MessageType;
 use Kanvas\Users\Models\Users;
@@ -48,6 +49,18 @@ class MessagesRepository
             ->first();
 
         return $popularMessage ?? null;
+    }
+
+    public static function getLastMessageByEntity(Model $entity, AppInterface $app): ?Message
+    {
+        $appModuleMessage = AppModuleMessage::where('apps_id', $app->getId())
+            ->where('system_modules', get_class($entity))
+            ->where('entity_id', $entity->getId())
+            ->where('is_deleted', 0)
+            ->orderBy('created_at', 'desc')
+            ->first();
+
+        return $appModuleMessage?->message;
     }
 
     public static function getUnrespondedMessagesByLead(int $leadId, AppInterface $app): Collection
