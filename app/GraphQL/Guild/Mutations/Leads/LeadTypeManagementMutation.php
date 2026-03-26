c1ea9a9c2d3d6d1bc7ffbec56fced7cd0c5810dc
Maximo Castro Pou <max@mctekk.com>
2026-03-26 19:58:15 -0400

    feat: lead source and type

--- app/GraphQL/Guild/Mutations/Leads/LeadTypeManagementMutation.php
+++ app/GraphQL/Guild/Mutations/Leads/LeadTypeManagementMutation.php
@@ -5,35 +5,31 @@ declare(strict_types=1);
 namespace App\GraphQL\Guild\Mutations\Leads;
 
 use Kanvas\Apps\Models\Apps;
-use Kanvas\Companies\Repositories\CompaniesRepository;
 use Kanvas\Guild\Leads\Actions\CreateLeadTypeAction;
 use Kanvas\Guild\Leads\DataTransferObject\LeadType;
 use Kanvas\Guild\Leads\Models\LeadType as LeadTypeModel;
 
 class LeadTypeManagementMutation
 {
-    /**
-     * Create a new lead type.
-     */
     public function create(mixed $root, array $req): LeadTypeModel
     {
         $data = $req['input'];
         $data['apps'] = app(Apps::class);
-        $data['companies'] = CompaniesRepository::getByUuid($data['companies_id']);
+        $data['companies'] = auth()->user()->getCurrentCompany();
 
         $leadType = LeadType::from($data);
 
-        return (new CreateLeadTypeAction($leadType))->execute();
+        return new CreateLeadTypeAction($leadType)->execute();
     }
 
     public function update(mixed $root, array $req): LeadTypeModel
     {
         $app = app(Apps::class);
-        $companies = CompaniesRepository::getByUuid($req['input']['companies_id'], $app, auth()->user());
-        $req['input']['companies_id'] = $companies->getId();
+        $company = auth()->user()->getCurrentCompany();
+        $input = $req['input'];
 
-        $leadType = LeadTypeModel::getByUuidFromCompanyApp($req['id'], app: app(Apps::class), company: $companies);
-        $leadType->update($req['input']);
+        $leadType = LeadTypeModel::getByUuidFromCompanyApp($req['id'], company: $company, app: $app);
+        $leadType->update($input);
 
         return $leadType;
     }
@@ -41,8 +37,8 @@ class LeadTypeManagementMutation
     public function delete(mixed $root, array $req): bool
     {
         $app = app(Apps::class);
-        $leadType = LeadTypeModel::getByUuidFromCompanyApp($req['id'], app: app(Apps::class));
-        $companies = CompaniesRepository::getByUuid($leadType->company->uuid, $app, auth()->user());
+        $company = auth()->user()->getCurrentCompany();
+        $leadType = LeadTypeModel::getByUuidFromCompanyApp($req['id'], company: $company, app: $app);
 
         return $leadType->delete();
     }
