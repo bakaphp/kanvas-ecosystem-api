9db8f401f4d11853af6af5e92283211295e38d55
arfenis <arfen00@gmail.com>
2026-03-31 17:09:09 -0400

    save recording url

--- src/Domains/Connectors/VoiceBridge/Jobs/SaveVoiceTranscriptJob.php
+++ src/Domains/Connectors/VoiceBridge/Jobs/SaveVoiceTranscriptJob.php
@@ -61,10 +61,18 @@ class SaveVoiceTranscriptJob implements ShouldQueue
             return;
         }
 
+        try {
+            $session = Client::getInstance($app)->getSessionTranscript($this->sessionId);
+        } catch (Throwable $e) {
+            report($e);
+            $session = [];
+        }
+
         new SaveVoiceTranscriptAction(
             $this->lead,
             $transcript,
             $this->sessionId,
+            $session['session']['recording_url'] ?? null,
         )->execute();
     }
 }
