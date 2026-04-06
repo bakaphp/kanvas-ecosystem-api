a5ce7220dba9532e81e5ef7a59628e826960a418
Maximo Castro Pou <max@mctekk.com>
2026-04-06 15:34:23 -0400

    hotfix: adf email

--- src/Domains/Connectors/SalesAssist/Activities/SendLeadAdfByEmailActivity.php
+++ src/Domains/Connectors/SalesAssist/Activities/SendLeadAdfByEmailActivity.php
@@ -7,10 +7,9 @@ namespace Kanvas\Connectors\SalesAssist\Activities;
 use Baka\Contracts\AppInterface;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Notification;
-use Kanvas\Apps\Models\Apps;
 use Kanvas\Connectors\SalesAssist\Actions\BuildLeadAdfXmlAction;
-use Kanvas\Connectors\SalesAssist\Notifications\LeadAdfXmlNotification;
 use Kanvas\Guild\Leads\Models\Lead;
+use Kanvas\Notifications\Templates\Blank;
 use Kanvas\Workflow\Contracts\WorkflowActivityInterface;
 use Kanvas\Workflow\Enums\IntegrationsEnum;
 use Kanvas\Workflow\KanvasActivity;
@@ -50,25 +49,19 @@ class SendLeadAdfByEmailActivity extends KanvasActivity implements WorkflowActiv
 
         $subject = $params['subject'] ?? 'New ADF Lead from Sales Assist';
         $xml = $this->buildAdfXml($lead, $params);
-        $attachmentName = 'lead-' . $lead->getId() . '.xml';
-
-        $sendAsAttachment = (bool) ($params['send_as_attachment'] ?? false);
 
         return $this->executeIntegration(
             entity: $lead,
             app: $app,
             integration: IntegrationsEnum::INTERNAL,
             additionalParams: $params,
-            integrationOperation: function (Lead $lead, AppInterface $app, mixed $integrationCompany, array $additionalParams) use ($to, $subject, $xml, $attachmentName, $sendAsAttachment): array {
-                /** @var Apps $app */
+            integrationOperation: function (Lead $lead, AppInterface $app, mixed $integrationCompany, array $additionalParams) use ($to, $subject, $xml): array {
                 $this->sendAdfEmail(
                     lead: $lead,
                     app: $app,
                     to: $to,
                     subject: (string) $subject,
                     xml: $xml,
-                    attachmentName: $attachmentName,
-                    sendAsAttachment: $sendAsAttachment,
                 );
 
                 return [
@@ -77,8 +70,6 @@ class SendLeadAdfByEmailActivity extends KanvasActivity implements WorkflowActiv
                     'lead_id' => $lead->getId(),
                     'to' => $to,
                     'subject' => $subject,
-                    'attachment' => $sendAsAttachment ? $attachmentName : null,
-                    'delivery_mode' => $sendAsAttachment ? 'attachment' : 'body',
                 ];
             },
             company: $lead->company,
@@ -92,7 +83,7 @@ class SendLeadAdfByEmailActivity extends KanvasActivity implements WorkflowActiv
 
     protected function buildAdfXml(Lead $lead, array $params): string
     {
-        return (new BuildLeadAdfXmlAction())->execute($lead, [
+        return new BuildLeadAdfXmlAction()->execute($lead, [
             'source' => $params['source'] ?? 'Sales Assist',
             'providerName' => $params['provider_name'] ?? 'Sales Assist',
             'service' => $params['service'] ?? 'Sales Assist Lead Delivery',
@@ -101,26 +92,24 @@ class SendLeadAdfByEmailActivity extends KanvasActivity implements WorkflowActiv
 
     protected function sendAdfEmail(
         Lead $lead,
-        Apps $app,
+        AppInterface $app,
         string $to,
         string $subject,
         string $xml,
-        string $attachmentName,
-        bool $sendAsAttachment = false
     ): void {
-        $emailContent = $sendAsAttachment
-            ? 'Please find attached the ADF lead XML export.'
-            : $xml;
-
-        $notification = new LeadAdfXmlNotification(
-            lead: $lead,
-            app: $app,
-            subject: $subject,
-            emailContent: $emailContent,
-            xmlContent: $xml,
-            attachmentName: $attachmentName,
-            sendAsAttachment: $sendAsAttachment,
+        $notification = new Blank(
+            'blank',
+            [
+                'app' => $app,
+                'subject' => $subject,
+                'company' => $lead->company,
+                'html' => $xml,
+            ],
+            ['mail'],
+            $lead,
         );
+
+        $notification->setSubject($subject);
 
         Notification::route('mail', $to)->notify($notification);
     }
