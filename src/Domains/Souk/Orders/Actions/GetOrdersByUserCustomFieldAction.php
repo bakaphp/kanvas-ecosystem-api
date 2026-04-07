ebc537c447c64a2a6963cedf3c7f8780bfa5171f
Manuel Pellerano <pelleranomanuel@gmail.com>
2026-04-07 15:35:29 -0400

    change to make it usable globally

--- src/Domains/Souk/Orders/Actions/GetOrdersByUserCustomFieldAction.php
+++ src/Domains/Souk/Orders/Actions/GetOrdersByUserCustomFieldAction.php
@@ -2,27 +2,26 @@
 
 declare(strict_types=1);
 
-namespace Kanvas\Connectors\Movipass\Actions;
+namespace Kanvas\Souk\Orders\Actions;
 
 use Baka\Contracts\AppInterface;
 use Illuminate\Database\Eloquent\Builder;
-use Kanvas\Connectors\Movipass\Enums\CustomFieldEnum;
 use Kanvas\Souk\Orders\Models\Order;
-use Kanvas\Users\Models\Users;
 
-class GetMechanicAssignedOrdersAction
+class GetOrdersByUserCustomFieldAction
 {
     public function __construct(
-        protected readonly Users $mechanic,
         protected readonly AppInterface $app,
+        protected readonly string $customFieldName,
+        protected readonly int|string $userId,
     ) {
     }
 
     public function execute(): Builder
     {
         return Order::getByCustomFieldBuilder(
-            CustomFieldEnum::ORDER_MECHANIC_USERS_ID->value,
-            (string) $this->mechanic->getId(),
+            $this->customFieldName,
+            (string) $this->userId,
             null,
             false
         )
