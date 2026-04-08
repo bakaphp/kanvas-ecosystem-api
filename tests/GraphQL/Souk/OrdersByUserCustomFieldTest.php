d1d29751a6c09e71315edbd08500f68d2b096360
Manuel Pellerano <pelleranomanuel@gmail.com>
2026-04-08 12:08:34 -0400

    add test a add it to orders graphql

---
+++ tests/GraphQL/Souk/OrdersByUserCustomFieldTest.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\GraphQL\Souk;
+
+use Illuminate\Support\Facades\Auth;
+
+class OrdersByUserCustomFieldTest extends OrderBase
+{
+    protected string $variantId;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $productResponse = $this->createProduct(attributes: [
+            ['name' => 'slots', 'value' => 100],
+        ])->json()['data']['createProduct'];
+
+        $variantResponse = $this->createVariant(
+            productId: $productResponse['id'],
+            warehouseData: ['id' => $this->warehouseResponse['id']],
+        )->json()['data']['createVariant'];
+
+        $this->addVariantToChannel(
+            variantId: $variantResponse['id'],
+            channelId: $this->channelResponse['id'],
+            warehouseData: ['id' => $this->warehouseResponse['id']],
+        );
+
+        $this->addVariantToWarehouse(
+            variantId: $variantResponse['id'],
+            warehouseId: $this->warehouseResponse['id'],
+            amount: 100,
+        );
+
+        $this->variantId = $variantResponse['id'];
+    }
+
+    public function testOrdersByUserCustomField(): void
+    {
+        $user = Auth::user();
+        $customFieldName = 'test_user_ref_' . fake()->lexify('????');
+
+        $order = $this->createOrderFromCart([], $this->variantId);
+        $order->set($customFieldName, (string) $user->getId());
+
+        $this->graphQL('
+            query($custom_field_name: String!) {
+                ordersByUserCustomField(custom_field_name: $custom_field_name) {
+                    data {
+                        id
+                    }
+                }
+            }
+        ', ['custom_field_name' => $customFieldName])
+        ->assertSuccessful()
+        ->assertJsonStructure(['data' => ['ordersByUserCustomField' => ['data' => [['id']]]]]);
+    }
+
+    public function testOrdersByUserCustomFieldWithUserId(): void
+    {
+        $user = Auth::user();
+        $customFieldName = 'test_user_ref_admin_' . fake()->lexify('????');
+
+        $order = $this->createOrderFromCart([], $this->variantId);
+        $order->set($customFieldName, (string) $user->getId());
+
+        $this->graphQL('
+            query($custom_field_name: String!, $user_id: ID) {
+                ordersByUserCustomField(custom_field_name: $custom_field_name, user_id: $user_id) {
+                    data {
+                        id
+                    }
+                }
+            }
+        ', [
+            'custom_field_name' => $customFieldName,
+            'user_id' => $user->getId(),
+        ])
+        ->assertSuccessful()
+        ->assertJsonStructure(['data' => ['ordersByUserCustomField' => ['data' => [['id']]]]]);
+    }
+}
