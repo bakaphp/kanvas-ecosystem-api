c98b30bb7b62affe4b079e1e338131450b0060e6
Maximo Castro Pou <max@mctekk.com>
2026-04-09 11:23:37 -0400

    hotfix

--- src/Baka/Support/Random.php
+++ src/Baka/Support/Random.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Baka\Support;
 
 use Baka\Contracts\AppInterface;
-use Faker\Factory as FakerFactory;
+use Illuminate\Support\Str as LaravelStr;
 use InvalidArgumentException;
 use Kanvas\Users\Models\UsersAssociatedApps;
 
@@ -65,11 +65,7 @@ class Random
 
     private static function generatePrivateRelayUsername(): string
     {
-        $faker = FakerFactory::create();
-        $adjective = $faker->word();
-        $noun = $faker->word();
-
-        return strtolower($adjective . $noun);
+        return LaravelStr::lower(LaravelStr::random(10));
     }
 
     private static function cleanupEmailUsername(string $username): ?string
