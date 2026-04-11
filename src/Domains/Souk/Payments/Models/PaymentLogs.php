363af0bc106af0d4c04acc0ed3e75b89695c36ed
Maximo Castro Pou <max@mctekk.com>
2026-04-10 20:28:20 -0400

    add history

--- src/Domains/Souk/Payments/Models/PaymentLogs.php
+++ src/Domains/Souk/Payments/Models/PaymentLogs.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace Kanvas\Souk\Payments\Models;
 
 use Baka\Casts\Json;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Kanvas\Companies\Models\Companies;
 use Kanvas\Souk\Models\BaseModel;
+use Kanvas\Users\Models\Users;
 
 class PaymentLogs extends BaseModel
 {
@@ -15,4 +18,19 @@ class PaymentLogs extends BaseModel
     protected $casts = [
         'metadata' => Json::class,
     ];
+
+    public function payment(): BelongsTo
+    {
+        return $this->belongsTo(Payments::class, 'payments_id', 'id');
+    }
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Companies::class, 'companies_id', 'id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(Users::class, 'users_id', 'id');
+    }
 }
