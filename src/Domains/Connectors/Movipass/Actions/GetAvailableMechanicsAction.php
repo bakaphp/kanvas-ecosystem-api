8365f39bc2e3c27f2f04262bf7a2702931539d6e
Manuel Pellerano <pelleranomanuel@gmail.com>
2026-04-14 18:37:34 -0400

    remove provider filtering bug

--- src/Domains/Connectors/Movipass/Actions/GetAvailableMechanicsAction.php
+++ src/Domains/Connectors/Movipass/Actions/GetAvailableMechanicsAction.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Kanvas\Connectors\Movipass\Actions;
 
 use Illuminate\Support\Collection;
-use Kanvas\Companies\Models\Companies;
 use Kanvas\Connectors\Movipass\Enums\CustomFieldEnum;
 use Kanvas\Connectors\Movipass\Enums\MechanicAvailabilityEnum;
 use Kanvas\Users\Models\Users;
@@ -13,7 +12,6 @@ use Kanvas\Users\Models\Users;
 class GetAvailableMechanicsAction
 {
     public function __construct(
-        protected readonly ?Companies $providerCompany = null,
         protected readonly array $excludeIds = [],
     ) {
     }
@@ -26,11 +24,6 @@ class GetAvailableMechanicsAction
         ->where('user_config.name', CustomFieldEnum::MECHANIC_AVAILABILITY->value)
         ->where('user_config.value', MechanicAvailabilityEnum::ACTIVO->value)
         ->where('users.is_deleted', 0)
-        ->when(
-            $this->providerCompany,
-            fn ($q) => $q->join('users_associated_apps', 'users_associated_apps.users_id', '=', 'users.id')
-                ->where('users_associated_apps.companies_id', $this->providerCompany->getId())
-        )
         ->when(
             $this->excludeIds !== [],
             fn ($q) => $q->whereNotIn('users.id', $this->excludeIds)
