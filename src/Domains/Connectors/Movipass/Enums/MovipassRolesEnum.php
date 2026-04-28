4626b61459709c375b250ac6f36b2eaed5a998d4
Manuel Pellerano <pelleranomanuel@gmail.com>
2026-04-28 14:13:58 -0400

    add more mechanic role logic

--- src/Domains/Connectors/Movipass/Enums/MovipassRolesEnum.php
+++ src/Domains/Connectors/Movipass/Enums/MovipassRolesEnum.php
@@ -9,5 +9,6 @@ enum MovipassRolesEnum: string
     case AGENT = 'agente';
     case TRUCK_DRIVER = 'gruero';
     case ROADSIDE_ASSISTANCE_OPERATOR = 'roadside_assistance_operator';
+    case OPERATOR = 'Operador';
     case RDVIAL_CONSULTANT = 'RdvialConsultant';
 }
