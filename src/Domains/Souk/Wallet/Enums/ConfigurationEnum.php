41ae208fbc1a7589c223307c0c39e91188e32037
freesgen <jesusant.guerrero@gmail.com>
2026-05-26 10:52:47 -0400

    fix: add funds to wallet

--- src/Domains/Souk/Wallet/Enums/ConfigurationEnum.php
+++ src/Domains/Souk/Wallet/Enums/ConfigurationEnum.php
@@ -25,4 +25,6 @@ enum ConfigurationEnum: string
     case PRODUCT_TYPE_WALLET_SUBSCRIPTION_AMOUNT = 'wallet-subscription-amount';
     case USE_VARIANT_CREDIT_INSTEAD_OF_VARIANT_PRICE_SLUG = 'use_variant_credit_instead_of_variant_price';
     case VARIANT_WALLET_CREDIT_AMOUNT = 'credit';
+
+    case WALLET_RESOLVE_COMPANY_FROM_METADATA = 'wallet_resolve_company_from_metadata';
 }
