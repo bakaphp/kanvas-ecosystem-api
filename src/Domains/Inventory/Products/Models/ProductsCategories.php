a35502375fff06895d7c119036431e406b45686d
Maximo Castro Pou <max@mctekk.com>
2026-06-03 23:47:04 -0400

    hotfix: conficit

--- src/Domains/Inventory/Products/Models/ProductsCategories.php
+++ src/Domains/Inventory/Products/Models/ProductsCategories.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace Kanvas\Inventory\Products\Models;
 
 use Awobaz\Compoships\Compoships;
-use Baka\Traits\HasCompositePrimaryKeyTrait;
 use Baka\Traits\NoAppRelationshipTrait;
 use Baka\Traits\NoCompanyRelationshipTrait;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Kanvas\Inventory\Categories\Models\Categories;
 use Kanvas\Inventory\Models\BaseModel;
+use Thiagoprz\CompositeKey\HasCompositeKey;
 
 /**
  * Class Products Categories.
@@ -24,32 +24,26 @@ use Kanvas\Inventory\Models\BaseModel;
  */
 class ProductsCategories extends BaseModel
 {
-    use HasCompositePrimaryKeyTrait;
+    use HasCompositeKey;
     use NoAppRelationshipTrait;
     use NoCompanyRelationshipTrait;
-    use Compoships;
+    use Compoships {
+        HasCompositeKey::setKeysForSaveQuery insteadof Compoships;
+    }
 
     protected $table = 'products_categories';
     protected $fillable = [
         'products_id',
-        'categories_id'
+        'categories_id',
     ];
 
     protected $primaryKey = ['products_id', 'categories_id'];
 
-    /**
-     * Get the product.
-     *
-     */
     public function product(): BelongsTo
     {
         return $this->belongsTo(Products::class, 'products_id');
     }
 
-    /**
-     * Get the category.
-     *
-     */
     public function category(): BelongsTo
     {
         return $this->belongsTo(Categories::class, 'categories_id');
