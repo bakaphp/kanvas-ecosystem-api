67df95ce1d2699e5216b9c5ddba522600f8977c7
FredPeal <frederickpeal@gmail.com>
2026-07-15 19:04:19 -0400

    refactor: comment apply lead ai mode

--- src/Domains/Intelligence/Triggers/Actions/ApplyLeadAiModeAction.php
+++ src/Domains/Intelligence/Triggers/Actions/ApplyLeadAiModeAction.php
@@ -185,7 +185,7 @@ class ApplyLeadAiModeAction
         $aiModeKey = new LeadConfigurationService()->getAiModeKey($this->lead);
 
         $this->lead->set($aiModeKey, $aiMode);
-        $this->lead->set(LeadConfigurationEnum::AI_MODE->value, $aiMode);
+        // $this->lead->set(LeadConfigurationEnum::AI_MODE->value, $aiMode);
     }
 
     protected function setFollowUp(FollowUpValueEnum $followUpValue)
