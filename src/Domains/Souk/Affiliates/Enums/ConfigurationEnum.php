83fcc0cb465c363fc2b9d101b95d5b9351b373a7
Maximo Castro Pou <max@mctekk.com>
2026-08-05 11:55:05 -0400

    refact: hotfix affialite links

--- src/Domains/Souk/Affiliates/Enums/ConfigurationEnum.php
+++ src/Domains/Souk/Affiliates/Enums/ConfigurationEnum.php
@@ -6,11 +6,5 @@ namespace Kanvas\Souk\Affiliates\Enums;
 
 enum ConfigurationEnum: string
 {
-    /**
-     * App setting holding a map of WooCommerce referrer affiliate slug => affiliate_links.short_code,
-     * e.g. {"ua_republica_dominicana": "UA20", "ua_do": "UA20", "ua_cr": "UA20-1", ...}. Used to resolve
-     * the affiliate from the order's _wc_order_attribution_referrer instead of the eSIM destination
-     * country the sender writes into affiliate_shortcode.
-     */
     case REFERRER_AFFILIATE_MAPPING = 'affiliate_referrer_mapping';
 }
